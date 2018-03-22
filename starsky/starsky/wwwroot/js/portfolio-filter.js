02628eeb51854424accbe1bddf5c570071496b11
Dion <dionvanvelde@gmail.com>
2018-03-22 21:13:27 +0100

    update filter

--- starsky/starsky/wwwroot/js/portfolio-filter.js
+++ starsky/starsky/wwwroot/js/portfolio-filter.js
@@ -13,7 +13,6 @@ tagList = tagList.sort();
 
 writeFilterList (tagList);
 
-filterPage();
 
 // Write down a list of portfolio catogories
 function makeTagList() {
@@ -44,8 +43,6 @@ function makeTagList() {
 }
 
 
-
-
 function removeArrayDuplicate (array) {
 
     var temp = {};
@@ -64,13 +61,15 @@ function writeFilterList (tags) {
     if (document.querySelectorAll("#portfolio-data").length === 1 && 
         document.querySelectorAll("#portfolio-filter").length === 1)
     {
+        // add reset
+        document.querySelector("#portfolio-filter").innerHTML = "";
+        
         var ul = document.createElement("ul");
         ul.className = "tags";
         document.querySelector("#portfolio-filter").appendChild(ul);
 
         var filterarticle = document.querySelector("#portfolio-filter ul")
 
-        console.log(tags);
         if(tags.length >= 2){
             // ALLES!!
             var li_alles = document.createElement("li");
@@ -78,7 +77,7 @@ function writeFilterList (tags) {
             var currentitem_alles = filterarticle.appendChild(li_alles).appendChild(a_alles);
             currentitem_alles.innerHTML = "Alles";
             currentitem_alles.className = "alles";
-            currentitem_alles.href = "#";
+            currentitem_alles.addEventListener("click", function(e){ var those = this; reset(); }, false);
             // EINDE ALLES
 
             var newTags = [];
@@ -117,27 +116,23 @@ function writeFilterList (tags) {
                 newTags.push(tags[tags.indexOf("None")])
             }
 
+            window.subject = newTags;
 
             for (var i = 0; i < newTags.length; i++) {
                 var li = document.createElement("li");
                 var a = document.createElement("a");
 
                 var currentitem = filterarticle.appendChild(li).appendChild(a);
-                currentitem.href = "#" + newTags[i].toLowerCase().replace(/ /ig, "-");
-                currentitem.className = newTags[i].toLowerCase().replace(/ /ig, "-");
-                currentitem.innerHTML = newTags[i];
+
+                currentitem.addEventListener("click", function(e){ var those = this; readVariable(those); }, false);
+                //   currentitem.href = "#" + newTags[i].toLowerCase().replace(/ /ig, "-");
+                
+                currentitem.id = newTags[i].toLowerCase().replace(/ /ig, "-");
+                currentitem.innerHTML = "<span class='checkbox'></span>" + newTags[i];
+                //"<span class='checkbox'></span>"
             }
+            
         }
-        
-        // for (var i = 0; i < tags.length; i++) {
-        //     var li = document.createElement("li");
-        //     var a = document.createElement("a");
-        //
-        //     var currentitem = filterarticle.appendChild(li).appendChild(a);
-        //     currentitem.href = "#" + tags[i].toLowerCase().replace(/ /ig, "-");
-        //     currentitem.className = tags[i].toLowerCase().replace(/ /ig, "-");
-        //     currentitem.innerHTML = tags[i];
-        // }
     }
 
 }///e/writeFilterList
@@ -152,98 +147,300 @@ function createSlug(tagList) {
 }
 
 
+//
+// window.onhashchange = function() {
+//     filterPage();
+// };
+//
 
-window.onhashchange = function() {
-    filterPage();
-};
+function reset() {
+    var object = document.querySelector("#portfolio-filter ul").children;
+    for (var i = 0; i < object.length; i++) {
 
+        if (object[i].children.length >= 1) {
+            linkobject = object[i].children[0];
 
+            linkobject.classList.remove("active");
+            linkobject.classList.add("none");
 
-function filterPage() {
-
-    function showLegenda() {
-
-        function reset() {
-            var object = document.querySelector("#portfolio-filter ul").children;
-            for (var i = 0; i < object.length; i++) {
-
-                if (object[i].children.length >= 1) {
-                    linkobject = object[i].children[0];
-                    linkobject.className = linkobject.className.replace(/ off| on/ig,"") + " off"
-                }
-            }
-        }
-        if (window.location.hash != "") {
-            reset();
-            var legendaobject = document.querySelector("#portfolio-filter" + " ." +  window.location.hash.substring(1,window.location.hash.length));
-            legendaobject.className  = legendaobject.className.replace(/ off| on/ig,"") + " on"
-        }
-        if (window.location.hash === "") {
-            reset();
-            document.querySelector("#portfolio-filter .alles").className = "alles on"
-        }
-    }
-    
-    var hashword = "";
-    linkTagList = createSlug(tagList);
-
-    if (window.location.hash !== "") {
-        hashword = window.location.hash.substring(1,window.location.hash.length);
-    }
-
-    if (window.location.hash !== "" && linkTagList.indexOf(hashword) >= 0) {
-        showLegenda();
-
-        currentFilter = hashword;
-        if (linkTagList.indexOf(hashword) >= 0) {
-            hideElementsByTag(hashword);
-        }
-    }
-
-    if (window.location.hash === "") {
-        showLegenda();
-        var object = document.querySelector("#portfolio-data").children;
-        for (var i = 0; i < object.length; i++) {
-            object[i].classList.remove("hide");
-            object[i].classList.add("show");
         }
     }
 }
 
+//
+// function showLegenda() {
+//    
+//     if (window.location.hash != "") {
+//
+//         var hashname = window.location.hash.substring(1,window.location.hash.length).split(",");
+//         console.log(hashname);
+//        
+//         reset();
+//        
+//         var legendaobject = document.querySelector("#portfolio-filter" + " ." +  hashname[0]);
+//         legendaobject.className  = legendaobject.className.replace(/ off| on/ig,"") + " on"
+//     }
+//     if (window.location.hash === "") {
+//         reset();
+//         if (document.querySelectorAll("#portfolio-filter .alles").length === 1) {
+//             document.querySelector("#portfolio-filter .alles").className = "alles on"
+//         }
+//     }
+// }
+//
+// function filterPage() {
+//     console.log("sd")
+//
+//     var hashword = "";
+//     linkTagList = createSlug(tagList);
+//
+//     if (window.location.hash !== "") {
+//         hashword = window.location.hash.substring(1,window.location.hash.length);
+//     }
+//
+//     if (window.location.hash !== "" && linkTagList.indexOf(hashword) >= 0) {
+//         showLegenda();
+//
+//         currentFilter = hashword;
+//         if (linkTagList.indexOf(hashword) >= 0) {
+//             hideElementsByTag(hashword);
+//         }
+//     }
+//
+//     if (window.location.hash === "") {
+//         showLegenda();
+//         var object = document.querySelector("#portfolio-data").children;
+//         for (var i = 0; i < object.length; i++) {
+//             object[i].classList.remove("hide");
+//             object[i].classList.add("show");
+//         }
+//     }
+// }
+//
+//
+// function hideElementsByTag(hashword) {
+//     if (document.querySelectorAll("#portfolio-data").length === 1 
+//         && document.querySelectorAll("#portfolio-filter").length === 1)
+//     {
+//    
+//         var object = document.querySelector("#portfolio-data").children;
+//         for (var i = 0; i < object.length; i++) {
+//
+//             var tags = object[i].getAttribute('data-tag');
+//             var thisTags = [];
+//
+//             if (tags !== null) {
+//                 tags = 	tags.split(",");
+//
+//                 for (var j = 0; j < tags.length; j++) {
+//                     thisTags.push(tags[j])
+//                 }
+//                 thisTags = createSlug(thisTags);
+//
+//             }
+//             if (thisTags.indexOf(hashword) >= 0) {
+//                 // object[i].className = "halfitem show";
+//                 object[i].classList.remove("hide");
+//                 object[i].classList.add("show");
+//
+//             }
+//             if (thisTags.indexOf(hashword) === -1) {
+//                 // object[i].className = "halfitem hide";
+//                 object[i].classList.remove("show");
+//                 object[i].classList.add("hide");
+//             }
+//
+//         }
+//
+//     }
+// }
+//
 
-function hideElementsByTag(hashword) {
+
+
+
+
+
+var selectedVar = [];
+function readVariable (those) {
+    // those is this of the eventListerer
+
+
+    // When you click on the checkbox
+
+
+    // There used to be a reset;	
+    // isFilterCountryActive = false;
+
+    var i;
+
+    if (those !== undefined) {
+
+        if ( selectedVar.indexOf(those.id) === -1 ) {
+            selectedVar.push(those.id);
+        }
+        else {
+            var index = selectedVar.indexOf(those.id);
+            // delete storeActiveVar[index]; // replace this item of the list with: ""
+            selectedVar.splice( index, 1 ); // delete this item of the array;
+        }
+
+        // reset all checkboxes to none;
+        for (i = 0; i < window.subject.length; i++) {
+            reset();
+        }
+
+        // set the selected checkboxes to active;
+        for (i = 0; i < selectedVar.length; i++) {
+            document.querySelector("#portfolio-filter ul #" + selectedVar[i]).className = "active";
+        }
+    }
+    else {
+        // direct input; for example using a url (no reset needed);
+        for (i = 0; i < selectedVar.length; i++) {
+            document.querySelector("#portfolio-filter ul #" + selectedVar[i]).className = "active";
+        }
+    }
+
+    // exept for direct input;	> create an unique url;
+    if (those !== undefined) {
+        constructURL();
+    }
+
+    setVariable(selectedVar);
+}
+
+
+var prevURL;
+function constructURL() {
+    // Please check: buildURL() for usage;
+
+    if (history.pushState) { // for old browsers like: IE9/IE10
+
+        var url = "#subject=";
+        for (var i = 0; i < selectedVar.length; i++) {
+
+            if (i === selectedVar.length-1) {
+                url += selectedVar[i];
+            }
+            else {
+                url += selectedVar[i] + ",";
+            }
+        }
+        
+        if (selectedVar.length === 0) {
+            url = "#";
+        }
+        
+        // Check if this function isn't repeated excuded;
+        if (url !== prevURL) {
+            var stateObj = { url: url };
+            history.pushState(stateObj, "Qdraw", url);
+        }
+        prevURL = url;
+    }
+
+}
+
+function setVariable(hashList) {
+    
     if (document.querySelectorAll("#portfolio-data").length === 1 
         && document.querySelectorAll("#portfolio-filter").length === 1)
     {
-    
+
+        if(hashList[0] === "") {
+            hashList = [];
+        }
+        
         var object = document.querySelector("#portfolio-data").children;
+        console.log(object)
+
         for (var i = 0; i < object.length; i++) {
 
-            var tags = object[i].getAttribute('data-tag');
-            var thisTags = [];
-
-            if (tags !== null) {
-                tags = 	tags.split(",");
-
-                for (var j = 0; j < tags.length; j++) {
-                    thisTags.push(tags[j])
-                }
-                thisTags = createSlug(thisTags);
-
-            }
-            if (thisTags.indexOf(hashword) >= 0) {
-                // object[i].className = "halfitem show";
+            var tag = object[i].getAttribute('data-tag').toLowerCase();
+           
+            if(hashList.length >= 0) {
                 object[i].classList.remove("hide");
-                object[i].classList.add("show");
-
-            }
-            if (thisTags.indexOf(hashword) === -1) {
-                // object[i].className = "halfitem hide";
-                object[i].classList.remove("show");
-                object[i].classList.add("hide");
+                object[i].classList.add("show");     
             }
 
+            if(hashList.length >= 1) {
+    
+                if (hashList.indexOf(tag) >= 0) {
+                    object[i].classList.remove("hide");
+                    object[i].classList.add("show");
+                }
+                if (hashList.indexOf(tag) === -1) {
+                    object[i].classList.remove("show");
+                    object[i].classList.add("hide");
+                }
+            }
         }
-
+        
     }
 }
+
+function buildPage() {
+    var urlsubject = location.hash.replace("#subject=", "");
+    urlsubject = urlsubject.split(",");
+
+    var object = document.querySelector("#portfolio-filter .tags").children;
+    console.log(urlsubject);
+
+    for (var i = 0; i < object.length; i++) {
+        object[i].children[0].classList.remove("active");
+        console.log()
+    }
+    
+    console.log(urlsubject);
+
+    setVariable(urlsubject);
+
+    if (urlsubject.length >= 1 && urlsubject[0] !== ""){
+    
+        for (var i = 0; i < urlsubject.length; i++) {
+            if (document.querySelectorAll("#portfolio-filter #" + urlsubject[i]).length === 1){
+                (document.querySelector("#portfolio-filter #" + urlsubject[i]).classList.remove("active"));
+            }
+        }
+        
+        for (var i = 0; i < urlsubject.length; i++) {
+            if (document.querySelectorAll("#portfolio-filter #" + urlsubject[i]).length === 1){
+                (document.querySelector("#portfolio-filter #" + urlsubject[i]).classList.add("active"));
+            }
+        }
+        
+        var object = document.querySelector("#portfolio-filter .tags").children;
+        for (var i = 0; i < object.length; i++) {
+            // console.log(object[i].children[0].id)
+        }
+    }
+}
+window.onhashchange = function() {
+    if (window.innerDocClick) {
+        // Thanks: http://stackoverflow.com/questions/25806608/how-to-detect-browser-back-button-event-cross-browser
+        window.innerDocClick = false;
+
+        console.log("innerDocClick");
+
+        buildPage()
+
+    } else {
+        if (window.location.hash != '#undefined') {
+
+            console.log("eiwn");
+
+            buildPage()
+            
+
+        } else {
+            // Go back to (for expample) Google;
+            history.pushState("", document.title, window.location.pathname);
+            location.reload();
+        }
+    }
+};
+
+console.log("dskfnl");
+
+buildPage();
