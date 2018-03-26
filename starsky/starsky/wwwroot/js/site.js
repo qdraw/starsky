cc01ea5a0d4a147f8126f71ca5687fbb168b91fc
Dion <dionvanvelde@gmail.com>
2018-03-26 15:33:13 +0200

    add keydown feature

--- starsky/starsky/wwwroot/js/site.js
+++ starsky/starsky/wwwroot/js/site.js
@@ -1,31 +1,38 @@
 ﻿// Write your JavaScript code.
 
 
-// var FileItems =   document.querySelectorAll(".halfitem .caption h2");
-// if(FileItems.length >= 1) {
-//     for(var i = 0; i < FileItems.length; i++) {
-//         FileItems[i].innerHTML = FileItems[i].innerHTML.replace(".jpg","");
-//         if(FileItems[i].innerHTML.length >= 23){
-//             FileItems[i].innerHTML = FileItems[i].innerHTML.substr(0,23) + "…";
-//         }
-//     }
-// }
-// var tagItems =   document.querySelectorAll(".halfitem .caption p");
-// if(tagItems.length >= 1) {
-//     for(var i = 0; i < tagItems.length; i++) {
-//         if(tagItems[i].innerHTML.length >= 36){
-//             tagItems[i].innerHTML = tagItems[i].innerHTML.substr(0,36) + "…";
-//         }
-//     }
-// }
-
-// var breadcrumbItem =   document.querySelectorAll(".breadcrumb");
-// if (breadcrumbItem.length === 1){
-//     for(var i = 0; i < breadcrumbItem[0].children.length; i++) {
-//         console.log(breadcrumbItem[0].children[i].innerHTML.length);
-//         console.log(breadcrumbItem[0].children[i].innerHTML);
-//         if(breadcrumbItem[0].children[i].innerHTML.length >= 36){
-//             breadcrumbItem[0].children[i].innerHTML = breadcrumbItem[0].children[i].innerHTML.substr(0,36) + "...";
-//         }
-//     }
-// }
+if (document.querySelectorAll(".nextprev").length >= 1) {
+    var next = null;
+    var prev = null;
+    if (document.querySelectorAll(".nextprev .next").length >= 1) next = document.querySelector(".nextprev .next").href;
+    if (document.querySelectorAll(".nextprev .prev").length >= 1) prev = document.querySelector(".nextprev .prev").href;
+    
+    window.onkeydown = function(e) {
+        switch (e.keyCode) {
+            case 37:
+                // left
+                if (prev != null) {
+                    window.location.href = prev;
+                }
+                break;
+            case 38:
+                // // up
+                // if (prev != null) {
+                //     window.location.href = prev;
+                // }
+                break;
+            case 39:
+                // right
+                if (next != null) {
+                    window.location.href = next;
+                }
+                break;
+            case 40:
+                // // down
+                // if (next != null) {
+                //     window.location.href = next;
+                // }
+                break;
+        }
+    };
+}
