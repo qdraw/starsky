a16f5a75488a711881df649e7dd452f467168d35
Dion <dionvanvelde@gmail.com>
2018-03-28 22:09:11 +0200

    add init react ++ update xcode proj

---
+++ starskyapp/HomeScreen.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  SectionList,
+  Image,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+//   ScrollView,
+
+
+export default class FetchExample extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+    );
+  }
+}
--- starskyapp/index.js
+++ starskyapp/index.js
@@ -1,4 +1,12 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
 
-AppRegistry.registerComponent('starskyapp', () => App);
+import { StackNavigator } from 'react-navigation';
+import HomeScreen from './HomeScreen';
+// import DetailScreen from './DetailScreen';
+//
+// const App = StackNavigator({
+//   Home: { screen: HomeScreen },
+//   Detail: { screen: DetailScreen },
+// });
+
+AppRegistry.registerComponent('starskyapp', () => HomeScreen);
