ef41de744b5da784c7dc4569b58592609c3fc68a
Dion <dionvanvelde@gmail.com>
2018-03-29 21:43:39 +0200

    preloaded bug fix ++ add keyboard shortcuts with focus check

--- starsky/starsky/wwwroot/js/site.js
+++ starsky/starsky/wwwroot/js/site.js
@@ -6,32 +6,26 @@ if (document.querySelectorAll(".nextprev").length >= 1) {
     var prev = null;
     if (document.querySelectorAll(".nextprev .next").length >= 1) next = document.querySelector(".nextprev .next").href;
     if (document.querySelectorAll(".nextprev .prev").length >= 1) prev = document.querySelector(".nextprev .prev").href;
-    
+
     window.onkeydown = function(e) {
         switch (e.keyCode) {
             case 37:
                 // left
-                if (prev != null) {
+                if (prev != null && document.activeElement.className.indexOf("form-control") === -1) {
                     window.location.href = prev;
                 }
                 break;
             case 38:
                 // // up
-                // if (prev != null) {
-                //     window.location.href = prev;
-                // }
                 break;
             case 39:
                 // right
-                if (next != null) {
+                if (next != null && document.activeElement.className.indexOf("form-control") === -1) {
                     window.location.href = next;
                 }
                 break;
             case 40:
                 // // down
-                // if (next != null) {
-                //     window.location.href = next;
-                // }
                 break;
         }
     };
