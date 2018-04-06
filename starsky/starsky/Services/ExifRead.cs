3c308c5ffea0be52db388684385c3231517f824f
Dion <dionvanvelde@gmail.com>
2018-04-06 18:23:04 +0200

    refactor exifread

--- starsky/starsky/Services/ExifRead.cs
+++ starsky/starsky/Services/ExifRead.cs
@@ -16,66 +16,52 @@ namespace starsky.Services
             try
             {
                 var allExifItems = ImageMetadataReader.ReadMetadata(fileFullPath);
-                var colorClassSting = string.Empty;
-    
+                
+                if(AppSettingsProvider.Verbose) {
+                    foreach (var exifItem in allExifItems) {
+                        foreach (var tag in exifItem.Tags) Console.WriteLine($"[{exifItem.Name}] {tag.Name} = {tag.Description}");
+                    }
+                }
+                
+                // Set the default value
+                item.SetColorClass();
+
                 foreach (var exifItem in allExifItems)
                 {
-                    
-                    //Console.WriteLine(exifItem);
-                    //foreach (var tag in exifItem.Tags) Console.WriteLine($"[{exifItem.Name}] {tag.Name} = {tag.Description}");
-    
-    
-                    var tCounts = exifItem.Tags.Count(p => p.DirectoryName == "IPTC" && p.Name == "Keywords");
-                    if (tCounts >= 1)
+                    //  exifItem.Tags
+                    var tags = _getExifTags(exifItem);
+                    if(tags != null) // null = is not the right tag or emthy tag
                     {
-                        item.Tags = exifItem.Tags.FirstOrDefault(p => p.DirectoryName == "IPTC" && p.Name == "Keywords")?.Description.ToLower();
-                        if (!string.IsNullOrWhiteSpace(item.Tags))
-                        {
-                            item.Tags = item.Tags.Replace(";", ", ");
-                        }
+                        item.Tags = tags;
                     }
-    
                     // Colour Class => ratings
-                    var ratingCounts = exifItem.Tags.Count(p => p.DirectoryName == "IPTC" && p.Name.Contains("0x02dd"));
-                    if (ratingCounts >= 1)
+                    var colorClassString = _getColorClassString(exifItem);
+                    if(colorClassString != null) // null = is not the right tag or emthy tag
                     {
-                        var prefsTag = exifItem.Tags.FirstOrDefault(p => p.DirectoryName == "IPTC" && p.Name.Contains("0x02dd"))?.Description;
-    
-                        // Results for example
-                        //     0:1:0:-00001
-                        //     ~~~~~~
-                        //     0:8:0:-00001
-                        
-                        if (!string.IsNullOrWhiteSpace(prefsTag))
-                        {
-                            var prefsTagSplit = prefsTag.Split(":");
-                            colorClassSting = prefsTagSplit[1];     
-                        }
+                        item.SetColorClass(colorClassString);
                     }
-    
-                    var dtCounts = exifItem.Tags.Count(p => p.DirectoryName == "Exif SubIFD" && p.Name == "Date/Time Digitized");
-                    if (dtCounts >= 1)
+                    
+                    // [IPTC] Caption/Abstract
+                    var caption = _getCaptionAbstract(exifItem);
+                    if(caption != null) // null = is not the right tag or emthy tag
                     {
-    
-                        var dateString = exifItem.Tags.FirstOrDefault(p => p.DirectoryName == "Exif SubIFD" && p.Name == "Date/Time Digitized")?.Description;
-    
-                        // https://odedcoster.com/blog/2011/12/13/date-and-time-format-strings-in-net-understanding-format-strings/
-                        //2018:01:01 11:29:36
-                        string pattern = "yyyy:MM:dd HH:mm:ss";
-                        CultureInfo provider = CultureInfo.InvariantCulture;
-                        DateTime.TryParseExact(dateString, pattern, provider, DateTimeStyles.AdjustToUniversal, out var itemDateTime);
-    
-                        if (itemDateTime.Year == 1 && itemDateTime.Month == 1)
-                        {
-                            dateString = exifItem.Tags.FirstOrDefault(p => p.DirectoryName == "Exif SubIFD" && p.Name == "Date/Time Original")?.Description;
-                            DateTime.TryParseExact(dateString, pattern, provider, DateTimeStyles.AdjustToUniversal, out itemDateTime);
-                        }
-    
-                        item.DateTime = itemDateTime;
+                        item.Description = caption;
+                    }    
+                    
+                    // [IPTC] Object Name = Title
+                    var title = _getCaptionAbstract(exifItem);
+                    if(title != null) // null = is not the right tag or emthy tag
+                    {
+                        // item.Title = title;
                     }
-    
+                    
+                    // DateTime of image
+                    var dateTime = _getDateTime(exifItem);
+                    if(dateTime.Year > 2) // 0 = is not the right tag or emthy tag
+                    {
+                        item.DateTime = dateTime;
+                    }
                 }
-                item.SetColorClass(colorClassSting);
                 
             }
             catch (ImageProcessingException e)
@@ -86,6 +72,98 @@ namespace starsky.Services
             }
             
             return item;
+        }
+
+        // Update Database structure first
+        private static string _getObjectName (Directory exifItem)
+        {
+            var tCounts = exifItem.Tags.Count(p => p.DirectoryName == "IPTC" && p.Name == "Object Name");
+            if (tCounts < 1) return null;
+            
+            var caption = exifItem.Tags.FirstOrDefault(
+                p => p.DirectoryName == "IPTC" 
+                     && p.Name == "Object Name")?.Description;
+            return caption;
+        }
+
+        
+        private static string _getCaptionAbstract(Directory exifItem)
+        {
+            var tCounts = exifItem.Tags.Count(p => p.DirectoryName == "IPTC" && p.Name == "Caption/Abstract");
+            if (tCounts < 1) return null;
+            
+            var caption = exifItem.Tags.FirstOrDefault(
+                p => p.DirectoryName == "IPTC" 
+                     && p.Name == "Caption/Abstract")?.Description;
+            return caption;
+            
+        }
+        
+        private static string _getExifTags(Directory exifItem)
+        {
+            var tCounts = exifItem.Tags.Count(p => p.DirectoryName == "IPTC" && p.Name == "Keywords");
+            if (tCounts >= 1)
+            {
+                var tags = exifItem.Tags.FirstOrDefault(
+                    p => p.DirectoryName == "IPTC" 
+                         && p.Name == "Keywords")?.Description.ToLower();
+                if (!string.IsNullOrWhiteSpace(tags))
+                {
+                    tags = tags.Replace(";", ", ");
+                }
+
+                return tags;
+            }
+            return null;
+        }
+
+        private static string _getColorClassString(Directory exifItem)
+        {
+            var ratingCounts = exifItem.Tags.Count(p => p.DirectoryName == "IPTC" && p.Name.Contains("0x02dd"));
+            if (ratingCounts >= 1)
+            {
+                var colorClassSting = string.Empty;
+                var prefsTag = exifItem.Tags.FirstOrDefault(p => p.DirectoryName == "IPTC" && p.Name.Contains("0x02dd"))?.Description;
+    
+                // Results for example
+                //     0:1:0:-00001
+                //     ~~~~~~
+                //     0:8:0:-00001
+                        
+                if (!string.IsNullOrWhiteSpace(prefsTag))
+                {
+                    var prefsTagSplit = prefsTag.Split(":");
+                    colorClassSting = prefsTagSplit[1];     
+                }
+                return colorClassSting;
+            }
+
+            return null;
+        }
+
+        private static DateTime _getDateTime(Directory exifItem)
+        {
+            var itemDateTime = new DateTime();
+            var dtCounts = exifItem.Tags.Count(p => p.DirectoryName == "Exif SubIFD" && p.Name == "Date/Time Digitized");
+            if (dtCounts >= 1)
+            {
+    
+                var dateString = exifItem.Tags.FirstOrDefault(p => p.DirectoryName == "Exif SubIFD" && p.Name == "Date/Time Digitized")?.Description;
+    
+                // https://odedcoster.com/blog/2011/12/13/date-and-time-format-strings-in-net-understanding-format-strings/
+                //2018:01:01 11:29:36
+                string pattern = "yyyy:MM:dd HH:mm:ss";
+                CultureInfo provider = CultureInfo.InvariantCulture;
+                DateTime.TryParseExact(dateString, pattern, provider, DateTimeStyles.AdjustToUniversal, out itemDateTime);
+    
+                if (itemDateTime.Year == 1 && itemDateTime.Month == 1)
+                {
+                    dateString = exifItem.Tags.FirstOrDefault(p => p.DirectoryName == "Exif SubIFD" && p.Name == "Date/Time Original")?.Description;
+                    DateTime.TryParseExact(dateString, pattern, provider, DateTimeStyles.AdjustToUniversal, out itemDateTime);
+                }
+            }
+
+            return itemDateTime;
         }
     }
 }
