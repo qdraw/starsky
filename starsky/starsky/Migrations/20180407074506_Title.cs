0bc419e67b0040b2b05af0d92da5ae4e418f2b3b
Dion <dionvanvelde@gmail.com>
2018-04-07 11:07:24 +0200

    add migration

---
+++ starsky/starsky/Migrations/20180407074506_Title.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace starsky.Migrations
+{
+    public partial class Title : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "FileIndex",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "FileIndex");
+        }
+    }
+}
