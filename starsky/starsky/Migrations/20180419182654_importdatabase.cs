198faf04adf1c0ec47f0eaa73d6a59d5b794c6f1
Dion <dionvanvelde@gmail.com>
2018-04-19 21:05:49 +0200

    add migrations

---
+++ starsky/starsky/Migrations/20180419182654_importdatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace starsky.Migrations
+{
+    public partial class importdatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ImportIndex",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AddToDatabase = table.Column<DateTime>(nullable: false),
+                    FileHash = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImportIndex", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ImportIndex");
+        }
+    }
+}
