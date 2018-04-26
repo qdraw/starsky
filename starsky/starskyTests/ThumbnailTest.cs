3471229e5aa29bbbe5048f0c7fe74a306c01edde
Dion <dionvanvelde@gmail.com>
2018-04-26 14:45:19 +0200

    add Thumbnail Folder test

--- starsky/starskyTests/ThumbnailTest.cs
+++ starsky/starskyTests/ThumbnailTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.Models;
 using starsky.Services;
 
@@ -8,15 +10,60 @@ namespace starskytests
     public class ThumbnailTest
     {
         [TestMethod]
-        public void CreateThumbTest()
+        public void CreateAndRenamteThumbTest()
         {
 
             var newImage = new CreateAnImage();
             AppSettingsProvider.ThumbnailTempFolder = newImage.BasePath;
             AppSettingsProvider.BasePath = newImage.BasePath;
-            
-            Thumbnail.CreateThumb(newImage.FullFilePath);
-            
+
+            var hashString = FileHash.GetHashCode(newImage.FullFilePath);
+
+            // Delete if exist, to optimize test
+            var thumbnailPAth = Path.Combine(newImage.BasePath, hashString + ".jpg");
+            if (File.Exists(thumbnailPAth))
+            {
+                File.Delete(thumbnailPAth);
+            }
+
+            // Create an thumbnail based on the image
+            Thumbnail.CreateThumb(newImage.DbPath);
+            Assert.AreEqual(true,File.Exists(thumbnailPAth));
+
+            // Test Rename feature and delete if passed
+            new Thumbnail().RenameThumb(hashString, "AAAAA");
+            var thumbnailApAth = Path.Combine(newImage.BasePath, "AAAAA" + ".jpg");
+            if (File.Exists(thumbnailApAth))
+            {
+                File.Delete(thumbnailApAth);
+            }
         }
+
+        [TestMethod]
+        public void ThumbnailByDirectoryTest()
+        {
+
+            var newImage = new CreateAnImage();
+            AppSettingsProvider.ThumbnailTempFolder = newImage.BasePath;
+            AppSettingsProvider.BasePath = newImage.BasePath;
+
+            var hashString = FileHash.GetHashCode(newImage.FullFilePath);
+
+            // Delete if exist, to optimize test
+            var thumbnailPAth = Path.Combine(newImage.BasePath, hashString + ".jpg");
+            if (File.Exists(thumbnailPAth))
+            {
+                File.Delete(thumbnailPAth);
+            }
+        
+            // Create an thumbnail based on the image
+            ThumbnailByDirectory.CreateThumb();
+            
+            Assert.AreEqual(true,File.Exists(thumbnailPAth));
+            File.Delete(thumbnailPAth);
+
+        }
+        
+        
     }
 }
