16214976e57c7921849ee86d51e7ff5ff18ea271
Dion <dionvanvelde@gmail.com>
2018-05-17 17:02:03 +0200

    add unit test for search start

--- starsky/starskyTests/SearchServiceTest.cs
+++ starsky/starskyTests/SearchServiceTest.cs
@@ -1,15 +1,30 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.Attributes;
+using starsky.Data;
+using starsky.Services;
 
 namespace starskytests
 {
     [TestClass]
     public class SearchServiceTest
     {
+        private SearchService _search;
+
+        public SearchServiceTest()
+        {
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseInMemoryDatabase("search");
+            var options = builder.Options;
+            var context = new ApplicationDbContext(options);
+            _search = new SearchService(context);
+        }
+
         [ExcludeFromCoverage]
         [TestMethod]
         public void Todo()
         {
+            _search.Search("");
         }
         // Todo: add this unit test
     }
