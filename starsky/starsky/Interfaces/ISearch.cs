cb75bdc1c78300966189055d12c768f5eea20a81
Dion <dionvanvelde@gmail.com>
2018-05-22 16:09:56 +0200

    rm unused code

--- starsky/starsky/Interfaces/ISearch.cs
+++ starsky/starsky/Interfaces/ISearch.cs
@@ -1,14 +1,9 @@
-﻿using System.Collections.Generic;
-using starsky.Models;
-using starsky.ViewModels;
+﻿using starsky.ViewModels;
 
 namespace starsky.Interfaces
 {
     public interface ISearch
     {
-//        IEnumerable<FileIndexItem> SearchObjectItem(string tag = "", int pageNumber = 0);
-//        int SearchLastPageNumber(string tag);
-//        int SearchCount(string tag = "");
         SearchViewModel Search(string query = "", int p = 0);
     }
 }
