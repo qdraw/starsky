670c109409ee950e29e72519a8b33d26d3c0eaac
Dion <dionvanvelde@gmail.com>
2018-05-22 15:59:19 +0200

    rm unused code

--- starsky/starsky/Controllers/SearchController.cs
+++ starsky/starsky/Controllers/SearchController.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using starsky.Interfaces;
 using starsky.Models;
-using starsky.ViewModels;
 
 namespace starsky.Controllers
 {
@@ -15,6 +13,7 @@ namespace starsky.Controllers
         {
             _search = search;
         }
+        
         [HttpPost]
         public IActionResult Index(string t)
         {
@@ -24,52 +23,10 @@ namespace starsky.Controllers
         [HttpGet]
         public IActionResult Index(string t, int p = 0, bool json = false)
         {
-            
+            // Json api && View()            
             var model = _search.Search(t, p);
             if (json) return Json(model);
             return View("Index", model);
-            
-//            // Json api && View()
-//            
-//            if (p <= 0) p = p * -1;
-//            Stopwatch stopWatch = Stopwatch.StartNew();
-//            
-//            // t = tag name | p == pagenr.
-//
-//            var model = new SearchViewModel
-//            {
-//                PageNumber = p,
-//                SearchQuery = t,
-//                Breadcrumb = new List<string> {"/"}
-//            };
-//            if (!string.IsNullOrEmpty(t))
-//            {
-//                model.Breadcrumb.Add(t);
-//            }
-//
-//            if (string.IsNullOrWhiteSpace(t))
-//            {
-//                model.FileIndexItems = new List<FileIndexItem>();
-//                if (json) return Json(model);
-//                return View("Index", model);
-//            }
-//
-//            model.LastPageNumber = _search.SearchLastPageNumber(t);
-//            model.SearchCount = _search.SearchCount(t);
-//
-//            if (p > model.SearchCount)
-//            {
-//                Response.StatusCode = 404;
-//                if (json) return Json("not found");
-//                return View("Error", model);
-//            }
-//            
-//            model.FileIndexItems = _search.SearchObjectItem(model.SearchQuery, model.PageNumber);
-//            stopWatch.Stop();
-//            model.ElapsedSeconds = stopWatch.Elapsed.TotalSeconds;
-//
-//            if (json) return Json(model);
-//            return View("Index", model);
         }
 
         [HttpGet]
@@ -79,25 +36,6 @@ namespace starsky.Controllers
             if (json) return Json(model);
             return View("Trash", model);
         }
-
-//        [HttpGet]
-//        public IActionResult Trash(int p = 0)
-//        {
-//            if (p <= 0) p = p * -1;
-//
-//            // t = tag name | p == pagenr.
-//
-//            var model = new SearchViewModel();
-//            model.PageNumber = p;
-////            model.SearchQuery = "!delete!";
-////            model.Breadcrumb = new List<string>();
-////            model.Breadcrumb.Add("/");
-////            model.Breadcrumb.Add("/Search/Trash");
-////
-////            model.LastPageNumber = _search.SearchLastPageNumber("!delete!");
-////            model.FileIndexItems = _search.SearchObjectItem(model.SearchQuery, model.PageNumber);
-//            return View("Trash", model);
-//        }
 
         public IActionResult Error()
         {
