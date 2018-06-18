028d7e6ebb4bee6b63b8daf3e50a0a3801dbb3ac
Dion <dionvanvelde@gmail.com>
2018-06-18 10:14:39 +0200

    ImportIndexItemParseFileName ++ test

--- starsky/starsky/Models/ImportIndexItem.cs
+++ starsky/starsky/Models/ImportIndexItem.cs
@@ -27,63 +27,25 @@ namespace starsky.Models
             var structuredFileName = AppSettingsProvider.Structure.Split("/").LastOrDefault();
             structuredFileName = structuredFileName?.Replace("*", "");
 
-            // Position of dot ext
-            var searchForExt = ".ext".ToCharArray();
-            var extPosition = AllIndexesOf(structuredFileName, searchForExt.ToString()).FirstOrDefault();
-//            char[] ch = somestring.ToCharArray();
-//            ch[3] = 'X'; // index starts at 0!
+            var extPosition = structuredFileName.IndexOf(".ext", StringComparison.Ordinal);
+            structuredFileName = structuredFileName.Substring(0, extPosition);
             
-            var fileName = DateTime.ToString(structuredFileName, CultureInfo.InvariantCulture).ToCharArray();
-            for (var i = extPosition; i < searchForExt.Length; i++)
+            var fileName = DateTime.ToString(structuredFileName, CultureInfo.InvariantCulture);
+            fileName += "." + fileExtenstion;
+            return fileName;
+        }
+        
+        private static List<string> _parseListDateFormat(List<string> patternList, DateTime fileDateTime)
+        {
+            var parseListDate = new List<string>();
+            foreach (var patternItem in patternList)
             {
-                
+                if (patternItem == "/") return patternList;
+                var item = fileDateTime.ToString(patternItem, CultureInfo.InvariantCulture);
+                parseListDate.Add(item);
             }
-
-//            ch[3] = 'X'; // index starts at 0!
-
-            
-            
-            Console.WriteLine();
-//            var structureList = AppSettingsProvider.Structure;
-//
-//            var item = fileDateTime.ToString(patternItem, CultureInfo.InvariantCulture);
-//
-//            
-//            fileNameStructureList = _parseListDateFormat(fileNameStructureList, DateTime);
-//                
-//            foreach (var item in fileNameStructureList)
-//            {
-//                Regex rgx = new Regex(".ex[A-Z]$");
-//                var result = rgx.Replace(item, "." + fileExtenstion);
-//                return result;
-//            }
-            return string.Empty;
+            return parseListDate;
         }
         
-//        private static List<string> _parseListDateFormat(List<string> patternList, DateTime fileDateTime)
-//        {
-//            var parseListDate = new List<string>();
-//            foreach (var patternItem in patternList)
-//            {
-//                if (patternItem == "/") return patternList;
-//                var item = fileDateTime.ToString(patternItem, CultureInfo.InvariantCulture);
-//                parseListDate.Add(item);
-//            }
-//            return parseListDate;
-//        }
-//        
-//        AllIndexesOf(string str, string value)
-        
-        public List<int> AllIndexesOf(string inputString, string searchFor) {
-            if (String.IsNullOrEmpty(searchFor))
-                throw new ArgumentException("the string to find may not be empty", nameof(searchFor));
-            List<int> indexes = new List<int>();
-            for (int index = 0;; index += searchFor.Length) {
-                index = inputString.IndexOf(searchFor, index, StringComparison.Ordinal);
-                if (index == -1)
-                    return indexes;
-                indexes.Add(index);
-            }
-        }
     }
 }
---
+++ starsky/starskyTests/ImportIndexItemTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using starsky.Data;
+using starsky.Models;
+using starsky.Services;
+
+namespace starskytests
+{
+    [TestClass]
+    public class ImportIndexItemTest
+    {
+        [TestMethod]
+        public void ImportIndexItemParseFileNameTest()
+        {
+            var createAnImage = new CreateAnImage();
+
+            var importItem = new ImportIndexItem();
+            importItem.SourceFullFilePath = createAnImage.FullFilePath;
+
+            var fileName = importItem.ParseFileName();
+            Assert.AreEqual("00010101_000000.jpg", fileName);
+        }
+
+    }
+}
