2b78efcd59113711ccbcc2c3798cb4ef8adc4272
Dion <dionvanvelde@gmail.com>
2018-06-18 09:26:11 +0200

    rm unused includeds

--- starsky/starsky/Models/ImportIndexItem.cs
+++ starsky/starsky/Models/ImportIndexItem.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using starsky.Services;
 
 namespace starsky.Models
