e4e3813eb20b0b353dfa112e2446dcb75b842ec4
Dion <dionvanvelde@gmail.com>
2018-06-21 11:57:09 +0200

    update Structure

--- starsky/starsky/Models/BasePathConfig.cs
+++ starsky/starsky/Models/BasePathConfig.cs
@@ -31,19 +31,19 @@ namespace starsky.Models
             {
                 if (string.IsNullOrEmpty(_structure))
                 {
-                    //   - "dd" 	    The day of the month, from 01 through 31.
-                    //   - "MM" 	    The month, from 01 through 12.
-                    //   - "yyyy" 	    The year as a four-digit number.
-                    //   - "HH" 	    The hour, using a 24-hour clock from 00 to 23.
-                    //   - "mm" 	    The minute, from 00 through 59.
-                    //   - "ss" 	    The second, from 00 through 59.
+                    //   - dd 	        The day of the month, from 01 through 31.
+                    //   - MM 	        The month, from 01 through 12.
+                    //   - yyyy 	    The year as a four-digit number.
+                    //   - HH 	        The hour, using a 24-hour clock from 00 to 23.
+                    //   - mm 	        The minute, from 00 through 59.
+                    //   - ss 	        The second, from 00 through 59.
                     //   - https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings
-                    //   - \\           Double escape; to escape dd use this: \\d\\d 
-                    //   - /            is split in folder (Windows / Linux / Mac)
-                    //   - .ext         (dot ext) extension for example jpeg/jpg
+                    //   - \\           (double escape sign or double backslash); to escape dd use this: \\d\\d 
+                    //   - /            (slash); is split in folder (Windows / Linux / Mac)
+                    //   - .ext         (dot ext); extension for example jpeg/jpg
                     //   - (nothing)    extension is forced
-                    //   - *            match anything
-                    //   - *od*        match 'de'-folder so for example the folder: good
+                    //   - *            (asterisk); match anything
+                    //   - *od*         match 'de'-folder so for example the folder: good
                     
                     
                     //    Please update /starskyimportercli/readme.md when this changes
