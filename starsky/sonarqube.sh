c1091e92ef688bb613fc688c241f997cfc0aab50
Dion <dionvanvelde@gmail.com>
2018-06-22 14:14:46 +0200

    update sonar link

--- starsky/sonarqube.sh
+++ starsky/sonarqube.sh
@@ -1,6 +1,5 @@
-
-
-dotnet /Users/dionvanvelde/Downloads/sonarqube-7.0/sonar-scanner-msbuild-4.1.1.1164-netcoreapp2.0/SonarScanner.MSBuild.dll begin /k:"starsky" /d:sonar.host.url="http://localhost:9000" /d:sonar.login="e23083ae4031b7e01c2045ea5815e1e7a8292a03" /d:sonar.cs.opencover.reportsPaths="/data/git/starsky/starsky/starskyTests/coverage.opencover.xml" && dotnet build && dotnet /Users/dionvanvelde/Downloads/sonarqube-7.0/sonar-scanner-msbuild-4.1.1.1164-netcoreapp2.0/SonarScanner.MSBuild.dll end /d:sonar.login="e23083ae4031b7e01c2045ea5815e1e7a8292a03"
+#!/bin/bash
+dotnet /Applications/sonarqube/sonar-scanner-msbuild-4.1.1.1164-netcoreapp2.0/SonarScanner.MSBuild.dll begin /k:"starsky" /d:sonar.host.url="http://localhost:9000" /d:sonar.login="e23083ae4031b7e01c2045ea5815e1e7a8292a03" /d:sonar.cs.opencover.reportsPaths="/data/git/starsky/starsky/starskyTests/coverage.opencover.xml" && dotnet build && dotnet /Applications/sonarqube/sonar-scanner-msbuild-4.1.1.1164-netcoreapp2.0/SonarScanner.MSBuild.dll end /d:sonar.login="e23083ae4031b7e01c2045ea5815e1e7a8292a03"
 
 # this server is not accesable from outside my computer
 # so the passwords are useless!
