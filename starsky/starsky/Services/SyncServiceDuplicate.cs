932acf09e4ad918b7441bdd036cb12be9bf0c452
Dion <dionvanvelde@gmail.com>
2018-07-17 11:43:38 +0200

    RemoveDuplicate feature rewrite

--- starsky/starsky/Services/SyncServiceDuplicate.cs
+++ starsky/starsky/Services/SyncServiceDuplicate.cs
@@ -10,15 +10,17 @@ namespace starsky.Services
         // Add duplicate check
         public List<FileIndexItem> RemoveDuplicate(List<FileIndexItem> databaseSubFolderList)
         {
-
-            var duplicateItems = databaseSubFolderList.Where(x => x.FilePath.Length > 1).Select(x => x.FilePath).ToList();
             
-            // Delete removed items
+            // Get a list of duplicate items
+            var duplicateItems = databaseSubFolderList.GroupBy(item => item.FilePath)
+                .SelectMany(grp => grp.Skip(1).Take(1)).ToList();
+            
+            // Delete duplicate items
             foreach (var item in duplicateItems)
             {
-                var ditem = databaseSubFolderList.FirstOrDefault(p => p.FilePath == item);
+                var ditem = databaseSubFolderList.FirstOrDefault(p => p == item);
                 databaseSubFolderList.Remove(ditem);
-                Console.WriteLine();
+                if (AppSettingsProvider.Verbose) Console.WriteLine("> RemoveDuplicate - " + item.FilePath);
                 _query.RemoveItem(ditem);
             }
 
