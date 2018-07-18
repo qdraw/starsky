8f7f7f86199903f3354966a936ea206b9cb308f8
Dion <dionvanvelde@gmail.com>
2018-07-18 15:26:40 +0200

    update account Controller ++ remove old AccountController content

--- starsky/starskyTests/Controller/AccountControllerTest.cs
+++ starsky/starskyTests/Controller/AccountControllerTest.cs
@@ -1,115 +1,167 @@
-﻿
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using starsky.Controllers;
-using starsky.Data;
-using starsky.Interfaces;
-using starsky.Middleware;
-using starsky.Services;
-using starsky.ViewModels.Account;
-
-namespace starskytests.Controller
-{
-    [TestClass]
-    public class AccountControllerTest
-    {
-        private IUserManager _userManager;
-        
-        public AccountControllerTest()
-        {
-            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            builder.UseInMemoryDatabase("test");
-            var options = builder.Options;
-            var context = new ApplicationDbContext(options);
-            _userManager = new UserManager(context);
-        }
-
-        
-        
-        [TestMethod]
-        public async Task AccountController_Register_newAccount_Test()
-        {
-            // Arrange
-            AccountController controller = new AccountController(_userManager)
-            {
-                ControllerContext = new ControllerContext {HttpContext = new DefaultHttpContext()}
-            };
-            var newAccount = new RegisterViewModel
-            {
-                Password = "test12345678",
-                ConfirmPassword = "test12345678",
-                Email = "test@dion.local"
-            };
-            
-            // Act
-            var registerResult = await controller.Register(newAccount,true,string.Empty) as JsonResult;
-            Assert.AreNotEqual(registerResult,null);
-            var accountCreatedString = registerResult.Value as string;
-            Assert.AreEqual("Account Created",accountCreatedString);
-
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-//        private UserManager<ApplicationUser> _userManager;
-//        private SignInManager<ApplicationUser> _signInManager;
+﻿//
+//using System;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Authentication;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.DependencyInjection.Extensions;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using starsky.Controllers;
+//using starsky.Data;
+//using starsky.Interfaces;
+//using starsky.Middleware;
+//using starsky.Services;
+//using starsky.ViewModels.Account;
 //
+//namespace starskytests.Controller
+//{
+//    [TestClass]
+//    public class AccountControllerTest
+//    {
+//        private IUserManager _userManager;
+//        
 //        public AccountControllerTest()
 //        {
+//            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+//            builder.UseInMemoryDatabase("test");
+//            var options = builder.Options;
+//            var context = new ApplicationDbContext(options);
+//            _userManager = new UserManager(context);
+//            
+//            // >
 //            var serviceCollection = new ServiceCollection();
+//            serviceCollection.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+//            var services = serviceCollection.BuildServiceProvider();
+//           
+//            serviceCollection.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
 //            
-//            serviceCollection.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("db_Test"));
-//            
-//            serviceCollection.AddIdentity<ApplicationUser, IdentityRole>(options =>
-//                {
-//                    // Password settings
-//                    options.Password.RequireDigit = false;
-//                    options.Password.RequiredLength = 5;
-//                    options.Password.RequiredUniqueChars = 0;
-//                    options.Password.RequireLowercase = false;
-//                    options.Password.RequireNonAlphanumeric = false;
-//                    options.Password.RequireUppercase = false;
-//                })
-//                .AddEntityFrameworkStores<ApplicationDbContext>()
-//                .AddDefaultTokenProviders();
-//            
-//            _userManager = serviceCollection.BuildServiceProvider().GetService<UserManager<ApplicationUser>>();
-//            _signInManager = serviceCollection.BuildServiceProvider().GetService<SignInManager<ApplicationUser>>();
+//            services.GetService<IAuthenticationService>();
+////            serviceCollection.TryAddSingleton<IAuthenticationService, AuthenticationService>();
 //
 //        }
-
-        
-
-
-
-        
-//        [TestMethod]
-//        public async Task HomeControllerIndexDetailViewTest()
-//        {
-//            var newAccount = new RegisterViewModel
-//            {
-//                Password = "test12345678",
-//                ConfirmPassword = "test12345678",
-//                Email = "test@dion.local"
-//            };
 //
-//            AccountController controller = new AccountController(_userManager, _signInManager)
+//        [TestMethod]
+//        public async Task AccountController_Register_newAccount_Test()
+//        {
+//            // Arrange
+//            AccountController controller = new AccountController(_userManager)
 //            {
 //                ControllerContext = new ControllerContext {HttpContext = new DefaultHttpContext()}
 //            };
-//            controller.ControllerContext.HttpContext.Request.Headers["device-id"] = "20317";
-//            Console.WriteLine(controller.HttpContext);
-//            var d = await controller.Register(newAccount) as RegisterViewModel;
-//            
-//            
+//            var newAccount = new RegisterViewModel
+//            {
+//                Password = "test",
+//                ConfirmPassword = "test",
+//                Email = "AccountController_Register_newAccount_Test@dion.local"
+//            };
+//            // Act
+//            var registerResult = await controller.Register(newAccount,true,string.Empty) as JsonResult;
+//            Assert.AreNotEqual(registerResult,null);
+//            var accountCreatedString = registerResult.Value as string;
+//            Assert.AreEqual("Account Created",accountCreatedString);
 //        }
-    }
-}
+//
+////        private async Task AddSharedAccount()
+////        {
+////            var isLogedin = _userManager.Validate("email", "shared@dion.local", "test");
+////            if (isLogedin.Success) return;
+////            // Arrange
+////            AccountController controller = new AccountController(_userManager)
+////            {
+////                ControllerContext = new ControllerContext {HttpContext = new DefaultHttpContext()}
+////            };
+////            var newAccount = new RegisterViewModel
+////            {
+////                Password = "test",
+////                ConfirmPassword = "test",
+////                Email = "shared@dion.local"
+////            };
+////            await controller.Register(newAccount,true,string.Empty);
+////        }
+//        
+////        [TestMethod]
+////        public async Task AccountController_Login_newAccount_Test()
+////        {
+////            await AddSharedAccount();
+////            
+////            // Arrange
+////            AccountController controller = new AccountController(_userManager)
+////            {
+////                ControllerContext = new ControllerContext {HttpContext = new DefaultHttpContext()}
+////            };
+////            
+////            controller.HttpContext.RequestServices.GetService<IAuthenticationService>();
+////            controller.HttpContext.RequestServices.GetService<IServiceProvider>();
+////            controller.HttpContext.RequestServices.GetService<IHttpContextAccessor>();
+////
+////            var login = new LoginViewModel
+////            {
+////                Email = "shared@dion.local",
+////                Password = "test"
+////            };
+////            await controller.LoginPost(login);
+////
+////            
+////        }
+//
+//
+//
+//
+//
+//
+////        private UserManager<ApplicationUser> _userManager;
+////        private SignInManager<ApplicationUser> _signInManager;
+////
+////        public AccountControllerTest()
+////        {
+////            var serviceCollection = new ServiceCollection();
+////            
+////            serviceCollection.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("db_Test"));
+////            
+////            serviceCollection.AddIdentity<ApplicationUser, IdentityRole>(options =>
+////                {
+////                    // Password settings
+////                    options.Password.RequireDigit = false;
+////                    options.Password.RequiredLength = 5;
+////                    options.Password.RequiredUniqueChars = 0;
+////                    options.Password.RequireLowercase = false;
+////                    options.Password.RequireNonAlphanumeric = false;
+////                    options.Password.RequireUppercase = false;
+////                })
+////                .AddEntityFrameworkStores<ApplicationDbContext>()
+////                .AddDefaultTokenProviders();
+////            
+////            _userManager = serviceCollection.BuildServiceProvider().GetService<UserManager<ApplicationUser>>();
+////            _signInManager = serviceCollection.BuildServiceProvider().GetService<SignInManager<ApplicationUser>>();
+////
+////        }
+//
+//        
+//
+//
+//
+//        
+////        [TestMethod]
+////        public async Task HomeControllerIndexDetailViewTest()
+////        {
+////            var newAccount = new RegisterViewModel
+////            {
+////                Password = "test12345678",
+////                ConfirmPassword = "test12345678",
+////                Email = "test@dion.local"
+////            };
+////
+////            AccountController controller = new AccountController(_userManager, _signInManager)
+////            {
+////                ControllerContext = new ControllerContext {HttpContext = new DefaultHttpContext()}
+////            };
+////            controller.ControllerContext.HttpContext.Request.Headers["device-id"] = "20317";
+////            Console.WriteLine(controller.HttpContext);
+////            var d = await controller.Register(newAccount) as RegisterViewModel;
+////            
+////            
+////        }
+//    }
+//}
---
+++ starsky/starskyTests/Controller/AccountControllerTest2.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using starsky.Controllers;
+using starsky.Data;
+using starsky.Interfaces;
+using starsky.Models;
+using starsky.Services;
+using starsky.ViewModels.Account;
+
+namespace starskytests.Controller
+{
+    [TestClass]
+    public class AccountControllerTest2
+    {
+        private IUserManager _userManager;
+        private readonly IServiceProvider _serviceProvider;
+        private DefaultHttpContext _context;
+
+        public AccountControllerTest2()
+        {
+            var efServiceProvider = new ServiceCollection().AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+            var services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
+            services.AddOptions();
+            services
+                .AddDbContext<ApplicationDbContext>(b =>
+                    b.UseInMemoryDatabase("test123").UseInternalServiceProvider(efServiceProvider));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddMvc();
+            services.AddSingleton<IAuthenticationService, NoOpAuth>();
+            services.AddSingleton<IUserManager, UserManager>();
+
+            services.AddLogging();
+
+            // IHttpContextAccessor is required for SignInManager, and UserManager
+            var context = new DefaultHttpContext();
+            services.AddSingleton<IHttpContextAccessor>(
+                new HttpContextAccessor()
+                {
+                    HttpContext = context,
+                });
+
+            _serviceProvider = services.BuildServiceProvider();
+            
+            
+            // InMemory
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseInMemoryDatabase("test123");
+            var options = builder.Options;
+            var context2 = new ApplicationDbContext(options);
+            _userManager = new UserManager(context2);
+            
+        }
+        
+        [TestMethod]
+        public async Task AccountController_NoLogin_Login_And_newAccount_Test()
+        {
+            // Arrange
+            var userId = "TestUserA";
+            var phone = "abcdefg";
+            var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, userId) };
+
+            _serviceProvider.GetRequiredService<IUserManager>();
+            var httpContext = _serviceProvider.GetRequiredService<IHttpContextAccessor>().HttpContext;
+            httpContext.User = new ClaimsPrincipal(new ClaimsIdentity(claims));
+            httpContext.RequestServices = _serviceProvider;
+ 
+            var schemeProvider = _serviceProvider.GetRequiredService<IAuthenticationSchemeProvider>();
+  
+            var controller = new AccountController(_userManager);
+            controller.ControllerContext.HttpContext = httpContext;
+            
+            var login = new LoginViewModel
+            {
+                Email = "shared@dion.local",
+                Password = "test"
+            };
+            
+            // Try login > result login false
+            await controller.LoginPost(login);
+            // Test login
+            Assert.AreEqual(false,httpContext.User.Identity.IsAuthenticated);
+            
+            // Make new account; 
+            var newAccount = new RegisterViewModel
+            {
+                Password = "test",
+                ConfirmPassword = "test",
+                Email = "shared@dion.local"
+            };
+            // Arange > new account
+            await controller.Register(newAccount,true,string.Empty);
+            
+            // Try login again > now it must be succesfull
+            await controller.LoginPost(login);
+            // Test login
+            Assert.AreEqual(true,httpContext.User.Identity.IsAuthenticated);
+            
+        }
+
+    }
+
+    // Mock class for IAuth..
+    public class NoOpAuth : IAuthenticationService
+    {
+        public Task<AuthenticateResult> AuthenticateAsync(HttpContext context, string scheme)
+        {
+            return Task.FromResult(AuthenticateResult.NoResult());
+        }
+
+        public Task ChallengeAsync(HttpContext context, string scheme, AuthenticationProperties properties)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task ForbidAsync(HttpContext context, string scheme, AuthenticationProperties properties)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task SignInAsync(HttpContext context, string scheme, ClaimsPrincipal principal, AuthenticationProperties properties)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task SignOutAsync(HttpContext context, string scheme, AuthenticationProperties properties)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
