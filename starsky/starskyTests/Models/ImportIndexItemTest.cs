b21dcfa80326f30724d90b22be733fec26d730e8
Dion <dionvanvelde@gmail.com>
2018-07-19 16:09:48 +0200

    move

---
+++ starsky/starskyTests/Models/ImportIndexItemTest.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using starsky.Models;
+
+namespace starskytests.Models
+{
+    [TestClass]
+    public class ImportIndexItemTest
+    {
+        [TestMethod]
+        public void ImportIndexItemParseFileNameTest()
+        {
+            var createAnImage = new CreateAnImage();
+            AppSettingsProvider.Structure = "/yyyy/MM/yyyy_MM_dd/yyyyMMdd_HHmmss.ext";
+
+            var importItem = new ImportIndexItem();
+            importItem.SourceFullFilePath = createAnImage.FullFilePath;
+
+            var fileName = importItem.ParseFileName();
+            Assert.AreEqual("00010101_000000.jpg", fileName);
+        }
+
+        [TestMethod]
+        public void ImportIndexItemParseSubfoldersTest()
+        {
+            var createAnImage = new CreateAnImage();
+            var importItem = new ImportIndexItem();
+            importItem.SourceFullFilePath = createAnImage.FullFilePath;
+            AppSettingsProvider.Structure = "/yyyy/MM/yyyy_MM_dd/yyyyMMdd_HHmmss.ext";
+            AppSettingsProvider.BasePath = createAnImage.BasePath;
+            var s = importItem.ParseSubfolders(false);
+            Assert.AreEqual("/0001/01/0001_01_01/",s);
+        }
+
+        [TestMethod]
+        public void ImportIndexItemParseSubfolders_TRslashABC_Test()
+        {
+            var createAnImage = new CreateAnImage();
+            var importItem = new ImportIndexItem();
+            AppSettingsProvider.Structure = "/\\t\\r/\\a\\b\\c/test";
+
+            importItem.SourceFullFilePath = createAnImage.FullFilePath;
+            AppSettingsProvider.BasePath = createAnImage.BasePath;
+            var s = importItem.ParseSubfolders(false);
+            Assert.AreEqual("/tr/abc/",s);
+        }
+        
+        [TestMethod]
+        public void ImportIndexItemParseSubfolders_Tzzz_slashABC_Test()
+        {
+            var createAnImage = new CreateAnImage();
+            var importItem = new ImportIndexItem();
+            AppSettingsProvider.Structure = "/\\t\\z/\\a\\b\\c/test";
+
+            importItem.SourceFullFilePath = createAnImage.FullFilePath;
+            AppSettingsProvider.BasePath = createAnImage.BasePath;
+            var s = importItem.ParseSubfolders(false);
+            Assert.AreEqual("/tz/abc/",s);
+        }
+        
+        [TestMethod]
+        public void ImportIndexItemParse_filenamebase_filename_Test()
+        {
+            var createAnImage = new CreateAnImage();
+            var importItem = new ImportIndexItem();
+            AppSettingsProvider.Structure = "/\\t\\z/\\a\\b{filenamebase}/{filenamebase}.ext";
+
+            importItem.SourceFullFilePath = createAnImage.FullFilePath;
+            AppSettingsProvider.BasePath = createAnImage.BasePath;
+            var fileName = importItem.ParseFileName();
+            Assert.AreEqual(createAnImage.DbPath.Replace("/",string.Empty),fileName);
+        }
+        
+        [TestMethod]
+        public void ImportIndexItemParse_filenamebase_subfolder_Test()
+        {
+            var createAnImage = new CreateAnImage();
+            var importItem = new ImportIndexItem();
+            AppSettingsProvider.Structure = "/{filenamebase}/{filenamebase}.ext";
+
+            importItem.SourceFullFilePath = createAnImage.FullFilePath;
+            AppSettingsProvider.BasePath = createAnImage.BasePath;
+            var subfolders = importItem.ParseSubfolders(false);
+            Assert.AreEqual("/" + createAnImage.DbPath.Replace("/",string.Empty).Replace(".jpg",string.Empty) + "/",subfolders);
+        }
+
+
+        [TestMethod]
+        public void ImportIndexItemParse_ParseDateTimeFromFileName_Test()
+        {
+            
+        }
+
+    }
+}
