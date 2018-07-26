7dcdda8d98b2a0b9d7a0c25878168c3e711b4da5
Dion <dionvanvelde@gmail.com>
2018-07-26 19:41:07 +0200

    update build name

--- starsky/publish-linux-arm.sh
+++ starsky/publish-linux-arm.sh
@@ -4,7 +4,7 @@ mkdir linux-arm
 pushd starsky
 dotnet publish -c release -r linux-arm --framework netcoreapp2.0 --output ../linux-arm
 popd
-pushd starsky-cli
+pushd starskysynccli
 dotnet publish -c release -r linux-arm --framework netcoreapp2.0 --no-dependencies --output ../linux-arm
 popd
 pushd starskyimportercli
