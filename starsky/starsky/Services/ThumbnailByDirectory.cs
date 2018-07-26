86ccc9e2f998837fb41cd5645d27844d41a13040
Dion <dionvanvelde@gmail.com>
2018-07-26 21:35:34 +0200

    fix thumbnail again

--- starsky/starsky/Services/ThumbnailByDirectory.cs
+++ starsky/starsky/Services/ThumbnailByDirectory.cs
@@ -18,47 +18,44 @@ namespace starsky.Services
 
         public void CreateThumb(string subpath = "/")
         {
-            throw new NotImplementedException();
-        }
+            // Thumbnail check service
+            
+            var subFoldersFullPath =  Files.GetAllFilesDirectory(
+                _appSettings.DatabasePathToFilePath(subpath)).ToList();
+            
+            // Add Subpath to scan the root folder for thumbs       
+            subFoldersFullPath.Add(_appSettings.DatabasePathToFilePath(subpath));
+            
+            foreach (var singleFolderFullPath in subFoldersFullPath)
+            {
+                
+                Console.WriteLine(singleFolderFullPath);
+                
+                string[] filesInDirectoryFullPath = Files.GetFilesInDirectory(singleFolderFullPath,_appSettings);
+                var localFileListFileHash = FileHash.GetHashCode(filesInDirectoryFullPath);
 
-//        public void CreateThumb(string subpath = "/")
-//        {
-//            // Thumbnail check service
-//            var subFoldersFullPath =  Files.GetAllFilesDirectory(subpath).ToList();
-//            
-//            // Add Subpath to scan the root folder for thumbs       
-//            subFoldersFullPath.Add(FileIndexItem.DatabasePathToFilePath(subpath));
-//            
-//            foreach (var singleFolderFullPath in subFoldersFullPath)
-//            {
-//                
-//                Console.WriteLine(singleFolderFullPath);
-//                
-//                string[] filesInDirectoryFullPath = Files.GetFilesInDirectory(//\\\singleFolderFullPath,false);
-//                var localFileListFileHash = FileHash.GetHashCode(filesInDirectoryFullPath);
-//
-//                for (int i = 0; i < filesInDirectoryFullPath.Length; i++)
-//                {
-//                    var value = new FileIndexItem()
-//                    {
-//                        ParentDirectory = Breadcrumbs.BreadcrumbHelper(FileIndexItem.FullPathToDatabaseStyle(filesInDirectoryFullPath[i])).LastOrDefault(),
-//                        FileName = Path.GetFileName(FileIndexItem.FullPathToDatabaseStyle(filesInDirectoryFullPath[i])),
-//                        FileHash = localFileListFileHash[i]
-//                    };
-//                    // FilePath = FileIndexItem.FullPathToDatabaseStyle(filesInDirectoryFullPath[i]),
-//
-//
-//                    if (AppSettingsProvider.Verbose) Console.WriteLine("localFileListFileHash[i] " + localFileListFileHash[i]); 
-//                    
-//                    new Thumbnail(_appSettings).CreateThumb(value);
-//                }
-//
-//                if (filesInDirectoryFullPath.Length >= 1)
-//                {
-//                    Console.WriteLine("~ " + filesInDirectoryFullPath.Length + " ~ "+  FileIndexItem.FullPathToDatabaseStyle(singleFolderFullPath));
-//                }
-//
-//            }
-//        }
+                for (int i = 0; i < filesInDirectoryFullPath.Length; i++)
+                {
+                    var value = new FileIndexItem()
+                    {
+                        ParentDirectory = Breadcrumbs.BreadcrumbHelper(_appSettings.FullPathToDatabaseStyle(filesInDirectoryFullPath[i])).LastOrDefault(),
+                        FileName = Path.GetFileName(_appSettings.FullPathToDatabaseStyle(filesInDirectoryFullPath[i])),
+                        FileHash = localFileListFileHash[i]
+                    };
+                    // FilePath = FileIndexItem.FullPathToDatabaseStyle(filesInDirectoryFullPath[i]),
+
+
+                    if (AppSettingsProvider.Verbose) Console.WriteLine("localFileListFileHash[i] " + localFileListFileHash[i]); 
+                    
+                    new Thumbnail(_appSettings).CreateThumb(value);
+                }
+
+                if (filesInDirectoryFullPath.Length >= 1)
+                {
+                    Console.WriteLine("~ " + filesInDirectoryFullPath.Length + " ~ "+  _appSettings.FullPathToDatabaseStyle(singleFolderFullPath));
+                }
+
+            }
+        }
     }
 }
