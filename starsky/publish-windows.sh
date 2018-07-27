76482b39f1d39c617bb2edfb4f5f969e5aee172a
Dion van Velde <qdraw@users.noreply.github.com>
2018-07-27 18:30:34 +0200

    starskysynccli

--- starsky/publish-windows.sh
+++ starsky/publish-windows.sh
@@ -4,7 +4,7 @@ mkdir win7-x86
 pushd starsky
 dotnet publish -c release -r win7-x86 --framework netcoreapp2.1 --output ../win7-x86
 popd
-pushd starsky-cli
+pushd starskysynccli
 dotnet publish -c release -r win7-x86 --framework netcoreapp2.1 --output ../win7-x86
 popd
 pushd starskyimportercli
