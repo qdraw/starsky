1a8b815a0fe85803b2962a65a318e9039f0c186b
Dion van Velde <qdraw@users.noreply.github.com>
2018-07-27 18:30:50 +0200

    starskysynccli

--- starsky/publish-mac.sh
+++ starsky/publish-mac.sh
@@ -4,7 +4,7 @@ mkdir osx.10.12-x64
 pushd starsky
 dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --output ../osx.10.12-x64
 popd
-pushd starsky-cli
+pushd starskysynccli
 dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --output ../osx.10.12-x64
 popd
 pushd starskyimportercli
