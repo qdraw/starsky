6c17b16b6f4c06100494d63555642552942ee6d2
Dion <dionvanvelde@gmail.com>
2018-07-31 08:39:41 +0200

    add hide migrations to test

--- starsky/starskyTest.sh
+++ starsky/starskyTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 pushd starskyTests
-dotnet test /p:ExcludeByFile=\"Migrations/*\" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+dotnet test /p:hideMigrations=\"true\" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 #| tee readme.coverage.bak
 #bash readme-listoftests-update.sh
 #bash readme-coverage-update.sh
