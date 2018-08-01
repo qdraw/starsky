72d0b04a7423c4a19c7e2c6042c541d8655f4a5e
Dion <dionvanvelde@gmail.com>
2018-08-01 10:41:50 +0200

    migration

--- starsky/starsky/Migrations/ApplicationDbContextModelSnapshot.cs
+++ starsky/starsky/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
 using starsky.Data;
+using starsky.Helpers;
 using starsky.Models;
 using System;
 
@@ -18,7 +19,7 @@ namespace starsky.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.0.2-rtm-10011");
+                .HasAnnotation("ProductVersion", "2.0.3-rtm-10026");
 
             modelBuilder.Entity("starsky.Models.Account.Credential", b =>
                 {
@@ -167,6 +168,8 @@ namespace starsky.Migrations
                     b.Property<string>("FileName");
 
                     b.Property<string>("FilePath");
+
+                    b.Property<int>("ImageFormat");
 
                     b.Property<bool>("IsDirectory");
 
