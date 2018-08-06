87e641cd786325fb0ba76124014789c8ce954c4a
Dion <dionvanvelde@gmail.com>
2018-08-06 12:03:43 +0200

    add ignore ++ comments

--- starsky/starsky/ViewModels/DetailView.cs
+++ starsky/starsky/ViewModels/DetailView.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 using starsky.Models;
 
 namespace starsky.ViewModels
@@ -8,10 +9,14 @@ namespace starsky.ViewModels
         public FileIndexItem FileIndexItem { get; set; }
         public RelativeObjects RelativeObjects { get; set; }
         public List<string> Breadcrumb { get; set; }
-        public IEnumerable<FileIndexItem.ColorUserInterface> GetAllColor { get; set; }
         public List<FileIndexItem.Color> ColorClassFilterList { get; set; }
         // Used by react client
         public string PageType => PageViewType.PageType.DetailView.ToString();
+        // To return error codes// in the json it is always false
         public bool IsDirectory { get; set; }
+
+        // Used by Razor view
+        [JsonIgnore]
+        public IEnumerable<FileIndexItem.ColorUserInterface> GetAllColor { get; set; }
     }
 }
