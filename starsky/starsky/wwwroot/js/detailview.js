add46f9a7c541b0b1f8874f02e4cf9c85838d7b4
Dion <dionvanvelde@gmail.com>
2018-08-07 14:02:44 +0200

    unused feature

--- starsky/starsky/wwwroot/js/detailview.js
+++ starsky/starsky/wwwroot/js/detailview.js
@@ -521,62 +521,62 @@ function rotateOn409() {
     }
 }
 
-function imageToDataUri(img, width, height) {
-
-    // create an off-screen canvas
-    var canvas = document.createElement('canvas'),
-        ctx = canvas.getContext('2d');
-
-    // set its dimension to target size
-    canvas.width = width;
-    canvas.height = height;
-
-    // draw source image into the off-screen canvas:
-    ctx.drawImage(img, 0, 0, width, height);
-
-    // encode image to data-uri with base64 version of compressed image
-    return canvas.toDataURL();
-}
-
-function rotateImage(srcBase64, srcOrientation, callback) {
-    var img = new Image();
-
-    img.onload = function() {
-        var width = img.width,
-            height = img.height,
-            canvas = document.createElement('canvas'),
-            ctx = canvas.getContext("2d");
-
-        // set proper canvas dimensions before transform & export
-        if (4 < srcOrientation && srcOrientation < 9) {
-            canvas.width = height;
-            canvas.height = width;
-        } else {
-            canvas.width = width;
-            canvas.height = height;
-        }
-
-        // transform context before drawing image
-        switch (srcOrientation) {
-            case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
-            case 3: ctx.transform(-1, 0, 0, -1, width, height ); break;
-            case 4: ctx.transform(1, 0, 0, -1, 0, height ); break;
-            case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
-            case 6: ctx.transform(0, 1, -1, 0, height , 0); break;
-            case 7: ctx.transform(0, -1, -1, 0, height , width); break;
-            case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
-            default: break;
-        }
-
-        // draw image
-        ctx.drawImage(img, 0, 0);
-
-        // export base64
-        callback(canvas.toDataURL());
-    };
-
-    img.src = srcBase64;
-}
+// function imageToDataUri(img, width, height) {
+//
+//     // create an off-screen canvas
+//     var canvas = document.createElement('canvas'),
+//         ctx = canvas.getContext('2d');
+//
+//     // set its dimension to target size
+//     canvas.width = width;
+//     canvas.height = height;
+//
+//     // draw source image into the off-screen canvas:
+//     ctx.drawImage(img, 0, 0, width, height);
+//
+//     // encode image to data-uri with base64 version of compressed image
+//     return canvas.toDataURL();
+// }
+//
+// function rotateImage(srcBase64, srcOrientation, callback) {
+//     var img = new Image();
+//
+//     img.onload = function() {
+//         var width = img.width,
+//             height = img.height,
+//             canvas = document.createElement('canvas'),
+//             ctx = canvas.getContext("2d");
+//
+//         // set proper canvas dimensions before transform & export
+//         if (4 < srcOrientation && srcOrientation < 9) {
+//             canvas.width = height;
+//             canvas.height = width;
+//         } else {
+//             canvas.width = width;
+//             canvas.height = height;
+//         }
+//
+//         // transform context before drawing image
+//         switch (srcOrientation) {
+//             case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+//             case 3: ctx.transform(-1, 0, 0, -1, width, height ); break;
+//             case 4: ctx.transform(1, 0, 0, -1, 0, height ); break;
+//             case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+//             case 6: ctx.transform(0, 1, -1, 0, height , 0); break;
+//             case 7: ctx.transform(0, -1, -1, 0, height , width); break;
+//             case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+//             default: break;
+//         }
+//
+//         // draw image
+//         ctx.drawImage(img, 0, 0);
+//
+//         // export base64
+//         callback(canvas.toDataURL());
+//     };
+//
+//     img.src = srcBase64;
+// }
 
 
 
