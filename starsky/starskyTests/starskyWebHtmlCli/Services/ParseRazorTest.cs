35d60c628f9d53818b7f90e02fa26cbff5752262
Dion <dionvanvelde@gmail.com>
2018-09-03 16:49:18 +0200

    add test

---
+++ starsky/starskyTests/starskyWebHtmlCli/Services/ParseRazorTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using starskywebhtmlcli.Services;
+
+namespace starskytests.starskyWebHtmlCli.Services
+{
+    [TestClass]
+    public class ParseRazorTest
+    {
+        [TestMethod]
+        public async Task ParseRazorTestNotFound()
+        {
+            var result = await new ParseRazor().EmbeddedViews(null, null);
+            Assert.AreEqual(string.Empty,result);
+        }
+    }
+}
--- starsky/starskywebhtmlcli/Services/ParseRazor.cs
+++ starsky/starskywebhtmlcli/Services/ParseRazor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Server.Kestrel.Internal.System.Runtime;
 using RazorLight;
 using starsky.Helpers;
 using starsky.Models;
