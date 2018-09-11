3dec1f78c6a5f015d9075d7e403a9cc6c8438378
Dion <dionvanvelde@gmail.com>
2018-09-11 14:55:44 +0200

    move thumb

--- starsky/starskyTests/Services/ThumbnailTest.cs
+++ starsky/starskyTests/Services/ThumbnailTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,7 +7,7 @@ using starsky.Middleware;
 using starsky.Models;
 using starsky.Services;
 
-namespace starskytests
+namespace starskytests.Services
 {
     [TestClass]
     public class ThumbnailTest
