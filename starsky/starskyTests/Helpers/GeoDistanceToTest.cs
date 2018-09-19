37147accebe42d018aa2b3989920d01e9e9ea96a
Dion <dionvanvelde@gmail.com>
2018-09-19 11:58:31 +0200

    add large distance test

--- starsky/starskyTests/Helpers/GeoDistanceToTest.cs
+++ starsky/starskyTests/Helpers/GeoDistanceToTest.cs
@@ -15,6 +15,14 @@ namespace starskytests.Helpers
             // https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
             Assert.AreEqual(0.4223,distance,0.001);
         }
+        
+        [TestMethod]
+        public void GetDistanceTestLargeNumber()
+        {
+            var distance = GeoDistanceTo.GetDistance(-51.6824134, -5.2984455, 51.6852471, 5.2943688);
+            Assert.AreEqual(11549.857139273592,distance,0.001);
+        }
+        
             
             
     }
