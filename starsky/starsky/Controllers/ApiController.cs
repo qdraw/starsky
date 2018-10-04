b899fb35e4a8e980a470d11878be1cb50d5221e2
Dion <dionvanvelde@gmail.com>
2018-10-04 15:44:15 +0200

    move separate class

--- starsky/starsky/Controllers/ApiController.cs
+++ starsky/starsky/Controllers/ApiController.cs
@@ -554,55 +554,12 @@ namespace starsky.Controllers
             return BadRequest("ignored, please check if the 'f' path exist or use a folder string to clear the cache");
         }
 
-        [HttpGet]
         [HttpPost]
-        public IActionResult Rename(string f, string to, bool json = false, bool collections = true)
+        public IActionResult Rename(string f, string to, bool collections = true)
         {
-            var inputFileSubPaths = ConfigRead.SplitInputFilePaths(f);
-            var toFileSubPaths = ConfigRead.SplitInputFilePaths(to);
-            
-            // the result list
-            var fileIndexResultsList = new List<FileIndexItem>();
-            
-            // To check if the file has a unique name (in database)
-            for (var i = 0; i < toFileSubPaths.Length; i++)
-            {
-                var detailView = _query.SingleItem(toFileSubPaths[i], null, collections, false);
-                if (detailView != null) toFileSubPaths[i] = null;
-            }
+            new RenameFs(_appSettings,_query).Rename(f,to,collections);
 
-            for (var i = 0; i < inputFileSubPaths.Length; i++)
-            {
-                var detailView = _query.SingleItem(inputFileSubPaths[i], null, collections, false);
-                if (detailView != null) inputFileSubPaths[i] = null;
-            }
-
-            // Remove null from list
-            toFileSubPaths = toFileSubPaths.Where(p => p != null).ToArray();
-            inputFileSubPaths = inputFileSubPaths.Where(p => p != null).ToArray();
-
-            // Check if two list are the same lenght - Change this in the future
-            if (toFileSubPaths.Length != inputFileSubPaths.Length) return BadRequest("f != to");
-
-            for (var i = 0; i < toFileSubPaths.Length; i++)
-            {
-                var inputFileSubPath = inputFileSubPaths[i];
-                var toFileSubPath = toFileSubPaths[i];
-
-                var detailView = _query.SingleItem(inputFileSubPath, null, collections, false);
-                // files that not exist
-                if(detailView == null) continue;
-                if (detailView.IsDirectory)
-                {
-                    var toFileFullPath = _appSettings.DatabasePathToFilePath(toFileSubPath);
-                    var inputFileFullPath = _appSettings.DatabasePathToFilePath(inputFileSubPath);
-
-                    Directory.Move(inputFileFullPath,toFileFullPath);
-                    // move also in the database
-                }
-            }
-
-            return Json(fileIndexResultsList);
+            return Json("");
         }
 
 
---
+++ starsky/starsky/Helpers/RenameFs.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using starsky.Interfaces;
+using starsky.Models;
+using starsky.Services;
+
+namespace starsky.Helpers
+{
+    public class RenameFs
+    {
+        private readonly IQuery _query;
+        private readonly AppSettings _appSettings;
+
+        public RenameFs(AppSettings appSettings, IQuery query)
+        {
+            _query = query;
+            _appSettings = appSettings;
+        }
+
+        public void Rename(string f, string to, bool collections = true)
+        {
+            var inputFileSubPaths = ConfigRead.SplitInputFilePaths(f);
+            var toFileSubPaths = ConfigRead.SplitInputFilePaths(to);
+            
+            // the result list
+            var fileIndexResultsList = new List<FileIndexItem>();
+            
+            // To check if the file has a unique name (in database)
+            for (var i = 0; i < toFileSubPaths.Length; i++)
+            {
+                var detailView = _query.SingleItem(toFileSubPaths[i], null, collections, false);
+                if (detailView != null) toFileSubPaths[i] = null;
+            }
+
+            for (var i = 0; i < inputFileSubPaths.Length; i++)
+            {
+                var detailView = _query.SingleItem(inputFileSubPaths[i], null, collections, false);
+                if (detailView != null) inputFileSubPaths[i] = null;
+            }
+
+            // Remove null from list
+            toFileSubPaths = toFileSubPaths.Where(p => p != null).ToArray();
+            inputFileSubPaths = inputFileSubPaths.Where(p => p != null).ToArray();
+
+            // Check if two list are the same lenght - Change this in the future BadRequest("f != to")
+            if (toFileSubPaths.Length != inputFileSubPaths.Length) return;
+
+            for (var i = 0; i < toFileSubPaths.Length; i++)
+            {
+                var inputFileSubPath = inputFileSubPaths[i];
+                var toFileSubPath = toFileSubPaths[i];
+
+                var detailView = _query.SingleItem(inputFileSubPath, null, collections, false);
+                // files that not exist
+                if(detailView == null) continue;
+                if (detailView.IsDirectory)
+                {
+                    var toFileFullPath = _appSettings.DatabasePathToFilePath(toFileSubPath);
+                    var inputFileFullPath = _appSettings.DatabasePathToFilePath(inputFileSubPath);
+
+                    Directory.Move(inputFileFullPath,toFileFullPath);
+                    // move also in the database
+                }
+            }
+        }
+    }
+}
