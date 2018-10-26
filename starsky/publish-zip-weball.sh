e401ebb3d17d500e6b34290a1c03445ec2ad41a8
Dion <dionvanvelde@gmail.com>
2018-10-26 16:57:18 +0200

    spacing

--- starsky/publish-zip-weball.sh
+++ starsky/publish-zip-weball.sh
@@ -13,23 +13,23 @@ zip -r --filesync starsky-win7-x86.zip win7-x86
 
 # Remove appsettings.*
 zip -d starsky-linux-arm.zip "linux-arm/appsettings.*.json"
-zip -d starsky-osx.10.12-x64.zip "starsky-osx.10.12-x64/appsettings.*.json"
+zip -d starsky-osx.10.12-x64.zip "osx.10.12-x64/appsettings.*.json"
 zip -d starsky-win7-x86.zip "win7-x86/appsettings.*.json"
 
 # remove temp folder
-zip -d starsky-linux-arm.zip "temp/*"
-zip -d starsky-osx.10.12-x64.zip "temp/*"
-zip -d starsky-win7-x86.zip "temp/*"
+zip -d starsky-linux-arm.zip "linux-arm/temp/*"
+zip -d starsky-osx.10.12-x64.zip "osx.10.12-x64/temp/*"
+zip -d starsky-win7-x86.zip "win7-x86/temp/*"
 
 # remove thumbnailTempFolder
-zip -d starsky-linux-arm.zip "thumbnailTempFolder/*"
-zip -d starsky-osx.10.12-x64.zip "thumbnailTempFolder/*"
-zip -d starsky-win7-x86.zip "thumbnailTempFolder/*"
+zip -d starsky-linux-arm.zip "linux-arm/thumbnailTempFolder/*"
+zip -d starsky-osx.10.12-x64.zip "osx.10.12-x64/thumbnailTempFolder/*"
+zip -d starsky-win7-x86.zip "win7-x86/thumbnailTempFolder/*"
 
 # remove storageFolder
-zip -d starsky-linux-arm.zip "storageFolder/*"
-zip -d starsky-osx.10.12-x64.zip "storageFolder/*"
-zip -d starsky-win7-x86.zip "storageFolder/*"
+zip -d starsky-linux-arm.zip "linux-arm/storageFolder/*"
+zip -d starsky-osx.10.12-x64.zip "osx.10.12-x64/storageFolder/*"
+zip -d starsky-win7-x86.zip "win7-x86/storageFolder/*"
 
 rsync -v -e ssh starsky-linux-arm.zip x:/mnt/juno/storage/www/public_apollo/
 rsync -v -e ssh starsky-osx.10.12-x64.zip x:/mnt/juno/storage/www/public_apollo/
