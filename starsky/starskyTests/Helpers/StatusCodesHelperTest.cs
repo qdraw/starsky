ce3c068b77d789ec3de55b20f87c42c02a015fa2
Dion <dionvanvelde@gmail.com>
2018-11-01 14:29:04 +0100

    add tests for ReturnExifStatusError

--- starsky/starskyTests/Helpers/StatusCodesHelperTest.cs
+++ starsky/starskyTests/Helpers/StatusCodesHelperTest.cs
@@ -10,7 +10,7 @@ namespace starskytests.Helpers
 	public class StatusCodesHelperTest
 	{
 		[TestMethod]
-		public void StatusCodesHelperTest_NotFoundNotInIndex()
+		public void StatusCodesHelperTest_FileCollectionsCheck_NotFoundNotInIndex()
 		{
 			var appSettings = new AppSettings();
 			var status = new StatusCodesHelper(appSettings).FileCollectionsCheck(null);
@@ -18,7 +18,7 @@ namespace starskytests.Helpers
 		}
 		
 		[TestMethod]
-		public void StatusCodesHelperTest_DirReadOnly()
+		public void StatusCodesHelperTest_FileCollectionsCheck_DirReadOnly()
 		{
 			var appSettings = new AppSettings{ReadOnlyFolders = new List<string>{"/"}};
 			var detailView = new DetailView
@@ -31,7 +31,7 @@ namespace starskytests.Helpers
 		}
 		
 		[TestMethod]
-		public void StatusCodesHelperTest_NotFoundIsDir()
+		public void StatusCodesHelperTest_FileCollectionsCheck_NotFoundIsDir()
 		{
 			var appSettings = new AppSettings();
 			var detailView = new DetailView
@@ -41,6 +41,66 @@ namespace starskytests.Helpers
 			};
 			var status = new StatusCodesHelper(appSettings).FileCollectionsCheck(detailView);
 			Assert.AreEqual(FileIndexItem.ExifStatus.NotFoundIsDir,status);
+		}
+
+		[TestMethod]
+		public void StatusCodesHelperTest_ReturnExifStatusError_NotFoundIsDir()
+		{
+			var appSettings = new AppSettings();
+			var statusModel = new FileIndexItem();
+			var statusResults = FileIndexItem.ExifStatus.NotFoundIsDir;
+			var fileIndexResultsList = new List<FileIndexItem>();
+			var statusBool = new StatusCodesHelper(appSettings).ReturnExifStatusError(statusModel, statusResults,
+					fileIndexResultsList);
+			Assert.AreEqual(true,statusBool);
+		}
+
+		[TestMethod]
+		public void StatusCodesHelperTest_ReturnExifStatusError_DirReadOnly()
+		{
+			var appSettings = new AppSettings();
+			var statusModel = new FileIndexItem();
+			var statusResults = FileIndexItem.ExifStatus.DirReadOnly;
+			var fileIndexResultsList = new List<FileIndexItem>();
+			var statusBool = new StatusCodesHelper(appSettings).ReturnExifStatusError(statusModel, statusResults,
+				fileIndexResultsList);
+			Assert.AreEqual(true,statusBool);
+		}
+
+		[TestMethod]
+		public void StatusCodesHelperTest_ReturnExifStatusError_NotFoundNotInIndex()
+		{
+			var appSettings = new AppSettings();
+			var statusModel = new FileIndexItem();
+			var statusResults = FileIndexItem.ExifStatus.NotFoundNotInIndex;
+			var fileIndexResultsList = new List<FileIndexItem>();
+			var statusBool = new StatusCodesHelper(appSettings).ReturnExifStatusError(statusModel, statusResults,
+				fileIndexResultsList);
+			Assert.AreEqual(true,statusBool);
+		}
+		
+		[TestMethod]
+		public void StatusCodesHelperTest_ReturnExifStatusError_NotFoundSourceMissing()
+		{
+			var appSettings = new AppSettings();
+			var statusModel = new FileIndexItem();
+			var statusResults = FileIndexItem.ExifStatus.NotFoundSourceMissing;
+			var fileIndexResultsList = new List<FileIndexItem>();
+			var statusBool = new StatusCodesHelper(appSettings).ReturnExifStatusError(statusModel, statusResults,
+				fileIndexResultsList);
+			Assert.AreEqual(true,statusBool);
+		}
+	
+		[TestMethod]
+		public void StatusCodesHelperTest_ReturnExifStatusError_ReadOnly()
+		{
+			var appSettings = new AppSettings();
+			var statusModel = new FileIndexItem();
+			var statusResults = FileIndexItem.ExifStatus.ReadOnly;
+			var fileIndexResultsList = new List<FileIndexItem>();
+			var statusBool = new StatusCodesHelper(appSettings).ReturnExifStatusError(statusModel, statusResults,
+				fileIndexResultsList);
+			Assert.AreEqual(true,statusBool);
 		}
 		
 	}
