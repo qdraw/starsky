47b948b817c1fd5974bb6b9a5c4dfc82b0595b51
Dion <dionvanvelde@gmail.com>
2018-11-28 21:38:20 +0100

    add redirect controller

---
+++ starsky/starsky/Controllers/RedirectController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using starsky.Models;
+
+namespace starsky.Controllers
+{
+	public class RedirectController : Controller
+	{
+		private readonly AppSettings _appsettings;
+
+		public RedirectController(AppSettings appsettings = null)
+		{
+			_appsettings = appsettings;
+		}
+			    
+		public IActionResult SubpathRelative(int value, bool json = false)
+		{
+			var importmodel = new ImportIndexItem(_appsettings)
+			{
+				DateTime = DateTime.Today.AddDays(value), 
+				SourceFullFilePath = "notimplemented.jpg"
+			};
+			// expect something like this: /2018/09/2018_09_02/
+			var subpath = importmodel.ParseSubfolders(false);
+			if(json) return Json(subpath);
+			return RedirectToAction("Index", "Home", new { f = subpath });
+		}
+
+	}
+}
