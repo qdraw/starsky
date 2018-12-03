8ea4a5a929efbe8ec24ec74f11d0e84b25eadeb9
Dion <dionvanvelde@gmail.com>
2018-12-03 22:20:37 +0100

    add delete example

--- starsky/tools/delete.sql
+++ starsky/tools/delete.sql
@@ -1,4 +1,5 @@
 DELETE FROM FileIndex WHERE `ParentDirectory` = '/2018/03/2018_03_18'
 
-
 DELETE FROM FileIndex WHERE `IsDirectory` = 'true'
+
+DELETE FROM FileIndex WHERE `FilePath` LIKE '%/2017/11%'
