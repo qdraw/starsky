68cf74a8312f702ec327ac97638dcaf58fe6b00e
Dion van Velde <dionvanvelde@gmail.com>
2018-12-04 16:08:41 +0100

    add check for duplicates

--- starsky/starsky/Services/ConfigRead.cs
+++ starsky/starsky/Services/ConfigRead.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -101,7 +101,18 @@ namespace starsky.Services
             // input devided by dot comma and blank values are removed
             var inputFilePaths = f.Split(";");
             inputFilePaths = inputFilePaths.Where(x => !string.IsNullOrEmpty(x)).ToArray();
-            return inputFilePaths;
+
+			// Remove duplicates from list
+			// have a single slash in front the path
+	        HashSet<string> inputHashSet = new HashSet<string>(); 
+	        foreach ( var path in inputFilePaths )
+	        {
+		        var subpath = RemovePrefixDbSlash(path);
+		        subpath = PrefixDbSlash(subpath);
+
+		        inputHashSet.Add(subpath);
+	        }
+            return inputHashSet.ToArray();
         }
       
         
