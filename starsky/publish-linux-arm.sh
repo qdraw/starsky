35fb3fd0bb966d4e332f8c1b9609aa0cef046255
Dion <dionvanvelde@gmail.com>
2018-12-07 19:16:52 +0100

    use 2.1

--- starsky/publish-linux-arm.sh
+++ starsky/publish-linux-arm.sh
@@ -2,17 +2,17 @@
 cd "$(dirname "$0")"
 mkdir linux-arm
 pushd starsky
-dotnet publish -c release -r linux-arm --framework netcoreapp2.0 --output ../linux-arm
+dotnet publish -c release -r linux-arm --framework netcoreapp2.1 --output ../linux-arm
 popd
 pushd starskysynccli
-dotnet publish -c release -r linux-arm --framework netcoreapp2.0 --no-dependencies --output ../linux-arm
+dotnet publish -c release -r linux-arm --framework netcoreapp2.1 --no-dependencies --output ../linux-arm
 popd
 pushd starskyimportercli
-dotnet publish -c release -r linux-arm --framework netcoreapp2.0 --no-dependencies --output ../linux-arm
+dotnet publish -c release -r linux-arm --framework netcoreapp2.1 --no-dependencies --output ../linux-arm
 popd
 pushd starskywebhtmlcli
-dotnet publish -c release -r linux-arm --framework netcoreapp2.0 --output ../linux-arm
+dotnet publish -c release -r linux-arm --framework netcoreapp2.1 --output ../linux-arm
 popd
 pushd starskygeocli
-dotnet publish -c release -r linux-arm --framework netcoreapp2.0 --output ../linux-arm
+dotnet publish -c release -r linux-arm --framework netcoreapp2.1 --output ../linux-arm
 popd
--- starsky/publish-mac.sh
+++ starsky/publish-mac.sh
@@ -2,17 +2,17 @@
 cd "$(dirname "$0")"
 mkdir osx.10.12-x64
 pushd starsky
-dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.0 --output ../osx.10.12-x64
+dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --output ../osx.10.12-x64
 popd
 pushd starskysynccli
-dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.0 --no-dependencies --output ../osx.10.12-x64
+dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --no-dependencies --output ../osx.10.12-x64
 popd
 pushd starskyimportercli
-dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.0 --no-dependencies --output ../osx.10.12-x64
+dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --no-dependencies --output ../osx.10.12-x64
 popd
 pushd starskywebhtmlcli
-dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.0 --output ../osx.10.12-x64
+dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --output ../osx.10.12-x64
 popd
 pushd starskygeocli
-dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.0 --output ../osx.10.12-x64
+dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --output ../osx.10.12-x64
 popd
