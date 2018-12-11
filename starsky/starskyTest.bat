81b0e9854db14f93bc04acb40d846c082eb60f5d
Dion <dionvanvelde@gmail.com>
2018-12-11 17:05:39 +0100

    comment

--- starsky/starskyTest.bat
+++ starsky/starskyTest.bat
@@ -1,4 +1,4 @@
-
+REM Windows batch file
 PUSHD starskyTests
 dotnet test /p:hideMigrations=\"true\" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 dotnet reportgenerator "-reports:coverage.opencover.xml" "-targetdir:coveragereport" -reporttypes:HtmlInline_AzurePipelines
