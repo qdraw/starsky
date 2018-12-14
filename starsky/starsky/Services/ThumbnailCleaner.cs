c21c19cfc85d1903a9939f715e3770cd86a34db0
Dion <dionvanvelde@gmail.com>
2018-12-14 15:11:30 +0100

    add cleaner and test

---
+++ starsky/starsky/Services/ThumbnailCleaner.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+using System.Linq;
+using starsky.Helpers;
+using starsky.Interfaces;
+using starsky.Models;
+
+namespace starsky.Services
+{
+	public class ThumbnailCleaner
+	{
+		private readonly AppSettings _appSettings;
+		private readonly IQuery _query;
+
+		public ThumbnailCleaner(IQuery iquery, AppSettings appSettings)
+		{
+			_appSettings = appSettings;
+			_query = iquery;
+
+			if (!Directory.Exists(_appSettings.ThumbnailTempFolder))
+			{
+				throw new DirectoryNotFoundException("ThumbnailTempFolder not found " 
+				                                + _appSettings.ThumbnailTempFolder);
+			}
+		}
+
+//		private readonly char[] _digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".ToCharArray();
+		
+		public void CleanAllUnusedFiles()
+		{
+
+			var allThumbnailFiles = GetAllThumbnailFiles();
+			if(_appSettings.Verbose) Console.WriteLine(allThumbnailFiles.Length);
+			
+			foreach ( var thumbnailFile in allThumbnailFiles )
+			{
+				var fileHash = Path.GetFileNameWithoutExtension(thumbnailFile.Name);
+				var itemByHash = _query.GetItemByHash(fileHash);
+				if ( itemByHash == null )
+				{
+					Files.DeleteFile(thumbnailFile.FullName);
+				}
+			}
+		}
+
+		public FileInfo[] GetAllThumbnailFiles()
+		{
+			DirectoryInfo dirInfo = new DirectoryInfo(_appSettings.ThumbnailTempFolder);
+			return dirInfo.EnumerateFiles($"*.jpg", SearchOption.TopDirectoryOnly)
+				.AsParallel().ToArray();
+		}
+	}
+}
