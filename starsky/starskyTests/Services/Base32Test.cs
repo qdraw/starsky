cf5baebcf9c2c78b0be423d4768fe4d8e560833d
Dion <dionvanvelde@gmail.com>
2018-12-14 14:20:40 +0100

    move file

---
+++ starsky/starskyTests/Services/Base32Test.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using starsky.Attributes;
+using starsky.Services;
+
+namespace starskytests.Services
+{
+	[TestClass]
+	public class Base32Test
+	{
+		[TestMethod]
+		[ExcludeFromCoverage]
+		public void Base32EncodeDecodeTest()
+		{
+			var encodeBytes = Base32.Decode("VWQII6FKPI26LLWB3TRDQJU3CM");
+			var encodeString = Base32.Encode(encodeBytes);
+			Assert.AreEqual("VWQII6FKPI26LLWB3TRDQJU3CM",encodeString);
+			
+			// Emthy result
+			encodeString = Base32.Encode(new byte[0]);
+			Assert.AreEqual(string.Empty,encodeString);
+			
+			Base32.Encode(encodeBytes,true);
+		}
+	
+	}
+}
