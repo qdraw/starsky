bc9fce51fa20eeadd3787841cd1af9a8a6f4dc38
Dion <dionvanvelde@gmail.com>
2018-12-19 17:24:06 +0100

    add example symlinks

---
+++ starsky/tools/symlink.sh
@@ -0,0 +1,12 @@
+
+chmod +x starsky
+chmod +x starskygeocli
+chmod +x starskysynccli
+chmod +x starskyimportercli
+chmod +x starskywebhtmlcli
+
+ln -s /data/git/starsky/starsky/osx.10.12-x64/starskygeocli /opt/bin/starskygeocli
+ln -s /data/git/starsky/starsky/osx.10.12-x64/starsky /opt/bin/starsky
+ln -s /data/git/starsky/starsky/osx.10.12-x64/starskysynccli /opt/bin/starskysynccli
+ln -s /data/git/starsky/starsky/osx.10.12-x64/starskyimportercli /opt/bin/starskyimportercli
+ln -s /data/git/starsky/starsky/osx.10.12-x64/starskywebhtmlcli /opt/bin/starskywebhtmlcli
