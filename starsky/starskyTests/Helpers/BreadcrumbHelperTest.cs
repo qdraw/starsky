52573b68e4277c3c88dc4f1474a1867645149802
Dion <dionvanvelde@gmail.com>
2019-01-16 10:03:37 +0100

    merge class

---
+++ starsky/starskyTests/Helpers/BreadcrumbHelperTest.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using starsky.Attributes;
+using starsky.Services;
+
+namespace starskytests.Helpers
+{
+	[TestClass]
+	public class BreadcrumbHelperTest
+	{
+		[TestMethod]
+		[ExcludeFromCoverage]
+		public void BreadcrumbSlashMethodTest()
+		{
+			var breadcrumbExample = Breadcrumbs.BreadcrumbHelper("/");
+
+			var breadcrumblist = new List<string> { "/" };
+			CollectionAssert.AreEqual(breadcrumbExample, breadcrumblist);
+		}
+
+		[ExcludeFromCoverage]
+		[TestMethod]
+		public void BreadcrumbFileNameMethodTest()
+		{
+			var breadcrumbExample = Breadcrumbs.BreadcrumbHelper("/2018/2.jpg");
+			var breadcrumblist = new List<string> { "/", "/2018" };
+			CollectionAssert.AreEqual(breadcrumbExample, breadcrumblist);
+		}
+
+		[ExcludeFromCoverage]
+		[TestMethod]
+		public void BreadcrumbNullTest()
+		{
+			var breadcrumbExample = Breadcrumbs.BreadcrumbHelper(null);
+			var breadcrumblist = new List<string>();
+			CollectionAssert.AreEqual(breadcrumbExample, breadcrumblist);
+		}
+
+		[TestMethod]
+		public void BreadcrumbHelperTest_WithoutStartSlash()
+		{
+			var breadcrumb = Breadcrumbs.BreadcrumbHelper("test");
+			Assert.AreEqual("/", breadcrumb.FirstOrDefault());
+		}
+
+		[TestMethod]
+		public void BreadcrumbHelperTest_ExampleBreadcrumb()
+		{
+			var breadcrumb = Breadcrumbs.BreadcrumbHelper("/test/file.jpg");
+			Assert.AreEqual("/", breadcrumb.FirstOrDefault());
+			Assert.AreEqual("/test", breadcrumb.LastOrDefault());
+		}
+	}
+}
