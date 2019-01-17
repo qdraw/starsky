2d8c759105cf9f6524f48006ad1d6e225df1051a
Dion <dionvanvelde@gmail.com>
2019-01-17 12:04:06 +0100

    add test

---
+++ starsky/starskyTests/Models/AppSettingsPublishProfilesTest.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using starsky.Models;
+
+namespace starskytests.Models
+{
+	[TestClass]
+	public class AppSettingsPublishProfilesTest
+	{
+		[TestMethod]
+		public void AppSettingsPublishProfilesTest_OverlayMaxWidthTestLessThan100()
+		{
+			var model = new AppSettingsPublishProfiles();
+			model.OverlayMaxWidth = 1;
+			Assert.AreEqual(100, model.OverlayMaxWidth);
+		}
+
+		[TestMethod]
+		public void AppSettingsPublishProfilesTest_OverlayMaxWidthTestMoreThan100()
+		{
+			var model = new AppSettingsPublishProfiles();
+			model.OverlayMaxWidth = 101;
+			Assert.AreEqual(101, model.OverlayMaxWidth);
+		}
+
+		[TestMethod]
+		public void AppSettingsPublishProfilesTest_FolderNull()
+		{
+			var model = new AppSettingsPublishProfiles();
+			model.Folder = null;
+
+			var getFolder = model.Folder;
+
+			Assert.AreEqual(string.Empty, getFolder);
+		}
+
+		[TestMethod]
+		public void AppSettingsPublishProfilesTest_AddSlash()
+		{
+			var model = new AppSettingsPublishProfiles();
+			model.Folder = "/test";
+			var getFolder = model.Folder;
+
+			Assert.AreEqual("/test/", getFolder);
+
+		}
+	}
+}
