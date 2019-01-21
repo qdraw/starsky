e52196a9fb22647f4046194d0d4955791c41aa5e
Dion <dionvanvelde@gmail.com>
2019-01-21 11:18:30 +0100

    update build script ++ update gitingore ++ add import project

--- starsky.netframework/release-msbuild.sh
+++ starsky.netframework/release-msbuild.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+
 nuget restore
+
+mkdir msbuild.netframework
 
 pushd starskySyncNetFramework
 MSBuild "starskySyncNetFramework.csproj" /P:Configuration=release
+cp -a bin/Release/. ../msbuild.netframework
 popd
 
 
-pushd starskyimportercliFramework
-MSBuild "starskyimportercliFramework.csproj" /P:Configuration=release
+pushd starskyimportercliNetFramework
+MSBuild "starskyimportercliNetFramework.csproj" /P:Configuration=release
+cp -a bin/Release/. ../msbuild.netframework
 popd
