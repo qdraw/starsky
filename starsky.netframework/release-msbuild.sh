d4181d90cbdf4da04dabbe575895bf2b28eb4911
Dion <dionvanvelde@gmail.com>
2019-01-23 20:29:56 +0100

    update release script

--- starsky.netframework/release-msbuild.sh
+++ starsky.netframework/release-msbuild.sh
@@ -3,15 +3,15 @@ cd "$(dirname "$0")"
 
 nuget restore
 
-mkdir msbuild.netframework
+mkdir starsky.netframework-msbuild
 
 pushd starskySyncNetFramework
 MSBuild "starskySyncNetFramework.csproj" /P:Configuration=release
-cp -a bin/Release/. ../msbuild.netframework
+cp -a bin/Release/. ../starsky.netframework-msbuild
 popd
 
 
 pushd starskyimportercliNetFramework
 MSBuild "starskyimportercliNetFramework.csproj" /P:Configuration=release
-cp -a bin/Release/. ../msbuild.netframework
+cp -a bin/Release/. ../starsky.netframework-msbuild
 popd
