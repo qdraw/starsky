fe171f50901f46f06af6e4b15bd6d0e819221761
Dion <dionvanvelde@gmail.com>
2019-01-25 13:50:50 +0100

    to 2.1.7 ++ fix bash scripts

--- starsky/publish-mac.sh
+++ starsky/publish-mac.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 mkdir osx.10.12-x64
-pushd starsky
-dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --output ../osx.10.12-x64
-popd
-pushd starskysynccli
-dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --no-dependencies --output ../osx.10.12-x64
-popd
+
+# First CLI, then MVC, to avoid this error:
+# Unhandled Exception: System.IO.FileLoadException: Could not load file or assembly 'Microsoft.AspNetCore.Authentication, Version=2.1.2.0, Culture=neutral, PublicKeyToken=adb9793829ddae60'.
+# The located assembly's manifest definition does not match the assembly reference. (Exception from HRESULT: 0x80131040)
+
 pushd starskyimportercli
-dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --no-dependencies --output ../osx.10.12-x64
+	dotnet publish --no-dependencies -c release -r osx.10.12-x64 --output ../osx.10.12-x64
 popd
+
 pushd starskywebhtmlcli
-dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --output ../osx.10.12-x64
+	dotnet publish --no-dependencies -c release -r osx.10.12-x64 --output ../osx.10.12-x64
 popd
+
 pushd starskygeocli
-dotnet publish -c release -r osx.10.12-x64 --framework netcoreapp2.1 --output ../osx.10.12-x64
+	dotnet publish -c release -r osx.10.12-x64 --output ../osx.10.12-x64
+popd
+
+pushd starskysynccli
+	dotnet publish --no-dependencies -c release -r osx.10.12-x64 --output ../osx.10.12-x64
+popd
+
+pushd starsky
+	dotnet publish -c release -r osx.10.12-x64 --self-contained --output ../osx.10.12-x64
 popd
