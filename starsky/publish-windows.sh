76e6a4e2ecddad33e0ee33f39c7723e7754f02db
Dion <dionvanvelde@gmail.com>
2019-02-04 20:38:07 +0100

    add to build

--- starsky/publish-windows.sh
+++ starsky/publish-windows.sh
@@ -16,6 +16,10 @@ pushd starskywebhtmlcli
 	dotnet publish -c release -r win7-x86 --framework netcoreapp2.1 --output ../win7-x86
 popd
 
+pushd starskywebftpcli
+	dotnet publish --no-dependencies -c release -r win7-x86 --output ../win7-x86
+popd
+
 pushd starskygeocli
 	dotnet publish -c release -r win7-x86 --framework netcoreapp2.1 --output ../win7-x86
 popd
