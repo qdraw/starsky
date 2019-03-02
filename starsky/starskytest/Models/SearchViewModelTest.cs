7cbcda88015f8cce946be40f2b9963107b6e2946
Dion <dionvanvelde@gmail.com>
2019-03-02 21:22:44 +0100

    add offset test

--- starsky/starskytest/Models/SearchViewModelTest.cs
+++ starsky/starskytest/Models/SearchViewModelTest.cs
@@ -9,8 +9,15 @@ namespace starskytest.Models
         [TestMethod]
         public void SearchViewModel_ElapsedSeconds_Test()
         {
-            var q = new SearchViewModel{ElapsedSeconds = 0.0006};
-            Assert.AreEqual(true, q.ElapsedSeconds <= 0.001);
+            var searchViewModel = new SearchViewModel{ElapsedSeconds = 0.0006};
+            Assert.AreEqual(true, searchViewModel.ElapsedSeconds <= 0.001);
         }
+
+	    [TestMethod]
+	    public void SearchViewModel_Offset_Test()
+	    {
+		    var searchViewModel = new SearchViewModel();
+		    Assert.AreEqual(0,searchViewModel.Offset );
+	    }
     }
 }
