8936d4a1017e2ef795136a3798b85085b80227b7
Dion <dionvanvelde@gmail.com>
2019-03-21 19:48:46 +0100

    file hash - not loading it from disk to make it faster

--- starsky/starsky/Controllers/ApiController.cs
+++ starsky/starsky/Controllers/ApiController.cs
@@ -566,7 +566,8 @@ namespace starsky.Controllers
                         .Split("/").LastOrDefault(),
                     ParentDirectory = Breadcrumbs.BreadcrumbHelper(_appSettings.
                         FullPathToDatabaseStyle(sourceFullPath)).LastOrDefault(),
-                    FileHash = new FileHash(_iStorage).GetHashCode(_appSettings.FullPathToDatabaseStyle(sourceFullPath))
+	                FileHash = singleItem.FileIndexItem.FileHash // not loading it from disk to make it faster
+//                    FileHash = new FileHash(_iStorage).GetHashCode(_appSettings.FullPathToDatabaseStyle(sourceFullPath))
                 };
                 
                 // When you have a different tag in the database than on disk
