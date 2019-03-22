c4a9d5f9f39011b4b0b261d75c85a2e3dad4c914
Dion <dionvanvelde@gmail.com>
2019-03-22 11:06:48 +0100

    rm unused code

--- starsky/starsky/Controllers/ExportController.cs
+++ starsky/starsky/Controllers/ExportController.cs
@@ -149,13 +149,6 @@ namespace starsky.Controllers
 						);
 				}
 				
-//				// when there is .xmp sidecar file
-//				if ( !thumbnail && ExtensionRolesHelper.IsExtensionForceXmp(sourceFile) 
-//				                && FilesHelper.ExistFile(ExtensionRolesHelper.GetXmpSidecarFile(sourceFile)))
-//				{
-////					filePaths.Add(ExtensionRolesHelper.GetXmpSidecarFile(sourceFile));
-//				}
-
 			}
 
 			return filePaths;
