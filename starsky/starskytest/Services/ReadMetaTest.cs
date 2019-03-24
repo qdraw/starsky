e60c5e4c9e2e4ad8727bc098e55383605d7f8783
Dion <dionvanvelde@gmail.com>
2019-03-24 13:37:48 +0100

    spacing

--- starsky/starskytest/Services/ReadMetaTest.cs
+++ starsky/starskytest/Services/ReadMetaTest.cs
@@ -16,22 +16,22 @@ namespace starskytest.Services
 		[TestMethod]
 	    public void ReadMetaTest_HalfCompleteFile()
 	    {
-		    var xmpString = "<x:xmpmeta xmlns:x=\'adobe:ns:meta/\' x:xmptk=\'Image::ExifTool 11.11\'>" +
-		                  "<rdf:RDF xmlns:rdf=\'http://www.w3.org/1999/02/22-rdf-syntax-ns#\'>" +
-		                  " <rdf:Description rdf:about=\'\'  xmlns:dc=\'http://purl.org/dc/elements/1.1/\'> " +
-		                  " <dc:subject>   <rdf:Bag>    <rdf:li>example</rdf:li>    <rdf:li>keyword</rdf:li>  " +
-		                  "  <rdf:li>test</rdf:li>   </rdf:Bag>  </dc:subject> </rdf:Description> " +
-		                  "<rdf:Description rdf:about=\'\'  xmlns:pdf=\'http://ns.adobe.com/pdf/1.3/\'>  " +
-		                  "<pdf:Keywords>example, test</pdf:Keywords> </rdf:Description> <rdf:Description rdf:about=\'\' " +
-		                  " xmlns:photomechanic=\'http://ns.camerabits.com/photomechanic/1.0/\'>  " +
-		                  "<photomechanic:ColorClass>8</photomechanic:ColorClass>  <photomechanic:PMVersion>PM5</photomechanic:PMVersion> " +
-		                  " <photomechanic:Prefs>0:8:0:0</photomechanic:Prefs>  <photomechanic:Tagged>False</photomechanic:Tagged>" +
-		                  " </rdf:Description> <rdf:Description rdf:about=\'\'  xmlns:photoshop=\'http://ns.adobe.com/photoshop/1.0/\'> " +
-		                  " <photoshop:DateCreated>2019-03-02T11:29:18+01:00</photoshop:DateCreated> " +
-		                  "</rdf:Description> <rdf:Description rdf:about=\'\'  xmlns:xmp=\'http://ns.adobe.com/xap/1.0/\'> " +
-		                  " <xmp:CreateDate>2019-03-02T11:29:18+01:00</xmp:CreateDate>  <xmp:Rating>0</xmp:Rating> " +
-		                  "</rdf:Description></rdf:RDF></x:xmpmeta>";
-		    
+		    const string xmpString = 	"<x:xmpmeta xmlns:x=\'adobe:ns:meta/\' x:xmptk=\'Image::ExifTool 11.11\'>" +
+										"<rdf:RDF xmlns:rdf=\'http://www.w3.org/1999/02/22-rdf-syntax-ns#\'>" +
+										" <rdf:Description rdf:about=\'\'  xmlns:dc=\'http://purl.org/dc/elements/1.1/\'> " +
+										" <dc:subject>   <rdf:Bag>    <rdf:li>example</rdf:li>    <rdf:li>keyword</rdf:li>  " +
+										"  <rdf:li>test</rdf:li>   </rdf:Bag>  </dc:subject> </rdf:Description> " +
+										"<rdf:Description rdf:about=\'\'  xmlns:pdf=\'http://ns.adobe.com/pdf/1.3/\'>  " +
+										"<pdf:Keywords>example, test</pdf:Keywords> </rdf:Description> <rdf:Description rdf:about=\'\' " +
+										" xmlns:photomechanic=\'http://ns.camerabits.com/photomechanic/1.0/\'>  " +
+										"<photomechanic:ColorClass>8</photomechanic:ColorClass>  <photomechanic:PMVersion>PM5</photomechanic:PMVersion> " +
+										" <photomechanic:Prefs>0:8:0:0</photomechanic:Prefs>  <photomechanic:Tagged>False</photomechanic:Tagged>" +
+										" </rdf:Description> <rdf:Description rdf:about=\'\'  xmlns:photoshop=\'http://ns.adobe.com/photoshop/1.0/\'> " +
+										" <photoshop:DateCreated>2019-03-02T11:29:18+01:00</photoshop:DateCreated> " +
+										"</rdf:Description> <rdf:Description rdf:about=\'\'  xmlns:xmp=\'http://ns.adobe.com/xap/1.0/\'> " +
+										" <xmp:CreateDate>2019-03-02T11:29:18+01:00</xmp:CreateDate>  <xmp:Rating>0</xmp:Rating> " +
+										"</rdf:Description></rdf:RDF></x:xmpmeta>";
+
 		    byte[] xmpByteArray = Encoding.UTF8.GetBytes(xmpString);
 
 		    
