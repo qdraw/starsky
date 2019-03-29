71d4b5729807d48ea4bea2b185a3bbb2c0852595
Dion <dionvanvelde@gmail.com>
2019-03-29 19:19:27 +0100

    disable due not exist

--- starsky/starskytest/Services/ExifToolTest.cs
+++ starsky/starskytest/Services/ExifToolTest.cs
@@ -9,19 +9,19 @@ namespace starskytest.Services
 	public class ExifToolTest
 	{
 
-		[TestMethod]
-		public async Task ExifToolTest_()
-		{
-			var appSettings = new AppSettings
-			{
-				ExifToolPath = "/usr/local/bin/exiftool",
-				StorageFolder = "/data/isight/__starsky/01-dif"
-			};
-			var subPathStorage = new StorageSubPathFilesystem(appSettings);
-
-			var stream = await new ExifTool(subPathStorage, appSettings).WriteTagsAsync("/2018.01.01.17.00.01 kopie.jpg","-Software=\"Qdraw 2.0\"");
-			
-			
-		}
+//		[TestMethod]
+//		public async Task ExifToolTest_()
+//		{
+//			var appSettings = new AppSettings
+//			{
+//				ExifToolPath = "/usr/local/bin/exiftool",
+//				StorageFolder = "/data/isight/__starsky/01-dif"
+//			};
+//			var subPathStorage = new StorageSubPathFilesystem(appSettings);
+//
+//			var stream = await new ExifTool(subPathStorage, appSettings).WriteTagsAsync("/2018.01.01.17.00.01 kopie.jpg","-Software=\"Qdraw 2.0\"");
+//			
+//			
+//		}
 	}
 }
