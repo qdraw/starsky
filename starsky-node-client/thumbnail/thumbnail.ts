98d5d38629bb1e7a635677cad1f971f7d79accea
Dion <dionvanvelde@gmail.com>
2019-04-26 18:55:49 +0200

    work on client

--- starsky-node-client/thumbnail/thumbnail.ts
+++ starsky-node-client/thumbnail/thumbnail.ts
@@ -5,8 +5,9 @@ var path = require('path');
 import { Query } from './thumbnail-core';
 import { IResults } from './IResults';
 require('dotenv').config({path:path.join(__dirname,"../", ".env")});
+import { TaskQueue } from 'cwait';
 
-
+import {map as pMap } from 'p-iteration';
 
 function ShowHelpDialog() {
 	console.log("Starksy Remote Thumbnail Helper")
@@ -47,32 +48,104 @@ var access_token = process.env.STARKSYACCESSTOKEN;
 
 var query = new Query(base_url,access_token);
 
-query.isImportIndex(searchQuery).then((searchQueryResult : string) => {
-	var indexRequestOptions = query.indexRequestOptions(searchQueryResult);
-	return query.searchIndex(indexRequestOptions);
-}).catch( err => {
-	console.log('err- deleteFileChain', err);
-}).then((result : IResults) => {
-	// console.log('searchIndex => ', result);
-	return query.checkIfSingleFileNeedsToBeDownloadedApiChain(result.fileHashList);
-}).catch( err => {
-	console.log('err- checkIfSingleFileNeedsToBeDownloadedApiChain', err);
-}).then((result : Array<string>) => {
-	return query.downloadBinaryApiChain(result);
-}).catch( err => {
-	console.log('err- downloadBinaryApiChain', err);
-}).then((result : Array<string>) => {
-	// console.log(result.length)
-	return query.resizeChain(result);
-}).catch( err => {
-	console.log('err- resizeChain', err);
-}).then((result : Array<string>) => {
-	// console.log(result.length)
-	return query.uploadTempFileChain(result);
-}).catch( err => {
-	console.log('err- uploadTempFileChain', err);
-}).then((result : Array<string>) => {
-	return query.deleteFileChain(result);
-}).catch( err => {
-	console.log('err- deleteFileChain', err);
-});
+
+var skip = function() {
+	console.log("skip");
+};
+
+if(searchQuery === "IMPORT") {
+
+	query.isImportIndex().then(async (filePathList : Array<string>) => {
+
+
+		await query.searchIndexList(filePathList);
+
+		// var requestQueue = filePathList.map(file => query.searchIndex(query.indexRequestOptions("-filePath:" + file)));
+		// console.log(requestQueue);
+
+		// requestQueue.reduce((curr, next) => {
+		// 	console.log(curr)
+		// 	return curr.then(() => next); // <- here
+		//   }, Promise.resolve())
+		// 	  .then((res) => console.log(res));
+
+
+
+		// console.log(filePathList.length);
+		// const finalResult = await query.searchIndex(indexRequestOptions);
+
+		// filePathList.forEach(async filePath => {
+		// 	var indexRequestOptions = query.indexRequestOptions("-filePath:" + filePath);
+		// 	console.log(finalResult);
+		// });
+
+
+
+
+		// var combineFileHashGetPromises = Array<Promise<IResults>>();
+		// filePathList.forEach(async filePath => {
+		// 	var indexRequestOptions = query.indexRequestOptions("-filePath:" + filePath);
+
+		// 	combineFileHashGetPromises.push(query.searchIndex);
+		// });
+		
+
+		// var searchQueryWithFilePathList = Array<string>();
+		// filePathList.forEach(element => {
+		// 	searchQueryWithFilePathList.push(element);
+		// });
+
+		// pMap(filePathList, async filePath => {
+		// 	var indexRequestOptions = query.indexRequestOptions("-filePath:" + filePath);
+		// 	await query.searchIndex(indexRequestOptions);
+		// });
+
+		// combineFileHashGetPromises.reduce(function(cur, next){
+		// 	return cur('').then(skip, next);
+		// }, Promise.reject()).then(function(){
+		// 	console.log("done");
+		// }, function() {
+		// 	console.log("failed");
+		// });
+	
+	}).catch( err => {
+		console.log('err- deleteFileChain', err);
+	})
+
+}
+
+
+
+
+
+// query.isImportIndex().then((searchQueryResult : string) => {
+// 	var indexRequestOptions = query.indexRequestOptions(searchQueryResult);
+// 	return query.searchIndex(indexRequestOptions);
+// }).catch( err => {
+// 	console.log('err- deleteFileChain', err);
+// }).then((result : IResults) => {
+// 	// console.log('searchIndex => ', result);
+// 	return query.checkIfSingleFileNeedsToBeDownloadedApiChain(result.fileHashList);
+// }).catch( err => {
+// 	console.log('err- checkIfSingleFileNeedsToBeDownloadedApiChain', err);
+// })
+
+// .then((result : Array<string>) => {
+// 	return query.downloadBinaryApiChain(result);
+// }).catch( err => {
+// 	console.log('err- downloadBinaryApiChain', err);
+// }).then((result : Array<string>) => {
+// 	// console.log(result.length)
+// 	return query.resizeChain(result);
+// }).catch( err => {
+// 	console.log('err- resizeChain', err);
+// }).then((result : Array<string>) => {
+// 	// console.log(result.length)
+// 	return query.uploadTempFileChain(result);
+// }).catch( err => {
+// 	console.log('err- uploadTempFileChain', err);
+// }).then((result : Array<string>) => {
+// 	return query.deleteFileChain(result);
+// }).catch( err => {
+// 	console.log('err- deleteFileChain', err);
+// });
