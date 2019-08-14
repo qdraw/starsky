7417d756d29aa4f346076e0117872c3e062c7ced
Dion <dionvanvelde@gmail.com>
2019-08-14 12:43:42 +0200

    clean up

--- starsky/starsky/Controllers/SearchSuggestController.cs
+++ starsky/starsky/Controllers/SearchSuggestController.cs
@@ -15,11 +15,9 @@ namespace starsky.Controllers
 		}
 
 		/// <summary>
-		/// Gets the list of search results (cached) -- WORK IN PROGRESS
+		/// Gets the list of search results (cached)
 		/// </summary>
 		/// <param name="t">search query</param>
-		/// <param name="p">page number</param>
-		/// <param name="json">enable json response</param>
 		/// <returns>the search results</returns>
 		/// <response code="200">the search results (enable json to get json results)</response>
 		[HttpGet("/suggest")]
