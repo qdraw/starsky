846a4f07e268ce3b0a629aec845e8f20575d2846
Dion <dionvanvelde@gmail.com>
2019-08-15 20:01:58 +0200

    keep dots

--- starsky-node-client/mail/mail.js
+++ starsky-node-client/mail/mail.js
@@ -24,7 +24,7 @@ var config = {
 function slugify(text) {
   return text.toString().toLowerCase()
     .replace(/\s+/g, '-')           // Replace spaces with -
-    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/[^\w\-\.]+/g, '')       // Remove all non-word chars (keep dots)
     .replace(/\-\-+/g, '-')         // Replace multiple - with single -
     .replace(/^-+/, '')             // Trim - from start of text
     .replace(/-+$/, '');            // Trim - from end of text
