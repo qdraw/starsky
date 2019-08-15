a6fc2332c4a64555695710301b96e8e13529b2de
Dion <dionvanvelde@gmail.com>
2019-08-15 09:53:54 +0200

    clean up old scripts

--- starsky/starskyTest.bat
+++
@@ -1,4 +0,0 @@
-REM Windows batch file
-PUSHD starskytest
-dotnet test /p:hideMigrations=\"true\" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
-POPD
--- starsky/starskyTest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-pushd starskytest
-dotnet test /p:hideMigrations=\"true\" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
-popd
