f88ec408980d6959453c3d94fc387eda45707663
Dion <dionvanvelde@gmail.com>
2019-09-01 18:41:32 +0200

    usused code

--- starsky/starsky/clientapp/src/pages/search-page.tsx
+++ starsky/starsky/clientapp/src/pages/search-page.tsx
@@ -5,7 +5,6 @@ import MenuSearch from '../components/menu-search';
 import ArchiveContextWrapper from '../containers/archive-wrapper';
 import useLocation from '../hooks/use-location';
 import useSearchList from '../hooks/use-searchlist';
-import { IArchiveProps } from '../interfaces/IArchiveProps';
 import { URLPath } from '../shared/url-path';
 
 
@@ -14,7 +13,6 @@ interface ISearchPageProps {
 }
 
 const SearchPage: FunctionComponent<RouteComponentProps<ISearchPageProps>> = (props) => {
-  var sidebar = false;
   var history = useLocation();
 
   var urlObject = new URLPath().StringToIUrl(history.location.search);
@@ -23,23 +21,9 @@ const SearchPage: FunctionComponent<RouteComponentProps<ISearchPageProps>> = (pr
   if (!archive) return (<>Something went wrong</>)
   if (!archive.archive) return (<>Something went wrong</>)
 
-  var archiveProps = {} as IArchiveProps;
   return (<div>
     <MenuSearch detailView={undefined} parent={"parent"} isDetailMenu={false}></MenuSearch>
     <ArchiveContextWrapper {...archive.archive} />
-
-    {/* <div className={!sidebar ? "archive" : "archive collapsed"}>
-      {sidebar ? <ArchiveSidebar {...archive}></ArchiveSidebar> : ""}
-
-      <div className="content">
-        <Breadcrumb breadcrumb={archive.breadcrumb} subPath={archive.subPath}></Breadcrumb>
-        <RelativeLink relativeObjects={archive.relativeObjects}></RelativeLink>
-
-        <ColorClassFilter itemsCount={archive.collectionsCount} subPath={archive.subPath}
-          colorClassFilterList={archive.colorClassFilterList} colorClassUsage={archive.colorClassUsage}></ColorClassFilter>
-        <ItemListView {...archive} colorClassUsage={archive.colorClassUsage}> </ItemListView>
-      </div>
-    </div> */}
   </div>)
 }
 
