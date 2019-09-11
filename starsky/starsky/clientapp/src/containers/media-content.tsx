65e3361618aaa03ca39b00fcaae341185e007ee7
Dion <dionvanvelde@gmail.com>
2019-09-11 11:19:02 +0200

    add nice warning for not found

--- starsky/starsky/clientapp/src/containers/media-content.tsx
+++ starsky/starsky/clientapp/src/containers/media-content.tsx
@@ -7,6 +7,7 @@ import useFileList from '../hooks/use-filelist';
 import useLocation from '../hooks/use-location';
 import { IArchive } from '../interfaces/IArchive';
 import { IDetailView, PageType } from '../interfaces/IDetailView';
+import NotFoundPage from '../pages/not-found-page';
 import Login from './login';
 
 
@@ -26,9 +27,9 @@ const MediaContent: React.FC = () => {
   return (
     <div>
       {pageType === PageType.Loading ? <Preloader isOverlay={true} isDetailMenu={false}></Preloader> : null}
-      {pageType === PageType.NotFound ? <>not found</> : null}
+      {pageType === PageType.NotFound ? <NotFoundPage>not found</NotFoundPage> : null}
       {pageType === PageType.Unauthorized ? <Login></Login> : null}
-      {pageType === PageType.ApplicationException ? <><MenuDefault isEnabled={false}></MenuDefault><div className="content--header">We hebben op dit moment een verstoring op de applicatie</div><div className="content--text">Probeer de pagina te herladen</div></> : null}
+      {pageType === PageType.ApplicationException ? <><MenuDefault isEnabled={false}></MenuDefault><div className="content--header">We hebben op dit moment een verstoring op de applicatie</div><div className="content--subheader">Probeer de pagina te herladen</div></> : null}
       {pageType === PageType.Archive && archive ? <ArchiveContextWrapper {...archive} /> : null}
       {pageType === PageType.DetailView && detailView ? <DetailViewContextWrapper {...detailView} /> : null}
     </div>
