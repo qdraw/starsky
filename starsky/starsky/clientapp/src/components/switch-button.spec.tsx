26512668a87ad4e8bb5a620be2870717346c42d0
Dion <dionvanvelde@gmail.com>
2019-09-18 19:38:54 +0200

    add tests for switch button

---
+++ starsky/starsky/clientapp/src/components/switch-button.spec.tsx
@@ -0,0 +1,39 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import SwitchButton from './switch-button';
+
+describe("SwitchButton", () => {
+
+  it("renders", () => {
+    var toggle = jest.fn()
+    shallow(<SwitchButton onToggle={toggle} leftLabel={"on"} rightLabel={"off"}></SwitchButton>)
+  });
+
+  it("renders (disabled:state)", () => {
+    var toggle = jest.fn()
+    var wrapper = shallow(<SwitchButton isEnabled={false} onToggle={toggle} leftLabel={"on"} rightLabel={"off"}></SwitchButton>)
+    var name = wrapper.find('[name="switchToggle"]')
+    expect(name.last().props().disabled).toBeTruthy()
+  });
+
+  it("test if element triggers onToggle when changed (default)", () => {
+    var toggle = jest.fn()
+    var wrapper = shallow(<SwitchButton isOn={true} onToggle={toggle} leftLabel={"on"} rightLabel={"off"}></SwitchButton>)
+    var name = wrapper.find('[name="switchToggle"]')
+    name.last().simulate('change');
+    expect(toggle).toBeCalled()
+    expect(name.last().props().disabled).toBeFalsy()
+    expect(name.last().props().checked).toBeTruthy()
+  });
+
+  it("test if element triggers onToggle when changed (negative)", () => {
+    var toggle = jest.fn()
+    var wrapper = shallow(<SwitchButton isOn={false} onToggle={toggle} leftLabel={"on"} rightLabel={"off"}></SwitchButton>)
+    var name = wrapper.find('[name="switchToggle"]')
+    name.first().simulate('change');
+    expect(toggle).toBeCalled()
+    expect(name.last().props().disabled).toBeFalsy()
+    expect(name.first().props().checked).toBeTruthy()
+  });
+
+});
--- starsky/starsky/clientapp/src/components/switch-button.tsx
+++ starsky/starsky/clientapp/src/components/switch-button.tsx
@@ -8,38 +8,6 @@ interface ISwitchButtonProps {
   isOn?: boolean;
 }
 
-// function SwitchButton(props: ISwitchButtonProps) {
-
-//   const [random, setRandom] = React.useState(0);
-
-//   useEffect(() => {
-//     setRandom(Math.ceil(Math.random() * 100));
-//   }, []);
-
-//   const [checked, setChecked] = React.useState(props.isOn ? props.isOn : false);
-
-//   return (<div className={props.isEnabled !== false ? "switch" : "switch disabled"}>
-//     <input type="radio" checked={checked} disabled={props.isEnabled === false} className="switch-input"
-//       name="view" value="true" id={"on_" + random}
-//       onChange={() => {
-//         props.onToggle(false)
-//         console.log(random, checked);
-
-//         setChecked(false)
-//       }} />
-//     <label htmlFor={"on_" + random} className="switch-label switch-label-off">{props.on}</label>
-//     <input type="radio" checked={!checked} disabled={props.isEnabled === false} className="switch-input" name="view" value="true" id={"off_" + random} onChange={() => {
-//       props.onToggle(true)
-//       console.log(random, checked);
-
-//       setChecked(true)
-//     }} />
-//     <label htmlFor={"off_" + random} className="switch-label switch-label-on">{props.off}</label>
-//     <span className="switch-selection"></span>
-//   </div>);
-// }
-
-export default SwitchButton
 function SwitchButton(props: ISwitchButtonProps) {
 
   const [random, setRandom] = React.useState(0);
@@ -84,3 +52,4 @@ function SwitchButton(props: ISwitchButtonProps) {
   );
 }
 
+export default SwitchButton;
