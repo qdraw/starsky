f74511f7caf0eb41ef9f838c27f0e2280d206d7f
Dion <dionvanvelde@gmail.com>
2019-09-27 13:10:41 +0200

    (starsky-tools) allow change of backend

--- starsky-tools/localtunnel/localtunnel.js
+++ starsky-tools/localtunnel/localtunnel.js
@@ -5,9 +5,9 @@ var apiProxy = httpProxy.createProxyServer();
 var dotenv = require('dotenv');
 dotenv.config();
 
+// all urls to proxy
 app.all("/*", function (req, res, next) {
-
-  if (req.originalUrl.startsWith("/api") || req.originalUrl.startsWith("/account") ) {
+  if (req.originalUrl.startsWith("/api") || req.originalUrl.startsWith("/account")) {
     NetCoreAppRouteRoute(req, res, next);
   }
   else {
@@ -15,40 +15,47 @@ app.all("/*", function (req, res, next) {
   }
 });
 
-
 // To proxy the dev server
 var createReactAppRouteUrl = 'http://localhost:3000/';
 function CreateReactAppRoute(req, res, next) {
-    apiProxy.web(req, res,
-        {
-            target: createReactAppRouteUrl,
-            changeOrigin: true,
-            secure: false,
-            autoRewrite: true,
-            xfwd: true
-        }
-    );
+  apiProxy.web(req, res,
+    {
+      target: createReactAppRouteUrl,
+      changeOrigin: true,
+      secure: false,
+      autoRewrite: true,
+      xfwd: true
+    }
+  );
 }
 
-
+// To proxy the backend server
 var netCoreAppRouteUrl = 'http://localhost:5000/';
-function NetCoreAppRouteRoute(req, res, next) {
-    apiProxy.web(req, res,
-        {
-            target: netCoreAppRouteUrl,
-            changeOrigin: true,
-            secure: false,
-            autoRewrite: true,
-            xfwd: true
-        }
-    );
+if (!process.env.STARSKYURL) {
+  console.log('running on default: use STARSKYURL to customize ');
 }
 
+if (process.env.STARSKYURL) {
+  netCoreAppRouteUrl = process.env.STARSKYURL;
+  console.log('running on ' + netCoreAppRouteUrl);
+}
 
+function NetCoreAppRouteRoute(req, res, next) {
+  apiProxy.web(req, res,
+    {
+      target: netCoreAppRouteUrl,
+      changeOrigin: true,
+      secure: false,
+      autoRewrite: true,
+      xfwd: true
+    }
+  );
+}
+
+// setup express
 var port = process.env.PORT || process.env.port || 6501;
 app.listen(port);
 console.log("http://localhost:" + port);
-
 
 const localtunnel = require('localtunnel');
 
