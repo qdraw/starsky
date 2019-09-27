6d1e85b37712fc732822117403f47a3606b49558
Dion <dionvanvelde@gmail.com>
2019-09-27 17:04:10 +0200

    add feature to stop loading images after a url change

--- starsky/starsky/clientapp/src/components/list-image.tsx
+++ starsky/starsky/clientapp/src/components/list-image.tsx
@@ -1,5 +1,6 @@
 import React, { memo, useEffect, useRef, useState } from 'react';
 import useIntersection from '../hooks/use-intersection-observer';
+import useLocation from '../hooks/use-location';
 
 interface IListImageProps {
   src: string;
@@ -11,6 +12,8 @@ const ListImage: React.FunctionComponent<IListImageProps> = memo((props) => {
   const target = useRef<HTMLDivElement>(null);
   var alt = props.alt ? props.alt : 'afbeelding';
 
+  const [src, setSrc] = useState(props.src);
+
   // Reset Loading after changing page
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
@@ -21,10 +24,19 @@ const ListImage: React.FunctionComponent<IListImageProps> = memo((props) => {
     setIsLoading(true);
   }, [props.src]);
 
-  const intersected = useIntersection(target, {
+  var intersected = useIntersection(target, {
     rootMargin: '250px',
     once: true
-  });
+  })
+
+  // to stop loading images after a url change
+  var history = useLocation();
+  const historyLocation = history.location.search
+  useEffect(() => {
+    if (historyLocation !== history.location.search && isLoading) {
+      setSrc('data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEAAAAALAAAAAABAAEAAAI=;')
+    }
+  }, [history.location.search]);
 
   if (!props.src || props.src.toLowerCase().endsWith('null.jpg?issingleitem=true')) {
     return (<div ref={target} className="img-box--error"></div>);
@@ -32,7 +44,7 @@ const ListImage: React.FunctionComponent<IListImageProps> = memo((props) => {
 
   return (
     <div ref={target} className={error ? "img-box--error" : isLoading ? "img-box img-box--loading" : "img-box"}>
-      {intersected ? <img {...props} alt={alt}
+      {intersected ? <img src={src} alt={alt}
         onLoad={() => {
           setError(false)
           setIsLoading(false)
