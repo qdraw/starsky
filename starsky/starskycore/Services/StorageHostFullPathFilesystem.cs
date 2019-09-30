96d79e4fc1e8f1ea892ed23d6fda1e77a4d45fb4
Dion <dionvanvelde@gmail.com>
2019-09-30 20:18:24 +0200

    not working

--- starsky/starskycore/Services/StorageHostFullPathFilesystem.cs
+++ starsky/starskycore/Services/StorageHostFullPathFilesystem.cs
@@ -41,35 +41,7 @@ namespace starskycore.Services
 
 		public IEnumerable<string> GetDirectoryRecursive(string fullFilePath)
 		{
-//			return Directory.GetDirectories(fullFilePath, "*", SearchOption.AllDirectories);
-
-			var pending = new Stack<string>();
-			pending.Push(fullFilePath);
-			while (pending.Count != 0)
-			{
-				var path = pending.Pop();
-				string[] next = null;
-				try
-				{
-					next = Directory.GetFiles(path, "*");                    
-				}
-				catch
-				{
-					// ignored
-				}
-
-				if(next != null && next.Length != 0)
-					foreach (var file in next) yield return file;
-				try
-				{
-					next = Directory.GetDirectories(path);
-					foreach (var subdir in next) pending.Push(subdir);
-				}
-				catch
-				{
-					// ignored
-				}
-			}
+			return Directory.GetDirectories(fullFilePath, "*", SearchOption.AllDirectories);
 		}
 
 		public Stream ReadStream(string path, int maxRead = -1)
