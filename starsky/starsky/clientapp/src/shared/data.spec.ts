3f45e9e7b61f3741428f541794352e8408fee07c
Dion <dionvanvelde@gmail.com>
2019-11-13 19:57:43 +0100

    add unit tests

--- starsky/starsky/clientapp/src/shared/data.spec.ts
+++ starsky/starsky/clientapp/src/shared/data.spec.ts
@@ -19,6 +19,20 @@ describe("date", () => {
   });
 
   describe("parseRelativeDate", () => {
+    it("undefined", () => {
+      var result = parseRelativeDate(undefined);
+      expect(result).toBe("");
+    });
+
+    it("random", () => {
+      var result = parseRelativeDate("dd");
+      expect(result).toBe("");
+    });
+
+    it("non valid date", () => {
+      var result = parseRelativeDate("2019-02-40T01:00:00+00:00");
+      expect(result).toBe("");
+    });
 
     it("yesterday", () => {
       var yesterday = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate() - 1}T${leftPad(new Date().getHours())}:${leftPad(new Date().getMinutes())}:${leftPad(new Date().getSeconds())}+00:00`;
@@ -27,13 +41,17 @@ describe("date", () => {
     });
 
     it("less than a hour", () => {
-
       var tenMinutesStamp = new Date(new Date().getTime() - (36 * 60000));
       var tenMinutes = `${tenMinutesStamp.getFullYear()}-${tenMinutesStamp.getMonth() + 1}-${tenMinutesStamp.getDate()} ${leftPad(tenMinutesStamp.getHours())}:${leftPad(tenMinutesStamp.getMinutes())}:${leftPad(tenMinutesStamp.getSeconds())}`
 
       var result = parseRelativeDate(tenMinutes);
       expect(result).toBe("36 minuten");
     });
-  });
 
+    it("day before yesterday", () => {
+      var yesterday = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate() - 2}T${leftPad(new Date().getHours())}:${leftPad(new Date().getMinutes())}:${leftPad(new Date().getSeconds())}+00:00`;
+      var result = parseRelativeDate(yesterday);
+      expect(result).toBe(`${new Date().getDate() - 2}-${new Date().getMonth() + 1}-${new Date().getFullYear()}`);
+    });
+  });
 });
