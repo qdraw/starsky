14f79ea5ecdfe8c2ecb8f1ab36ed75d668712630
Dion <dionvanvelde@gmail.com>
2019-11-15 17:54:39 +0100

    should not be the same

--- starsky/starsky/clientapp/src/interfaces/ILogin.spec.ts
+++ starsky/starsky/clientapp/src/interfaces/ILogin.spec.ts
@@ -1,8 +1,8 @@
-import { newILogin } from './ILogin';
+import { ILogin, newILogin } from './ILogin';
 
 describe("ILogin", () => {
   it("newILogin", () => {
     var login = newILogin();
-    expect(login).toBe(login);
+    expect(login).toBe({} as ILogin);
   });
 });
