483dd6665a22474a27fb368f29359eac40c7daa5
Dion <dionvanvelde@gmail.com>
2019-11-15 17:55:00 +0100

    expect(login).toBe(login);

--- starsky/starsky/clientapp/src/interfaces/ILogin.spec.ts
+++ starsky/starsky/clientapp/src/interfaces/ILogin.spec.ts
@@ -3,6 +3,6 @@ import { ILogin, newILogin } from './ILogin';
 describe("ILogin", () => {
   it("newILogin", () => {
     var login = newILogin();
-    expect(login).toBe({} as ILogin);
+    expect(login).toStrictEqual({} as ILogin);
   });
 });
