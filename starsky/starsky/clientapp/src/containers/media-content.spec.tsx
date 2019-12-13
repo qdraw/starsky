3457d7d3b6971827f5e44193736abca99eeadce6
Dion <dionvanvelde@gmail.com>
2019-12-13 15:49:12 +0100

    add extra container tests

---
+++ starsky/starsky/clientapp/src/containers/media-content.spec.tsx
@@ -0,0 +1,18 @@
+import { mount, shallow } from "enzyme";
+import React from 'react';
+import * as useFileList from '../hooks/use-filelist';
+import MediaContent from './media-content';
+
+describe("MediaContent", () => {
+  it("renders", () => {
+    shallow(<MediaContent />)
+  });
+  it("application failed", () => {
+    jest.spyOn(useFileList, 'default').mockImplementationOnce(() => {
+      return null;
+    })
+    var result = mount(<MediaContent />);
+    expect(result.html()).toBe('<br>The application failed');
+  });
+
+});
