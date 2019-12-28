a2ae14596a9ac11d9a56f56579ff0f5bf6193c04
Dion <dionvanvelde@gmail.com>
2019-12-28 13:06:28 +0100

    change order

--- starsky/starsky/clientapp/src/components/detailview-gpx.tsx
+++ starsky/starsky/clientapp/src/components/detailview-gpx.tsx
@@ -1,6 +1,7 @@
 import L from 'leaflet';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useLocation from '../hooks/use-location';
+import { IConnectionDefault } from '../interfaces/IConnectionDefault';
 import FetchXml from '../shared/fetch-xml';
 import { URLPath } from '../shared/url-path';
 import { UrlQuery } from '../shared/url-query';
@@ -8,45 +9,52 @@ import { UrlQuery } from '../shared/url-query';
 const DetailViewGpx: React.FC = () => {
   var history = useLocation();
 
+  const [gpxClassName, setGpxClassName] = useState("main main--error");
+
+  function updateMap(response: IConnectionDefault) {
+    if (!response.data) return;
+
+    var tracks: any[] = [];
+    var tracksNodeList: NodeListOf<Element> = (response.data as XMLDocument).querySelectorAll('trkpt');
+
+    Array.from(tracksNodeList).forEach(element => {
+      tracks.push([element.getAttribute('lat'), element.getAttribute('lon')]);
+    });
+
+    // create map
+    var map = L.map('map', {
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution:
+            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }),
+      ]
+    });
+
+    map.dragging.disable();
+    map.touchZoom.disable();
+    map.doubleClickZoom.disable();
+    map.scrollWheelZoom.disable();
+    map.boxZoom.disable();
+    map.keyboard.disable();
+    if (map.tap) map.tap.disable();
+
+    map.fitBounds(tracks);
+
+    L.polygon(tracks, { color: 'red', fill: false }).addTo(map);
+
+    setGpxClassName("main main--error main--gpx");
+  }
+
   useEffect(() => {
     var filePathEncoded = new URLPath().encodeURI(new URLPath().getFilePath(history.location.search));
-
     FetchXml(new UrlQuery().UrlDownloadPhotoApi(filePathEncoded, false)).then((response) => {
-      if (!response.data) return;
-
-      var tracks: any[] = [];
-      var tracksNodeList: NodeListOf<Element> = (response.data as XMLDocument).querySelectorAll('trkpt');
-
-      Array.from(tracksNodeList).forEach(element => {
-        tracks.push([element.getAttribute('lat'), element.getAttribute('lon')]);
-      });
-
-      // create map
-      var map = L.map('map', {
-        layers: [
-          L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution:
-              '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          }),
-        ]
-      });
-
-      map.dragging.disable();
-      map.touchZoom.disable();
-      map.doubleClickZoom.disable();
-      map.scrollWheelZoom.disable();
-      map.boxZoom.disable();
-      map.keyboard.disable();
-      if (map.tap) map.tap.disable();
-
-      map.fitBounds(tracks);
-
-      L.polygon(tracks, { color: 'red', fill: false }).addTo(map);
+      updateMap(response)
     })
   }, []);
 
   return (
-    <div className="main main--error main--gpx" id="map" />
+    <div className={gpxClassName} id="map" />
   );
 };
 
