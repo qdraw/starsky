3f8cefff94f0634809413004bccbb7abfed9e5b7
Dion <dionvanvelde@gmail.com>
2019-12-29 16:29:15 +0100

    add test

--- starsky/starsky/clientapp/src/shared/leaflet-modify-empty-image-url-tilelayer.spec.ts
+++ starsky/starsky/clientapp/src/shared/leaflet-modify-empty-image-url-tilelayer.spec.ts
@@ -1,7 +1,31 @@
+import { Coords, InternalTiles } from 'leaflet';
+import { LeafletEmptyImageUrlTileLayer } from './leaflet-modify-empty-image-url-tilelayer';
 
-describe("LeafletEmptyImageUrlGridLayer [leaflet-extension]", () => {
+describe("LeafletEmptyImageUrlTileLayer [leaflet-extension]", () => {
   it("remove tile form leaflet [extension]", () => {
-    console.log('t');
+
+    var exampleCoords = {
+      x: 51,
+      y: 10,
+      z: 1
+    } as Coords;
+    var el = document.createElement('div') as any;
+
+    var tileLayer = new LeafletEmptyImageUrlTileLayer("51:10:1");
+
+    // mock a tile
+    (tileLayer as any)._tiles = {
+      "51:10:1": {
+        coords: exampleCoords,
+        current: true,
+        el,
+      }
+    } as InternalTiles
+
+    tileLayer._abortLoading();
+
+    // no content anymore
+    expect((tileLayer as any)._tiles).toStrictEqual({})
 
   });
 
