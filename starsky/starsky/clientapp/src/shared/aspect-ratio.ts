6cf35ca2c2d7aa0e610f2fbf53625fc9786fc76d
Dion <dionvanvelde@gmail.com>
2020-01-02 14:57:31 +0100

    remove line

--- starsky/starsky/clientapp/src/shared/aspect-ratio.ts
+++ starsky/starsky/clientapp/src/shared/aspect-ratio.ts
@@ -2,7 +2,6 @@
 class AspectRatio {
 
   /**
-   * 
    * In mathematics, the greatest common divisor (gcd) of two or more integers,
    * which are not all zero, is the largest positive integer that divides each of the integers.
    * For example, the gcd of 8 and 12 is 4.
