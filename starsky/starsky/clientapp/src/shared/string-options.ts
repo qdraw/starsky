0cf28e459566dbfb50a55854d560329e11452144
Dion <dionvanvelde@gmail.com>
2020-01-10 14:37:10 +0100

    remove code

--- starsky/starsky/clientapp/src/shared/string-options.ts
+++ starsky/starsky/clientapp/src/shared/string-options.ts
@@ -4,7 +4,7 @@
 export class StringOptions {
   public LimitLength(input: string, lenght: number) {
     if (input.length <= lenght) return input;
-    return input.substr(0, lenght) + "..."
+    return input.substr(0, lenght) + "&hellip;"
   }
 }
 
