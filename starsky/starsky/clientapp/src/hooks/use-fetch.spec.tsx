a7ee9ad0d713ba770f167164c59a9c4508ac70dd
Dion <dionvanvelde@gmail.com>
2020-01-13 18:21:17 +0100

    refactor use-fetch

--- starsky/starsky/clientapp/src/hooks/use-fetch.spec.tsx
+++ starsky/starsky/clientapp/src/hooks/use-fetch.spec.tsx
@@ -1,34 +1,54 @@
-import { mount } from 'enzyme';
-import React from 'react';
-import { TestHook } from './test-hook';
-import useFetch from './use-fetch';
+import { IConnectionDefault } from '../interfaces/IConnectionDefault';
+import useFetch, { fetchContent } from './use-fetch';
+import { mountReactHook } from './___tests___/test-hook';
 
 
 describe("UseFetch", () => {
+  let setupComponent;
+  let hook: IConnectionDefault;
 
-  const testHook = (callback: any) => {
-    return mount(<TestHook callback={callback} />);
-  };
-
-  it("call api", () => {
-    const setState = jest.fn();
-    const useStateSpy = jest.spyOn(React, 'useState').mockImplementationOnce(() => {
-      return [setState, setState]
-    });
-
-    var fetch = jest.spyOn(window, 'fetch').mockImplementationOnce(() => {
-      return Promise.resolve(
-        {
-          json: () => { },
-          status: 200
-        } as Response,
-      )
-    });
-
-    testHook(useFetch);
-
-    expect(fetch).toHaveBeenCalled()
-    expect(useStateSpy).toHaveBeenCalled()
-
+  beforeEach(() => {
+    setupComponent = mountReactHook(useFetch, ["/default/", "get"]); // Mount a Component with our hook
+    hook = setupComponent.componentHook as IConnectionDefault;
   });
+
+
+  it('default status code', () => {
+    expect(hook.statusCode).toBe(999)
+  })
+
+  it('dd', async () => {
+
+    var controller = new AbortController();
+    var content = await fetchContent("t", 'get', true, controller, jest.fn())
+    console.log(content);
+
+  })
+
+
+  // const testHook = (callback: any) => {
+  //   return mount(<TestHook callback={callback} />);
+  // };
+
+  // it("call api", () => {
+  //   const setState = jest.fn();
+  //   const useStateSpy = jest.spyOn(React, 'useState').mockImplementationOnce(() => {
+  //     return [setState, setState]
+  //   });
+
+  //   var fetch = jest.spyOn(window, 'fetch').mockImplementationOnce(() => {
+  //     return Promise.resolve(
+  //       {
+  //         json: () => { },
+  //         status: 200
+  //       } as Response,
+  //     )
+  //   });
+
+  //   testHook(useFetch);
+
+  //   expect(fetch).toHaveBeenCalled()
+  //   expect(useStateSpy).toHaveBeenCalled()
+
+  // });
 });
