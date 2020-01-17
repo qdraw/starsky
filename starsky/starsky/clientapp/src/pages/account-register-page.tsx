19c7daedc3f16733fa2c04b4bc5a70ad554ed669
Dion <dionvanvelde@gmail.com>
2020-01-17 21:29:44 +0100

    add handling new account setup

--- starsky/starsky/clientapp/src/pages/account-register-page.tsx
+++ starsky/starsky/clientapp/src/pages/account-register-page.tsx
@@ -1,8 +1,12 @@
 
 import { RouteComponentProps } from '@reach/router';
 import React, { FunctionComponent } from 'react';
+import Button from '../components/Button';
 import useGlobalSettings from '../hooks/use-global-settings';
+import FetchPost from '../shared/fetch-post';
 import { Language } from '../shared/language';
+import { UrlQuery } from '../shared/url-query';
+import { validateLoginForm } from '../shared/validate-login-form';
 
 const AccountRegisterPage: FunctionComponent<RouteComponentProps> = (props) => {
 
@@ -16,8 +20,46 @@ const AccountRegisterPage: FunctionComponent<RouteComponentProps> = (props) => {
   const MessageExampleUsername = "dont@mail.me";
   const MessagePassword = language.text("Geef je wachtwoord op", "Enter your password");
   const MessageConfirmPassword = language.text("Vul je wachtwoord nog een keer in", "Enter your password again");
+  const MessageNoUsernamePassword = language.text("Voer een emailadres en een wachtwoord in", "Enter an email address and password");
+  const MessageWrongFormatEmailAddress = language.text("Controleer je email adres", "Check your email address");
+  const MessagePasswordToShort = language.text("Gebruik minimaal 8 tekens voor je wachtwoord", "Use at least 8 characters for your password");
+  const MessagePasswordNoMatch = language.text("Deze wachtwoorden komen niet overeen. Probeer het opnieuw", "These passwords do not match. Please try again")
 
   const [userEmail, setUserEmail] = React.useState("");
+  const [userPassword, setUserPassword] = React.useState("");
+  const [userConfirmPassword, setUserConfirmPassword] = React.useState("");
+
+  const useErrorHandler = (initialState: string | null) => { return initialState };
+  const [error, setError] = React.useState(useErrorHandler(null));
+
+  const [loading, setLoading] = React.useState(false);
+
+  const setUpAccountHandler = async () => {
+    try {
+      setLoading(true);
+      const response = await FetchPost(new UrlQuery().UrlLogin(), 'Email=' + userEmail + '&Password=' + userPassword);
+      if (!response || !response.data) {
+        setError('MessageConnection');
+      }
+      // else if (response.statusCode === 401 || response.statusCode === 302) {
+      //   setLoading(false);
+      //   setError(MessageWrongUsernamePassword);
+      // }
+      // else {
+      //   // redirect
+      //   var returnUrl = new URLPath().GetReturnUrl(history.location.search);
+      //   history.navigate(returnUrl, { replace: true });
+
+      //   // for chrome navigate isn't enough
+      //   setTimeout(() => {
+      //     document.location.reload();
+      //   }, 100);
+      // }
+    } catch (err) {
+      setLoading(false);
+      setError(err.message);
+    }
+  };
 
   return (<>
     <header className="header header--main header--bluegray700">
@@ -30,8 +72,27 @@ const AccountRegisterPage: FunctionComponent<RouteComponentProps> = (props) => {
     <div className="content">
       <div className="content--header">{MessageCreateNewAccount}</div>
 
-      <form method="post" action="/account/register"
-        className="content--login-form form-inline form-nav">
+      <form method="post"
+        className="content--login-form form-inline form-nav"
+        onSubmit={e => {
+          e.preventDefault();
+          setError(null);
+          var loginValidation = validateLoginForm(userEmail, userPassword);
+
+          if (!loginValidation) {
+            setError(loginValidation === null ? MessageWrongFormatEmailAddress : MessageNoUsernamePassword)
+            return;
+          }
+          if (userPassword.length <= 7) {
+            setError(MessagePasswordToShort);
+            return;
+          }
+          if (userPassword !== userConfirmPassword) {
+            setError(MessagePasswordNoMatch);
+            return;
+          }
+          setUpAccountHandler();
+        }}>
 
         <label htmlFor="email">
           {MessageUsername}
@@ -55,8 +116,8 @@ const AccountRegisterPage: FunctionComponent<RouteComponentProps> = (props) => {
           type="password"
           name="password"
           placeholder={MessageExamplePassword}
-        // value={userEmail}
-        // onChange={e => setUserEmail(e.target.value)}
+          value={userPassword}
+          onChange={e => setUserPassword(e.target.value)}
         />
 
         <label htmlFor="email">
@@ -67,11 +128,14 @@ const AccountRegisterPage: FunctionComponent<RouteComponentProps> = (props) => {
           autoComplete="off"
           type="password"
           name="password"
-        // value={userEmail}
-        // onChange={e => setUserEmail(e.target.value)}
+          value={userConfirmPassword}
+          onChange={e => setUserConfirmPassword(e.target.value)}
         />
+        {error && <div className="content--error-true">{error}</div>}
 
-
+        <Button className="btn btn--default" type="submit" disabled={loading} onClick={e => { }}>
+          {loading ? "Loading..." : MessageCreateNewAccount}
+        </Button>
       </form>
     </div>
   </>)
