b6a3cc34f2e35b4251d31007c1a0f4c7b7ec5293
Dion <dionvanvelde@gmail.com>
2020-01-24 20:13:44 +0100

    Move this array "sort" operation to a separate statement.

--- starsky/starsky/clientapp/src/contexts/archive-context.tsx
+++ starsky/starsky/clientapp/src/contexts/archive-context.tsx
@@ -119,9 +119,8 @@ export function archiveReducer(state: State, action: Action): State {
 
     case "add":
       var concattedFileIndexItems = state.fileIndexItems.concat(action.add);
-      var fileIndexItems = concattedFileIndexItems
-        .sort((a, b) => (a.filePath > b.filePath) ? 1 : -1) // sort on filePath
-        .filter((v, i, a) => a.findIndex(t => (t.filePath === v.filePath)) === i) // duplicate check
+      var fileIndexItems = concattedFileIndexItems.sort((a, b) => (a.filePath > b.filePath) ? 1 : -1); // sort on filePath
+      fileIndexItems = fileIndexItems.filter((v, i, a) => a.findIndex(t => (t.filePath === v.filePath)) === i); // duplicate check
 
       return { ...state, fileIndexItems, lastUpdated: new Date() };
   }
