a7fbcc52544a122a50b93ced67501b692c5d5bd9
Dion <dionvanvelde@gmail.com>
2020-02-03 21:58:03 +0100

    fix small code smells

--- starsky/starsky/Startup.cs
+++ starsky/starsky/Startup.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
