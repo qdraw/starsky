b78b80d3bc537ba4e778a429c913b11ec9731443
Dion <dionvanvelde@gmail.com>
2020-02-06 20:30:31 +0100

    _breaking change_ add Software as field in database

---
+++ starsky/starskycore/Migrations/20200206192905_addSoftwareField.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace starskycore.Migrations
+{
+    public partial class addSoftwareField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Software",
+                table: "FileIndex",
+                maxLength: 40,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Software",
+                table: "FileIndex");
+        }
+    }
+}
