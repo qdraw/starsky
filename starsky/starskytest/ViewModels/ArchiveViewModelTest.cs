ceae44d680f5a5bb5180d54b5791d7947deeb116
Dion <dionvanvelde@gmail.com>
2020-02-08 17:18:18 +0100

    remove IsDirectory ++ add tests

--- starsky/starskytest/ViewModels/ArchiveViewModelTest.cs
+++ starsky/starskytest/ViewModels/ArchiveViewModelTest.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using starskycore.Models;
 using starskycore.ViewModels;
 
 namespace starskytest.ViewModels
@@ -9,9 +12,46 @@ namespace starskytest.ViewModels
         [TestMethod]
         public void ArchiveViewModelPageTypeTest()
         {
-            var t = new ArchiveViewModel {IsDirectory = true}; // IsDirectory= not used
+            var viewModel = new ArchiveViewModel(); 
+            Assert.AreEqual( PageViewType.PageType.Archive.ToString(),viewModel.PageType);            
+        }
+        
+        [TestMethod]
+        public void ArchiveViewModel_ColorClass()
+        {
+	        var viewModel = new ArchiveViewModel
+	        {
+		        ColorClassActiveList = new List<FileIndexItem.Color>{FileIndexItem.Color.None},
+		        ColorClassUsage = new List<FileIndexItem.Color>{FileIndexItem.Color.None}
+	        };
             
-            Assert.AreEqual(t.PageType, PageViewType.PageType.Archive.ToString());            
+	        Assert.AreEqual(FileIndexItem.Color.None, viewModel.ColorClassActiveList.FirstOrDefault());            
+	        Assert.AreEqual(FileIndexItem.Color.None, viewModel.ColorClassUsage.FirstOrDefault());            
+        }
+        
+        [TestMethod]
+        public void ArchiveViewModel_ExampleData()
+        {
+	        var archiveViewModel = new ArchiveViewModel
+	        {
+		        FileIndexItems = new List<FileIndexItem>(),
+		        Breadcrumb = new List<string>{"/"},
+		        RelativeObjects = new RelativeObjects
+		        {
+			        NextFilePath = "/"
+		        },
+		        SearchQuery = "test",
+		        SubPath = "/",
+		        IsReadOnly = false,
+		        CollectionsCount= 0,
+	        };
+	        
+	        Assert.AreEqual("/", archiveViewModel.Breadcrumb.FirstOrDefault());      
+	        Assert.AreEqual("/", archiveViewModel.RelativeObjects.NextFilePath);      
+	        Assert.AreEqual("test", archiveViewModel.SearchQuery);      
+	        Assert.AreEqual("/", archiveViewModel.SubPath);      
+	        Assert.AreEqual(false, archiveViewModel.IsReadOnly);      
+	        Assert.AreEqual(0, archiveViewModel.CollectionsCount);      
         }
     }
 }
