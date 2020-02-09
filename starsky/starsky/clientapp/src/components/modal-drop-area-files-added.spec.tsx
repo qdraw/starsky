9c36e00edc867d374f64b542233b5ddddc57f79a
Dion <dionvanvelde@gmail.com>
2020-02-09 13:42:59 +0100

    add list render

--- starsky/starsky/clientapp/src/components/modal-drop-area-files-added.spec.tsx
+++ starsky/starsky/clientapp/src/components/modal-drop-area-files-added.spec.tsx
@@ -7,22 +7,34 @@ import ModalDropAreaFilesAdded from './modal-drop-area-files-added';
 describe("ModalDropAreaFilesAdded", () => {
 
   it("renders", () => {
-    shallow(<ModalDropAreaFilesAdded isOpen={true} uploadFilesList={[]} handleExit={() => { }} />)
+    var component = shallow(<ModalDropAreaFilesAdded isOpen={true} uploadFilesList={[]} handleExit={() => { }} />);
+    component.unmount();
   });
 
   describe("with Context", () => {
-    xit("renders", () => {
 
-      var itemTextListViewSpy = jest.spyOn(ItemTextListView, 'default');
+    beforeEach(() => {
+      jest.spyOn(window, 'scrollTo')
+        .mockImplementationOnce(() => { })
+    });
+
+    it("list is rendered", () => {
+
+      jest.spyOn(ItemTextListView, 'default').mockImplementationOnce((props) => {
+        return <span id="data-test-0">{props.fileIndexItems[0].fileName}</span>
+      });
 
       var exampleList = [{
-        fileName: 'test'
+        fileName: 'test.jpg',
+        filePath: "/test.jpg"
       } as IFileIndexItem];
+
       var handleExitSpy = jest.fn();
+
       var component = mount(<ModalDropAreaFilesAdded isOpen={true} uploadFilesList={exampleList} handleExit={handleExitSpy} />);
 
-      //   expect.any(Function)
-      expect(itemTextListViewSpy).toBeCalledWith({ fileIndexItems: exampleList });
+      expect(component.exists("#data-test-0")).toBeTruthy()
+      expect(component.find("#data-test-0").text()).toBe('test.jpg')
 
       component.unmount();
 
