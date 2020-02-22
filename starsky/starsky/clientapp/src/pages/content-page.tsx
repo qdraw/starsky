2d9d7484b31c9d4c1aee21f1ebc58478405ca4e8
Dion <dionvanvelde@gmail.com>
2020-02-22 21:08:48 +0100

    IE11 is not supported (from version 0.1.6 +)

--- starsky/starsky/clientapp/src/pages/content-page.tsx
+++ starsky/starsky/clientapp/src/pages/content-page.tsx
@@ -8,8 +8,7 @@ interface IContentPageProps {
 
 const ContentPage: FunctionComponent<RouteComponentProps<IContentPageProps>> = (props) => {
   // for some reason "name" here is possibly undefined
-  if (props && props.location && props.navigate) {
-    if (new BrowserDetect().IsLegacy()) return <>Internet Explorer is not supported, please try Firefox or Chrome</>
+  if (props && props.location && props.navigate && !new BrowserDetect().IsLegacy()) {
     return (
       <MediaContent />
     );
