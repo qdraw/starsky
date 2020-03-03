ae22ac4ebfe6666746997d8af58cd98b0abf3bce
Dion <dionvanvelde@gmail.com>
2020-03-03 22:51:05 +0100

    bg service

---
+++ starsky/starskycore/Services/SearchSuggestionsInflateHostedService.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using starskycore.Data;
+using starskycore.Models;
+
+namespace starskycore.Services
+{
+	public class SearchSuggestionsInflateHostedService : IHostedService
+	{
+		private readonly AppSettings _appSettings;
+		private readonly IMemoryCache _memoryCache;
+		private readonly IServiceScopeFactory _scopeFactory;
+		
+		public SearchSuggestionsInflateHostedService(IServiceScopeFactory scopeFactory,
+			IMemoryCache memoryCache, AppSettings appSettings = null)
+		{
+			_scopeFactory = scopeFactory;
+			_memoryCache = memoryCache;
+			_appSettings = appSettings;
+		}
+
+		public async Task StartAsync(CancellationToken cancellationToken)
+		{
+			using (var scope = _scopeFactory.CreateScope())
+			{
+				var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+				new SearchSuggestionsService(dbContext, _memoryCache, _appSettings).Inflate();
+			}
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			// nope
+			return Task.CompletedTask;
+		}
+	}
+}
+
