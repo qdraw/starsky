9c375531dc122eaa7028d88a5feccbad2da4ed0b
Dion <dionvanvelde@gmail.com>
2020-04-14 16:04:16 +0200

    add tests for Search Pagination

--- starsky/starsky/clientapp/src/containers/search.spec.tsx
+++ starsky/starsky/clientapp/src/containers/search.spec.tsx
@@ -1,10 +1,42 @@
 import { shallow } from "enzyme";
 import React from 'react';
+import SearchPagination from '../components/search-pagination';
 import { newIArchive } from '../interfaces/IArchive';
+import { newIFileIndexItem, newIFileIndexItemArray } from '../interfaces/IFileIndexItem';
 import Search from './search';
 
 describe("Search", () => {
   it("renders", () => {
     shallow(<Search {...newIArchive()} />)
+  });
+
+  describe("Results count", () => {
+    it("No results", () => {
+      var component = shallow(<Search {...newIArchive()} fileIndexItems={[]} pageNumber={0} colorClassUsage={[]} />)
+      var text = component.find(".content--header").text()
+      expect(text).toBe('No result')
+    });
+
+    it("Page 2 of 1 results", () => {
+      var component = shallow(<Search {...newIArchive()} collectionsCount={1} fileIndexItems={[]} pageNumber={2} colorClassUsage={[]} />)
+      var text = component.find(".content--header").text()
+      expect(text).toBe('Page 2 of 1 results')
+    });
+
+    it("Page 1 of 1 results", () => {
+      var component = shallow(<Search {...newIArchive()} collectionsCount={1} fileIndexItems={[]} pageNumber={1} colorClassUsage={[]} />)
+      var text = component.find(".content--header").text()
+      expect(text).toBe('1 results')
+    });
+
+    it("SearchPagination exist", () => {
+      var numberOfFileIndexItems = newIFileIndexItemArray();
+      for (let index = 0; index < 21; index++) {
+        numberOfFileIndexItems.push(newIFileIndexItem())
+      }
+      var component = shallow(<Search {...newIArchive()} collectionsCount={1} fileIndexItems={numberOfFileIndexItems} pageNumber={1} colorClassUsage={[]} />)
+      var text = component.exists(SearchPagination)
+      expect(text).toBeTruthy();
+    });
   });
 });
