a63d0ccce4a3eca9402deff67a27ad24d038e78f
Dion <dionvanvelde@gmail.com>
2020-04-23 17:57:03 +0200

    array helper

--- starsky/starsky/clientapp/src/contexts/archive-context.tsx
+++ starsky/starsky/clientapp/src/contexts/archive-context.tsx
@@ -5,6 +5,7 @@ import { IArchiveProps } from '../interfaces/IArchiveProps';
 import { IRelativeObjects, newIRelativeObjects, PageType } from '../interfaces/IDetailView';
 import { IExifStatus } from '../interfaces/IExifStatus';
 import { IFileIndexItem } from '../interfaces/IFileIndexItem';
+import ArrayHelper from '../shared/array-helper';
 
 const ArchiveContext = React.createContext<IArchiveContext>({} as IArchiveContext)
 
@@ -104,13 +105,13 @@ export function archiveReducer(state: State, action: Action): State {
       var filterOkCondition = (value: IFileIndexItem) => {
         return (value.status === IExifStatus.Ok || value.status === IExifStatus.Default);
       };
-      var concattedFileIndexItems = state.fileIndexItems.concat(action.add);
-      // var fileIndexItems = [...concattedFileIndexItems].sort((a, b) => (a.fileName.toLowerCase() > b.fileName.toLowerCase() ? 1
-      //   : a.fileName.toLowerCase() < b.fileName.toLowerCase() ? -1 : 0)); // sort on fileName
 
-      var fileIndexItems = concattedFileIndexItems.sort((a, b) => a.fileName.localeCompare(b.fileName, 'nl', { sensitivity: 'base' }));
+      var concattedFileIndexItems = [...Array.from(action.add), ...state.fileIndexItems];
+      concattedFileIndexItems = new ArrayHelper().UniqueResults(concattedFileIndexItems, 'filePath');
 
-      fileIndexItems = fileIndexItems.filter((v, i, a) => a.findIndex(t => (t.filePath === v.filePath)) === i); // duplicate check
+      // order by this to match c#
+      var fileIndexItems = concattedFileIndexItems.sort((a, b) => a.fileName.localeCompare(b.fileName, 'en', { sensitivity: 'base' }));
+
       fileIndexItems = fileIndexItems.filter(filterOkCondition);
       return { ...state, fileIndexItems, lastUpdated: new Date() };
   }
