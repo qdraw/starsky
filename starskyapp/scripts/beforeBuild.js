5272d3bb2ae14abb2e5766ca2124de4c4758ae96
Dion <dionvanvelde@gmail.com>
2020-05-12 18:22:24 +0200

    path chaos

--- starskyapp/scripts/beforeBuild.js
+++ starskyapp/scripts/beforeBuild.js
@@ -7,10 +7,10 @@ exports.default = context => {
 
   switch (context.platform.buildConfigurationKey) {
     case "mac":
-      copyFile(path.join(__dirname, '..', '..', 'starsky', 'starsky-osx.10.12-x64.zip'), path.join(__dirname, 'include-starsky-mac.zip'))
+      copyFile(path.join(__dirname, '..', '..', 'starsky', 'starsky-osx.10.12-x64.zip'), path.join(__dirname, '..', 'include-starsky-mac.zip'))
       break;
     case "win":
-      copyFile(path.join(__dirname, '..', '..', 'starsky', 'starsky-win7-x86.zip'), path.join(__dirname, 'include-starsky-win.zip'))
+      copyFile(path.join(__dirname, '..', '..', 'starsky', 'starsky-win7-x86.zip'), path.join(__dirname, '..' ,'include-starsky-win.zip'))
       break;
     default:
   }
@@ -21,5 +21,7 @@ exports.default = context => {
 
 function copyFile(src, dest) {
   console.log(src, dest);
+  console.log(fs.existsSync(src))
+  
   fs.copyFileSync(src, dest);
 }
