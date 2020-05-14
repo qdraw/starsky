90b85f55562fd4cf7dace3e680ee51d840989890
Dion <dionvanvelde@gmail.com>
2020-05-14 22:02:41 +0200

    add class

--- starsky/starsky.foundation.platform/Exceptions/TelemetryServiceException.cs
+++ starsky/starsky.foundation.platform/Exceptions/TelemetryServiceException.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace starsky.foundation.platform.Exceptions
 {
+	[Serializable]
 	public class TelemetryServiceException : Exception
 	{
 		public TelemetryServiceException(string message)
