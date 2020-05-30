2678f778b368040c00e1c7bcfbdce9d8a5f0e098
Dion <dionvanvelde@gmail.com>
2020-05-30 16:39:02 +0200

    add change test

--- starsky/starsky/clientapp/src/components/organisms/preferences-app-settings/preferences-app-settings.spec.tsx
+++ starsky/starsky/clientapp/src/components/organisms/preferences-app-settings/preferences-app-settings.spec.tsx
@@ -2,6 +2,8 @@ import { mount, shallow } from "enzyme";
 import React from 'react';
 import * as useFetch from '../../../hooks/use-fetch';
 import { IConnectionDefault, newIConnectionDefault } from '../../../interfaces/IConnectionDefault';
+import * as FetchPost from '../../../shared/fetch-post';
+import { UrlQuery } from '../../../shared/url-query';
 import PreferencesAppSettings from './preferences-app-settings';
 
 describe("PreferencesAppSettings", () => {
@@ -18,8 +20,6 @@ describe("PreferencesAppSettings", () => {
         .mockImplementationOnce(() => newIConnectionDefault());
 
       var component = mount(<PreferencesAppSettings />);
-
-      console.log(component.html());
 
       expect((component.find('input[name="verbose"]').first().getDOMNode() as HTMLInputElement).disabled).toBeTruthy();
 
@@ -63,6 +63,40 @@ describe("PreferencesAppSettings", () => {
       expect(component.find('[data-name="storageFolder"]').text()).toBe('test');
 
       component.unmount()
+    });
+
+    it("change storageFolder", () => {
+      var permissions = { statusCode: 200, data: ["AppSettingsWrite"] } as IConnectionDefault;
+      var appSettings = {
+        statusCode: 200, data: {
+          verbose: true,
+          storageFolder: 'test'
+        }
+      } as IConnectionDefault;
+
+      // usage ==> import * as useFetch from '../../../hooks/use-fetch';
+      jest.spyOn(useFetch, 'default')
+        .mockImplementationOnce(() => permissions)
+        .mockImplementationOnce(() => appSettings)
+        .mockImplementationOnce(() => permissions)
+        .mockImplementationOnce(() => appSettings)
+        .mockImplementationOnce(() => permissions)
+        .mockImplementationOnce(() => appSettings);
+
+      const mockIConnectionDefault: Promise<IConnectionDefault> = Promise.resolve(newIConnectionDefault());
+      var fetchPostSpy = jest.spyOn(FetchPost, 'default').mockImplementationOnce(() => mockIConnectionDefault);
+
+      var component = mount(<PreferencesAppSettings />);
+
+      var storageFolderForm = component.find('[data-name="storageFolder"]');
+
+      (storageFolderForm.getDOMNode() as HTMLInputElement).innerText = "12345";
+      storageFolderForm.first().simulate('blur');
+
+      expect(fetchPostSpy).toBeCalled();
+      expect(fetchPostSpy).toBeCalledWith(new UrlQuery().UrlApiAppSettings(), "storageFolder=12345");
+
+
     });
 
   });
