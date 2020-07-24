0a81db6fbbb768853fd5078e54388184148fd918
Dion <dionvanvelde@gmail.com>
2020-07-24 21:11:46 +0200

    fix tests

--- starsky/starskytest/starsky.foundation.writemeta/JsonService/FileIndexItemJsonWriterTest.cs
+++ starsky/starskytest/starsky.foundation.writemeta/JsonService/FileIndexItemJsonWriterTest.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.database.Models;
@@ -140,5 +140,42 @@ namespace starskytest.starsky.foundation.writemeta.JsonService
 			Assert.AreEqual(ExampleItem.MakeModel,result.MakeModel);
 			Assert.AreEqual(ExampleItem.FocalLength,result.FocalLength);
 		}
+
+		[TestMethod]
+		public void ReadTest_FromCopiedText_T_Model()
+		{
+			var input =
+				"{\n  \"Title\": \"Title\",\n  \"Price\": 200,\n  \"ShowButtons\": " +
+				"true}";
+			var fakeStorage = new FakeIStorage();
+			fakeStorage.WriteStream(
+				new PlainTextFileHelper().StringToStream(input), "/test.json");
+			
+			var itemJsonParser = new FileIndexItemJsonParser(fakeStorage);
+
+			var result = itemJsonParser.Read<FileIndexItemJsonParserTest_TestModel>("/test.json");
+			
+			Assert.AreEqual(200, result.Price);
+			Assert.AreEqual("Title", result.Title);
+			Assert.AreEqual(true, result.ShowButtons);
+		}
+		
+		[TestMethod]
+		[ExpectedException(typeof(FileNotFoundException))]
+		public void ReadTest_NotFound()
+		{
+			var fakeStorage = new FakeIStorage();
+			var itemJsonParser = new FileIndexItemJsonParser(fakeStorage);
+
+			itemJsonParser.Read<FileIndexItemJsonParserTest_TestModel>("/notfound.json");
+			// expect error
+		}
+	}
+
+	public class FileIndexItemJsonParserTest_TestModel
+	{
+		public string Title { get; set; }
+		public int Price { get; set; }
+		public bool ShowButtons { get; set; }
 	}
 }
