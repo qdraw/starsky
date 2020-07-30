ae4e2c3a90c1212032ccf4cd96941e8ac9ad5654
Dion <dionvanvelde@gmail.com>
2020-07-30 18:46:36 +0200

    Add Migration for bool? and Size

---
+++ starsky/starsky.foundation.database/Migrations/20200730164439_addSize.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace starsky.foundation.database.Migrations
+{
+    public partial class addSize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDirectory",
+                table: "FileIndex",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddColumn<long>(
+                name: "Size",
+                table: "FileIndex",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "FileIndex");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDirectory",
+                table: "FileIndex",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+        }
+    }
+}
