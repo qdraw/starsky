433f366b1764c6798a377693f259715de1b9cef6
Dion <dionvanvelde@gmail.com>
2020-08-04 21:06:09 +0200

    add test ++ comments

---
+++ starsky/starskytest/starsky.foundation.storage/Storage/StorageHostFullPathFilesystemTest.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using starsky.foundation.storage.Models;
+using starsky.foundation.storage.Storage;
+using starskytest.FakeCreateAn;
+
+namespace starskytest.starsky.foundation.storage.Storage
+{
+	[TestClass]
+	public class StorageHostFullPathFilesystemTest
+	{
+		[TestMethod]
+		public void InfoNotFound()
+		{
+			var info = new StorageHostFullPathFilesystem().Info("C://folder-not-found-992544124712741");
+			Assert.AreEqual(FolderOrFileModel.FolderOrFileTypeList.Deleted, info.IsFolderOrFile );
+		}
+
+		[TestMethod]
+		public void FolderDelete_ChildFoldersAreDeleted()
+		{
+			var rootDir = Path.Combine(new CreateAnImage().BasePath, "test01010");
+			var childDir = Path.Combine(new CreateAnImage().BasePath, "test01010","included-folder");
+
+			var realStorage = new StorageHostFullPathFilesystem();
+			realStorage.CreateDirectory(rootDir);
+			realStorage.CreateDirectory(childDir);
+
+			realStorage.FolderDelete(rootDir);
+
+			Assert.AreEqual(false,realStorage.ExistFolder(rootDir));
+			Assert.AreEqual(false,realStorage.ExistFolder(childDir));
+		}
+	}
+}
