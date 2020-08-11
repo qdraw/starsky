9a5134fe6d6b52238986f066a1c7d7a4e5cfd2e7
Dion <dionvanvelde@gmail.com>
2020-08-11 22:23:45 +0200

    as background task

--- starsky/starsky/Controllers/PublishController.cs
+++ starsky/starsky/Controllers/PublishController.cs
@@ -9,6 +9,7 @@ using starsky.foundation.platform.Helpers;
 using starsky.foundation.platform.Models;
 using starsky.foundation.storage.Interfaces;
 using starsky.foundation.storage.Storage;
+using starskycore.Services;
 
 namespace starsky.Controllers
 {
@@ -20,15 +21,18 @@ namespace starsky.Controllers
 		private readonly IMetaInfo _metaInfo;
 		private readonly AppSettings _appSettings;
 		private readonly IStorage _hostStorage;
+		private readonly IBackgroundTaskQueue _bgTaskQueue;
 
 		public PublishController(AppSettings appSettings, IPublishPreflight publishPreflight, 
-			IWebHtmlPublishService publishService, IMetaInfo metaInfo, ISelectorStorage selectorStorage)
+			IWebHtmlPublishService publishService, IMetaInfo metaInfo, ISelectorStorage selectorStorage,
+			IBackgroundTaskQueue queue)
 		{
 			_appSettings = appSettings;
 			_publishPreflight = publishPreflight;
 			_publishService = publishService;
 			_metaInfo = metaInfo;
 			_hostStorage = selectorStorage.Get(SelectorStorage.StorageServices.HostFilesystem);
+			_bgTaskQueue = queue;
 		}
 
 		/// <summary>
@@ -61,7 +65,8 @@ namespace starsky.Controllers
 		[ProducesResponseType(typeof(void), 401)]
 		[HttpPost("/api/publish/create")]
 		[Produces("application/json")]
-		public async Task<IActionResult> PublishCreate(string f, string itemName, string publishProfileName, bool force = false)
+		public async Task<IActionResult> PublishCreate(string f, string itemName, 
+			string publishProfileName, bool force = false)
 		{
 			var inputFilePaths = PathHelper.SplitInputFilePaths(f).ToList();
 			var info = _metaInfo.GetInfo(inputFilePaths, false);
@@ -74,10 +79,14 @@ namespace starsky.Controllers
 				ForceCleanPublishFolderAndZip(location);
 			}
 
-			var renderCopyResult = await _publishService.RenderCopy(info, 
-				publishProfileName, itemName, location);
-			await _publishService.GenerateZip(_appSettings.TempFolder, itemName, 
-				renderCopyResult,true );
+			// Creating Publish is a background task
+			_bgTaskQueue.QueueBackgroundWorkItem(async token =>
+			{
+				var renderCopyResult = await _publishService.RenderCopy(info, 
+					publishProfileName, itemName, location);
+				await _publishService.GenerateZip(_appSettings.TempFolder, itemName, 
+					renderCopyResult,true );	
+			});
 			
 			// Get the zip 	by	[HttpGet("/export/zip/{f}.zip")]
 			return Json(slugItemName);
