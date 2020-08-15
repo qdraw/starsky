77b332dc58f96fc7e56e2d3a7ca4da047128ab16
Dion <dionvanvelde@gmail.com>
2020-08-15 15:54:53 +0200

    [pm2-new-instance.sh] give command line args excute rights

--- starsky/starsky/pm2-new-instance.sh
+++ starsky/starsky/pm2-new-instance.sh
@@ -55,13 +55,46 @@ fi
 /usr/bin/find . -type d -exec chmod 755 {} \;
 /usr/bin/find . -type f -exec chmod 644 {} \;
 
+
+# excute right for specific files
+if [ -f starskygeocli ]; then
+    chmod +rwx ./starskygeocli
+fi
+
+if [ -f starskyimportercli ]; then
+    chmod +rwx ./starskyimportercli
+fi
+
+if [ -f starskysynccli ]; then
+    chmod +rwx ./starskysynccli
+fi
+
+if [ -f starskywebftpcli ]; then
+    chmod +rwx ./starskywebftpcli
+fi
+
+if [ -f starskywebhtmlcli ]; then
+    chmod +rwx ./starskywebhtmlcli
+fi
+
+if [ -f starskyadmincli ]; then
+    chmod +rwx ./starskyadmincli
+fi
+
+if [ -f pm2-deploy-on-env.sh ]; then
+    chmod +rwx ./pm2-deploy-on-env.sh
+fi
+
+if [ -f pm2-warmup.sh ]; then
+    chmod +rwx ./pm2-warmup.sh
+fi
+
 if [ -f starsky ]; then
     chmod +rwx ./starsky
-    
+
     pm2 start --name $PM2NAME ./starsky
     echo $PM2NAME " started"
 
     pm2 status
+    echo "Need to run `pm2 save` yourself"
 fi
-
-
