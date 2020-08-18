4ada24f6225f8da96ce3c45965a6c13aa7fe3e06
Dion <dionvanvelde@gmail.com>
2020-08-18 11:29:19 +0200

    add tests

--- starsky/starsky/clientapp/src/components/atoms/current-location-button/current-location-button.spec.tsx
+++ starsky/starsky/clientapp/src/components/atoms/current-location-button/current-location-button.spec.tsx
@@ -10,11 +10,70 @@ describe("CurrentLocationButton", () => {
 
   describe("context", () => {
 
-    it("renders", () => {
-
+    it("no navigator.geolocation wrong_location", () => {
 
       var component = mount(<CurrentLocationButton />);
       component.find('button').simulate('click');
+      expect(component.find('button').hasClass('icon--wrong_location')).toBeTruthy();
+    });
+
+    it("getCurrentPosition success", () => {
+
+      const mockGeolocation = {
+        getCurrentPosition: jest.fn()
+          .mockImplementationOnce((success) => Promise.resolve(success({
+            coords: {
+              latitude: 51.1,
+              longitude: 45.3
+            }
+          })))
+      };
+      (global as any).navigator.geolocation = mockGeolocation;
+
+      var callback = jest.fn();
+      var component = mount(<CurrentLocationButton callback={callback} />);
+      component.find('button').simulate('click');
+
+      expect(callback).toBeCalled();
+      expect(callback).toBeCalledWith({ "latitude": 51.1, "longitude": 45.3 });
+
+      expect(component.find('button').hasClass('icon--location_on')).toBeTruthy();
+    });
+
+    it("getCurrentPosition success no callback", () => {
+
+      const mockGeolocation = {
+        getCurrentPosition: jest.fn()
+          .mockImplementationOnce((success) => Promise.resolve(success({
+            coords: {
+              latitude: 51.1,
+              longitude: 45.3
+            }
+          })))
+      };
+      (global as any).navigator.geolocation = mockGeolocation;
+
+      var component = mount(<CurrentLocationButton />);
+      component.find('button').simulate('click');
+
+      // no callback
+      expect(component.find('button').hasClass('icon--location_on')).toBeTruthy();
+    });
+
+
+    it("getCurrentPosition error", () => {
+
+      const mockGeolocation = {
+        getCurrentPosition: jest.fn()
+          .mockImplementationOnce((_, error) => Promise.resolve(error()))
+      };
+      (global as any).navigator.geolocation = mockGeolocation;
+
+      var callback = jest.fn();
+      var component = mount(<CurrentLocationButton callback={callback} />);
+      component.find('button').simulate('click');
+
+      expect(component.find('button').hasClass('icon--wrong_location')).toBeTruthy();
     });
 
   });
