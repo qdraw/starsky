fb11b3f4f45715dcd8a02224fad09b1200a94ddd
Dion <dionvanvelde@gmail.com>
2020-09-11 12:00:45 +0200

    fix for iOS 12 and lower

--- starsky/starsky/clientapp/src/shared/detect-automatic-rotation.ts
+++ starsky/starsky/clientapp/src/shared/detect-automatic-rotation.ts
@@ -1,5 +1,6 @@
-import detectAutomaticRotationJpeg from '../style/images/detect-automatic-rotation.jpg';
 // use the: IMAGE_INLINE_SIZE_LIMIT=1 due the fact that data: are not supported by the CSP
+import { BrowserDetect } from '../shared/browser-detect';
+import detectAutomaticRotationJpeg from '../style/images/detect-automatic-rotation.jpg';
 
 /**
  * Black 2x1 JPEG, with the following meta information set:
@@ -16,9 +17,17 @@ export const testAutoOrientationImageURL = 'data:image/jpeg;base64,/9j/4QAiRXhpZ
 
 /**
  * check if browser supports automatic image orientation
- * Supported: Chrome 81+ and Safari (Desktop) 13.1+ and Safari Mobile (?)
+ * Supported: Chrome 81+ and Safari (Desktop) 13.1+ and Safari Mobile 13+
+ * Hacked/Supported though BrowserDetect hack: Safari on iOS 12 and lower
  */
 const DetectAutomaticRotation = async (): Promise<boolean> => {
+
+  // iOS 12 and lower don't update the img.width but does rotate the image.
+  // this BrowserDetect method detect if the browser is iOS or iPad OS
+  if (new BrowserDetect().IsIOS()) {
+    return true;
+  }
+
   return new Promise((resolve) => {
     const img = new Image();
     img.onload = () => {
@@ -29,5 +38,36 @@ const DetectAutomaticRotation = async (): Promise<boolean> => {
     img.src = detectAutomaticRotationJpeg;
   });
 };
+
+
+/*
+Exif orientation values to correctly display the letter F:
+
+  1             2
+██████        ██████
+██                ██
+████            ████
+██                ██
+██                ██
+
+  3             4
+    ██        ██
+    ██        ██
+  ████        ████
+    ██        ██
+██████        ██████
+
+  5             6
+██████████    ██
+██  ██        ██  ██
+██            ██████████
+
+  7             8
+      ██    ██████████
+  ██  ██        ██  ██
+██████████            ██
+
+*/
+
 
 export default DetectAutomaticRotation;
