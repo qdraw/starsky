13be365bf051a6152d3b38d618d56981da77c8a5
Dion <dionvanvelde@gmail.com>
2020-09-16 20:40:04 +0200

    should this works?

--- starsky/docker_demo_setup.sh
+++ starsky/docker_demo_setup.sh
@@ -1,7 +1,24 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+pushd $DIR
+
+function makeDemoUser {
+  dotnet run --project ../starsky/starskyadmincli/starskyadmincli.csproj --name demo@qdraw.nl --password demopassword
+}
+
+function getSampleImages {
+  mkdir -p starsky/bin/Release/netcoreapp*/storageFolder
+  curl https://media.qdraw.nl/download/starsky-sample-photos.zip --output starsky/bin/Release/netcoreapp*/storageFolder/sample-photos.zip
+  unzip starsky/bin/Release/netcoreapp*/storageFolder/sample-photos.zip
+}
 
 if [ -z "$E_ISDEMO" ]; then
     echo "NO PARAM PASSED"
+    makeDemoUser
 else
     echo $E_ISDEMO
 fi
+
+popd
