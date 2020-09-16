989df14b98f6343dc641db83d1fe7611dc7d6e80
Dion <dionvanvelde@gmail.com>
2020-09-16 21:52:07 +0200

    rename function add extra debug info

--- starsky/docker_demo_setup.sh
+++ starsky/docker_demo_setup.sh
@@ -6,11 +6,17 @@ pushd $DIR
 
 function makeDemoUser {
   mylist=($(find . -type f -name "starskyadmincli.csproj"))
+  dotnet run --project ${mylist[0]} -h -v
   dotnet run --project ${mylist[0]} --name demo@qdraw.nl --password demopassword
 }
 
-function getSampleImages {
+function getSamplePhotos {
+  ls
+
   ls starsky/bin
+  echo "-"
+  ls starsky/bin
+
   mkdir -p starsky/bin/Release/netcoreapp*/storageFolder
   curl https://media.qdraw.nl/download/starsky-sample-photos.zip --output starsky/bin/Release/netcoreapp*/storageFolder/sample-photos.zip
   unzip starsky/bin/Release/netcoreapp*/storageFolder/sample-photos.zip
@@ -19,7 +25,7 @@ function getSampleImages {
 if [ -z "$E_ISDEMO" ]; then
     echo "NO PARAM PASSED"
     makeDemoUser
-    getSampleImages
+    getSamplePhotos
 else
     echo $E_ISDEMO
 fi
