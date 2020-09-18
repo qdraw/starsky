dbac6eac1d55ba4b8cc3ee9fcbd4ccdb4d224eb5
Dion <dionvanvelde@gmail.com>
2020-09-18 17:12:15 +0200

    use args and hard paths

--- starsky/docker_demo_setup.sh
+++ starsky/docker_demo_setup.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-pushd $DIR
-
-echo "go to: "$DIR
+APPLICATION_DIR=/app/
+STORAGE_FOLDER=/app/storageFolder
 
 function makeDemoUser {
   starskyadmincli=($(find . -type f -name "starskyadmincli.csproj"))
-  dotnet run --configuration Release --project ${starskyadmincli[0]} -- --verbose --help
-  dotnet run --configuration Release --project ${starskyadmincli[0]} -- --name demo@qdraw.nl --password demopassword
+  dotnet run --configuration Release --project ${starskyadmincli[0]} -- --connection "Data Source="$APPLICATION_DIR"/app__data.db" --basepath $STORAGE_FOLDER --verbose --help
+  dotnet run --configuration Release --project ${starskyadmincli[0]} -- --connection "Data Source="$APPLICATION_DIR"/app__data.db" --basepath $STORAGE_FOLDER --name demo@qdraw.nl --password demo@qdraw.nl
 }
 
 function getSamplePhotos {
-  ls
-
-  ls starsky/bin
-  echo "-"
-  ls starsky/bin
-
-  mkdir -p out/storageFolder
-  curl https://media.qdraw.nl/download/starsky-sample-photos/20190530_134303_DSC00279_e.jpg --output out/storageFolder/20190530_134303_DSC00279_e.jpg
-  curl https://media.qdraw.nl/download/starsky-sample-photos/20190530_142906_DSC00373_e.jpg --output out/storageFolder/20190530_142906_DSC00373_e.jpg
+  mkdir -p $STORAGE_FOLDER
+  curl https://media.qdraw.nl/download/starsky-sample-photos/20190530_134303_DSC00279_e.jpg --output $STORAGE_FOLDER"/20190530_134303_DSC00279_e.jpg"
+  curl https://media.qdraw.nl/download/starsky-sample-photos/20190530_142906_DSC00373_e.jpg --output $STORAGE_FOLDER"/20190530_142906_DSC00373_e.jpg"
 
   starskysynccli=($(find . -type f -name "starskysynccli.csproj"))
-  dotnet run --configuration Release --project ${starskysynccli[0]} -- --verbose --help
-  dotnet run --configuration Release --project ${starskysynccli[0]} -- -s /20190530_134303_DSC00279_e.jpg
-  dotnet run --configuration Release --project ${starskysynccli[0]} -- -s /20190530_142906_DSC00373_e.jpg
+  dotnet run --configuration Release --project ${starskysynccli[0]} -- --basepath $STORAGE_FOLDER --connection "Data Source="$APPLICATION_DIR"/app__data.db" --verbose --help
+  dotnet run --configuration Release --project ${starskysynccli[0]} -- --basepath $STORAGE_FOLDER --connection "Data Source="$APPLICATION_DIR"/app__data.db" -s /20190530_134303_DSC00279_e.jpg
+  dotnet run --configuration Release --project ${starskysynccli[0]} -- --basepath $STORAGE_FOLDER --connection "Data Source="$APPLICATION_DIR"/app__data.db" -s /20190530_142906_DSC00373_e.jpg
+}
+
+function start_pushd {
+  echo "go to: "$APPLICATION_DIR
+  mkdir -p $APPLICATION_DIR
+  pushd $APPLICATION_DIR
+}
+
+function  end_popd {
+  popd
 }
 
 if [ -z "$E_ISDEMO" ]; then
     echo "NO PARAM PASSED"
+    start_pushd
     makeDemoUser
     getSamplePhotos
+    end_popd
 else
     echo $E_ISDEMO
 fi
-
-popd
