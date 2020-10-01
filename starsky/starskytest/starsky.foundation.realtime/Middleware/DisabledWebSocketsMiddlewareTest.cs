781444a7f103d25d2b30ed16b09fdab102d068b1
Dion <dionvanvelde@gmail.com>
2020-10-01 22:03:41 +0200

    move to seperate class ++ add test

--- starsky/starskytest/starsky.foundation.realtime/Middleware/DisabledWebSocketsMiddlewareTest.cs
+++ starsky/starskytest/starsky.foundation.realtime/Middleware/DisabledWebSocketsMiddlewareTest.cs
@@ -1,7 +1,12 @@
+using System.Linq;
+using System.Net.WebSockets;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.realtime.Middleware;
+using starsky.foundation.realtime.Model;
+using starsky.foundation.realtime.Services;
+using starskytest.FakeMocks;
 
 namespace starskytest.starsky.foundation.realtime.Middleware
 {
@@ -14,7 +19,19 @@ namespace starskytest.starsky.foundation.realtime.Middleware
 			var httpContext = new DefaultHttpContext();
 			var disabledWebSocketsMiddleware = new DisabledWebSocketsMiddleware(next: (innerHttpContext) => Task.FromResult(0));
 			await disabledWebSocketsMiddleware.Invoke(httpContext);
-			Assert.AreEqual(204,httpContext.Response.StatusCode);
+			Assert.AreEqual(400,httpContext.Response.StatusCode);
+		}
+		[TestMethod]
+		public async Task WebSocketConnection_MessageTooBig()
+		{
+			var httpContext = new FakeWebSocketHttpContext(false);
+
+			var disabledWebSocketsMiddleware = new DisabledWebSocketsMiddleware(next: (innerHttpContext) => Task.FromResult(0));
+			await disabledWebSocketsMiddleware.Invoke(httpContext);
+			
+			var socketManager = httpContext.WebSockets as FakeWebSocketManager;
+			Assert.AreEqual(WebSocketCloseStatus.MessageTooBig, 
+				(socketManager.FakeWebSocket as FakeWebSocket).FakeCloseOutputAsync.LastOrDefault());
 		}
 	}
 }
--- starsky/starskytest/starsky.foundation.realtime/Middleware/WebSocketConnectionsMiddlewareTest.cs
+++ starsky/starskytest/starsky.foundation.realtime/Middleware/WebSocketConnectionsMiddlewareTest.cs
@@ -47,52 +47,6 @@ namespace starskytest.starsky.foundation.realtime.Middleware
 			Assert.AreEqual(400,httpContext.Response.StatusCode);
 		}
 		
-		private class FakeWebSocketHttpContext : HttpContext
-		{
-			public FakeWebSocketHttpContext(bool userLoggedIn = true)
-			{
-				WebSockets = new FakeWebSocketManager(true);
-				if ( userLoggedIn )
-				{
-					User = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>(), "Test"));
-					return;
-				}
-				User = new ClaimsPrincipal(new ClaimsIdentity());
-				Response = new DefaultHttpContext().Response;
-			}
-			public override void Abort() { }
-			public override ConnectionInfo Connection { get; }
-			public override IFeatureCollection Features { get; }
-			public override IDictionary<object, object> Items { get; set; }
-			public override HttpRequest Request { get; }
-			public override CancellationToken RequestAborted { get; set; }
-			public override IServiceProvider RequestServices { get; set; }
-			public override HttpResponse Response { get; }
-			public override ISession Session { get; set; }
-			public override string TraceIdentifier { get; set; }
-			public sealed override ClaimsPrincipal User { get; set; }
-			public override WebSocketManager WebSockets { get; }
-		}
-		private class FakeWebSocketManager : WebSocketManager
-		{
-			public FakeWebSocketManager(bool isWebSocketRequest)
-			{
-				IsWebSocketRequest = isWebSocketRequest;
-			}
-			public WebSocket FakeWebSocket { get; set; } = new FakeWebSocket();
-			
-#pragma warning disable 1998
-			// ReSharper disable once ArrangeModifiersOrder
-			public async override Task<WebSocket> AcceptWebSocketAsync(string subProtocol)
-#pragma warning restore 1998
-			{
-				return FakeWebSocket;
-			}
-
-			public override bool IsWebSocketRequest { get; }
-			public override IList<string> WebSocketRequestedProtocols { get; }
-		}
-		
 		[TestMethod]
 		public async Task WebSocketConnection()
 		{
