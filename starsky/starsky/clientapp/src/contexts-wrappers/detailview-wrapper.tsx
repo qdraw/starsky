26c8dc814c4459224748dcfa67665443f40c75ff
Dion <dionvanvelde@gmail.com>
2020-10-12 22:15:10 +0200

    add addEventListener in DetailViewWrapper

--- starsky/starsky/clientapp/src/contexts-wrappers/detailview-wrapper.tsx
+++ starsky/starsky/clientapp/src/contexts-wrappers/detailview-wrapper.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react';
 import DetailView from '../containers/detailview';
-import { DetailViewContextProvider, useDetailViewContext } from '../contexts/detailview-context';
+import { DetailViewAction, DetailViewContextProvider, useDetailViewContext } from '../contexts/detailview-context';
+import { useSocketsEventName } from '../hooks/realtime/use-sockets.const';
 import { IDetailView } from '../interfaces/IDetailView';
+import { IFileIndexItem } from '../interfaces/IFileIndexItem';
 import DocumentTitle from '../shared/document-title';
+import { URLPath } from '../shared/url-path';
 
 /**
  * Used for search and list of files
  * @param detailview Detailview content
  */
-function DetailViewContextWrapper(detailview: IDetailView) {
+export default function DetailViewContextWrapper(detailview: IDetailView) {
   return (<DetailViewContextProvider>
     <DetailViewWrapper {...detailview} />
   </DetailViewContextProvider>)
@@ -33,7 +36,27 @@ function DetailViewWrapper(detailViewProp: IDetailView) {
   if (!state) return (<>(DetailViewWrapper) = no state</>)
   if (!state.fileIndexItem) return (<></>);
 
+  // Catch events from updates
+  const update = (event: Event) => updateDetailViewFromEvent(event, dispatch);
+  useEffect(() => {
+    document.body.addEventListener(useSocketsEventName, update);
+    return () => {
+      document.body.removeEventListener(useSocketsEventName, update);
+    };
+    // only when start of view
+    // eslint-disable-next-line
+  }, []);
+
   return (<DetailView {...state} />)
 }
 
-export default DetailViewContextWrapper;
+function updateDetailViewFromEvent(event: Event, dispatch: React.Dispatch<DetailViewAction>) {
+  const pushMessage = event as CustomEvent<IFileIndexItem>;
+
+  // useLocation, state or detailView is here always the default value
+  var locationPath = new URLPath().StringToIUrl(window.location.search).f
+  if (locationPath !== pushMessage.detail.filePath) {
+    return;
+  }
+  dispatch({ type: 'update', ...pushMessage.detail, colorclass: pushMessage.detail.colorClass });
+}
