966b83b19282e1d119797b340f7e122d13c5ab4d
Dion <dionvanvelde@gmail.com>
2020-10-16 11:06:31 +0200

    add pref username component

---
+++ starsky/starsky/clientapp/src/components/organisms/preferences-username/preferences-username.spec.tsx
@@ -0,0 +1,58 @@
+import { mount, shallow } from "enzyme";
+import React from 'react';
+import * as useFetch from '../../../hooks/use-fetch';
+import { newIConnectionDefault } from '../../../interfaces/IConnectionDefault';
+import { PreferencesUsername } from './preferences-username';
+
+describe("PreferencesPassword", () => {
+
+  it("renders", () => {
+    shallow(<PreferencesUsername />)
+  });
+
+  describe("context", () => {
+    it("Error 500", () => {
+
+      jest.spyOn(useFetch, 'default').mockImplementationOnce(() => {
+        return { ...newIConnectionDefault(), statusCode: 500 };
+      });
+
+      var component = mount(<PreferencesUsername />);
+      expect(component.find('.content--text').text()).toBe("Unknown username");
+      component.unmount();
+    });
+
+    it("Incomplete data", () => {
+      jest.spyOn(useFetch, 'default').mockImplementationOnce(() => {
+        return {
+          ...newIConnectionDefault(), data: {
+            credentials: []
+          }, statusCode: 200
+        };
+      });
+
+      var component = mount(<PreferencesUsername />);
+      expect(component.find('.content--text').text()).toBe("Unknown username");
+      component.unmount();
+    });
+
+    it("it should get the identifier", () => {
+      var testReply = {
+        ...newIConnectionDefault(), data: {
+          credentials: [
+            {
+              identifier: 'test'
+            }
+          ]
+        }, statusCode: 200
+      };
+      jest.spyOn(useFetch, 'default')
+        .mockImplementationOnce(() => testReply)
+        .mockImplementationOnce(() => testReply)
+
+      var component = mount(<PreferencesUsername />);
+      expect(component.find('.content--text').text()).toBe("test")
+    });
+  });
+
+});
