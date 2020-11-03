5406af70ba0c9be153acbd487a71c627046d7fcd
Dion <dionvanvelde@gmail.com>
2020-11-03 21:09:57 +0100

    add test

--- starsky/starsky/clientapp/src/components/atoms/notification/notification.spec.tsx
+++ starsky/starsky/clientapp/src/components/atoms/notification/notification.spec.tsx
@@ -24,6 +24,18 @@ describe("ItemListView", () => {
       expect(document.getElementById(PortalId)?.innerHTML).toBeUndefined()
     });
 
+    it("Portal is already gone", () => {
+      var component = mount(<Notification type={NotificationType.default} />);
+
+      var portalElement = document.getElementById(PortalId)
+      if (!portalElement) throw new Error("portal should not be undefined");
+      portalElement.remove();
+
+      component.find('.icon--close').simulate('click');
+
+      expect(document.getElementById(PortalId)?.innerHTML).toBeUndefined()
+    });
+
     it("Callback test Ok close and remove element from DOM", () => {
       var callback = jest.fn();
       var component = mount(<Notification callback={callback} type={NotificationType.default} />);
