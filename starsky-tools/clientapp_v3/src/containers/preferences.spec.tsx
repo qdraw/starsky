d5e1464fdb442b3d34ff020ff61332a1f33cb6ab
Dion <dionvanvelde@gmail.com>
2020-11-30 16:58:48 +0100

    fix

--- starsky-tools/clientapp_v3/src/containers/preferences.spec.tsx
+++ starsky-tools/clientapp_v3/src/containers/preferences.spec.tsx
@@ -1,29 +1,34 @@
 import { mount, shallow } from "enzyme";
-import React from 'react';
-import * as PreferencesAppSettings from '../components/organisms/preferences-app-settings/preferences-app-settings';
-import * as PreferencesPassword from '../components/organisms/preferences-password/preferences-password';
-import * as PreferencesUsername from '../components/organisms/preferences-username/preferences-username';
-import { Preferences } from './preferences';
+import React from "react";
+import * as PreferencesAppSettings from "../components/organisms/preferences-app-settings/preferences-app-settings";
+import * as PreferencesPassword from "../components/organisms/preferences-password/preferences-password";
+import * as PreferencesUsername from "../components/organisms/preferences-username/preferences-username";
+import { Preferences } from "./preferences";
 
 describe("Preferences", () => {
-  it("renders", () => {
-    shallow(<Preferences />)
-  });
+	it("renders", () => {
+		shallow(<Preferences />);
+	});
 
-  describe("status", () => {
-    it("it should mount child components", () => {
+	describe("status", () => {
+		it("should mount child components", () => {
+			var preferencesUsernameSpy = jest
+				.spyOn(PreferencesUsername, "default")
+				.mockImplementationOnce(() => <></>);
+			var preferencesPasswordSpy = jest
+				.spyOn(PreferencesPassword, "default")
+				.mockImplementationOnce(() => <></>);
+			var preferencesAppSettingsSpy = jest
+				.spyOn(PreferencesAppSettings, "default")
+				.mockImplementationOnce(() => <></>);
 
-      var preferencesUsernameSpy = jest.spyOn(PreferencesUsername, 'default').mockImplementationOnce(() => <></>);
-      var preferencesPasswordSpy = jest.spyOn(PreferencesPassword, 'default').mockImplementationOnce(() => <></>);
-      var preferencesAppSettingsSpy = jest.spyOn(PreferencesAppSettings, 'default').mockImplementationOnce(() => <></>);
+			var component = mount(<Preferences />);
 
-      var component = mount(<Preferences />);
+			expect(preferencesUsernameSpy).toBeCalled();
+			expect(preferencesPasswordSpy).toBeCalled();
+			expect(preferencesAppSettingsSpy).toBeCalled();
 
-      expect(preferencesUsernameSpy).toBeCalled();
-      expect(preferencesPasswordSpy).toBeCalled();
-      expect(preferencesAppSettingsSpy).toBeCalled();
-
-      component.unmount();
-    });
-  });
+			component.unmount();
+		});
+	});
 });
