971c88f71d40fcf2d20b005e12475327810ee36f
Dion <dionvanvelde@gmail.com>
2020-12-08 16:02:55 +0100

    callback test

--- starsky/starsky/clientapp/src/containers/media-content.spec.tsx
+++ starsky/starsky/clientapp/src/containers/media-content.spec.tsx
@@ -1,8 +1,11 @@
 import { mount, shallow } from "enzyme";
 import React from "react";
+import * as Notification from "../components/atoms/notification/notification";
 import * as HealthStatusError from "../components/molecules/health-status-error/health-status-error";
 import * as useSockets from "../hooks/realtime/use-sockets";
 import * as useFileList from "../hooks/use-filelist";
+import { newIArchive } from "../interfaces/IArchive";
+import { PageType } from "../interfaces/IDetailView";
 import MediaContent from "./media-content";
 
 describe("MediaContent", () => {
@@ -22,9 +25,42 @@ describe("MediaContent", () => {
     // use ==> import * as HealthStatusError from '../components/health-status-error';
     jest.spyOn(HealthStatusError, "default").mockImplementationOnce(() => null);
 
-    var result = mount(<MediaContent />);
-    expect(result.html()).toBe(
+    const component = mount(<MediaContent />);
+    expect(component.html()).toBe(
       "<br>The application has failed. Please reload it to try it again"
     );
+    component.unmount();
+  });
+
+  it("when callback it should close issue", () => {
+    jest.spyOn(useFileList, "default").mockImplementationOnce(() => {
+      return {
+        archive: { ...newIArchive() },
+        pageType: PageType.Loading
+      } as any;
+    });
+
+    // import * as useSockets from "../hooks/realtime/use-sockets";
+    const setShowSocketErrorSpy = jest.fn();
+    jest.spyOn(useSockets, "default").mockImplementationOnce(() => {
+      return {
+        showSocketError: true,
+        setShowSocketError: setShowSocketErrorSpy
+      } as useSockets.IUseSockets;
+    });
+
+    jest.spyOn(HealthStatusError, "default").mockImplementationOnce(() => null);
+
+    jest.spyOn(Notification, "default").mockImplementationOnce((props) => {
+      if (props.callback) {
+        props.callback();
+      }
+      return <div className="test">testung</div>;
+    });
+
+    const component = mount(<MediaContent />);
+
+    expect(setShowSocketErrorSpy).toBeCalled();
+    component.unmount();
   });
 });
