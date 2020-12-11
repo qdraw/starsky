f728f6caa3ae3897bbcf879c3b5a881ebb422fb7
Dion <dionvanvelde@gmail.com>
2020-12-11 17:20:00 +0100

    add test

--- starskyapp/src/app/child-process/setup-child-process.spec.ts
+++ starskyapp/src/app/child-process/setup-child-process.spec.ts
@@ -1,9 +1,38 @@
+import * as spawn from "child_process";
+import { app } from "electron";
 import { setupChildProcess } from "./setup-child-process";
+
+jest.mock("electron", () => {
+  return {
+    app: {
+      getVersion: () => "99.99.99",
+      getPath: () => "tmp",
+      getLocale: () => "en",
+      on: () => "en"
+    },
+    net: {
+      request: () => {}
+    }
+  };
+});
 
 describe("setupChildProcess", () => {
   describe("setupChildProcess", () => {
     it("getting with null input", () => {
+      jest
+        .spyOn(spawn, "spawn")
+        .mockImplementationOnce(
+          () =>
+            ({ stdout: { on: jest.fn() }, stderr: { on: jest.fn() } } as any)
+        );
+
+      jest.spyOn(app, "on").mockImplementationOnce((event) => {
+        console.log(event);
+
+        return null;
+      });
       setupChildProcess();
+      //
     });
   });
 });
