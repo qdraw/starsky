27b5eba0b18868d4368098d63383099f54eba26a
Dion <dionvanvelde@gmail.com>
2020-12-12 13:41:23 +0100

    add test

--- starskyapp/src/client/script/reload-warmup-script.spec.ts
+++ starskyapp/src/client/script/reload-warmup-script.spec.ts
@@ -1,17 +1,48 @@
-describe("reload redirect", () => {
-  it("...", () => {
-    const mockJsonPromise = Promise.resolve("Health");
-    const mockFetchPromise = Promise.resolve({
-      statusCode: 200,
-      text: () => {
-        console.log("--t");
+import { warmupScript } from "./reload-warmup-script";
 
-        return mockJsonPromise;
-      }
+describe("reload redirect", () => {
+  function mockFetch(status: number) {
+    const mockFetchPromise = Promise.resolve({
+      status: status,
+      text: () => Promise.resolve("Health")
     });
     window.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+  }
 
+  function mockFetchReject() {
+    const mockFetchPromise = Promise.reject();
+    window.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+  }
+
+  afterEach(() => {
     (global.fetch as any).mockClear();
     delete global.fetch;
+  });
+
+  it("should success", (done) => {
+    mockFetch(200);
+
+    warmupScript("", 0, 2, (result: boolean) => {
+      expect(result).toBeTruthy();
+      done();
+    });
+  });
+
+  it("should fail due status", (done) => {
+    mockFetch(500);
+
+    warmupScript("", 0, 0, (result: boolean) => {
+      expect(result).toBeFalsy();
+      done();
+    });
+  });
+
+  it("should fail due rejection", (done) => {
+    mockFetchReject();
+
+    warmupScript("", 0, 0, (result: boolean) => {
+      expect(result).toBeFalsy();
+      done();
+    });
   });
 });
