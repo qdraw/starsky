796a226b7e2fee42d954879d09d12c3ff8ab9bcd
Dion <dionvanvelde@gmail.com>
2020-12-14 12:12:47 +0100

    WIP on net request

---
+++ starskyapp/src/app/check-for-updates-window/get-net-request.ts
@@ -0,0 +1,43 @@
+import { net } from "electron";
+
+export interface IGetNetRequestResponse {
+  data?: any;
+  error?: any;
+  statusCode: number;
+}
+
+function GetNetRequest(url: string): Promise<IGetNetRequestResponse> {
+  return new Promise(function (resolve, reject) {
+    const request = net.request({
+      url,
+      headers: {
+        Accept: "*/*"
+      }
+    } as any);
+
+    let body = "";
+    request.on("response", (response) => {
+      console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+
+      response.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      response.on("end", () => {
+        try {
+          resolve({
+            data: JSON.parse(body),
+            statusCode: response.statusCode
+          });
+        } catch (error) {
+          reject({ error, statusCode: response.statusCode });
+        }
+      });
+    });
+
+    request.on("error", (error) => {
+      reject({ error: error, statusCode: 999 });
+    });
+
+    request.end();
+  });
+}
--- starskyapp/src/app/config/update-policy-settings.const.ts
+++ starskyapp/src/app/config/update-policy-settings.const.ts
@@ -1,8 +1,13 @@
 import { isPackaged } from "../os-info/is-packaged";
 import { UpdatePolicyIpcKey } from "./update-policy-ipc-key.const";
-
+/**
+ * Is the feature enabled
+ */
 export const UpdatePolicySettings = `${UpdatePolicyIpcKey}:${isPackaged()}`;
 
 const LastCheckedDate = `UPDATE_POLICY_LAST_CHECKED_DATE`;
 
+/**
+ * When is it checked for the last time
+ */
 export const LastCheckedDateSettings = `${LastCheckedDate}:${isPackaged()}`;
