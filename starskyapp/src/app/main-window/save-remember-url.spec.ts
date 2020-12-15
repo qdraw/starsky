a4b91b00c93c1d2fa1babd48e84702f1a15faad2
Dion <dionvanvelde@gmail.com>
2020-12-15 20:31:42 +0100

    add remember options

--- starskyapp/src/app/main-window/save-remember-url.spec.ts
+++ starskyapp/src/app/main-window/save-remember-url.spec.ts
@@ -1,54 +1,94 @@
 import * as appConfig from "electron-settings";
 import RememberUrl from "../config/remember-url-settings.const";
-import { saveRememberUrl } from "./save-remember-url";
+import { removeRememberUrl, saveRememberUrl } from "./save-remember-url";
 
 describe("save remember url", () => {
-  it("new situation", async () => {
-    jest.spyOn(appConfig, "get").mockImplementationOnce(() => {
-      return Promise.resolve(undefined);
-    });
-
-    const appSettingsSetSpy = jest
-      .spyOn(appConfig, "set")
-      .mockImplementationOnce(() => {
+  describe("saveRememberUrl", () => {
+    it("new situation", async () => {
+      jest.spyOn(appConfig, "get").mockImplementationOnce(() => {
         return Promise.resolve(undefined);
       });
 
-    await saveRememberUrl({
-      id: 1,
-      webContents: {
-        getURL: () => {
-          return "https://google.com/?f=t";
-        }
-      }
-    } as any);
+      const appSettingsSetSpy = jest
+        .spyOn(appConfig, "set")
+        .mockImplementationOnce(() => {
+          return Promise.resolve(undefined);
+        });
 
-    expect(appSettingsSetSpy).toBeCalled();
-    expect(appSettingsSetSpy).toBeCalledWith(RememberUrl, { "1": "?f=t" });
+      await saveRememberUrl({
+        id: 1,
+        webContents: {
+          getURL: () => {
+            return "https://google.com/?f=t";
+          }
+        }
+      } as any);
+
+      expect(appSettingsSetSpy).toBeCalled();
+      expect(appSettingsSetSpy).toBeCalledWith(RememberUrl, { "1": "?f=t" });
+    });
+
+    it("should merge ", async () => {
+      jest.spyOn(appConfig, "get").mockImplementationOnce(() => {
+        return Promise.resolve({ 0: "test" });
+      });
+
+      const appSettingsSetSpy = jest
+        .spyOn(appConfig, "set")
+        .mockReset()
+        .mockImplementationOnce(() => {
+          return Promise.resolve(undefined);
+        });
+
+      await saveRememberUrl({
+        id: 1,
+        webContents: {
+          getURL: () => {
+            return "https://google.com/?f=t";
+          }
+        }
+      } as any);
+
+      expect(appSettingsSetSpy).toBeCalled();
+      expect(appSettingsSetSpy).toBeCalledWith(RememberUrl, {
+        "0": "test",
+        "1": "?f=t"
+      });
+    });
   });
-
-  it("should merge ", async () => {
-    jest.spyOn(appConfig, "get").mockImplementationOnce(() => {
-      return Promise.resolve({ 0: "test" });
-    });
-
-    const appSettingsSetSpy = jest
-      .spyOn(appConfig, "set")
-      .mockReset()
-      .mockImplementationOnce(() => {
-        return Promise.resolve(undefined);
+  describe("removeRememberUrl", () => {
+    it("should remove it from list", async () => {
+      jest.spyOn(appConfig, "get").mockImplementationOnce(() => {
+        return Promise.resolve({ 0: "test" });
       });
 
-    await saveRememberUrl({
-      id: 1,
-      webContents: {
-        getURL: () => {
-          return "https://google.com/?f=t";
-        }
-      }
-    } as any);
+      const appSettingsSetSpy = jest
+        .spyOn(appConfig, "set")
+        .mockReset()
+        .mockImplementationOnce(() => {
+          return Promise.resolve(undefined);
+        });
 
-    expect(appSettingsSetSpy).toBeCalled();
-    expect(appSettingsSetSpy).toBeCalledWith(RememberUrl, { "1": "?f=t" });
+      await removeRememberUrl({ id: 0 } as any);
+      expect(appSettingsSetSpy).toBeCalled();
+      expect(appSettingsSetSpy).toBeCalledWith(RememberUrl, {});
+    });
+
+    it("should not existing it from list", async () => {
+      jest.spyOn(appConfig, "get").mockImplementationOnce(() => {
+        return Promise.resolve({});
+      });
+
+      const appSettingsSetSpy = jest
+        .spyOn(appConfig, "set")
+        .mockReset()
+        .mockImplementationOnce(() => {
+          return Promise.resolve(undefined);
+        });
+
+      await removeRememberUrl({ id: 0 } as any);
+      expect(appSettingsSetSpy).toBeCalled();
+      expect(appSettingsSetSpy).toBeCalledWith(RememberUrl, {});
+    });
   });
 });
