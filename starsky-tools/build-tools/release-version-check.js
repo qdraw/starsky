c63e787e0d426829bd28a10be628a36a59793e35
Dion <dionvanvelde@gmail.com>
2020-12-17 15:54:07 +0100

    WIP code

--- starsky-tools/build-tools/release-version-check.js
+++ starsky-tools/build-tools/release-version-check.js
@@ -5,25 +5,41 @@ const path = require('path');
 
 function releaseVersionCheck() {
 
-// export GITHUB_REF=refs/tags/v0.3.0
+  // export GITHUB_REF=refs/tags/v0.3.0
   if (process.env.GITHUB_REF && process.env.GITHUB_REF.startsWith('refs/tags/v')) {
     const refVersion = process.env.GITHUB_REF.replace('refs/tags/v',"")
     if (refVersion !== packageJson.version) {
-      const r = spawn('node', [path.join(__dirname, 'app-version-update.js'), refVersion]);
-      r.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-      });
-
-      r.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-      ls.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-      });
-
+      runChildUpdate(refVersion);
+      return;
     }
-    console.log();
   }
-  console.log();
+
+  // ADO https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml
+  if (process.env["Build.SourceBranch"] && process.env["Build.SourceBranch"].startsWith('refs/heads/release/v' ) {
+    console.log('=> release branches should not start with a v');
+    process.exit(1);
+  }
+
+  // export Build.SourceBranch=refs/heads/master
+  if (process.env["Build.SourceBranch"]) {
+    console.log(process.env["Build.SourceBranch"]);
+  }
 }
+
 releaseVersionCheck();
+
+runChildUpdate(refChildVersion) {
+  const appVersionSpawn = spawn('node', [path.join(__dirname, 'app-version-update.js'), refChildVersion]);
+  appVersionSpawn.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  appVersionSpawn.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  appVersionSpawn.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+    process.exit(code);
+  });
+}
