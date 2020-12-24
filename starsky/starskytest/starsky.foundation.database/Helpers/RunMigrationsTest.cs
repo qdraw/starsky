e4b2a7a58ccad238bc357dfdeb73ac58301f719d
Dion <dionvanvelde@gmail.com>
2020-12-24 19:12:09 +0100

    add test

--- starsky/starskytest/starsky.foundation.database/Helpers/RunMigrationsTest.cs
+++ starsky/starskytest/starsky.foundation.database/Helpers/RunMigrationsTest.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
 using starsky.foundation.database.Data;
 using starsky.foundation.database.Helpers;
 
@@ -25,6 +30,58 @@ namespace starskytest.starsky.foundation.database.Helpers
 
 			await RunMigrations.Run(serviceScopeFactory.CreateScope());
 			// expect exception: Relational-specific methods can only be used when the context is using a relational database provider.
+		}
+
+		private class AppDbMySqlException : ApplicationDbContext
+		{
+			public AppDbMySqlException(DbContextOptions options) : base(options)
+			{
+			}
+
+			public override DatabaseFacade Database
+			{
+				get
+				{
+					var info = new SerializationInfo(typeof(Exception),
+						new FormatterConverter());
+					info.AddValue("Number", 1);
+					info.AddValue("SqlState", "SqlState");
+					info.AddValue("Message", "");
+					info.AddValue("InnerException", new Exception());
+					info.AddValue("HelpURL", "");
+					info.AddValue("StackTraceString", "");
+					info.AddValue("RemoteStackTraceString", "");
+					info.AddValue("RemoteStackIndex", 1);
+					info.AddValue("HResult", 1);
+					info.AddValue("Source", "");
+					info.AddValue("WatsonBuckets", new byte[0]);
+					
+					// private MySqlException(SerializationInfo info, StreamingContext context)
+					var ctor =
+						typeof(MySqlException).GetConstructors(BindingFlags.Instance |
+							BindingFlags.NonPublic | BindingFlags.InvokeMethod).FirstOrDefault();
+					var instance =
+						( MySqlException ) ctor.Invoke(new object[]
+						{
+							info,
+							new StreamingContext(StreamingContextStates.All)
+						});
+
+					throw instance;
+				}
+			}
+
+		}
+
+		[TestMethod]
+		public async Task MySqlException()
+		{
+			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+				.UseInMemoryDatabase(databaseName: "MovieListDatabase")
+				.Options;
+			await RunMigrations.Run(new AppDbMySqlException(options));
+			
+			// should not crash
 		}
 	}
 }
