7b997179a62b2afc977e14709e06322829a08a60
Dion <dionvanvelde@gmail.com>
2020-12-24 18:31:36 +0100

    add test

--- starsky/starskytest/FakeMocks/FakeIGeoReverseLookup.cs
+++ starsky/starskytest/FakeMocks/FakeIGeoReverseLookup.cs
@@ -6,11 +6,19 @@ namespace starskytest.FakeMocks
 {
 	public class FakeIGeoReverseLookup : IGeoReverseLookup
 	{
+		private readonly List<FileIndexItem> _fileIndexItems = new List<FileIndexItem>();
+
+		public FakeIGeoReverseLookup(List<FileIndexItem> fileIndexItems = null)
+		{
+			if ( fileIndexItems != null ) _fileIndexItems = fileIndexItems;
+		}
+
 		public int Count { get; set; }
 
 		public List<FileIndexItem> LoopFolderLookup(List<FileIndexItem> metaFilesInDirectory, bool overwriteLocationNames)
 		{
 			Count++;
+			metaFilesInDirectory.AddRange(_fileIndexItems);
 			return metaFilesInDirectory;
 		}
 	}
