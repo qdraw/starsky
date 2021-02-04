4e18aef1a03de8bc66eed0cdd48efdf2c7ea6f10
Dion <dionvanvelde@gmail.com>
2021-02-04 19:05:22 +0100

    add delete test for files && folders

--- starsky-tools/end2end/cypress/integration/10-upload-to-folder/10-upload-to-folder.ts
+++ starsky-tools/end2end/cypress/integration/10-upload-to-folder/10-upload-to-folder.ts
@@ -1,6 +1,7 @@
 import { envName, envFolder } from '../../support/commands'
 import configFile from './config.json'
 import { checkIfExistAndCreate } from '../helpers/create-directory-helper'
+import { uploadFileName1 } from './upload-filename1'
 const config = configFile[envFolder][envName]
 
 describe('Upload to folder', () => {
@@ -41,23 +42,7 @@ describe('Upload to folder', () => {
   }, () => {
     if (!config.isEnabled) return
 
-    cy.visit(config.url)
-
-    cy.get('.item.item--more').click()
-    cy.get('.menu-option--input label')
-
-    const fileType = 'image/jpeg'
-    const fileInput = '.menu-option--input input[type=file]'
-
-    cy.uploadFile(fileName1, fileType, fileInput)
-    cy.wait(1000)
-
-    cy.get('[data-test=upload-files] li').should(($lis) => {
-      expect($lis).to.have.length(1)
-      expect($lis.eq(0)).to.contain(fileName1)
-    })
-
-    cy.get('.modal-exit-button').click()
+    uploadFileName1(config.url, fileName1)
   })
 
   it('Upload more content and check if the name exist', {
---
+++ starsky-tools/end2end/cypress/integration/10-upload-to-folder/upload-filename1.ts
@@ -0,0 +1,23 @@
+export function uploadFileName1 (url: string, fileName1: string, check = true) {
+  cy.visit(url)
+
+  cy.get('.item.item--more').click()
+  cy.get('.menu-option--input label')
+
+  const fileType = 'image/jpeg'
+  const fileInput = '.menu-option--input input[type=file]'
+
+  cy.uploadFile(fileName1, fileType, fileInput)
+
+  cy.wait(1000)
+
+  if (!check) {
+    return
+  }
+  cy.get('[data-test=upload-files] li').should(($lis) => {
+    expect($lis).to.have.length(1)
+    expect($lis.eq(0)).to.contain(fileName1)
+  })
+
+  cy.get('.modal-exit-button').click()
+}
