9aea12540cb18fa958d6f591f82bd9e96c4edb0b
Dion <dionvanvelde@gmail.com>
2021-03-24 16:16:17 +0100

    add test

--- starsky/starsky/clientapp/src/components/atoms/file-hash-image/pan-and-zoom-image.spec.tsx
+++ starsky/starsky/clientapp/src/components/atoms/file-hash-image/pan-and-zoom-image.spec.tsx
@@ -2,6 +2,7 @@ import { mount, shallow } from "enzyme";
 import React from "react";
 import { act } from "react-dom/test-utils";
 import { Orientation } from "../../../interfaces/IFileIndexItem";
+import { OnMoveMouseTouchAction } from "./on-move-mouse-touch-action";
 import { OnWheelMouseAction } from "./on-wheel-mouse-action";
 import PanAndZoomImage from "./pan-and-zoom-image";
 
@@ -56,6 +57,42 @@ describe("PanAndZoomImage", () => {
       expect(component.find(".pan-zoom-image-container").html()).toContain(
         "transform: translate(-291px"
       );
+
+      component.unmount();
+    });
+
+    it("expect trigger onTouchMove", () => {
+      const onWheelCallback = jest.fn();
+
+      const moveSpy = jest
+        .spyOn(OnMoveMouseTouchAction.prototype, "move")
+        .mockImplementationOnce(() => {});
+
+      const component = mount(
+        <PanAndZoomImage
+          src=""
+          setIsLoading={null as any}
+          translateRotation={Orientation.Horizontal}
+          onWheelCallback={onWheelCallback}
+          onResetCallback={jest.fn()}
+        />
+      );
+
+      let ev = new MouseEvent("mousemove", {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        clientX: 9,
+        clientY: 9
+      });
+
+      act(() => {
+        document.dispatchEvent(ev);
+      });
+      component.render();
+
+      expect(moveSpy).toBeCalled();
+      expect(moveSpy).toBeCalledWith(9, 9);
 
       component.unmount();
     });
