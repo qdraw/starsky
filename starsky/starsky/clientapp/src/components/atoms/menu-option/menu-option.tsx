908789cdb93f61dc6f630cb3c3a77e56b23ae134
Dion <dionvanvelde@gmail.com>
2021-03-29 19:31:46 +0200

    include file

---
+++ starsky/starsky/clientapp/src/components/atoms/menu-option/menu-option.tsx
@@ -0,0 +1,35 @@
+import React, { memo } from "react";
+import useGlobalSettings from "../../../hooks/use-global-settings";
+import { Language } from "../../../shared/language";
+
+interface IMenuOptionProps {
+  testName: string;
+  isSet: boolean;
+  set: React.Dispatch<React.SetStateAction<boolean>>;
+  nl: string;
+  en: string;
+}
+
+const MenuOption: React.FunctionComponent<IMenuOptionProps> = memo(
+  ({ nl, en, isSet, set, testName }) => {
+    const settings = useGlobalSettings();
+    const language = new Language(settings.language);
+    const Message = language.text(nl, en);
+
+    return (
+      <>
+        {
+          <li
+            data-test={testName}
+            className="menu-option"
+            onClick={() => set(!isSet)}
+          >
+            {Message}
+          </li>
+        }
+      </>
+    );
+  }
+);
+
+export default MenuOption;
