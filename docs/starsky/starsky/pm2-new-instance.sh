20a9a2bc110450152093d307566123710f7b4390
DocsUpdateBot <qdraw@users.noreply.github.com>
2021-03-30 15:48:55 +0000

    [DocsUpdateBot] Auto commited html update

--- docs/starsky/starsky/pm2-new-instance.sh
+++ docs/starsky/starsky/pm2-new-instance.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Script goals:
+# Uses starsky-$RUNTIME.zip to deploy and setup pm2 instance
+# When zip file does not exist, download it from github releases
+# overwrite pm2 name if already exist
+
 PM2NAME="starsky"
 RUNTIME="linux-arm"
 
@@ -15,6 +20,7 @@ for ((i = 1; i <= $#; i++ )); do
         echo "--name pm2name"
         echo "--runtime linux-arm"
         echo "(or:) --runtime linux-arm64"
+        echo "(optional) --appinsights true - to ask for app insights keys"
     fi
 
     if [[ ${ARGUMENTS[PREV]} == "--name" ]];
@@ -26,32 +32,67 @@ for ((i = 1; i <= $#; i++ )); do
     then
         RUNTIME="${ARGUMENTS[CURRENT]}"
     fi
+    
+    if [[ ${ARGUMENTS[PREV]} == "--appinsights" ]];
+    then
+        USEAPPINSIGHTS="${ARGUMENTS[CURRENT]}"
+    fi
   fi
 done
 
+if ! command -v pm2 &> /dev/null
+then
+    echo "warning: pm2 is missing, the script continues but skips the last step"
+fi
+
 # settings
-echo "--name " $PM2NAME " --runtime" $RUNTIME
+echo "run with the following parameters "
+echo "--name " $PM2NAME " --runtime" $RUNTIME " --appinsights" $USEAPPINSIGHTS
 
 cd "$(dirname "$0")"
 
 export ASPNETCORE_URLS="http://localhost:4823/"
 export ASPNETCORE_ENVIRONMENT="Production"
 
-echo "Copy the App Insights key string and press [ENTER]:"
-echo "for example: "
-echo "11111111-2222-3333-4444-555555555555"
-echo ">>>"
-read -p "Enter: " INSTRUMENTATIONKEY
-
-export APPINSIGHTS_INSTRUMENTATIONKEY=$INSTRUMENTATIONKEY
-
-if [ -f starsky-$RUNTIME.zip ]; then
-   unzip -o starsky-$RUNTIME.zip
-else
-   echo "> starsky-$RUNTIME.zip File not found"
+# only asked with --appinsights true parameter
+if [ ! -z "$USEAPPINSIGHTS" ];
+then
+    echo "Copy the App Insights key string and press [ENTER]:"
+    echo "for example: "
+    echo "11111111-2222-3333-4444-555555555555"
+    echo ">>>"
+    read -p "Enter: " INSTRUMENTATIONKEY
+    export APPINSIGHTS_INSTRUMENTATIONKEY=$INSTRUMENTATIONKEY
 fi
 
-# reset rights if those are wrong
+if [ -f starsky-$RUNTIME.zip ]; then
+   echo "upgrade existing zip file" 
+   echo "going to unzip starsky-$RUNTIME.zip"
+   
+    unzip -q -o starsky-$RUNTIME.zip -x "pm2-new-instance.sh"
+else
+   echo "continue > starsky-$RUNTIME.zip File not found" 
+   echo "try to download latest release"
+   
+   # Get latest stable from Github Releases
+   # check also 'install-latest-release'
+   curl -s https://api.github.com/repos/qdraw/starsky/releases/latest \
+   | grep "browser_download_url.*starsky-$RUNTIME.zip" \
+   | cut -d ":" -f 2,3 \
+   | tr -d \" \
+   | wget -qi -
+   
+   if [ -f starsky-$RUNTIME.zip ]; then
+      echo "use latest stable,"
+      echo "going to unzip starsky-$RUNTIME.zip"
+      unzip -q -o starsky-$RUNTIME.zip
+   else
+      echo "FAILED > starsky-$RUNTIME.zip Download failed; exit now"
+      exit 1
+   fi
+fi
+
+echo "reset rights if those are wrong"
 /usr/bin/find . -type d -exec chmod 755 {} \;
 /usr/bin/find . -type f -exec chmod 644 {} \;
 
@@ -93,16 +134,76 @@ if [ -f pm2-deploy-on-env.sh ]; then
     chmod +rwx ./pm2-deploy-on-env.sh
 fi
 
+if [ -f pm2-install-latest-release.sh ]; then
+    chmod +rwx ./pm2-install-latest-release.sh
+fi
+
+if [ -f pm2-restore-x-rights.sh ]; then
+    chmod +rwx ./pm2-restore-x-rights.sh
+fi
+
 if [ -f pm2-warmup.sh ]; then
     chmod +rwx ./pm2-warmup.sh
 fi
 
 if [ -f starsky ]; then
     chmod +rwx ./starsky
-
-    pm2 start --name $PM2NAME ./starsky
-    echo $PM2NAME " started"
-
-    pm2 status
-    echo "Need to run `pm2 save` yourself"
 fi
+
+if [ -f starskyimportercli ]; then
+    echo "run starskyimportercli to auto download dependencies"
+    ./starskyimportercli -h > /dev/null 2>&1
+fi
+
+if [ -f starskygeocli ]; then
+    echo "run starskygeocli to auto download dependencies"
+    ./starskygeocli -h > /dev/null 2>&1
+fi
+
+if ! command -v pm2 &> /dev/null
+then
+    echo "FAIL pm2 is missing run: "
+    echo "sudo npm install -g pm2"
+    echo "and run it again to add it to pm2"
+    exit 1
+fi
+
+if [ -f starsky ]; then
+
+    echo "check if service exist >"
+    pm2 describe $PM2NAME > /dev/null
+    HASDESCRIBE=$?
+    
+    SHOULDSCRIPTPATH=$(pwd)"/starsky"
+    SCRIPTPATH=$(pm2 describe $PM2NAME | grep "script path" | grep -oP '\s│\K[^|]+' | sed 's/..$//' | xargs)
+    echo "<"
+    echo "$SCRIPTPATH"
+    echo "$SHOULDSCRIPTPATH"
+    echo "$HASDESCRIBE"
+    
+    if [ ! -z "$SCRIPTPATH" ] && [ "$SCRIPTPATH" != "$SHOULDSCRIPTPATH" ]; then
+        echo "remove pm2 instance due script path on a different location " 
+        echo "script path:" $SCRIPTPATH 
+        echo "should script path: " $SHOULDSCRIPTPATH 
+        pm2 delete $PM2NAME
+        pm2 save --force
+        # now the service does not exist anymore
+        HASDESCRIBE=1
+    fi
+    
+    if [ "${HASDESCRIBE}" -ne 0 ]; then
+      echo "start"
+      pm2 start --name $PM2NAME ./starsky
+    else
+      echo "re start"
+      pm2 restart $PM2NAME
+    fi;
+    
+    echo "pm2 show status of " $PM2NAME
+    pm2 status
+
+    echo "> AUTO SAVE - pm2 save " $PM2NAME
+    pm2 save --force
+fi
+
+echo "Done and saved :)"
