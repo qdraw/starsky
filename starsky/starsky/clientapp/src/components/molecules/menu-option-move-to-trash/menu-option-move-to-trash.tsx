d05ec59b83f51a284597c31bcaec6e6d0389e321
Dion <dionvanvelde@gmail.com>
2021-04-12 19:08:39 +0200

    undo

--- starsky/starsky/clientapp/src/components/molecules/menu-option-move-to-trash/menu-option-move-to-trash.tsx
+++ starsky/starsky/clientapp/src/components/molecules/menu-option-move-to-trash/menu-option-move-to-trash.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 import { ArchiveAction } from "../../../contexts/archive-context";
 import useGlobalSettings from "../../../hooks/use-global-settings";
 import useHotKeys from "../../../hooks/use-keyboard/use-hotkeys";
@@ -23,90 +23,86 @@ interface IMenuOptionMoveToTrashProps {
 /**
  * Used from Archive and Search
  */
-const MenuOptionMoveToTrash: React.FunctionComponent<IMenuOptionMoveToTrashProps> = ({
-  state,
-  dispatch,
-  select,
-  setSelect,
-  isReadOnly
-}) => {
-  const settings = useGlobalSettings();
-  const language = new Language(settings.language);
+const MenuOptionMoveToTrash: React.FunctionComponent<IMenuOptionMoveToTrashProps> = memo(
+  ({ state, dispatch, select, setSelect, isReadOnly }) => {
+    const settings = useGlobalSettings();
+    const language = new Language(settings.language);
 
-  var undoSelection = () =>
-    new Select(select, setSelect, state, history).undoSelection();
+    var undoSelection = () =>
+      new Select(select, setSelect, state, history).undoSelection();
 
-  const MessageMoveToTrash = language.text(
-    "Verplaats naar prullenmand",
-    "Move to Trash"
-  );
-
-  var history = useLocation();
-
-  async function moveToTrashSelection() {
-    if (!select || isReadOnly) return;
-
-    var toUndoTrashList = new URLPath().MergeSelectFileIndexItem(
-      select,
-      state.fileIndexItems
+    const MessageMoveToTrash = language.text(
+      "Verplaats naar prullenmand",
+      "Move to Trash"
     );
 
-    if (!toUndoTrashList) return;
-    var selectParams = new URLPath().ArrayToCommaSeperatedStringOneParent(
-      toUndoTrashList,
-      ""
-    );
-    if (selectParams.length === 0) return;
+    var history = useLocation();
 
-    var bodyParams = new URLSearchParams();
+    async function moveToTrashSelection() {
+      if (!select || isReadOnly) return;
 
-    bodyParams.append("f", selectParams);
-    bodyParams.set("Tags", "!delete!");
-    bodyParams.set("append", "true");
-    bodyParams.set("Colorclass", "8");
-    bodyParams.set(
-      "collections",
-      (
-        new URLPath().StringToIUrl(history.location.search).collections !==
-        false
-      ).toString()
-    );
+      var toUndoTrashList = new URLPath().MergeSelectFileIndexItem(
+        select,
+        state.fileIndexItems
+      );
 
-    var resultDo = await FetchPost(
-      new UrlQuery().UrlUpdateApi(),
-      bodyParams.toString()
-    );
-    if (resultDo.statusCode !== 404 && resultDo.statusCode !== 200) {
-      return;
+      if (!toUndoTrashList) return;
+      var selectParams = new URLPath().ArrayToCommaSeperatedStringOneParent(
+        toUndoTrashList,
+        ""
+      );
+      if (selectParams.length === 0) return;
+
+      var bodyParams = new URLSearchParams();
+
+      bodyParams.append("f", selectParams);
+      bodyParams.set("Tags", "!delete!");
+      bodyParams.set("append", "true");
+      bodyParams.set("Colorclass", "8");
+      bodyParams.set(
+        "collections",
+        (
+          new URLPath().StringToIUrl(history.location.search).collections !==
+          false
+        ).toString()
+      );
+
+      var resultDo = await FetchPost(
+        new UrlQuery().UrlUpdateApi(),
+        bodyParams.toString()
+      );
+      if (resultDo.statusCode !== 404 && resultDo.statusCode !== 200) {
+        return;
+      }
+
+      undoSelection();
+      dispatch({ type: "remove", toRemoveFileList: toUndoTrashList });
+      ClearSearchCache(history.location.search);
+      // Client side Caching: the order of files in a normal folder has changed
+      new FileListCache().CacheCleanEverything();
     }
 
-    undoSelection();
-    dispatch({ type: "remove", toRemoveFileList: toUndoTrashList });
-    ClearSearchCache(history.location.search);
-    // Client side Caching: the order of files in a normal folder has changed
-    new FileListCache().CacheCleanEverything();
+    /**
+     * When pressing delete its moved to the trash
+     */
+    useHotKeys({ key: "Delete" }, () => {
+      moveToTrashSelection();
+    });
+
+    return (
+      <>
+        {select.length >= 1 ? (
+          <li
+            data-test="trash"
+            className={!isReadOnly ? "menu-option" : "menu-option disabled"}
+            onClick={() => moveToTrashSelection()}
+          >
+            {MessageMoveToTrash}
+          </li>
+        ) : null}
+      </>
+    );
   }
-
-  /**
-   * When pressing delete its moved to the trash
-   */
-  useHotKeys({ key: "Delete" }, () => {
-    moveToTrashSelection();
-  });
-
-  return (
-    <>
-      {select.length >= 1 ? (
-        <li
-          data-test="trash"
-          className={!isReadOnly ? "menu-option" : "menu-option disabled"}
-          onClick={() => moveToTrashSelection()}
-        >
-          {MessageMoveToTrash}
-        </li>
-      ) : null}
-    </>
-  );
-};
+);
 
 export default MenuOptionMoveToTrash;
