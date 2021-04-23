6cfbf85f58266a1c785a3a7091d4a61698ea8580
Dion <dionvanvelde@gmail.com>
2021-04-23 17:13:43 +0200

    add bg test

--- starsky/starskytest/starsky.foundation.sync/SyncServices/ManualBackgroundSyncServiceTest.cs
+++ starsky/starskytest/starsky.foundation.sync/SyncServices/ManualBackgroundSyncServiceTest.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.database.Models;
+using starsky.foundation.platform.JsonConverter;
 using starsky.foundation.sync.SyncServices;
 using starskytest.FakeMocks;
 
@@ -44,6 +46,22 @@ namespace starskytest.starsky.foundation.sync.SyncServices
 					new FakeMemoryCache(new Dictionary<string, object>{{ManualBackgroundSyncService.QueryCacheName + "/test", string.Empty}}))
 				.ManualSync("/test");
 			Assert.AreEqual(FileIndexItem.ExifStatus.OperationNotSupported, result);
+		}
+
+		[TestMethod]
+		public async Task BackgroundTask_expect_socket_output()
+		{
+			var fakeSocket = new FakeIWebSocketConnectionsService();
+			var items = new List<FileIndexItem> {new FileIndexItem("/test")};
+			await new ManualBackgroundSyncService(
+					new FakeISynchronize(items),
+					new FakeIQuery(items),
+					fakeSocket,
+					new FakeMemoryCache(new Dictionary<string, object>{{ManualBackgroundSyncService.QueryCacheName + "/test", string.Empty}}))
+				.BackgroundTask("/test");
+
+			Assert.AreEqual(JsonSerializer.Serialize(items, 
+				DefaultJsonSerializer.CamelCase), fakeSocket.FakeSendToAllAsync[0]);
 		}
 	}
 }
