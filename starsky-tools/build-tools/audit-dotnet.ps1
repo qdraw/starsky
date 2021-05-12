9484746b947042b4a97af50d9e261a1e651e7b16
Dion <dionvanvelde@gmail.com>
2021-05-12 18:11:07 +0200

    add script to check

---
+++ starsky-tools/build-tools/audit-dotnet.ps1
@@ -0,0 +1,50 @@
+param (
+    [string]$projectDirectory = ""
+)
+
+if($projectDirectory -eq "") {
+   $projectDirectory =  Get-Location
+}
+
+# Check .NET version
+$version = dotnet --version
+if($version.StartsWith("3") || $version.StartsWith("2") || $version.StartsWith("1")) {
+    write-host "You need .NET SDK 5.0.200 or newer"
+    # https://devblogs.microsoft.com/nuget/how-to-scan-nuget-packages-for-security-vulnerabilities/
+    exit 1
+}
+
+function check-vulnerable($solution) {
+
+  write-host "run command: "
+  write-host dotnet list $solution package --vulnerable 
+
+  $output = dotnet list $solution package --vulnerable 
+
+  $errors = $output | Select-String '>'
+
+  if ($errors.Count -gt 0)
+  {
+    foreach ($err in $errors)
+    {
+        if($err.ToString().Trim().StartsWith( "<PROJECT | SOLUTION>")) {
+            continue;
+        }
+        Write-Host "##vso[task.logissue type=error]Error with $err"
+    }
+    exit 1
+  }
+  else
+  {
+    Write-Host "No vulnerable NuGet-packages"
+  }
+}
+
+# current
+$solutions = Get-ChildItem -Path $projectDirectory/** -Name -Include *.sln
+
+foreach ($solution in $solutions)
+{
+    $path = Join-Path -Path $projectDirectory -ChildPath  $solution
+    check-vulnerable -solution  $path
+}
