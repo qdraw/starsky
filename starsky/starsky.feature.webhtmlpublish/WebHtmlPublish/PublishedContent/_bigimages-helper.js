10300a41f2fba0b2254a84956e8500ffa94d078b
Dion <dionvanvelde@gmail.com>
2021-05-26 08:58:56 +0200

    code smell _bigimages-helper

--- starsky/starsky.feature.webhtmlpublish/WebHtmlPublish/PublishedContent/_bigimages-helper.js
+++ starsky/starsky.feature.webhtmlpublish/WebHtmlPublish/PublishedContent/_bigimages-helper.js
@@ -1,14 +1,15 @@
 // To replace base64 images with 500 px images
 var bigimages = document.querySelectorAll('a[class^=\'lightbox\'] img');
-for (var i = 0; i < bigimages.length; i++) {
-    var noscriptItem = bigimages[i].parentNode.getElementsByTagName('noscript')[0]
+
+for (var image of bigimages) {
+    var noscriptItem = image.parentNode.getElementsByTagName('noscript')[0]
     if ( noscriptItem.innerHTML.length >= 10 ) {
         var url = noscriptItem.innerHTML.match(/\"(.*?)\"/);
         url = url[0].replace(/\"/ig, "");
-        bigimages[i].src = url;
+		image.src = url;
     }
     var bigimagesTitle = document.createElement("span");
     bigimagesTitle.className = "lightbox-title";
-    bigimagesTitle.innerHTML = bigimages[i].title;
-    bigimages[i].parentElement.appendChild(bigimagesTitle);
+    bigimagesTitle.innerHTML = image.title;
+	image.parentElement.appendChild(bigimagesTitle);
 }
