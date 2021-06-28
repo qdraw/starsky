1093470de2fa86d74f1c62f312227cb56ed35d89
Dion <dionvanvelde@gmail.com>
2021-06-28 21:03:52 +0200

    should exit nice now

--- starsky/starsky.foundation.realtime/Services/HeartBeatService.cs
+++ starsky/starsky.foundation.realtime/Services/HeartBeatService.cs
@@ -11,27 +11,22 @@ namespace starsky.foundation.realtime.Services
 	[Service(typeof(IHostedService), InjectionLifetime = InjectionLifetime.Singleton)]
 	public class HeartbeatService : IHostedService
 	{
-		#region Fields
-
 		private const int SpeedInSeconds = 30;
 		private const string InsertDateToken = "INSERT_DATE_TOKEN";
 		private const string SpeedInSecondsToken = "SPEED_TOKEN";
-		private const string HeartbeatMessage = "{ \"speed\": " + SpeedInSecondsToken + ",  \"time\": \"" + InsertDateToken + "\"} ";
+		private const string HeartbeatMessage = "{ \"speed\": " + SpeedInSecondsToken + 
+		                                        ",  \"time\": \"" + InsertDateToken + "\"} ";
 
 		private readonly IWebSocketConnectionsService _webSocketConnectionsService;
 
 		private Task _heartbeatTask;
 		private CancellationTokenSource _cancellationTokenSource;
-		#endregion
 
-		#region Constructor
 		public HeartbeatService(IWebSocketConnectionsService webSocketConnectionsService)
 		{
 			_webSocketConnectionsService = webSocketConnectionsService;
 		}
-		#endregion
 
-		#region Methods
 		public Task StartAsync(CancellationToken cancellationToken)
 		{
 			_cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
@@ -49,7 +44,14 @@ namespace starsky.foundation.realtime.Services
 
 				await Task.WhenAny(_heartbeatTask, Task.Delay(-1, cancellationToken));
 
-				cancellationToken.ThrowIfCancellationRequested();
+				try
+				{
+					cancellationToken.ThrowIfCancellationRequested();
+				}
+				catch (AggregateException e )
+				{
+					Console.WriteLine(e);
+				}
 			}
 		}
 
@@ -57,12 +59,12 @@ namespace starsky.foundation.realtime.Services
 		{
 			while (!cancellationToken.IsCancellationRequested)
 			{
-				var message = HeartbeatMessage.Replace(SpeedInSecondsToken, SpeedInSeconds.ToString()).Replace(
+				var message = HeartbeatMessage.Replace(SpeedInSecondsToken, 
+					SpeedInSeconds.ToString()).Replace(
 					InsertDateToken, DateTime.UtcNow.ToString(CultureInfo.InvariantCulture));
 				await _webSocketConnectionsService.SendToAllAsync(message, cancellationToken);
 				await Task.Delay(TimeSpan.FromSeconds(SpeedInSeconds), cancellationToken);
 			}
 		}
-		#endregion
 	}
 }
