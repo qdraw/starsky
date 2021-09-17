0f49c3f56c322a23ca4dc87bae9a24a7509d9f08
Dion <dionvanvelde@gmail.com>
2021-09-17 20:24:02 +0200

    update docs / fix regex bug

--- starsky/starsky/clientapp/src/shared/file-extensions.spec.ts
+++ starsky/starsky/clientapp/src/shared/file-extensions.spec.ts
@@ -127,6 +127,42 @@ describe("keyboard", () => {
     });
   });
 
+  describe("GetFileName", () => {
+    it("get file name no slash", () => {
+      var result = fileExt.GetFileName("testung.jpg");
+      expect(result).toBe("testung.jpg");
+    });
+
+    it("get file name no ext", () => {
+      var result = fileExt.GetFileName("/te_st/test");
+      expect(result).toBe("test");
+    });
+
+    it("get file name  com file", () => {
+      var result = fileExt.GetFileName("/te_st/test.com");
+      expect(result).toBe("test.com");
+    });
+
+    it("get file name  mp4 file", () => {
+      var result = fileExt.GetFileName("/te_st/test.mp4");
+      expect(result).toBe("test.mp4");
+    });
+
+    it("get file name space and http file", () => {
+      var result = fileExt.GetFileName(
+        "http://path/Lists/Test/Attachments/1/Document Test.docx"
+      );
+      expect(result).toBe("Document Test.docx");
+    });
+
+    it("get file name on root", () => {
+      var result = fileExt.GetFileName(
+        "/0000000000aaaaa__exifreadingtest00.jpg"
+      );
+      expect(result).toBe("0000000000aaaaa__exifreadingtest00.jpg");
+    });
+  });
+
   describe("GetFileNameWithoutExtension", () => {
     it("get file name without extension com file", () => {
       var result = fileExt.GetFileNameWithoutExtension("/te_st/test.com");
