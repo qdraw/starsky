e1a3b83ebbe404a5b4663d14abf49ad5c58fc5c6
Dion <dionvanvelde@gmail.com>
2021-10-06 20:22:49 +0200

    fix tests

--- starsky/starsky/clientapp/src/components/atoms/modal/modal.spec.tsx
+++ starsky/starsky/clientapp/src/components/atoms/modal/modal.spec.tsx
@@ -1,9 +1,10 @@
-import { render } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import React from "react";
 import Modal from "./modal";
 
 describe("Modal", () => {
   it("renders", () => {
+    window.scrollTo = jest.fn();
     render(
       <Modal id="test2-modal" isOpen={true} handleExit={() => {}}>
         &nbsp;
@@ -12,43 +13,69 @@ describe("Modal", () => {
   });
 
   describe("Close Modal", () => {
-    var handleExit = jest.fn();
-    var element = render(
-      <Modal id="test-modal" isOpen={true} handleExit={handleExit}>
-        &nbsp;
-      </Modal>
-    );
+    function renderModal(): any {
+      window.scrollTo = jest.fn();
+      var handleExit = jest.fn();
+      const element = render(
+        <Modal id="test-modal" isOpen={true} handleExit={handleExit}>
+          &nbsp;
+        </Modal>
+      );
+      return {
+        handleExit,
+        element
+      };
+    }
 
     it("modal-exit-button", () => {
-      element.find(".modal-exit-button").simulate("click");
+      const { handleExit, element } = renderModal();
+
+      element.queryAllByTestId("modal-exit-button")[0].click();
       expect(handleExit).toBeCalled();
+      element.unmount();
     });
 
     it("modal-bg", () => {
-      element.find(".modal-bg").simulate("click");
+      const { handleExit, element } = renderModal();
+
+      element.queryAllByTestId("modal-bg")[0].click();
       expect(handleExit).toBeCalled();
+      element.unmount();
     });
   });
 
   describe("Open Modal", () => {
-    const spyScrollTo = jest.fn();
-    Object.defineProperty(window, "scrollTo", { value: spyScrollTo });
+    function renderModal2(): [
+      jest.Mock<any, any>,
+      RenderResult<
+        typeof import("/data/git/starsky/starsky/starsky/clientapp/node_modules/@testing-library/dom/types/queries"),
+        HTMLElement
+      >
+    ] {
+      const spyScrollTo = jest.fn();
+      window.scrollTo = spyScrollTo;
 
-    var handleExit = jest.fn();
-    render(
-      <div>
-        <Modal id="test-modal" isOpen={false} handleExit={handleExit}>
-          &nbsp;
-        </Modal>
-        <div className="root" />
-      </div>
-    );
+      var handleExit = jest.fn() as any;
+      const component = render(
+        <div>
+          <Modal id="test-modal" isOpen={true} handleExit={handleExit}>
+            &nbsp;
+          </Modal>
+          <div className="root" />
+        </div>
+      );
+      return [spyScrollTo, component];
+    }
 
     it("sould open modal", () => {
+      const [spyScrollTo, component] = renderModal2();
+
       var element = document.body.querySelector(".modal-bg--open");
+
       expect(element).toBeTruthy();
 
-      // compontent.unmount();
+      component.unmount();
+
       spyScrollTo.mockClear();
     });
   });
