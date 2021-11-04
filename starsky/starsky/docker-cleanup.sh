122f4b77510e257fbd292ef7ce9b5af8aaa9c161
Dion <dionvanvelde@gmail.com>
2021-11-04 22:49:00 +0100

    add comment

--- starsky/starsky/docker-cleanup.sh
+++ starsky/starsky/docker-cleanup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# when force everything
+# docker system prune -a -f
 docker builder prune --filter 'until=8h' -f
 docker image prune --filter 'until=8h' -f
 docker container prune --filter "until=8h" -f
