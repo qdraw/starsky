3e1df4cf58438c04b332c7e3b6a09b8fd2ed05fa
Dion <dionvanvelde@gmail.com>
2022-02-27 15:35:52 +0100

    move location of catch

--- starsky/starsky.foundation.databasetelemetry/Helpers/TelemetryConfigurationHelper.cs
+++ starsky/starsky.foundation.databasetelemetry/Helpers/TelemetryConfigurationHelper.cs
@@ -9,28 +9,28 @@ namespace starsky.foundation.databasetelemetry.Helpers
 	{
 		public static TelemetryClient InitTelemetryClient(string appInsightsConnectionString, string roleName)
 		{
-			var module = CreateDatabaseDependencyTrackingTelemetryModule();
-			var telemetryConfiguration = CreateTelemetryConfiguration(appInsightsConnectionString);
-			if ( telemetryConfiguration == null ) return null;
-			var telemetryClient = new TelemetryClient(telemetryConfiguration);
-			telemetryClient.Context.Cloud.RoleName = roleName;
-			telemetryClient.Context.Cloud.RoleInstance = Environment.MachineName;
-			module.Initialize(telemetryConfiguration);
-			return telemetryClient;
-		}
-
-		private static TelemetryConfiguration CreateTelemetryConfiguration(string appInsightsInstrumentationKey)
-		{
 			try
 			{
-				var telemetryConfiguration = TelemetryConfiguration.CreateDefault();
-				telemetryConfiguration.InstrumentationKey = appInsightsInstrumentationKey;
-				return telemetryConfiguration;
+				var module = CreateDatabaseDependencyTrackingTelemetryModule();
+				var telemetryConfiguration = CreateTelemetryConfiguration(appInsightsConnectionString);
+				if ( telemetryConfiguration == null ) return null;
+				var telemetryClient = new TelemetryClient(telemetryConfiguration);
+				telemetryClient.Context.Cloud.RoleName = roleName;
+				telemetryClient.Context.Cloud.RoleInstance = Environment.MachineName;
+				module.Initialize(telemetryConfiguration);
+				return telemetryClient;
 			}
 			catch (OutOfMemoryException)
 			{
 				return null;
 			}
+		}
+
+		private static TelemetryConfiguration CreateTelemetryConfiguration(string appInsightsInstrumentationKey)
+		{
+			var telemetryConfiguration = TelemetryConfiguration.CreateDefault();
+			telemetryConfiguration.InstrumentationKey = appInsightsInstrumentationKey;
+			return telemetryConfiguration;
 		}
 
 		private static DependencyTrackingTelemetryModule CreateDatabaseDependencyTrackingTelemetryModule()
