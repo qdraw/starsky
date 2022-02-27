204f4920dfb30c7705dddabbae34cac7630abbbe
Dion <dionvanvelde@gmail.com>
2022-02-27 12:48:22 +0100

    supress warnings

--- starsky/starskytest/starsky.foundation.database/QueryTest/QueryUpdateItem_Error.cs
+++ starsky/starskytest/starsky.foundation.database/QueryTest/QueryUpdateItem_Error.cs
@@ -109,9 +109,15 @@ namespace starskytest.starsky.foundation.database.QueryTest
 				throw new NotImplementedException();
 			}
 
+#pragma warning disable 8618
+			// ReSharper disable once UnassignedGetOnlyAutoProperty
 			public IEntityType EntityType { get; }
+#pragma warning restore 8618
 			public EntityState EntityState { get; set; }
+#pragma warning disable 8618
+			// ReSharper disable once UnassignedGetOnlyAutoProperty
 			public IUpdateEntry SharedIdentityEntry { get; }
+#pragma warning restore 8618
 		}
         
 		private class AppDbContextConcurrencyException : ApplicationDbContext
