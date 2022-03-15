6329ccac4b690e5f0be20c6652f2354785950398
Dion <dionvanvelde@gmail.com>
2022-03-15 16:09:40 +0100

    PackageTelemetryAttribute

--- starsky/starsky.foundation.platform/Attributes/PackageTelemetryAttribute.cs
+++ starsky/starsky.foundation.platform/Attributes/PackageTelemetryAttribute.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace starsky.foundation.platform.Attributes;
 
+[SuppressMessage("Design", "CA1018:Mark attributes with AttributeUsageAttribute")]
 public class PackageTelemetryAttribute: Attribute
 {
 	// nothing here
