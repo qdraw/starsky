724a2a6670429318912d00fe1ed0204032231040
Dion <dionvanvelde@gmail.com>
2022-04-01 18:38:28 +0200

    longer

--- starsky/starsky/clientapp/src/components/organisms/detail-view-sidebar/detail-view-sidebar.spec.tsx
+++ starsky/starsky/clientapp/src/components/organisms/detail-view-sidebar/detail-view-sidebar.spec.tsx
@@ -347,7 +347,7 @@ describe("DetailViewSidebar", () => {
 
       await waitFor(() => expect(tags?.classList).toContain("form-control"));
       await waitFor(() => expect(tags?.classList).toContain("disabled"), {
-        timeout: 2000
+        timeout: 3000
       });
       await waitFor(() => expect(description?.classList).toContain("disabled"));
       await waitFor(() => expect(title?.classList).toContain("disabled"));
