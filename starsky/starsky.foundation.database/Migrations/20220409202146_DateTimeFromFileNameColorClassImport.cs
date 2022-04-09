b35e37b7397c37b2ed13a62a99d262c6aa7c990a
Dion <dionvanvelde@gmail.com>
2022-04-09 22:23:11 +0200

    add database migration

---
+++ starsky/starsky.foundation.database/Migrations/20220409202146_DateTimeFromFileNameColorClassImport.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace starsky.foundation.database.Migrations
+{
+    public partial class DateTimeFromFileNameColorClassImport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ColorClass",
+                table: "ImportIndex",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DateTimeFromFileName",
+                table: "ImportIndex",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ColorClass",
+                table: "ImportIndex");
+
+            migrationBuilder.DropColumn(
+                name: "DateTimeFromFileName",
+                table: "ImportIndex");
+        }
+    }
+}
