fed09ccce7bdcd8beb4b3ce39e3bf3fb8b09c1a5
Dion <dionvanvelde@gmail.com>
2022-04-11 16:52:41 +0200

    add test

--- starsky/starskytest/starsky.foundation.realtime/Services/WebSocketConnectionsServiceTest.cs
+++ starsky/starskytest/starsky.foundation.realtime/Services/WebSocketConnectionsServiceTest.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using starsky.foundation.platform.Models;
 using starsky.foundation.realtime.Helpers;
 using starsky.foundation.realtime.Services;
 using starskytest.FakeMocks;
@@ -33,6 +34,18 @@ namespace starskytest.starsky.foundation.realtime.Services
 
 			await service.SendToAllAsync(null as string, CancellationToken.None);
 			Assert.AreEqual(1,logger.TrackedInformation.Count);
+		}
+		
+		[TestMethod]
+		public async Task SendToAllAsync_Model_success()
+		{
+			var service = new WebSocketConnectionsService(new FakeIWebLogger());
+			var fakeSocket = new FakeWebSocket();
+			service.AddConnection(new WebSocketConnection(fakeSocket));
+
+			await service.SendToAllAsync(new ApiNotificationResponseModel<string>("test"), CancellationToken.None);
+			
+			Assert.AreEqual("{\"data\":\"test\",\"type\":\"Unknown\"}",fakeSocket.FakeSendItems.LastOrDefault());
 		}
 	}
 }
