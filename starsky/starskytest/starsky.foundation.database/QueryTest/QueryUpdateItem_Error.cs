e449c4811c9c081a82f7f52c562cd103dbbd9ddc
Dion <dionvanvelde@gmail.com>
2022-04-14 20:05:20 +0200

    add test

--- starsky/starskytest/starsky.foundation.database/QueryTest/QueryUpdateItem_Error.cs
+++ starsky/starskytest/starsky.foundation.database/QueryTest/QueryUpdateItem_Error.cs
@@ -13,8 +13,14 @@ using Microsoft.EntityFrameworkCore.Update;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.database.Data;
+using starsky.foundation.database.Interfaces;
 using starsky.foundation.database.Models;
 using starsky.foundation.database.Query;
+using starsky.foundation.http.Interfaces;
+using starsky.foundation.http.Services;
+using starsky.foundation.platform.Interfaces;
+using starsky.foundation.platform.Models;
+using starsky.foundation.storage.Interfaces;
 using starskytest.FakeMocks;
 
 namespace starskytest.starsky.foundation.database.QueryTest
@@ -25,6 +31,8 @@ namespace starskytest.starsky.foundation.database.QueryTest
 	[TestClass]
 	public class QueryUpdateItemError
 	{
+		private IServiceScopeFactory _serviceScopeFactory;
+
 		private IServiceScopeFactory CreateNewScopeSqliteException()
 		{
 			var services = new ServiceCollection();
@@ -187,6 +195,21 @@ namespace starskytest.starsky.foundation.database.QueryTest
 			}
 		}
 
+		public static int InvalidOperationExceptionDbContextCount { get; set; }
+		
+		private class InvalidOperationExceptionDbContext : ApplicationDbContext
+		{
+			public InvalidOperationExceptionDbContext(DbContextOptions options) : base(options)
+			{
+			}
+			
+			public override Task<int> SaveChangesAsync(
+				CancellationToken cancellationToken = default)
+			{
+				InvalidOperationExceptionDbContextCount++;
+				throw new InvalidOperationException("from test");
+			}
+		}
 
 		[TestMethod]
 		public void Query_UpdateItem_DbUpdateConcurrencyException()
@@ -230,7 +253,36 @@ namespace starskytest.starsky.foundation.database.QueryTest
 			
 			Assert.IsTrue(IsCalledDbUpdateConcurrency);
 		}
+
+		private void CreateScope()
+		{
+			var services = new ServiceCollection();
+			services.AddSingleton<AppSettings>();
+
+			services.AddSingleton<IWebLogger, FakeIWebLogger>();
+			services.AddDbContext<ApplicationDbContext>(options => 
+				options.UseInMemoryDatabase(nameof(CreateScope)));
+			
+			var serviceProvider = services.BuildServiceProvider();
+			_serviceScopeFactory = serviceProvider.GetRequiredService<IServiceScopeFactory>();
+		}
 		
+		[TestMethod]
+		public async Task UpdateItemAsync_InvalidOperationExceptionDbContext()
+		{
+			InvalidOperationExceptionDbContextCount = 0;
+			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+				.UseInMemoryDatabase(databaseName: "MovieListDatabase")
+				.Options;
+			
+			CreateScope();
+			var fakeQuery = new Query(new InvalidOperationExceptionDbContext(options), 
+				null, _serviceScopeFactory ,new FakeIWebLogger(),null);
+			await fakeQuery.UpdateItemAsync(new FileIndexItem("test"));
+			
+			Assert.IsTrue(InvalidOperationExceptionDbContextCount == 1);
+		}
+
 				
 		[TestMethod]
 		public async Task Query_UpdateItemAsync_Multiple_DbUpdateConcurrencyException()
