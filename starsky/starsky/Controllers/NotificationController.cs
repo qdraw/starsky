074698e2feaa0fd49f76a534cd75cd49ea6df500
Dion <dionvanvelde@gmail.com>
2022-04-16 20:38:09 +0200

    fix api

--- starsky/starsky/Controllers/NotificationController.cs
+++ starsky/starsky/Controllers/NotificationController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -18,22 +19,38 @@ namespace starsky.Controllers
 		
 		/// <summary>
 		/// Get recent notifications
+		/// Use dateTime 2022-04-16T17:33:10.323974Z to get the latest notifications
+		/// or use numbers to get hours from now (e.g. 1 for last hour)
 		/// </summary>
 		/// <returns>list of notification items</returns>
 		/// <response code="200">list of recent items</response>
-		/// <response code="400">Longer than 1 ago requested, or null</response>
+		/// <response code="400">Longer than 1 dag ago requested, or null</response>
 		/// <response code="401">User unauthorized</response>
 		[HttpGet("/api/notification/notification")]
 		[Produces("application/json")]
 		public async Task<IActionResult> GetNotifications(string dateTime)
 		{
-			var isParsed = DateTime.TryParse(dateTime, out var parsedDateTime);
-			if ( !isParsed || (DateTime.UtcNow - parsedDateTime.ToUniversalTime() ).TotalDays >= 1 )
+			var (parsed, parsedDateTime) = ParseDate(dateTime);
+			
+			if ( !parsed || (DateTime.UtcNow - parsedDateTime ).TotalDays >= 1 )
 			{
 				return BadRequest("Please enter a valid dateTime");
 			}
 			return Json(await _notificationQuery.GetNewerThan(parsedDateTime.ToUniversalTime()));
-		}	
+		}
+
+		internal static Tuple<bool, DateTime> ParseDate(string dateTime)
+		{
+			var isParsed = DateTime.TryParse(dateTime, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out var parsedDateTime);
+			if ( isParsed )
+				return new Tuple<bool, DateTime>(true, parsedDateTime.ToUniversalTime());
+			
+			if ( !int.TryParse(dateTime, out var parsedInt) )
+				return new Tuple<bool, DateTime>(false, DateTime.UtcNow);
+
+			parsedDateTime = DateTime.UtcNow.AddHours(parsedInt * -1);
+			return new Tuple<bool, DateTime>(true, parsedDateTime.ToUniversalTime());
+		}
 	}
 }
 
