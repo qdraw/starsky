63706248dcc6243dd3f0703cbf73b5a1965e1372
Dion <dionvanvelde@gmail.com>
2022-04-28 19:51:39 +0200

    not needed anymore

--- starsky/starsky/clientapp/src/setupTests.js
+++ starsky/starsky/clientapp/src/setupTests.js
@@ -1,17 +1,3 @@
 import { configure } from "@testing-library/react";
 
-const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  removeItem: jest.fn(),
-  clear: jest.fn()
-};
-global.localStorage = localStorageMock;
-
 configure({ testIdAttribute: "data-test" });
-
-// // we are not using testing-library/jest-dom
-// import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-// import Enzyme from "enzyme";
-
-// Enzyme.configure({ adapter: new Adapter() });
