b1941647d8aa5ae941a1a5c6802f579bc68bf65e
Dion <dionvanvelde@gmail.com>
2022-09-19 15:17:35 +0200

    update

--- starsky/starsky/service-deploy-systemd.sh
+++ starsky/starsky/service-deploy-systemd.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
+SERVICE_NAME=starsky
+
+
+if ! command -v systemctl &> /dev/null
+then
+    echo "systemctl could not be found"
+    exit 1
+fi
+
 SCRIPT_DIR=$(dirname "$0")
 cd $SCRIPT_DIR
 
+mkdir -p ~/.config/systemd/user/
 
-USAGE=$(cat <<-END
+SYSTEMD_SERVICE_PATH="~/.config/systemd/user/"$SERVICE_NAME".service"
+
+echo <<< EOL
     [Unit]
     Description=S
     
     [Service]
     # This is the directory where our published files are
-    WorkingDirectory=/var/www/sample-app
+    WorkingDirectory=${SCRIPT_DIR}
     # We set up `dotnet` PATH in Step 1. The second one is path of our executable
-    ExecStart=/usr/bin/dotnet /var/www/sample-app/SampleApp.dll --urls "http://0.0.0.0:5000"
+    ExecStart=${SCRIPT_DIR}/starsky --urls "http://0.0.0.0:5001"
     Restart=always
     # Restart service after 10 seconds if the dotnet service crashes
     RestartSec=10
     KillSignal=SIGINT
-    SyslogIdentifier=sample-app-log
+    SyslogIdentifier=${SCRIPT_DIR}
     # We can even set environment variables
     Environment=ASPNETCORE_ENVIRONMENT=Production
     Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=false
@@ -25,14 +37,15 @@ USAGE=$(cat <<-END
     [Install]
     # When a systemd user instance starts, it brings up the per user target default.target
     WantedBy=default.target
-END
-)
-    
-
+EOL >> $SYSTEMD_SERVICE_PATH;
 
 
 
 systemctl --user daemon-reload
-systemctl --user enable sample-app.service
+systemctl --user enable $SERVICE_NAME".service"
 
-systemctl --user start sample-app.service
+systemctl --user start $SERVICE_NAME".service"
+
+echo "end"
+
+# credits to https://amelspahic.com/deploy-net-6-application-with-github-actions-to-self-hosted-linux-machine-virtual-private-server-raspberry-pi
