57ffb198c1d8126074552df120ba3cdac2bd5ab2
Dion <dionvanvelde@gmail.com>
2022-09-19 15:24:02 +0200

    test

--- starsky/starsky/service-deploy-systemd.sh
+++ starsky/starsky/service-deploy-systemd.sh
@@ -16,28 +16,44 @@ mkdir -p ~/.config/systemd/user/
 
 SYSTEMD_SERVICE_PATH="~/.config/systemd/user/"$SERVICE_NAME".service"
 
-echo <<< EOL
-    [Unit]
-    Description=S
-    
-    [Service]
-    # This is the directory where our published files are
-    WorkingDirectory=${SCRIPT_DIR}
-    # We set up `dotnet` PATH in Step 1. The second one is path of our executable
-    ExecStart=${SCRIPT_DIR}/starsky --urls "http://0.0.0.0:5001"
-    Restart=always
-    # Restart service after 10 seconds if the dotnet service crashes
-    RestartSec=10
-    KillSignal=SIGINT
-    SyslogIdentifier=${SCRIPT_DIR}
-    # We can even set environment variables
-    Environment=ASPNETCORE_ENVIRONMENT=Production
-    Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=false
-    
-    [Install]
-    # When a systemd user instance starts, it brings up the per user target default.target
-    WantedBy=default.target
-EOL >> $SYSTEMD_SERVICE_PATH;
+echo "[Unit]" > $SYSTEMD_SERVICE_PATH
+echo -e "\nDescription=S" >> $SYSTEMD_SERVICE_PATH
+echo -e "\n[Service]" >> $SYSTEMD_SERVICE_PATH
+echo -e "\nDescription=S" >> $SYSTEMD_SERVICE_PATH
+echo -e "\nWorkingDirectory=${SCRIPT_DIR}" >> $SYSTEMD_SERVICE_PATH
+echo -e "\nExecStart=${SCRIPT_DIR}/starsky --urls "http://0.0.0.0:5001"" >> $SYSTEMD_SERVICE_PATH
+echo -e "\nRestart=always" >> $SYSTEMD_SERVICE_PATH
+echo -e "\nRestartSec=10" >> $SYSTEMD_SERVICE_PATH
+echo -e "\nKillSignal=SIGINT" >> $SYSTEMD_SERVICE_PATH
+echo -e "\nSyslogIdentifier=${SCRIPT_DIR}" >> $SYSTEMD_SERVICE_PATH
+echo -e "\nEnvironment=ASPNETCORE_ENVIRONMENT=Production" >> $SYSTEMD_SERVICE_PATH
+echo -e "\nEnvironment=DOTNET_PRINT_TELEMETRY_MESSAGE=false" >> $SYSTEMD_SERVICE_PATH
+echo -e "\n" >> $SYSTEMD_SERVICE_PATH
+echo -e "\n[Install]" >> $SYSTEMD_SERVICE_PATH
+echo -e "\nWantedBy=default.target" >> $SYSTEMD_SERVICE_PATH
+
+#echo <<< EOL
+#    [Unit]
+#    Description=S
+#    
+#    [Service]
+#    # This is the directory where our published files are
+#    WorkingDirectory=${SCRIPT_DIR}
+#    # We set up `dotnet` PATH in Step 1. The second one is path of our executable
+#    ExecStart=${SCRIPT_DIR}/starsky --urls "http://0.0.0.0:5001"
+#    Restart=always
+#    # Restart service after 10 seconds if the dotnet service crashes
+#    RestartSec=10
+#    KillSignal=SIGINT
+#    SyslogIdentifier=${SCRIPT_DIR}
+#    # We can even set environment variables
+#    Environment=ASPNETCORE_ENVIRONMENT=Production
+#    Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=false
+#    
+#    [Install]
+#    # When a systemd user instance starts, it brings up the per user target default.target
+#    WantedBy=default.target
+#EOL >> $SYSTEMD_SERVICE_PATH;
 
 
 
