3c68295a67c97610fe529bec641ee56461bc99e4
Dion <qdraw@users.noreply.github.com>
2022-10-07 14:49:00 +0200

    add no logo to default

--- starsky/build.sh
+++ starsky/build.sh
@@ -59,5 +59,12 @@ fi
 
 echo "Microsoft (R) .NET SDK version $("$DOTNET_EXE" --version)"
 
-"$DOTNET_EXE" build "$BUILD_PROJECT_FILE" /nodeReuse:false /p:UseSharedCompilation=false -nologo -clp:NoSummary --verbosity quiet
-"$DOTNET_EXE" run --project "$BUILD_PROJECT_FILE" --no-build -- "$@"
+if result=$("$DOTNET_EXE" build "$BUILD_PROJECT_FILE" /nodeReuse:false /p:UseSharedCompilation=false -nologo -clp:NoSummary --verbosity quiet 2>&1); then
+    echo "next: run"
+else
+    echo "failed"
+    echo $result
+    exit 1
+fi
+
+"$DOTNET_EXE" run --project "$BUILD_PROJECT_FILE" --no-build -- --no-logo "$@"
