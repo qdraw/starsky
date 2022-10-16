f742c118479256a55ad58b72a37885f28d9c4056
Dion <qdraw@users.noreply.github.com>
2022-10-16 12:08:30 +0200

    get this easier

--- starskyapp/src/app/get-free-port/get-free-port.ts
+++ starskyapp/src/app/get-free-port/get-free-port.ts
@@ -1,26 +1,14 @@
 import * as net from "net";
 
-function createServer(port: number, callback?: () => void) {
-  const server = net.createServer();
-  server.listen(port, callback).unref();
-}
-
-// https://stackoverflow.com/a/54229665
 export async function GetFreePort(): Promise<number> {
-  return new Promise((resolve, _) => {
-    createServer(0, () => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-      const { port } = this.address();
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/restrict-plus-operands
-      createServer(port + 1, () => {
-        createServer(0, () => {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-shadow, @typescript-eslint/no-unsafe-member-access
-          const { port } = this.address();
-          // This line will show that the OS skipped the occupied port and assigned the next available port.
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-          resolve(port);
-        });
-      });
+  return new Promise((resolve) => {
+    const srv = net.createServer((sock) => {
+      sock.end('Hello world\n');
+    });
+    srv.listen(0, () => {
+      const { port } = srv.address() as net.AddressInfo;
+      console.log(`Listening on port ${port}`);
+      resolve(port);
     });
   });
 }
