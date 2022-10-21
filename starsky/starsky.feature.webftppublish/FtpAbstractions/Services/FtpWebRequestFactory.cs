24e452f81f15aa248ed4a2ab7453ad99098d4953
Dion <qdraw@users.noreply.github.com>
2022-10-21 16:33:06 +0200

    disable warning

--- starsky/starsky.feature.webftppublish/FtpAbstractions/Services/FtpWebRequestFactory.cs
+++ starsky/starsky.feature.webftppublish/FtpAbstractions/Services/FtpWebRequestFactory.cs
@@ -2,6 +2,7 @@ using System.Net;
 using starsky.feature.webftppublish.FtpAbstractions.Helpers;
 using starsky.feature.webftppublish.FtpAbstractions.Interfaces;
 using starsky.foundation.injection;
+#pragma warning disable SYSLIB0014
 
 namespace starsky.feature.webftppublish.FtpAbstractions.Services
 {
@@ -12,6 +13,11 @@ namespace starsky.feature.webftppublish.FtpAbstractions.Services
 	[Service(typeof(IFtpWebRequestFactory), InjectionLifetime = InjectionLifetime.Scoped)]
 	public class FtpWebRequestFactory : IFtpWebRequestFactory
 	{
+		/// <summary>
+		/// (FtpWebRequest)WebRequest is deprecated
+		/// </summary>
+		/// <param name="uri">ftp url</param>
+		/// <returns>new Requester</returns>
 		public IFtpWebRequest Create(string uri)
 		{
 			return new WrapFtpWebRequest((FtpWebRequest)WebRequest.Create(uri));
