f47e5d65c16a9a61ded5146965a63275fdccbb42
Dion <qdraw@users.noreply.github.com>
2022-11-02 20:41:02 +0100

    test is not working

--- starsky/starskytest/starsky.foundation.database/Helpers/RunMigrationsTest.cs
+++ starsky/starskytest/starsky.foundation.database/Helpers/RunMigrationsTest.cs
@@ -99,67 +99,6 @@ namespace starskytest.starsky.foundation.database.Helpers
 			
 			// should not crash
 		}
-
 		
-
-		private class AppDbMySqlException2 : ApplicationDbContext
-		{
-			public AppDbMySqlException2(DbContextOptions options) : base(options)
-			{
-			}
-
-			public override DatabaseFacade Database => new DatabaseFacade(this);
-
-
-		}
-		
-		public static void HijackMethod(Type sourceType, string sourceMethod, Type targetType, string targetMethod)
-		{
-			// Get methods using reflection
-			var source = sourceType.GetMethod(sourceMethod,new []{typeof(CancellationToken)});
-			var target = targetType.GetMethod(targetMethod);
-
-			// Prepare methods to get machine code (not needed in this example, though)
-			RuntimeHelpers.PrepareMethod(source.MethodHandle);
-			RuntimeHelpers.PrepareMethod(target.MethodHandle);
-
-			var sourceMethodDescriptorAddress = source.MethodHandle.Value;
-			var targetMethodMachineCodeAddress = target.MethodHandle.GetFunctionPointer();
-
-			// Pointer is two pointers from the beginning of the method descriptor
-			Marshal.WriteIntPtr(sourceMethodDescriptorAddress, 2 * IntPtr.Size, targetMethodMachineCodeAddress);
-		}
-
-		public class MyClass
-		{
-			public Task OpenAsync()
-			{
-				Console.WriteLine("Sdfsdf");
-				return Task.CompletedTask;
-			}
-		}
-		
-		[TestMethod]
-		[ExpectedException(typeof(InvalidOperationException))]
-		[Timeout(300)]
-		public async Task MigrateAsync1()
-		{
-			var sql = new MySqlConnection(null);
-			HijackMethod(typeof(MySqlConnection), nameof(MySqlConnection.OpenAsync), 
-				typeof(MyClass), nameof(MyClass.OpenAsync));
-			
-			await sql.OpenAsync(); // only for test
-			
-			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-				.UseInMemoryDatabase(databaseName: "MovieListDatabase")
-				.Options;
-			
-			await RunMigrations.MysqlFixes(sql,
-				new AppSettings
-				{
-					DatabaseType = AppSettings.DatabaseTypeList.Mysql
-				}, new AppDbMySqlException2(options));
-		}
-
 	}
 }
