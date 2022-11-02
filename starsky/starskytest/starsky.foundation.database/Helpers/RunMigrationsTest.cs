bef048d3be20c7e8cc5f1507ca692945306d6c5f
Dion <qdraw@users.noreply.github.com>
2022-11-02 20:33:11 +0100

    add test

--- starsky/starskytest/starsky.foundation.database/Helpers/RunMigrationsTest.cs
+++ starsky/starskytest/starsky.foundation.database/Helpers/RunMigrationsTest.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -42,44 +47,43 @@ namespace starskytest.starsky.foundation.database.Helpers
 			// expect exception: Relational-specific methods can only be used when the context is using a relational database provider.
 		}
 
+		private static MySqlException CreateMySqlException(string message)
+		{
+			var info = new SerializationInfo(typeof(Exception),
+				new FormatterConverter());
+			info.AddValue("Number", 1);
+			info.AddValue("SqlState", "SqlState");
+			info.AddValue("Message", message);
+			info.AddValue("InnerException", new Exception());
+			info.AddValue("HelpURL", "");
+			info.AddValue("StackTraceString", "");
+			info.AddValue("RemoteStackTraceString", "");
+			info.AddValue("RemoteStackIndex", 1);
+			info.AddValue("HResult", 1);
+			info.AddValue("Source", "");
+			info.AddValue("WatsonBuckets",  Array.Empty<byte>() );
+					
+			// private MySqlException(SerializationInfo info, StreamingContext context)
+			var ctor =
+				typeof(MySqlException).GetConstructors(BindingFlags.Instance |
+					BindingFlags.NonPublic | BindingFlags.InvokeMethod).FirstOrDefault();
+			var instance =
+				( MySqlException ) ctor.Invoke(new object[]
+				{
+					info,
+					new StreamingContext(StreamingContextStates.All)
+				});
+			return instance;
+		}
+		
 		private class AppDbMySqlException : ApplicationDbContext
 		{
 			public AppDbMySqlException(DbContextOptions options) : base(options)
 			{
 			}
 
-			public override DatabaseFacade Database
-			{
-				get
-				{
-					var info = new SerializationInfo(typeof(Exception),
-						new FormatterConverter());
-					info.AddValue("Number", 1);
-					info.AddValue("SqlState", "SqlState");
-					info.AddValue("Message", "");
-					info.AddValue("InnerException", new Exception());
-					info.AddValue("HelpURL", "");
-					info.AddValue("StackTraceString", "");
-					info.AddValue("RemoteStackTraceString", "");
-					info.AddValue("RemoteStackIndex", 1);
-					info.AddValue("HResult", 1);
-					info.AddValue("Source", "");
-					info.AddValue("WatsonBuckets",  Array.Empty<byte>() );
-					
-					// private MySqlException(SerializationInfo info, StreamingContext context)
-					var ctor =
-						typeof(MySqlException).GetConstructors(BindingFlags.Instance |
-							BindingFlags.NonPublic | BindingFlags.InvokeMethod).FirstOrDefault();
-					var instance =
-						( MySqlException ) ctor.Invoke(new object[]
-						{
-							info,
-							new StreamingContext(StreamingContextStates.All)
-						});
+			public override DatabaseFacade Database => throw CreateMySqlException("");
 
-					throw instance;
-				}
-			}
 		}
 
 		[TestMethod]
@@ -94,6 +98,67 @@ namespace starskytest.starsky.foundation.database.Helpers
 				new AppDbMySqlException(options), new FakeIWebLogger(),new AppSettings{DatabaseType = AppSettings.DatabaseTypeList.Mysql},1);
 			
 			// should not crash
+		}
+
+		
+
+		private class AppDbMySqlException2 : ApplicationDbContext
+		{
+			public AppDbMySqlException2(DbContextOptions options) : base(options)
+			{
+			}
+
+			public override DatabaseFacade Database => new DatabaseFacade(this);
+
+
+		}
+		
+		public static void HijackMethod(Type sourceType, string sourceMethod, Type targetType, string targetMethod)
+		{
+			// Get methods using reflection
+			var source = sourceType.GetMethod(sourceMethod,new []{typeof(CancellationToken)});
+			var target = targetType.GetMethod(targetMethod);
+
+			// Prepare methods to get machine code (not needed in this example, though)
+			RuntimeHelpers.PrepareMethod(source.MethodHandle);
+			RuntimeHelpers.PrepareMethod(target.MethodHandle);
+
+			var sourceMethodDescriptorAddress = source.MethodHandle.Value;
+			var targetMethodMachineCodeAddress = target.MethodHandle.GetFunctionPointer();
+
+			// Pointer is two pointers from the beginning of the method descriptor
+			Marshal.WriteIntPtr(sourceMethodDescriptorAddress, 2 * IntPtr.Size, targetMethodMachineCodeAddress);
+		}
+
+		public class MyClass
+		{
+			public Task OpenAsync()
+			{
+				Console.WriteLine("Sdfsdf");
+				return Task.CompletedTask;
+			}
+		}
+		
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		[Timeout(300)]
+		public async Task MigrateAsync1()
+		{
+			var sql = new MySqlConnection(null);
+			HijackMethod(typeof(MySqlConnection), nameof(MySqlConnection.OpenAsync), 
+				typeof(MyClass), nameof(MyClass.OpenAsync));
+			
+			await sql.OpenAsync(); // only for test
+			
+			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+				.UseInMemoryDatabase(databaseName: "MovieListDatabase")
+				.Options;
+			
+			await RunMigrations.MysqlFixes(sql,
+				new AppSettings
+				{
+					DatabaseType = AppSettings.DatabaseTypeList.Mysql
+				}, new AppDbMySqlException2(options));
 		}
 
 	}
