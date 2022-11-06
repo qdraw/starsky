c742c3e76662fcc216f2611201742bd82e55ff1f
Dion <qdraw@users.noreply.github.com>
2022-11-06 17:08:47 +0100

    fixes for startup

--- starsky/starsky.foundation.database/DataProtection/SqlXmlRepository.cs
+++ starsky/starsky.foundation.database/DataProtection/SqlXmlRepository.cs
@@ -3,78 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using starsky.foundation.database.Data;
-using starsky.foundation.database.Models;
 using starsky.foundation.database.Query;
 using starsky.foundation.injection;
 using starsky.foundation.platform.Helpers;
 
-namespace starsky.foundation.database.DataProtection;
-
-[Service(typeof(IXmlRepository), InjectionLifetime = InjectionLifetime.Scoped)]
-public class SqlXmlRepository : IXmlRepository
+namespace starsky.foundation.database.DataProtection
 {
-	private readonly ApplicationDbContext _dbContext;
-	private readonly IServiceScopeFactory _scopeFactory;
-
-	public SqlXmlRepository(ApplicationDbContext dbContext, IServiceScopeFactory scopeFactory)
+	[Service(typeof(IXmlRepository), InjectionLifetime = InjectionLifetime.Scoped)]
+	public class SqlXmlRepository : IXmlRepository
 	{
-		_dbContext = dbContext;
-		_scopeFactory = scopeFactory;
-	}
-
-	public IReadOnlyCollection<XElement> GetAllElements()
-	{
-		try
+		private readonly ApplicationDbContext _dbContext;
+		private readonly IServiceScopeFactory _scopeFactory;
+    
+		public SqlXmlRepository(ApplicationDbContext dbContext, IServiceScopeFactory scopeFactory)
 		{
-			var result = _dbContext.DataProtectionKeys
-				.Where(p => p.Xml != null).AsEnumerable()
-				.Select(x => XElement.Parse(x.Xml!)).ToList();
-			
-			return result;
+			_dbContext = dbContext;
+			_scopeFactory = scopeFactory;
 		}
-		catch ( MySqlConnector.MySqlException exception )
+    
+		public IReadOnlyCollection<XElement> GetAllElements()
 		{
-			// MySqlConnector.MySqlException (0x80004005): Table 'starsky.DataProtectionKeys' doesn't exist
-			if ( exception.Message.Contains("0x80004005") && exception.Message.Contains("DataProtectionKeys") )
+			try
 			{
-				_dbContext.Database.EnsureCreated();
+				var result = _dbContext.DataProtectionKeys
+					.Where(p => p.Xml != null).AsEnumerable()
+					.Select(x => XElement.Parse(x.Xml!)).ToList();
+    
+				return result;
 			}
-			
-			return new List<XElement>();
-		}
-	}
-
-	public void StoreElement(XElement element, string friendlyName)
-	{
-		bool LocalDefault(ApplicationDbContext ctx)
-		{
-			ctx.DataProtectionKeys.Add(new DataProtectionKey
+			catch ( Exception exception )
 			{
-				Xml = element.ToString(SaveOptions.DisableFormatting),
-				FriendlyName = friendlyName
-			});
-			ctx.SaveChanges();
-			return true;
+				if ( exception is not MySqlConnector.MySqlException &&
+				     exception is not Microsoft.Data.Sqlite.SqliteException ) throw;
+    			
+				// MySqlConnector.MySqlException (0x80004005): Table 'starsky.DataProtectionKeys' doesn't exist
+				// or Microsoft.Data.Sqlite.SqliteException (0x80004005): SQLite Error 1: 'no such table: DataProtectionKeys
+				if ( exception.Message.Contains("0x80004005") && exception.Message.Contains("DataProtectionKeys") )
+				{
+					_dbContext.Database.Migrate();
+				}
+				return new List<XElement>();
+			}
 		}
-		
-		bool LocalDefaultQuery()
+    
+		public void StoreElement(XElement element, string friendlyName)
 		{
-			var context = new InjectServiceScope(_scopeFactory).Context();
-			return LocalDefault(context);
-		}
-		
-		try
-		{
-			LocalDefault(_dbContext);
-		}
-		catch ( Microsoft.EntityFrameworkCore.DbUpdateException )
-		{
-			var retryInterval = _dbContext.GetType().FullName?.Contains("test") == true ? 
-				TimeSpan.FromSeconds(0) : TimeSpan.FromSeconds(5);
-			RetryHelper.Do(
-				LocalDefaultQuery, retryInterval, 2);
+			bool LocalDefault(ApplicationDbContext ctx)
+			{
+				ctx.DataProtectionKeys.Add(new DataProtectionKey
+				{
+					Xml = element.ToString(SaveOptions.DisableFormatting),
+					FriendlyName = friendlyName
+				});
+				ctx.SaveChanges();
+				return true;
+			}
+    		
+			bool LocalDefaultQuery()
+			{
+				var context = new InjectServiceScope(_scopeFactory).Context();
+				return LocalDefault(context);
+			}
+    		
+			try
+			{
+				LocalDefault(_dbContext);
+			}
+			catch ( DbUpdateException )
+			{
+				var retryInterval = _dbContext.GetType().FullName?.Contains("test") == true ? 
+					TimeSpan.FromSeconds(0) : TimeSpan.FromSeconds(5);
+				RetryHelper.Do(
+					LocalDefaultQuery, retryInterval, 2);
+			}
 		}
 	}
 }
