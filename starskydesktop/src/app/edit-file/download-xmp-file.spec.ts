d888a409f7a6429ba8643c82356eebb4b0bdfba3
Dion <qdraw@users.noreply.github.com>
2022-11-14 18:36:56 +0100

    add test

---
+++ starskydesktop/src/app/edit-file/download-xmp-file.spec.ts
@@ -0,0 +1,26 @@
+import * as GetBaseUrlFromSettings from "../config/get-base-url-from-settings";
+import { IlocationUrlSettings } from "../config/IlocationUrlSettings";
+import * as downloadNetRequest from "../net-request/download-net-request";
+import { downloadXmpFile } from "./download-xmp-file";
+import * as GetParentDiskPath from "./get-parent-disk-path";
+
+describe("downloadXmpFile", () => {
+  it("should download", async () => {
+    jest.spyOn(GetParentDiskPath, "GetParentDiskPath").mockImplementationOnce(() => { return Promise.resolve("test"); });
+    jest.spyOn(downloadNetRequest, "downloadNetRequest").mockImplementationOnce(() => { return Promise.resolve("test"); });
+    jest.spyOn(GetBaseUrlFromSettings, "GetBaseUrlFromSettings").mockImplementationOnce(() => {
+      return Promise.resolve({
+        location: ""
+      } as IlocationUrlSettings);
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = await downloadXmpFile({
+      collectionPaths: ["test"],
+      sidecarExtensionsList: ["test"]
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    } as any, {} as any);
+
+    expect(result).toBe("test/undefined.test");
+  });
+});
