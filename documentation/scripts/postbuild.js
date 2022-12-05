a3c7938047ba9146f187d8906a662fb78f93875f
Dion <qdraw@users.noreply.github.com>
2022-12-05 22:00:12 +0100

    add version to docs

--- documentation/scripts/postbuild.js
+++ documentation/scripts/postbuild.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const path = require("path");
+const { spawnSync } = require('child_process');
 
 const sourceHtaccess = path.join(__dirname, "..", ".htaccess");
 const toHtaccess = path.join(__dirname, "..", "build", ".htaccess");
@@ -14,3 +15,22 @@ const toFavicon = path.join(__dirname, "..", "build", "favicon.ico");
 
 console.log(`${sourceFavicon} -> ${toFavicon}`);
 fs.copyFileSync(sourceFavicon, toFavicon);
+
+
+const gitHash = spawnSync("git", ["log", "-1", "--format=\"%H\""], {
+    cwd: __dirname,
+    env: process.env,
+    encoding: "utf-8"
+  });
+
+if (!gitHash.stdout) {
+    return;
+}
+
+const hash = gitHash.stdout.replace(/\"|\n/ig,"")
+
+const versionTextPath = path.join(__dirname, "..", "build", "version.txt");
+
+console.log(versionTextPath + " -> " + hash);
+
+fs.writeFileSync(versionTextPath, hash)
