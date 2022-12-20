dc1f566b6dd173d5074a17f32518066691f92698
Dion <qdraw@users.noreply.github.com>
2022-12-20 10:41:48 +0100

    update links

--- documentation/Program.cs
+++ documentation/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+static string  GetSolutionParentFolder()
+{
+	var strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+	return Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(strExeFilePath))))!;
+}
+
+static Process NpmCommand(string command)
+{
+	var startInfo = new ProcessStartInfo()
+	{
+		FileName = "npm", 
+		Arguments = command,
+		WorkingDirectory = GetSolutionParentFolder()
+	}; 
+	var proc = new Process() { StartInfo = startInfo };
+	return proc;
+}
+
+NpmCommand("run start").Start();
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadLine();
