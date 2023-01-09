84b5fd0a8bb5f2fb3830c26bacda9356fad3e9e3
Dion <qdraw@users.noreply.github.com>
2023-01-09 18:46:44 +0100

    add test

--- starsky/starskytest/FakeMocks/FakeIThumbnailQuery.cs
+++ starsky/starskytest/FakeMocks/FakeIThumbnailQuery.cs
@@ -26,35 +26,52 @@ public class FakeIThumbnailQuery : IThumbnailQuery
 	{
 		// should bind to the context
 	}
-	
-	// public Task<List<ThumbnailItem>?> AddThumbnailRangeAsync(List<ThumbnailSize> sizes, IReadOnlyCollection<string> fileHashes,
-	// 	bool? setStatus = null)
-	// {
-	// 	foreach ( var hash in fileHashes )
-	// 	{
-	// 		var index = _content.FindIndex(p => p.FileHash == hash);
-	// 		if ( index == -1 )
-	// 		{
-	// 			foreach ( var size in sizes )
-	// 			{
-	// 				_content.Add(new ThumbnailItem(hash, size, setStatus));
-	// 			}
-	// 			continue;
-	// 		}
-	//
-	// 		foreach ( var size in sizes )
-	// 		{
-	// 			_content[index].Change(size, setStatus);
-	// 		}
-	//
-	// 	}
-	// 	
-	// 	return Task.FromResult(_content)!;
-	// }
 
 	public Task<List<ThumbnailItem>?> AddThumbnailRangeAsync(List<ThumbnailResultDataTransferModel> thumbnailItems)
 	{
-		return Task.FromResult(new List<ThumbnailItem>());
+		var results = new List<ThumbnailItem?>();
+		foreach ( var thumbnailItem in thumbnailItems )
+		{
+			var index = _content.FindIndex(p => p.FileHash == thumbnailItem.FileHash);
+			if ( index == -1 )
+			{
+				var item = new ThumbnailItem()
+				{
+					FileHash = thumbnailItem.FileHash,
+					Large = thumbnailItem.Large,
+					ExtraLarge = thumbnailItem.ExtraLarge,
+					Reasons = thumbnailItem.Reasons,
+					Small = thumbnailItem.Small,
+					TinyMeta = thumbnailItem.TinyMeta
+				};
+				_content.Add(item);
+				results.Add(item);
+				continue;
+			}
+
+			if ( thumbnailItem.Large != null )
+			{
+				_content[index].Large = thumbnailItem.Large;
+			}
+			if ( thumbnailItem.ExtraLarge != null )
+			{
+				_content[index].ExtraLarge = thumbnailItem.ExtraLarge;
+			}
+			if ( thumbnailItem.Reasons != null )
+			{
+				_content[index].Reasons = thumbnailItem.Reasons;
+			}
+			if ( thumbnailItem.Small != null )
+			{
+				_content[index].Small = thumbnailItem.Small;
+			}
+			if ( thumbnailItem.TinyMeta != null )
+			{
+				_content[index].TinyMeta = thumbnailItem.TinyMeta;
+			}
+			results.Add(_content[index]);
+		}
+		return Task.FromResult(results)!;
 	}
 
 	public Task<List<ThumbnailItem>> Get(string? fileHash = null)
