0cc49c9794c78443dbdc349da98e5a3eb195a1bf
Dion <qdraw@users.noreply.github.com>
2023-01-12 18:16:17 +0100

    add test

---
+++ starsky/starskytest/starsky.foundation.writemeta/Services/ExifToolServiceTest.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Medallion.Shell;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using starsky.foundation.platform.Models;
+using starsky.foundation.storage.Helpers;
+using starsky.foundation.storage.Storage;
+using starsky.foundation.writemeta.Services;
+using starskytest.FakeCreateAn;
+using starskytest.FakeMocks;
+
+namespace starskytest.starsky.foundation.writemeta.Services;
+
+[TestClass]
+public class ExifToolServiceTest
+{
+	private readonly string _exifToolPath;
+
+	public ExifToolServiceTest()
+	{
+		if ( !new AppSettings().IsWindows )
+		{
+			var stream = PlainTextFileHelper.StringToStream("#!/bin/bash\necho Fake ExifTool");
+			_exifToolPath = Path.Join(new CreateAnImage().BasePath, "exiftool-tmp");
+			new StorageHostFullPathFilesystem().WriteStream(stream,
+				_exifToolPath);
+
+			var result = Command.Run("chmod", "+x",
+				_exifToolPath).Task.Result;
+			if ( !result.Success )
+			{
+				throw new FileNotFoundException(result.StandardError);
+			}
+		}
+
+	}
+	[TestMethod]
+	public async Task WriteTagsAndRenameThumbnailAsync__UnixOnly()
+	{
+		if ( new AppSettings().IsWindows )
+		{
+			Assert.Inconclusive("This test if for Unix Only");
+			return;
+		}
+		
+		var storage = new FakeIStorage(new List<string>{"/"}, 
+			new List<string>{"/image.jpg"}, new List<byte[]>
+			{
+				CreateAnImage.Bytes
+			});
+		
+		var service = new ExifToolService(new FakeSelectorStorage(storage), new AppSettings
+		{
+			ExifToolPath = _exifToolPath
+		}, new FakeIWebLogger());
+		
+		var result = await service.WriteTagsAndRenameThumbnailAsync("/image.jpg", 
+			null, "");
+		
+		Assert.AreEqual(false,result.Key);
+	}
+}
