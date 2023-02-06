8526a84c1e6c58668228ad15e9acf4eba6c30628
Dion <qdraw@users.noreply.github.com>
2023-02-06 23:48:22 +0100

    this should work [ except ssl issue]

--- starsky/starsky/service-deploy-windows.ps1
+++ starsky/starsky/service-deploy-windows.ps1
@@ -26,7 +26,11 @@ if($IsWindows -eq $False) {
 
 if ($help -eq $True) {
     write-host "help"
-
+    write-host "-port 5000"
+    write-host "-anyWhere"
+    write-host "-output folder_path"
+    write-host "-serviceName name_service"
+    write-host "-exeName starsky.exe"
     exit 0
 }
 
@@ -40,12 +44,14 @@ $exePath = Join-Path $outPut $exeName
 
 
 if ((Test-Path -Path $exePath) -eq $false) {
-    write-host "Path doesn't exist." $exePath
+    write-host "-output path doesn't exist." $exePath
     write-host "end script due fail"
     exit 1
 } 
 
 write-host $exePath
+
+Invoke-Expression -Command "taskkill /F /IM mmc.exe"
 
 $currentService = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
 
@@ -60,17 +66,82 @@ If([string]::IsNullOrWhitespace($currentService) -eq $false) {
     # or sc.exe delete ServiceName
 }
 
-$binaryPathName = '"'+ $exePath + '"'
+$appSettingsPath = Join-Path -Path $outPut -ChildPath "appsettings.json"
 
-write-host  "test"
-write-host $binaryPathName
+$jsonAppSettings = (Get-Content -Raw $appSettingsPath | ConvertFrom-Json )
 
-$params = @{
-  Name = $serviceName
-  BinaryPathName = $exePath
-  DependsOn = "NetLogon"
-  DisplayName = "Test Service"
-  StartupType = "Automatic"
-  Description = "This is a test service."
+write-host $jsonAppSettings
+
+function ReinstallService ($localServiceName, $binaryPath, $description, $login, $password, $startUpType, $displayName)
+{
+    Write-Host "Trying to create service: $localServiceName - $binaryPath"
+
+    #Check Parameters
+    if ((Test-Path $binaryPath)-eq $false)
+    {
+        Write-Host "BinaryPath to service not found: $binaryPath"
+        Write-Host "Service was NOT installed."
+        return
+    }
+
+    if (("Automatic", "Manual", "Disabled") -notcontains $startUpType)
+    {
+        Write-Host "Value for startUpType parameter should be (Automatic or Manual or Disabled) and it was $startUpType"
+        Write-Host "Service was NOT installed."
+        return
+    }
+
+    # Verify if the service already exists, and if yes remove it first
+    if (Get-Service $localServiceName -ErrorAction SilentlyContinue)
+    {
+        # using WMI to remove Windows service because PowerShell does not have CmdLet for this
+        $serviceToRemove = Get-WmiObject -Class Win32_Service -Filter "name='$localServiceName'"
+
+        $serviceToRemove.delete()
+        Write-Host "Service removed: $localServiceName"
+    }
+
+    # if password is empty, create a dummy one to allow have credentias for system accounts: 
+    #NT AUTHORITY\LOCAL SERVICE
+    #NT AUTHORITY\NETWORK SERVICE
+    if ($password -eq "") 
+    {
+        #$secpassword = (new-object System.Security.SecureString)
+        # Bug detected by @GaTechThomas
+        $secpasswd = (new-object System.Security.SecureString)
+    }
+    else
+    {
+        $secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
+    }
+    $mycreds = New-Object System.Management.Automation.PSCredential ($login, $secpasswd)
+
+    # Creating Windows Service using all provided parameters
+    Write-Host "Installing service: $localServiceName"
+    New-Service -name $localServiceName -binaryPathName $binaryPath -Description $description -displayName $displayName -startupType $startUpType -credential $mycreds
+
+    Write-Host "Installation completed: $localServiceName"
+
+    # Trying to start new service
+    Write-Host "Trying to start new service: $localServiceName"
+    $serviceToStart = Get-WmiObject -Class Win32_Service -Filter "name='$localServiceName'"
+    $serviceToStart.startservice()
+    Write-Host "Service started: $localServiceName"
+
+    #SmokeTest
+    Write-Host "Waiting 5 seconds to give time service to start..."
+    Start-Sleep -s 5
+    $SmokeTestService = Get-Service -Name $localServiceName
+    if ($SmokeTestService.Status -ne "Running")
+    {
+        Write-Host "Smoke test: FAILED. (SERVICE FAILED TO START)"
+        Throw "Smoke test: FAILED. (SERVICE FAILED TO START)"
+    }
+    else
+    {
+        Write-Host "Smoke test: OK."
+    }
+  # https://stackoverflow.com/questions/14708825/how-to-create-a-windows-service-in-powershell-for-network-service-account
 }
-New-Service @params
+
+ReinstallService $serviceName $exePath "Windows service" "NT AUTHORITY\NETWORK SERVICE" "" "Automatic" "Starsky Web App"
