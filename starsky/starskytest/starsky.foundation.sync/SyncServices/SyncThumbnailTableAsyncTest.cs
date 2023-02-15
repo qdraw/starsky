f671d57e6c496a911babe0a3f5f25414a78a6fe4
Dion <qdraw@users.noreply.github.com>
2023-02-15 18:28:08 +0100

    rm usings && add extra time

--- starsky/starskytest/starsky.foundation.sync/SyncServices/SyncThumbnailTableAsyncTest.cs
+++ starsky/starskytest/starsky.foundation.sync/SyncServices/SyncThumbnailTableAsyncTest.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.database.Data;
 using starsky.foundation.database.Models;
