7a934450220b09b19c442144ad4a38dfa5d67508
Dion <qdraw@users.noreply.github.com>
2023-02-15 22:17:13 +0100

    fix test

--- starsky/starskytest/starsky.foundation.platform/Trash/WindowsShellTrashBindingHelperTest.cs
+++ starsky/starskytest/starsky.foundation.platform/Trash/WindowsShellTrashBindingHelperTest.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.native.Trash;
 
@@ -10,14 +11,14 @@ public class WindowsShellTrashBindingHelperTest
 	[TestMethod]
 	public void WindowsShellTrashBindingHelper1()
 	{
-		var result = WindowsShellTrashBindingHelper.Trash("C:\\temp\\test.bmp");
+		var result = WindowsShellTrashBindingHelper.Trash("C:\\temp\\test.bmp", OSPlatform.Windows);
 		Assert.AreEqual(false,result.Item1);
 	}
 	
 	[TestMethod]
 	public void WindowsShellTrashBindingHelper12()
 	{
-		var result = WindowsShellTrashBindingHelper.Trash("C:\\temp\\test.bmp");
+		var result = WindowsShellTrashBindingHelper.Trash("C:\\temp\\test.bmp", OSPlatform.Windows);
 		Assert.AreEqual(false,result.Item1);
 	}
 }
