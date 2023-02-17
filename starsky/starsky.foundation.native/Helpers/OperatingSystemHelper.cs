b6c9e21eb11ed655e72d274eef6e06db596c2ad3
Dion <qdraw@users.noreply.github.com>
2023-02-17 23:20:09 +0100

    add test

--- starsky/starsky.foundation.native/Helpers/OperatingSystemHelper.cs
+++ starsky/starsky.foundation.native/Helpers/OperatingSystemHelper.cs
@@ -4,19 +4,26 @@ namespace starsky.foundation.native.Helpers;
 
 public static class OperatingSystemHelper
 {
-	public static OSPlatform GetPlatform() {
-		if ( RuntimeInformation.IsOSPlatform(OSPlatform.Windows) )
+	public static OSPlatform GetPlatform()
+	{
+		return GetPlatformInternal(RuntimeInformation.IsOSPlatform);
+	}
+
+	internal delegate bool IsOsPlatformDelegate(OSPlatform osPlatform);
+
+	internal static OSPlatform GetPlatformInternal(IsOsPlatformDelegate isOsPlatformDelegate) {
+		if ( isOsPlatformDelegate(OSPlatform.Windows) )
 		{
 			return OSPlatform.Windows;
 		}
-		if ( RuntimeInformation.IsOSPlatform(OSPlatform.OSX) )
+		if ( isOsPlatformDelegate(OSPlatform.OSX) )
 		{
 			return OSPlatform.OSX;
 		}
-		if ( RuntimeInformation.IsOSPlatform(OSPlatform.Linux) )
+		if ( isOsPlatformDelegate(OSPlatform.Linux) )
 		{
 			return OSPlatform.Linux;
 		}
-		return RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD) ? OSPlatform.FreeBSD : OSPlatform.Create("Unknown");
+		return isOsPlatformDelegate(OSPlatform.FreeBSD) ? OSPlatform.FreeBSD : OSPlatform.Create("Unknown");
 	}
 }
