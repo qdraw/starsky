af3a0df69fb6f0c9fa560a5c2a1a5ac983b35bec
Dion <qdraw@users.noreply.github.com>
2023-02-22 17:01:22 +0100

    suppress

--- starsky/starsky/clientapp/src/shared/validate-login-form.ts
+++ starsky/starsky/clientapp/src/shared/validate-login-form.ts
@@ -23,6 +23,7 @@ export const validateLoginForm = (
 
 function validateEmail(email: string) {
   // before feb'22:    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // NOSONAR
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
