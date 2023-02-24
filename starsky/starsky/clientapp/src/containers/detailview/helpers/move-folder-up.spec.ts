394780375ec59cb3353739d2c225cbb686d1488b
Dion <qdraw@users.noreply.github.com>
2023-02-24 20:37:50 +0100

    add test

--- starsky/starsky/clientapp/src/containers/detailview/helpers/move-folder-up.spec.ts
+++ starsky/starsky/clientapp/src/containers/detailview/helpers/move-folder-up.spec.ts
@@ -1,6 +1,8 @@
 import { HistoryLocation } from "@reach/router";
+import { ModalOpenClassName } from "../../../components/atoms/modal/modal";
 import { IUseLocation } from "../../../hooks/use-location";
 import { IDetailView } from "../../../interfaces/IDetailView";
+import { Keyboard } from "../../../shared/keyboard";
 import { moveFolderUp } from "./move-folder-up";
 
 describe("statusRemoved", () => {
@@ -35,5 +37,41 @@ describe("statusRemoved", () => {
     } as IDetailView);
 
     expect(history.navigate).toHaveBeenCalled();
+  });
+
+  it("should not navigate due isInForm", () => {
+    const history = { location: {} as HistoryLocation } as IUseLocation;
+    history.navigate = jest.fn();
+    jest
+      .spyOn(Keyboard.prototype, "isInForm")
+      .mockImplementationOnce(() => true);
+
+    moveFolderUp(new KeyboardEvent("delete"), history, true, {
+      fileIndexItem: {
+        filePath: "/"
+      }
+    } as IDetailView);
+
+    expect(history.navigate).toHaveBeenCalledTimes(0);
+  });
+
+  it("should not navigate due portal", () => {
+    const history = { location: {} as HistoryLocation } as IUseLocation;
+    history.navigate = jest.fn();
+    jest
+      .spyOn(Keyboard.prototype, "isInForm")
+      .mockImplementationOnce(() => false);
+
+    const portalDivName = document.createElement("div");
+    portalDivName.className = ModalOpenClassName;
+    document.body.appendChild(portalDivName);
+
+    moveFolderUp(new KeyboardEvent("delete"), history, true, {
+      fileIndexItem: {
+        filePath: "/"
+      }
+    } as IDetailView);
+
+    expect(history.navigate).toHaveBeenCalledTimes(0);
   });
 });
