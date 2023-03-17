f0bc270f6a73a374a87641f7c40cb7dbad449123
Dion <qdraw@users.noreply.github.com>
2023-03-17 20:05:50 +0100

    add extra test

--- starsky/starsky/clientapp/src/components/atoms/modal/modal-insert-portal-div.spec.ts
+++ starsky/starsky/clientapp/src/components/atoms/modal/modal-insert-portal-div.spec.ts
@@ -13,6 +13,25 @@ describe("modalInserPortalDiv", () => {
     expect(element).not.toBeNull();
   });
 
+  it("should not add div element when already exists", () => {
+    const exampleDiv = document.createElement("div");
+    exampleDiv.id = "test-id-2";
+    exampleDiv.innerHTML = "test";
+
+    document.body.appendChild(exampleDiv);
+    modalInsertPortalDiv(
+      { current: null } as any,
+      false,
+      jest.fn() as any,
+      "test-id-2"
+    );
+
+    const element = document.querySelector("#test-id-2");
+    expect(element).not.toBeNull();
+    expect(element?.innerHTML).not.toBeNull();
+    expect(element?.innerHTML).toBe("test");
+  });
+
   it("should setForceUpdate", () => {
     const forceUpdateSpy = jest.fn();
     modalInsertPortalDiv(
