0741d1af7e4b2b9c0e30b5c59f388481607b89ed
Dion <qdraw@users.noreply.github.com>
2023-03-21 18:11:41 +0100

    rename

--- starsky/starsky.foundation.platform/Models/MetadataContainer.cs
+++ starsky/starsky.foundation.platform/Models/MetadataContainer.cs
@@ -4,8 +4,12 @@ namespace starsky.foundation.platform.Models;
 
 public class MetadataContainer
 {
+	/// <summary>
+	/// Only for JsonSchema
+	/// And Keep on top of the class
+	/// </summary>
 	[JsonPropertyName("$id")]
-	public string Id { get; set; } = "https://docs.qdraw.nl/openapi/schema.json";
+	public string Id { get; set; } = "https://docs.qdraw.nl/schema/meta-data-container.json";
 	
 	[JsonPropertyName("$schema")]
 	public string Schema { get; set; } = "https://json-schema.org/draft/2020-12/schema";
