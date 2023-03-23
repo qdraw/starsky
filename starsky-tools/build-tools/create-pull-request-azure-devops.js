2cf1be0bd63a0af6b3c23439fcfae3cb4b72e935
Dion <qdraw@users.noreply.github.com>
2023-03-23 20:33:46 +0100

    sharing is caring

--- starsky-tools/build-tools/create-pull-request-azure-devops.js
+++ starsky-tools/build-tools/create-pull-request-azure-devops.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
 
 const { join } = require("path");
-const { exit } = require('process');
-const { httpsPost } = require('./lib/https-post.js');
+const { exit } = require("process");
+const { httpsPost } = require("./lib/https-post.js");
 const { prefixPath } = require("./lib/prefix-path.const.js");
 const { httpsGet } = require("./lib/https-get.js");
 
 let searchPath = join(__dirname, prefixPath);
 
 // Which company? and prefix for devops
-// set: System.CollectionUri 
+// set: System.CollectionUri
 // should contain slash at end
 let systemCollectionUri = process.env.SYSTEM_COLLECTIONURI;
 // systemCollectionUri = "https://dev.azure.com/wea*****/";
 // System.CollectionUri = https://dev.azure.com/fabrikamfiber/
 if (!systemCollectionUri) {
-    console.log('SYSTEM_COLLECTIONURI is not defined');
-    exit(1);
+	console.log("SYSTEM_COLLECTIONURI is not defined");
+	exit(1);
 }
 
 // Which project name?
@@ -24,21 +24,19 @@ if (!systemCollectionUri) {
 let systemTeamProject = process.env.SYSTEM_TEAM_PROJECT;
 // systemTeamProject = "S***O***";
 if (!systemTeamProject) {
-    console.log('SYSTEM_TEAM_PROJECT is not defined');
-    exit(1);
+	console.log("SYSTEM_TEAM_PROJECT is not defined");
+	exit(1);
 }
-
 
 // Which repo
 // set: Build.Repository.ID
-let buildRepositoryID = process.env.BUILD_REPOSITORY_ID; 
+let buildRepositoryID = process.env.BUILD_REPOSITORY_ID;
 // buildRepositoryID = "My******"
 // eg. 3411ebc1-d5aa-464f-9615-0b527bc66719
 if (!buildRepositoryID) {
-    console.log('BUILD_REPOSITORY_ID is not defined');
-    exit(1);
+	console.log("BUILD_REPOSITORY_ID is not defined");
+	exit(1);
 }
-
 
 // Which branch the PR is created from
 // set: Build.SourceBranch
@@ -48,10 +46,9 @@ let buildSourceBranch = process.env.BUILD_SOURCE_BRANCH;
 // buildSourceBranch = "refs/heads/feature/202209_ocelot_external"
 
 if (!buildSourceBranch) {
-    console.log('BUILD_SOURCE_BRANCH is not defined');
-    exit(1);
+	console.log("BUILD_SOURCE_BRANCH is not defined");
+	exit(1);
 }
-
 
 // Where to branch name
 // set: name of master/main/dev branch -- refs/heads/ prefix needed
@@ -59,15 +56,16 @@ let targetBranch = process.env.TARGET_BRANCH;
 // targetBranch = "refs/heads/development"
 
 if (!targetBranch) {
-    console.log('TARGET_BRANCH is not defined');
-    exit(1);
+	console.log("TARGET_BRANCH is not defined");
+	exit(1);
 }
 
-// $(System.AccessToken)    
-let personalAccessToken = process.env.WEAREYOU_DEVOPS_PAT || process.env.SYSTEM_ACCESSTOKEN;
+// $(System.AccessToken)
+let personalAccessToken =
+	process.env.WEAREYOU_DEVOPS_PAT || process.env.SYSTEM_ACCESSTOKEN;
 if (!personalAccessToken) {
-    console.log('PAT is not defined');
-    exit(1);
+	console.log("SYSTEM_ACCESSTOKEN or WEAREYOU_DEVOPS_PAT is not defined");
+	exit(1);
 }
 
 const argv = process.argv.slice(2);
@@ -77,7 +75,7 @@ if (argv) {
 	for (const argItem of argv) {
 		if (existsSync(argItem)) {
 			searchPath = argItem;
-			console.log(`use: path: ${argItem}`)
+			console.log(`use: path: ${argItem}`);
 		}
 	}
 }
@@ -87,87 +85,95 @@ console.log(`searchPath: ${searchPath}`);
 const url = `${systemCollectionUri}${systemTeamProject}/_apis/git/repositories/${buildRepositoryID}/pullrequests?searchCriteria.sourceRefName=${buildSourceBranch}&api-version=7.0`;
 
 // First check if there is already a PR for this branch
-const base64authorizationHeader = 'Basic ' + Buffer.from(":" + personalAccessToken).toString('base64', 'utf8');
-httpsGet(url, base64authorizationHeader).then((data)=>{
-    if (data.count === 1 && data.value.length === 1) {
-        console.log("PR already exists");
-        console.log(data.value[0].pullRequestId);
-        console.log(data.value[0].url);
-        return;
-    }
+const base64authorizationHeader =
+	"Basic " +
+	Buffer.from(":" + personalAccessToken).toString("base64", "utf8");
+httpsGet(url, base64authorizationHeader).then((data) => {
+	if (data.count === 1 && data.value.length === 1) {
+		console.log("PR already exists");
+		console.log(data.value[0].pullRequestId);
+		console.log(data.value[0].url);
+		return;
+	}
 
-    if (data.count === 0) {
-        // if there is no pr, check if the branch exists
-        checkIfBranchExists().then(()=>{    
-            console.log('No PR found, creating one');
-            createPullRequest();
-        }).catch((err)=>{
-            console.log('checking branch exists failed');
-            console.log(err);
-        });
-        return;
-    }
+	if (data.count === 0) {
+		// if there is no pr, check if the branch exists
+		checkIfBranchExists()
+			.then(() => {
+				console.log("No PR found, creating one");
+				createPullRequest();
+			})
+			.catch((err) => {
+				console.log("checking branch exists failed");
+				console.log(err);
+			});
+		return;
+	}
 
-    console.log("Getting list of PR's FAILED");
-    console.log(data);
-    exit(1);
+	console.log("Getting list of PR's FAILED");
+	console.log(data);
+	exit(1);
 });
 
-
-function checkIfBranchExists(){
+function checkIfBranchExists() {
 	return new Promise((resolve, reject) => {
-        const url = `${systemCollectionUri}${systemTeamProject}/_apis/git/repositories/${buildRepositoryID}/refs?api-version=7.0&filter=${buildSourceBranch.replace('refs/', '')}`;
-        console.log(`checkIfBranchExists: ${url}`);
-        httpsGet(url, base64authorizationHeader).then((data)=>{
-            if (data.count === 1) {
-                resolve();
-                return;
-            }
-            if (data.count === 0) {
-                reject(new Error("Branch does not exist"));
-                return;
-            }
-            reject(new Error("Getting list of branches FAILED"));
-        });
+		const url = `${systemCollectionUri}${systemTeamProject}/_apis/git/repositories/${buildRepositoryID}/refs?api-version=7.0&filter=${buildSourceBranch.replace(
+			"refs/",
+			""
+		)}`;
+		console.log(`checkIfBranchExists: ${url}`);
+		httpsGet(url, base64authorizationHeader).then((data) => {
+			if (data.count === 1) {
+				resolve();
+				return;
+			}
+			if (data.count === 0) {
+				reject(new Error("Branch does not exist"));
+				return;
+			}
+			reject(new Error("Getting list of branches FAILED"));
+		});
 	});
 }
 
-function createPullRequest(){
-    const content = {
-        "sourceRefName": buildSourceBranch,
-        "targetRefName": targetBranch,
-        "title": process.env.PR_TITLE ? process.env.PR_TITLE : "A new feature",
-        "description": process.env.PR_DESCRIPTION ? process.env.PR_DESCRIPTION : "Updates",
-        "reviewers": []
-      };
-      
-      const url = `${systemCollectionUri}${systemTeamProject}/_apis/git/repositories/${buildRepositoryID}/pullrequests?api-version=6.0`;
-      // exampleurl https://dev.azure.com/fabrikam/_apis/git/repositories/3411ebc1-d5aa-464f-9615-0b527bc66719/pullrequests?api-version=6.0
-      // POST https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/pullrequests?api-version=6.0
-      
-      
-      console.log(`going to POST to: ${url}`);
-      console.log(JSON.stringify(content));
-      console.log("----");
-      
-      const base64authorizationHeader = 'Basic ' + Buffer.from(":" + personalAccessToken).toString('base64', 'utf8');
-      httpsPost(url, JSON.stringify(content),base64authorizationHeader).then((data)=>{
-          if (!data.pullRequestId) {
-              console.log("PR creation FAILED");
-              console.log(data);
-              exit(1);
-          }
-          console.log("PR creation done");
-          console.log(data);
-          console.log('------------------');
-          console.log('-> PR created: ' + data.pullRequestId);
-          console.log(content.url);
-      });
+function createPullRequest() {
+	const content = {
+		sourceRefName: buildSourceBranch,
+		targetRefName: targetBranch,
+		title: process.env.PR_TITLE ? process.env.PR_TITLE : "A new feature",
+		description: process.env.PR_DESCRIPTION
+			? process.env.PR_DESCRIPTION
+			: "Updates",
+		reviewers: [],
+	};
 
-      // Error case:
-      //   message: "TF401027: You need the Git 'PullRequestContribute' permission to perform this action. Details: identity 'Build\\099acf0****e47b40', scope 'repository'.",
-      // https://cloudlumberjack.com/assets/img/ado-analyzer/build-service-permissions.gif
+	const url = `${systemCollectionUri}${systemTeamProject}/_apis/git/repositories/${buildRepositoryID}/pullrequests?api-version=6.0`;
+	// exampleurl https://dev.azure.com/fabrikam/_apis/git/repositories/3411ebc1-d5aa-464f-9615-0b527bc66719/pullrequests?api-version=6.0
+	// POST https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/pullrequests?api-version=6.0
+
+	console.log(`going to POST to: ${url}`);
+	console.log(JSON.stringify(content));
+	console.log("----");
+
+	const base64authorizationHeader =
+		"Basic " +
+		Buffer.from(":" + personalAccessToken).toString("base64", "utf8");
+	httpsPost(url, JSON.stringify(content), base64authorizationHeader).then(
+		(data) => {
+			if (!data.pullRequestId) {
+				console.log("PR creation FAILED");
+				console.log(data);
+				exit(1);
+			}
+			console.log("PR creation done");
+			console.log(data);
+			console.log("------------------");
+			console.log("-> PR created: " + data.pullRequestId);
+			console.log(content.url);
+		}
+	);
+
+	// Error case:
+	//   message: "TF401027: You need the Git 'PullRequestContribute' permission to perform this action. Details: identity 'Build\\099acf0****e47b40', scope 'repository'.",
+	// https://cloudlumberjack.com/assets/img/ado-analyzer/build-service-permissions.gif
 }
-
-
-
--- starsky-tools/build-tools/git-commit-message.js
+++ starsky-tools/build-tools/git-commit-message.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const { spawnSync } = require('child_process');
+const { spawnSync } = require("child_process");
 const { join } = require("path");
-const { exit } = require('process');
+const { exit } = require("process");
 const { prefixPath } = require("./lib/prefix-path.const.js");
 
 let searchPath = join(__dirname, prefixPath);
@@ -13,119 +13,139 @@ if (argv) {
 	for (const argItem of argv) {
 		if (existsSync(argItem)) {
 			searchPath = argItem;
-			console.log(`use: path: ${argItem}`)
+			console.log(`use: path: ${argItem}`);
 		}
 	}
 }
 
 console.log(`searchPath: ${searchPath}`);
 
-let gitCommitMessage = process.env.GIT_COMMIT_MESSAGE; 
+let gitCommitMessage = process.env.GIT_COMMIT_MESSAGE;
 if (!gitCommitMessage) {
-    console.log('GIT_COMMIT_MESSAGE is not defined');
-    exit(1);
+	console.log("GIT_COMMIT_MESSAGE is not defined");
+	exit(1);
 }
 
-let gitUserEmail = process.env.GIT_USER_EMAIL; 
+let gitUserEmail = process.env.GIT_USER_EMAIL;
 if (!gitUserEmail) {
-    console.log('GIT_USER_EMAIL is not defined');
-    exit(1);
+	console.log("GIT_USER_EMAIL is not defined");
+	exit(1);
 }
 
-let gitUserName = process.env.GIT_USER_NAME; 
+let gitUserName = process.env.GIT_USER_NAME;
 if (!gitUserName) {
-    console.log('GIT_USER_NAME is not defined');
-    exit(1);
+	console.log("GIT_USER_NAME is not defined");
+	exit(1);
 }
 
 // -----------------------  check if git is there ----------------------------
 
-const gitVersion = spawnSync('git', ['--version'], {
-    cwd: searchPath,
-    env: process.env,
-    encoding: 'utf-8'
+const gitVersion = spawnSync("git", ["--version"], {
+	cwd: searchPath,
+	env: process.env,
+	encoding: "utf-8",
 });
 
 if (gitVersion.stdout.indexOf("git version") === -1) {
-    console.error("git not found");
-    exit(1);
+	console.error("git not found");
+	exit(1);
 }
 
 // -----------------------  file changes ----------------------------
 
-const gitStatusPorcelain = spawnSync('git', ['status', '--porcelain'], {
-    cwd: searchPath,
-    env: process.env,
-    encoding: 'utf-8'
+const gitStatusPorcelain = spawnSync("git", ["status", "--porcelain"], {
+	cwd: searchPath,
+	env: process.env,
+	encoding: "utf-8",
 });
 
 if (!gitStatusPorcelain.stdout) {
-    console.log("no changes");
-    exit(0);
+	console.log("no changes");
+	exit(0);
 }
 
 // -----------------------  set git username /email ----------------------------
 
-console.log('next set git user/email');
+console.log("next set git user/email");
 
-const getPreviousGitUserEmail = spawnSync('git', ['config', '--global', 'user.email'], {
-    cwd: searchPath,
-    env: process.env,
-    encoding: 'utf-8'
-});
+const getPreviousGitUserEmail = spawnSync(
+	"git",
+	["config", "--global", "user.email"],
+	{
+		cwd: searchPath,
+		env: process.env,
+		encoding: "utf-8",
+	}
+);
 
-const getPreviousGitUserName = spawnSync('git', ['config', '--global', 'user.name'], {
-    cwd: searchPath,
-    env: process.env,
-    encoding: 'utf-8'
-});
+const getPreviousGitUserName = spawnSync(
+	"git",
+	["config", "--global", "user.name"],
+	{
+		cwd: searchPath,
+		env: process.env,
+		encoding: "utf-8",
+	}
+);
 
-const setGitUserEmail = spawnSync('git', ['config', '--global', 'user.email', gitUserEmail], {
-    cwd: searchPath,
-    env: process.env,
-    encoding: 'utf-8'
-});
+const setGitUserEmail = spawnSync(
+	"git",
+	["config", "--global", "user.email", gitUserEmail],
+	{
+		cwd: searchPath,
+		env: process.env,
+		encoding: "utf-8",
+	}
+);
 
-const setGitUserName = spawnSync('git', ['config', '--global', 'user.name', gitUserName], {
-    cwd: searchPath,
-    env: process.env,
-    encoding: 'utf-8'
-});
+const setGitUserName = spawnSync(
+	"git",
+	["config", "--global", "user.name", gitUserName],
+	{
+		cwd: searchPath,
+		env: process.env,
+		encoding: "utf-8",
+	}
+);
 
-console.log(`setGitUserEmail: ${gitUserEmail} -  ${setGitUserEmail.stdout} \n ${setGitUserEmail.stderr}`);
-console.log(`setGitUserName: ${gitUserName} -  ${setGitUserName.stdout}\n ${setGitUserName.stderr}`);
+console.log(
+	`setGitUserEmail: ${gitUserEmail} -  ${setGitUserEmail.stdout} \n ${setGitUserEmail.stderr}`
+);
+console.log(
+	`setGitUserName: ${gitUserName} -  ${setGitUserName.stdout}\n ${setGitUserName.stderr}`
+);
 
 // -----------------------  add ----------------------------
 
-console.log('next add');
+console.log("next add");
 
-const addMessage = spawnSync('git', ['add', '.'], {
-    cwd: searchPath,
-    env: process.env,
-    encoding: 'utf-8'
+const addMessage = spawnSync("git", ["add", "."], {
+	cwd: searchPath,
+	env: process.env,
+	encoding: "utf-8",
 });
 
 console.log(`addMessage: ${addMessage.stdout}`);
 console.log(`${addMessage.stderr}`);
 
 // -----------------------  commit ---------------------------------
-console.log('next commit');
+console.log("next commit");
 
-const commitMessage = spawnSync('git', ['commit', '-m',  gitCommitMessage], {
-    cwd: searchPath,
-    env: process.env,
-    encoding: 'utf-8'
+const commitMessage = spawnSync("git", ["commit", "-m", gitCommitMessage], {
+	cwd: searchPath,
+	env: process.env,
+	encoding: "utf-8",
 });
 
 console.log(`commitMessage ${commitMessage.stdout}`);
 console.log(`${commitMessage.stderr}`);
 
-console.log('next push');
+console.log("next push");
 
-const pushMessage = spawnSync('git', ['push'], {
-    cwd: searchPath,
-    env: process.env,
-    encoding: 'utf-8'
+const pushMessage = spawnSync("git", ["push"], {
+	cwd: searchPath,
+	env: process.env,
+	encoding: "utf-8",
 });
 
 console.log(`pushMessage ${pushMessage.stdout}`);
@@ -134,22 +154,29 @@ console.log(`${pushMessage.stderr}`);
 // ------------------------- set back -------------------------------
 
 // and put it back
-const setGitUserEmailBack = spawnSync('git', ['config', '--global', 'user.email', getPreviousGitUserEmail.stdout], {
-    cwd: searchPath,
-    env: process.env,
-    encoding: 'utf-8'
-});
+const setGitUserEmailBack = spawnSync(
+	"git",
+	["config", "--global", "user.email", getPreviousGitUserEmail.stdout],
+	{
+		cwd: searchPath,
+		env: process.env,
+		encoding: "utf-8",
+	}
+);
 
-const setGitUserNameBack = spawnSync('git', ['config', '--global', 'user.name', getPreviousGitUserName.stdout], {
-    cwd: searchPath,
-    env: process.env,
-    encoding: 'utf-8'
-});
+const setGitUserNameBack = spawnSync(
+	"git",
+	["config", "--global", "user.name", getPreviousGitUserName.stdout],
+	{
+		cwd: searchPath,
+		env: process.env,
+		encoding: "utf-8",
+	}
+);
 
 console.log(`setGitUserEmailBack: ${setGitUserEmailBack.stdout}`);
 console.log(`setGitUserNameBack ${setGitUserNameBack.stdout}`);
 
 if (process.env.TF_BUILD) {
-    console.log(`##vso[task.setvariable variable=GIT_FILES_CHANGED;]true`);
+	console.log(`##vso[task.setvariable variable=GIT_FILES_CHANGED;]true`);
 }
-
