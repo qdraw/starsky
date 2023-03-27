8c0968fd5a303900c6027a6525cdcccc43d84264
Dion <qdraw@users.noreply.github.com>
2023-03-27 17:13:28 +0200

    make test more clear

--- starsky/starskytest/Helpers/MimeHelperTest.cs
+++ starsky/starskytest/Helpers/MimeHelperTest.cs
@@ -7,16 +7,40 @@ namespace starskytest.Helpers
 	public sealed class MimeHelperTest
 	{
 		[TestMethod]
-		public void GetMimeTypeByFileNameTest()
+		public void GetMimeTypeByFileNameTestUnknown()
 		{
 			Assert.AreEqual("application/octet-stream",MimeHelper.GetMimeTypeByFileName("test.unknown"));
+		}
+
+		[TestMethod]
+		public void GetMimeTypeByFileNameTestJpg()
+		{
 			Assert.AreEqual("image/jpeg",MimeHelper.GetMimeTypeByFileName("test.jpg"));
 		}
 		
 		[TestMethod]
+		public void GetMimeTypeByFileNameTestJpeg()
+		{
+			Assert.AreEqual("image/jpeg",MimeHelper.GetMimeTypeByFileName("test.jpeg"));
+		}
+
+		[TestMethod]
 		public void GetMimeTypeByExtensionTest_NoExtension()
 		{
 			Assert.AreEqual("application/octet-stream",MimeHelper.GetMimeTypeByFileName(string.Empty));
+		}
+		
+		[TestMethod]
+		public void GetMimeType_NoExtension()
+		{
+			Assert.AreEqual("application/octet-stream",MimeHelper.GetMimeType(string.Empty));
+		}
+		
+				
+		[TestMethod]
+		public void GetMimeType_Jpeg()
+		{
+			Assert.AreEqual("image/jpeg",MimeHelper.GetMimeType("jpg"));
 		}
 	}
 }
