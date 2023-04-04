e1b3ff88b273d47879d314f0fabe5f59d631357f
Dion <qdraw@users.noreply.github.com>
2023-04-04 18:49:37 +0200

    update

---
+++ starsky/starsky/clientapp/.storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions"
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  core: {
+    disableTelemetry: true,
+    enableCrashReports: false
+  },
+  docs: {
+    autodocs: "tag"
+  },
+  staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    // build-storybook url
+    config.output!.publicPath = "/";
+    return config;
+  }
+};
+export default config;
