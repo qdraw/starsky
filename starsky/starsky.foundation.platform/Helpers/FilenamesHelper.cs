28c12b5b35f55fb2f2b5c42904cee13637336d01
Dion <qdraw@users.noreply.github.com>
2023-04-10 20:30:33 +0200

    set to comipled

--- starsky/starsky.foundation.platform/Helpers/FilenamesHelper.cs
+++ starsky/starsky.foundation.platform/Helpers/FilenamesHelper.cs
@@ -16,7 +16,7 @@ namespace starsky.foundation.platform.Helpers
 			// use the same as in the front-end
 			var extensionRegex =
 				new Regex("^[a-zA-Z0-9_](?:[a-zA-Z0-9 ._-]*[a-zA-Z0-9])?\\.[a-zA-Z0-9_-]+$",
-					RegexOptions.CultureInvariant,
+					RegexOptions.Compiled,
 					TimeSpan.FromMilliseconds(100));
 
 			return extensionRegex.IsMatch(filename);
@@ -34,7 +34,7 @@ namespace starsky.foundation.platform.Helpers
 			// [^\/]+(?=\.[\w]+\.$)|[^\/]+$
 			var extensionRegex =
 				new Regex("[^\\/]+(?=\\.[\\w]+\\.$)|[^\\/]+$", // set slower due timeouts
-					RegexOptions.CultureInvariant, TimeSpan.FromMilliseconds(300));
+					RegexOptions.Compiled, TimeSpan.FromMilliseconds(300));
 			return extensionRegex.Match(filePath).Value;
 		}
 
@@ -48,7 +48,7 @@ namespace starsky.foundation.platform.Helpers
 		{
 			var fileName = GetFileName(filePath);
 			return  Regex.Replace(fileName, "\\.[a-zA-Z0-9]{1,4}$", string.Empty, 
-				RegexOptions.None, TimeSpan.FromSeconds(1) );
+				RegexOptions.Compiled, TimeSpan.FromSeconds(1) );
 		}
 		
 		/// <summary>
@@ -61,7 +61,7 @@ namespace starsky.foundation.platform.Helpers
 			// ReSharper disable once ConvertIfStatementToReturnStatement
 			if ( !filename.Contains('.') ) return string.Empty;
 			return Regex.Match(filename, @"[^.][a-zA-Z0-9]{1,4}$", 
-				RegexOptions.None, TimeSpan.FromMilliseconds(100)).Value.ToLowerInvariant();
+				RegexOptions.Compiled, TimeSpan.FromMilliseconds(100)).Value.ToLowerInvariant();
 		}
 		
 		/// <summary>
