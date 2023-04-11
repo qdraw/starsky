827c6bf0aa848a0aa86299b4f95abc0afd3db692
Dion <qdraw@users.noreply.github.com>
2023-04-11 19:40:30 +0200

    add test

---
+++ starsky/starskytest/FakeCreateAn/CreateAnZipFile12/CreateAnZipFile12.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using starsky.foundation.platform.Helpers;
+
+namespace starskytest.FakeCreateAn.CreateAnZipFile12
+{
+	public static class CreateAnZipFile12
+	{
+		/// <summary>
+		/// @see: https://superuser.com/a/1467266 and 80 chars
+		/// </summary>
+		private const string Base64CreateAnZipFile12String = "UEsDBAoAAAAAAJGKi1YxvvG8EAAAABAAAAAJABwAZmlsZTEudHh0VVQJAAPhljVk1ZY1ZHV4CwABBOgD" 
+			+ "AAAE6AMAAFRoaXMgaXMgZmlsZSAxLgpQSwMECgAAAAAAloqLVmgAt74QAAAAEAAAAAkAHABmaWxlMi50" 
+			+ "eHRVVAkAA+yWNWTsljVkdXgLAAEE6AMAAAToAwAAVGhpcyBpcyBmaWxlIDIuClBLAQIeAwoAAAAAAJGK" 
+			+ "i1YxvvG8EAAAABAAAAAJABgAAAAAAAEAAACkgQAAAABmaWxlMS50eHRVVAUAA+GWNWR1eAsAAQToAwAA" 
+			+ "BOgDAABQSwECHgMKAAAAAACWiotWaAC3vhAAAAAQAAAACQAYAAAAAAABAAAApIFTAAAAZmlsZTIudHh0" 
+			+ "VVQFAAPsljVkdXgLAAEE6AMAAAToAwAAUEsFBgAAAAACAAIAngAAAKYAAAAAAA==";
+
+		public static readonly byte[] Bytes = Base64Helper.TryParse(Base64CreateAnZipFile12String);
+		
+		public static Dictionary<string, string> Content = new Dictionary<string, string>
+		{
+			{ "file1.txt", "This is file 1.\n" },
+			{ "file2.txt", "This is file 2.\n" }
+		};
+	}
+}
+
--- starsky/starskytest/starsky.foundation.storage/ArchiveFormats/ZipperTest.cs
+++ starsky/starskytest/starsky.foundation.storage/ArchiveFormats/ZipperTest.cs
@@ -1,5 +1,10 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.storage.ArchiveFormats;
+using starsky.foundation.storage.Helpers;
+using starskytest.FakeCreateAn.CreateAnZipFile12;
 
 namespace starskytest.starsky.foundation.storage.ArchiveFormats
 {
@@ -11,6 +16,27 @@ namespace starskytest.starsky.foundation.storage.ArchiveFormats
 		{
 			var result =  new Zipper().ExtractZip("not-found","t");
 			Assert.IsFalse(result);
+		}
+		
+		[TestMethod]
+		public void TestExtractZip()
+		{
+			// Arrange
+			var zipped = CreateAnZipFile12.Bytes;
+
+			// Act
+			var result = Zipper.ExtractZip(zipped);
+
+			// Assert
+			Assert.IsNotNull(result);
+			Assert.AreEqual(CreateAnZipFile12.Content.Count, result.Count);
+
+			foreach (var entry in CreateAnZipFile12.Content)
+			{
+				Assert.IsTrue(result.ContainsKey(entry.Key));
+				var resultText = Encoding.UTF8.GetString(result[entry.Key]);
+				Assert.AreEqual(entry.Value, resultText);
+			}
 		}
 	}
 }
