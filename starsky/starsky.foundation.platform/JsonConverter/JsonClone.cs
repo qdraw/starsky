eff3a122c532f30f3d06763eceebf41a64b2e2eb
Dion <qdraw@users.noreply.github.com>
2023-08-07 17:30:35 +0200

    add comment for json

--- starsky/starsky.foundation.platform/JsonConverter/JsonClone.cs
+++ starsky/starsky.foundation.platform/JsonConverter/JsonClone.cs
@@ -4,11 +4,16 @@ namespace starsky.foundation.platform.JsonConverter
 {
 	public static class JsonClone
 	{
+		/// <summary>
+		/// Clone via JsonConverter
+		/// </summary>
+		/// <param name="source">The object</param>
+		/// <typeparam name="T">type to clone from and to</typeparam>
+		/// <returns>typed object</returns>
 		public static T CloneViaJson<T>(this T source)
 		{
 			var serialized = JsonSerializer.Serialize(
 				source, DefaultJsonSerializer.CamelCase);
-
 			return JsonSerializer.Deserialize<T>(serialized, DefaultJsonSerializer.CamelCase);
 		}
 	}	
