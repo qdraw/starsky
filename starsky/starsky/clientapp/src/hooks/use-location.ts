2033adbd95956294af90f75f5289542d0342d9b2
Dion <qdraw@users.noreply.github.com>
2023-08-15 19:16:01 +0200

    use location is not working yet

--- starsky/starsky/clientapp/src/hooks/use-location.ts
+++ starsky/starsky/clientapp/src/hooks/use-location.ts
@@ -1,16 +1,37 @@
-import { Location, NavigateFunction, useNavigate } from "react-router-dom";
+import { INavigateState } from "../interfaces/INavigateState";
 import history from "../shared/global-history/global-history";
 
+export interface INavigateOptions {
+  replace?: boolean;
+  state?: INavigateState;
+}
+
+export interface ILocationObject {
+  href: string;
+  search: string;
+  state: INavigateState;
+}
+
 export interface IUseLocation {
-  location: Location;
-  navigate: NavigateFunction;
+  location: ILocationObject;
+  navigate: INavigateFunction;
+}
+export interface INavigateFunction {
+  (to: string, options?: INavigateOptions): void;
+}
+
+function navigateFn(to: string, options?: INavigateOptions) {
+  if (options?.replace === true) {
+    history.replace(to, options.state);
+    return;
+  }
+  history.push(to, options?.state);
 }
 
 const useLocation = () => {
-  const navigate = useNavigate();
   const result = {
-    navigate,
-    location: history.location as Location
+    navigate: navigateFn,
+    location: history.location as unknown as ILocationObject
   } as IUseLocation;
 
   return result;
--- starsky/starsky/clientapp/vite.config.ts
+++ starsky/starsky/clientapp/vite.config.ts
@@ -2,10 +2,12 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "./build",
+    outDir: "build",
+    assetsDir: "assets",
     assetsInlineLimit: 0,
     chunkSizeWarningLimit: 600
   },
