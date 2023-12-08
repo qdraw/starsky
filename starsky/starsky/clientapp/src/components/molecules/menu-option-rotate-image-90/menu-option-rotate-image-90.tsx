ff0bbb1bd4f9371c0c96aaa188293c675b40240a
Dion <qdraw@users.noreply.github.com>
2023-12-08 19:25:50 +0100

    move fn outside

--- starsky/starsky/clientapp/src/components/molecules/menu-option-rotate-image-90/menu-option-rotate-image-90.tsx
+++ starsky/starsky/clientapp/src/components/molecules/menu-option-rotate-image-90/menu-option-rotate-image-90.tsx
@@ -18,6 +18,42 @@ interface IMenuOptionMenuOptionRotateImage90Props {
 }
 
 /**
+ * Checks if the hash is changes and update Context:  orientation + fileHash
+ */
+export async function requestNewFileHash(
+  state: IDetailView,
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
+  dispatch: Dispatch<DetailViewAction>
+): Promise<boolean | null> {
+  const resultGet = await FetchGet(
+    new UrlQuery().UrlIndexServerApi({ f: state.subPath })
+  );
+  if (!resultGet) return null;
+  if (resultGet.statusCode !== 200) {
+    console.error(resultGet);
+    setIsLoading(false);
+    return null;
+  }
+  const media = new CastToInterface().MediaDetailView(resultGet.data).data;
+  const orientation = media?.fileIndexItem?.orientation
+    ? media.fileIndexItem.orientation
+    : Orientation.Horizontal;
+
+  // the hash changes if you rotate an image
+  if (media.fileIndexItem.fileHash === state.fileIndexItem.fileHash)
+    return false;
+
+  dispatch({
+    type: "update",
+    orientation,
+    fileHash: media.fileIndexItem.fileHash,
+    filePath: media.fileIndexItem.filePath
+  });
+  setIsLoading(false);
+  return true;
+}
+
+/**
  * Used from DetailView
  */
 const MenuOptionRotateImage90: React.FunctionComponent<IMenuOptionMenuOptionRotateImage90Props> =
@@ -29,38 +65,6 @@ const MenuOptionRotateImage90: React.FunctionComponent<IMenuOptionMenuOptionRota
       const bodyParams = new URLSearchParams();
       bodyParams.set("f", state.subPath);
       return bodyParams;
-    }
-
-    /**
-     * Checks if the hash is changes and update Context:  orientation + fileHash
-     */
-    async function requestNewFileHash(): Promise<boolean | null> {
-      const resultGet = await FetchGet(
-        new UrlQuery().UrlIndexServerApi({ f: state.subPath })
-      );
-      if (!resultGet) return null;
-      if (resultGet.statusCode !== 200) {
-        console.error(resultGet);
-        setIsLoading(false);
-        return null;
-      }
-      const media = new CastToInterface().MediaDetailView(resultGet.data).data;
-      const orientation = media?.fileIndexItem?.orientation
-        ? media.fileIndexItem.orientation
-        : Orientation.Horizontal;
-
-      // the hash changes if you rotate an image
-      if (media.fileIndexItem.fileHash === state.fileIndexItem.fileHash)
-        return false;
-
-      dispatch({
-        type: "update",
-        orientation,
-        fileHash: media.fileIndexItem.fileHash,
-        filePath: media.fileIndexItem.filePath
-      });
-      setIsLoading(false);
-      return true;
     }
 
     /**
@@ -83,10 +87,10 @@ const MenuOptionRotateImage90: React.FunctionComponent<IMenuOptionMenuOptionRota
 
       // there is an async backend event triggered, sometimes there is an que
       setTimeout(() => {
-        requestNewFileHash().then((result) => {
+        requestNewFileHash(state, setIsLoading, dispatch).then((result) => {
           if (result === false) {
             setTimeout(() => {
-              requestNewFileHash().then(() => {
+              requestNewFileHash(state, setIsLoading, dispatch).then(() => {
                 // when it didn't change after two tries
                 setIsLoading(false);
               });
