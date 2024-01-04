99efb08be006baa5fe7c94ba7b84e1dbdc709ae2
Dion <qdraw@users.noreply.github.com>
2024-01-04 19:32:45 +0100

    add tests

--- starsky/starskytest/starsky.foundation.platform/Models/AppSettingsTest.cs
+++ starsky/starskytest/starsky.foundation.platform/Models/AppSettingsTest.cs
@@ -528,5 +528,26 @@ namespace starskytest.starsky.foundation.platform.Models
 			Assert.AreEqual(2,appSettings.AccountRolesByEmailRegisterOverwrite.Count);
 			Assert.AreEqual("Administrator", appSettings.AccountRolesByEmailRegisterOverwrite["bogusEmail2"]);
 		}
+
+
+		[TestMethod]
+		public void DatabasePathToFilePath_Null()
+		{
+			var appSettings = new AppSettings();
+			
+			var result = appSettings.DatabasePathToFilePath("\\test");
+			
+			Assert.IsNull(result);
+		}
+		
+		[TestMethod]
+		public void DatabasePathToFilePath_NoNull()
+		{
+			var appSettings = new AppSettings();
+			
+			var result = appSettings.DatabasePathToFilePath("\\test",false);
+			
+			Assert.IsNotNull(result);
+		}
 	}
 }
