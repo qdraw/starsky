c5bb8644d120a3752d7ef8b42bee6b39a76e7327
Dion <qdraw@users.noreply.github.com>
2024-01-29 18:36:23 +0100

    try

--- starsky/starsky/clientapp/src/components/organisms/detail-view-media/shared/get-mouse-position.ts
+++ starsky/starsky/clientapp/src/components/organisms/detail-view-media/shared/get-mouse-position.ts
@@ -4,6 +4,11 @@ export function GetMousePosition(event: React.MouseEvent | MouseEvent) {
   const offsetParent = targetElement.offsetParent as HTMLElement;
   const offsetParentLeft = offsetParent?.offsetLeft;
 
+  console.log(offsetParentLeft);
+  console.log(targetElement.offsetLeft);
+  console.log(event.pageX);
+  console.log(targetElement.offsetWidth);
+
   return (
     (event.pageX - (targetElement.offsetLeft + offsetParentLeft)) /
     targetElement.offsetWidth
