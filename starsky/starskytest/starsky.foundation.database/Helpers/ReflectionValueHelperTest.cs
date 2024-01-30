bc7f6792410b626c3c4000b291fa9407b7850de6
Dion <qdraw@users.noreply.github.com>
2024-01-30 18:08:10 +0100

    SupressMessage for unused classes

--- starsky/starskytest/starsky.foundation.database/Helpers/ReflectionValueHelperTest.cs
+++ starsky/starskytest/starsky.foundation.database/Helpers/ReflectionValueHelperTest.cs
@@ -7,8 +7,10 @@ namespace starskytest.starsky.foundation.database.Helpers;
 [TestClass]
 public class ReflectionValueHelperTest
 {
-	[SuppressMessage("Usage", "S1144:Unused private types or members should be removed")]
-	[SuppressMessage("Usage", "S2933:Fields that are only assigned in the constructor should be \"readonly\"")]
+	[SuppressMessage("Usage",
+		"S1144:Unused private types or members should be removed")]
+	[SuppressMessage("Usage",
+		"S2933:Fields that are only assigned in the constructor should be \"readonly\"")]
 	private class Foo
 	{
 #pragma warning disable CS0414
@@ -16,18 +18,19 @@ public class ReflectionValueHelperTest
 #pragma warning restore CS0414
 	}
 
+	[SuppressMessage("Usage", "S2094:Classes should not be empty")]
 	private class FooNothing
 	{
 		// nothing here
 	}
-	
+
 	[TestMethod]
 	public void TestReadField()
 	{
 		var result = new Foo().GetReflectionFieldValue<string>("_bar");
 		Assert.AreEqual("test", result);
 	}
-	
+
 	[TestMethod]
 	public void TestReadFieldNothing()
 	{
