c0b0a4d7d9065f4b51bb2f9fc1c2b2362a6ff2ce
Dion <qdraw@users.noreply.github.com>
2024-02-03 23:14:08 +0100

    Colorclass filter refactor

--- starsky/starsky/clientapp/src/components/molecules/color-class-filter/color-class-filter.tsx
+++ starsky/starsky/clientapp/src/components/molecules/color-class-filter/color-class-filter.tsx
@@ -4,10 +4,12 @@ import useGlobalSettings from "../../../hooks/use-global-settings";
 import useLocation from "../../../hooks/use-location/use-location";
 import { newIArchive } from "../../../interfaces/IArchive";
 import { Language } from "../../../shared/language";
-import { SelectCheckIfActive } from "../../../shared/select-check-if-active";
-import { URLPath } from "../../../shared/url-path";
 import Link from "../../atoms/link/link";
 import Preloader from "../../atoms/preloader/preloader";
+import { GetFilterUrlColorClass } from "./shared/get-filter-url-color-class.ts";
+import { CleanColorClass } from "./shared/clean-color-class.ts";
+import { ClassNameContainer } from "./shared/class-name-container.ts";
+import localization from "../../../localization/localization.json";
 
 //  <ColorClassFilter itemsCount={this.props.collectionsCount} subPath={this.props.subPath}
 // colorClassActiveList={this.props.colorClassActiveList} colorClassUsage={this.props.colorClassUsage}></ColorClassFilter>
@@ -19,161 +21,116 @@ export interface IColorClassProp {
   sticky?: boolean;
 }
 
-const ColorClassFilter: React.FunctionComponent<IColorClassProp> = memo(
-  (props) => {
-    // content
-    const settings = useGlobalSettings();
-    const language = new Language(settings.language);
+const ColorClassFilter: React.FunctionComponent<IColorClassProp> = memo((props) => {
+  // content
+  const settings = useGlobalSettings();
+  const language = new Language(settings.language);
 
-    const colorContent: Array<string> = [
-      language.text("Kleurloos", "Colorless"),
-      language.text("Roze", "Pink"),
-      language.text("Rood", "Red"),
-      language.text("Oranje", "Orange"),
-      language.text("Geel", "Yellow"),
-      language.text("Groen", "Green"),
-      language.text("Azuur", "Azure"),
-      language.text("Blauw", "Blue"),
-      language.text("Grijs", "Grey"),
-      language.text("Herstel filter", "Reset filter")
-    ];
+  const colorContent: Array<string> = [
+    language.key(localization.ColorClassColour0),
+    language.key(localization.ColorClassColour1),
+    language.key(localization.ColorClassColour2),
+    language.key(localization.ColorClassColour3),
+    language.key(localization.ColorClassColour4),
+    language.key(localization.ColorClassColour5),
+    language.key(localization.ColorClassColour6),
+    language.key(localization.ColorClassColour7),
+    language.key(localization.ColorClassColour8),
+    language.key(localization.ColorClassColourResetFilter)
+  ];
 
-    // used for reading current location
-    const history = useLocation();
+  // used for reading current location
+  const history = useLocation();
 
-    let { state } = React.useContext(ArchiveContext);
-    // props is used as default, state only for update
-    if (!state) {
-      state = {
-        ...newIArchive(),
-        colorClassUsage: props.colorClassUsage,
-        colorClassActiveList: props.colorClassActiveList,
-        collectionsCount: props.itemsCount ? props.itemsCount : 0
-      };
-    }
-    const [colorClassUsage, setColorClassUsage] = useState(
-      props.colorClassUsage
-    );
-
-    useEffect(() => {
-      setColorClassUsage(state.colorClassUsage);
-      // it should not update when the prop are changing
-    }, [state.colorClassUsage]);
-
-    const [colorClassActiveList, setColorClassActiveList] = useState(
-      props.colorClassActiveList
-    );
-    useEffect(() => {
-      setColorClassActiveList(state.colorClassActiveList);
-      // it should not update when the prop are changing
-    }, [state.colorClassActiveList]);
-
-    const [collectionsCount, setCollectionsCount] = useState(props.itemsCount);
-    useEffect(() => {
-      setCollectionsCount(state.collectionsCount);
-      // it should not update when the prop are changing
-    }, [state.collectionsCount]);
-
-    const [isLoading, setIsLoading] = useState(false);
-    // When change-ing page the loader should be gone
-    useEffect(() => {
-      setIsLoading(false);
-    }, [props.colorClassActiveList]);
-
-    function cleanColorClass(): string {
-      if (!props.subPath) return "/";
-      const urlObject = new URLPath().StringToIUrl(history.location.search);
-      urlObject.colorClass = [];
-      return new URLPath().IUrlToString(urlObject);
-    }
-
-    function getFilterUrlColorClass(item: number): string {
-      const urlObject = new URLPath().StringToIUrl(history.location.search);
-
-      if (!urlObject.colorClass) {
-        urlObject.colorClass = [];
-      }
-
-      if (!urlObject.colorClass || urlObject.colorClass.indexOf(item) === -1) {
-        urlObject.colorClass.push(item);
-      } else {
-        const index = urlObject.colorClass.indexOf(item);
-        if (index !== -1) urlObject.colorClass.splice(index, 1);
-      }
-
-      if (urlObject.select)
-        urlObject.select = new SelectCheckIfActive().IsActive(
-          urlObject.select,
-          urlObject.colorClass,
-          state.fileIndexItems
-        );
-
-      return new URLPath().IUrlToString(urlObject);
-    }
-
-    const resetButton = (
-      <Link
-        data-test="color-class-filter-reset"
-        to={cleanColorClass()}
-        className="btn colorclass colorclass--reset"
-      >
-        {colorContent[9]}
-      </Link>
-    );
-    const resetButtonDisabled = (
-      <div className="btn colorclass colorclass--reset disabled">
-        {colorContent[9]}
-      </div>
-    );
-
-    const classNameContainer = !props.sticky
-      ? "colorclass colorclass--filter"
-      : "colorclass colorclass--filter colorclass--sticky";
-
-    // there is no content ?
-    if (colorClassUsage.length === 1 && colorClassActiveList.length >= 1) {
-      return <div className={classNameContainer}> {resetButton}</div>;
-    }
-
-    if (collectionsCount === 0 || colorClassUsage.length === 1) {
-      return <></>;
-    }
-
-    return (
-      <div className={classNameContainer}>
-        {isLoading ? <Preloader isWhite={false} isOverlay={true} /> : null}
-        {props.colorClassActiveList.length !== 0
-          ? resetButton
-          : resetButtonDisabled}
-        {colorClassUsage.map((item) =>
-          item >= 0 && item <= 8 ? (
-            <Link
-              onClick={() => setIsLoading(true)}
-              key={item}
-              to={getFilterUrlColorClass(item)}
-              data-test={"color-class-filter-" + item}
-              className={
-                props.colorClassActiveList.indexOf(item) >= 0
-                  ? "btn btn--default colorclass colorclass--" +
-                    item +
-                    " active"
-                  : "btn colorclass colorclass--" + item
-              }
-            >
-              <span className="label" />
-              <span>{colorContent[item]}</span>{" "}
-            </Link>
-          ) : (
-            <span key={item} />
-          )
-        )}
-        <div className="btn btn--default sort">
-          <span className="text">{!state.sort ? "fileName" : state.sort}</span>
-          <span className="icon"></span>
-        </div>
-      </div>
-    );
+  let { state } = React.useContext(ArchiveContext);
+  // props is used as default, state only for update
+  if (!state) {
+    state = {
+      ...newIArchive(),
+      colorClassUsage: props.colorClassUsage,
+      colorClassActiveList: props.colorClassActiveList,
+      collectionsCount: props.itemsCount ? props.itemsCount : 0
+    };
   }
-);
+  const [colorClassUsage, setColorClassUsage] = useState(props.colorClassUsage);
+
+  useEffect(() => {
+    setColorClassUsage(state.colorClassUsage);
+    // it should not update when the prop are changing
+  }, [state.colorClassUsage]);
+
+  const [colorClassActiveList, setColorClassActiveList] = useState(props.colorClassActiveList);
+  useEffect(() => {
+    setColorClassActiveList(state.colorClassActiveList);
+    // it should not update when the prop are changing
+  }, [state.colorClassActiveList]);
+
+  const [collectionsCount, setCollectionsCount] = useState(props.itemsCount);
+  useEffect(() => {
+    setCollectionsCount(state.collectionsCount);
+    // it should not update when the prop are changing
+  }, [state.collectionsCount]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  // When change-ing page the loader should be gone
+  useEffect(() => {
+    setIsLoading(false);
+  }, [props.colorClassActiveList]);
+
+  const resetButton = (
+    <Link
+      data-test="color-class-filter-reset"
+      to={CleanColorClass(props.subPath, history.location.search)}
+      className="btn colorclass colorclass--reset"
+    >
+      {colorContent[9]}
+    </Link>
+  );
+  const resetButtonDisabled = (
+    <div className="btn colorclass colorclass--reset disabled">{colorContent[9]}</div>
+  );
+
+  const classNameContainer = ClassNameContainer(props.sticky);
+
+  // there is no content ?
+  if (colorClassUsage.length === 1 && colorClassActiveList.length >= 1) {
+    return <div className={classNameContainer}> {resetButton}</div>;
+  }
+
+  if (collectionsCount === 0 || colorClassUsage.length === 1) {
+    return <></>;
+  }
+
+  return (
+    <div className={classNameContainer}>
+      {isLoading ? <Preloader isWhite={false} isOverlay={true} /> : null}
+      {props.colorClassActiveList.length !== 0 ? resetButton : resetButtonDisabled}
+      {colorClassUsage.map((item) =>
+        item >= 0 && item <= 8 ? (
+          <Link
+            onClick={() => setIsLoading(true)}
+            key={item}
+            to={GetFilterUrlColorClass(item, history.location.search, state)}
+            data-test={"color-class-filter-" + item}
+            className={
+              props.colorClassActiveList.indexOf(item) >= 0
+                ? "btn btn--default colorclass colorclass--" + item + " active"
+                : "btn colorclass colorclass--" + item
+            }
+          >
+            <span className="label" />
+            <span>{colorContent[item]}</span>{" "}
+          </Link>
+        ) : (
+          <span key={item} />
+        )
+      )}
+      <div className="btn btn--default sort">
+        <span className="text">{!state.sort ? "fileName" : state.sort}</span>
+        <span className="icon"></span>
+      </div>
+    </div>
+  );
+});
 
 export default ColorClassFilter;
