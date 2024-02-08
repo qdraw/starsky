98ed59a4fe4c5bda3cd6f0e69137880552b9a7d1
Dion <qdraw@users.noreply.github.com>
2024-02-08 22:37:16 +0100

    nullable

--- starsky/starskytest/FakeMocks/FakeIWebHtmlPublishService.cs
+++ starsky/starskytest/FakeMocks/FakeIWebHtmlPublishService.cs
@@ -19,7 +19,7 @@ namespace starskytest.FakeMocks
 		}
 
 		public async Task GenerateZip(string fullFileParentFolderPath, string itemName,
-			Dictionary<string, bool> renderCopyResult,
+			Dictionary<string, bool>? renderCopyResult,
 			bool deleteFolderAfterwards = false)
 		{
 			ItemNamesGenerateZip.Add(itemName);
