df768675ebbc177eafbfad7d81c9eec54a160c59
Dion <qdraw@users.noreply.github.com>
2024-02-09 18:58:09 +0100

    Data migration && add limit

--- starsky/starsky.foundation.database/Migrations/20240209175725_limitdataprotectionkeyslength.Designer.cs
+++ starsky/starsky.foundation.database/Migrations/20240209175725_limitdataprotectionkeyslength.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using starsky.foundation.database.Data;
 
@@ -11,9 +12,11 @@ using starsky.foundation.database.Data;
 namespace starsky.foundation.database.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240209175725_limitdataprotectionkeyslength")]
+    partial class limitdataprotectionkeyslength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
