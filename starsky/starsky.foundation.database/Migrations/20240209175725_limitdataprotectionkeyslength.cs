f7efc1e5c0bdbd0325a7b3b65270bffb30bbd8f2
Dion <qdraw@users.noreply.github.com>
2024-02-09 19:20:33 +0100

    suppress warnings

--- starsky/starsky.foundation.database/Migrations/20240209175725_limitdataprotectionkeyslength.cs
+++ starsky/starsky.foundation.database/Migrations/20240209175725_limitdataprotectionkeyslength.cs
@@ -1,22 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
 
 #nullable disable
 
 namespace starsky.foundation.database.Migrations
 {
+	[SuppressMessage("Usage", "S101: Rename class 'limitdataprotectionkeyslength' " +
+	                          "to match pascal case naming rules, consider using 'Limitdataprotectionkeyslength'.")]
     /// <inheritdoc />
     public partial class limitdataprotectionkeyslength : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+			// only used for the migration
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+			// only used for the migration
         }
     }
 }
