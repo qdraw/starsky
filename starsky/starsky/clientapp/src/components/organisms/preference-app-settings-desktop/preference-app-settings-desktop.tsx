48b8e180baa943119642b9658ac493d6167bd7c3
Dion <qdraw@users.noreply.github.com>
2024-02-25 14:31:57 +0100

    updates

--- starsky/starsky/clientapp/src/components/organisms/preference-app-settings-desktop/preference-app-settings-desktop.tsx
+++ starsky/starsky/clientapp/src/components/organisms/preference-app-settings-desktop/preference-app-settings-desktop.tsx
@@ -2,7 +2,9 @@ import React, { ChangeEvent, useState } from "react";
 import useFetch from "../../../hooks/use-fetch";
 import useGlobalSettings from "../../../hooks/use-global-settings";
 import { IAppSettings } from "../../../interfaces/IAppSettings";
+import { IAppSettingsDefaultEditorApplication } from "../../../interfaces/IAppSettingsDefaultEditorApplication";
 import { RawJpegMode } from "../../../interfaces/ICollectionsOpenType";
+import { ImageFormat } from "../../../interfaces/IFileIndexItem";
 import localization from "../../../localization/localization.json";
 import FetchPost from "../../../shared/fetch/fetch-post";
 import { Language } from "../../../shared/language";
@@ -10,12 +12,40 @@ import { UrlQuery } from "../../../shared/url-query";
 import FormControl from "../../atoms/form-control/form-control";
 import SwitchButton from "../../atoms/switch-button/switch-button";
 
+const defaultEditorApplication = {
+  imageFormats: [ImageFormat.jpg, ImageFormat.png, ImageFormat.bmp, ImageFormat.tiff]
+} as IAppSettingsDefaultEditorApplication;
+
 async function updateDefaultEditorPhotos(
   event: ChangeEvent<HTMLDivElement>,
-  setIsMessage: React.Dispatch<React.SetStateAction<string>>
+  setIsMessage: React.Dispatch<React.SetStateAction<string>>,
+  defaultDesktopEditor?: IAppSettingsDefaultEditorApplication[]
 ) {
+  if (!defaultDesktopEditor) {
+    setIsMessage("FAIL");
+    return;
+  }
   const bodyParams = new URLSearchParams();
-  bodyParams.set("desktopC1111ollectionsOpen", event.target.innerText);
+
+  defaultEditorApplication.applicationPath = event.target.innerText;
+  const index = defaultDesktopEditor.findIndex(
+    (p) => p.imageFormats.includes(ImageFormat.jpg) || p.imageFormats.includes(ImageFormat.tiff)
+  );
+  if (index === -1) {
+    defaultDesktopEditor.push(defaultEditorApplication);
+  } else {
+    defaultDesktopEditor[index] = defaultEditorApplication;
+  }
+
+  defaultDesktopEditor.forEach((editorApp, index) => {
+    defaultEditorApplication.imageFormats.forEach((imageFormat, idx) => {
+      bodyParams.append(
+        `DefaultDesktopEditor[${index}].ImageFormats[${idx}]`,
+        imageFormat.toString()
+      );
+    });
+    bodyParams.append(`DefaultDesktopEditor[${index}].ApplicationPath`, editorApp.applicationPath);
+  });
 
   const result = await FetchPost(new UrlQuery().UrlApiAppSettings(), bodyParams.toString());
   if (result.statusCode != 200) {
@@ -48,6 +78,10 @@ const PreferencesAppSettingsDesktop: React.FunctionComponent = () => {
 
   const settings = useGlobalSettings();
 
+  const imageDefaultEditor = appSettings?.defaultDesktopEditor.find(
+    (p) => p.imageFormats.includes(ImageFormat.jpg) || p.imageFormats.includes(ImageFormat.tiff)
+  );
+
   const language = new Language(settings.language);
   const MessageSwitchButtonDesktopCollectionsRawOn = language.key(
     localization.MessageSwitchButtonDesktopCollectionsRawOn
@@ -55,36 +89,53 @@ const PreferencesAppSettingsDesktop: React.FunctionComponent = () => {
   const MessageSwitchButtonDesktopCollectionsJpegDefaultOff = language.key(
     localization.MessageSwitchButtonDesktopCollectionsJpegDefaultOff
   );
-
-  // appSettings?.desktopCollectionsOpen
-  // List<AppSettingsDefaultEditorApplication> DefaultDesktopEditor
-  // CollectionsOpenType.RawJpegMode DesktopCollectionsOpen
+  const MessageSwitchButtonDesktopApplication = language.key(
+    localization.MessageSwitchButtonDesktopApplication
+  );
+  const MessageSwitchButtonDesktopApplicationDescription = language.key(
+    localization.MessageSwitchButtonDesktopApplicationDescription
+  );
+  const MessageAppSettingDefaultEditorPhotos = language.key(
+    localization.MessageAppSettingDefaultEditorPhotos
+  );
+  const MessageAppSettingDefaultEditorPhotosDescription = language.key(
+    localization.MessageAppSettingDefaultEditorPhotosDescription
+  );
 
   // for showing a notification
   const [isMessage, setIsMessage] = useState("");
 
   return (
     <>
-      <p>Desktop</p>
-      Only used when using Starsky as desktop
-      {isMessage !== "" ? (
-        <div className="warning-box warning-box--optional">{isMessage}</div>
-      ) : null}
-      <SwitchButton
-        isOn={appSettings?.desktopCollectionsOpen === RawJpegMode.Raw}
-        data-test="desktop-collections-open-toggle"
-        isEnabled={true}
-        leftLabel={MessageSwitchButtonDesktopCollectionsJpegDefaultOff}
-        onToggle={(value) => toggleCollections(value, setIsMessage)}
-        rightLabel={MessageSwitchButtonDesktopCollectionsRawOn}
-      />
-      <h4>Default application to edit photos:</h4>
-      <FormControl
-        spellcheck={true}
-        onInput={(value) => updateDefaultEditorPhotos(value, setIsMessage)}
-        name="tags"
-        contentEditable={true}
-      ></FormControl>
+      <div className="content--subheader">{MessageSwitchButtonDesktopApplication}</div>
+      <div className="content--text no-left-padding">
+        <p>{MessageSwitchButtonDesktopApplicationDescription}</p>
+
+        {isMessage !== "" ? (
+          <div className="warning-box warning-box--optional">{isMessage}</div>
+        ) : null}
+        <SwitchButton
+          isOn={appSettings?.desktopCollectionsOpen === RawJpegMode.Raw}
+          data-test="desktop-collections-open-toggle"
+          isEnabled={true}
+          leftLabel={MessageSwitchButtonDesktopCollectionsJpegDefaultOff}
+          onToggle={(value) => toggleCollections(value, setIsMessage)}
+          rightLabel={MessageSwitchButtonDesktopCollectionsRawOn}
+        />
+
+        <h3>{MessageAppSettingDefaultEditorPhotos}</h3>
+        <p>{MessageAppSettingDefaultEditorPhotosDescription} </p>
+        <FormControl
+          spellcheck={true}
+          onBlur={(value) =>
+            updateDefaultEditorPhotos(value, setIsMessage, appSettings?.defaultDesktopEditor)
+          }
+          name="tags"
+          contentEditable={true}
+        >
+          {imageDefaultEditor?.applicationPath}
+        </FormControl>
+      </div>
     </>
   );
 };
