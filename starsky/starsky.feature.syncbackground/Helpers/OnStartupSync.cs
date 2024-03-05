d97a2dc95c8f8d5e40a93d97c8ff8962b0df879d
Dion <qdraw@users.noreply.github.com>
2024-03-05 12:55:03 +0100

    undo

--- starsky/starsky.feature.syncbackground/Helpers/OnStartupSync.cs
+++ starsky/starsky.feature.syncbackground/Helpers/OnStartupSync.cs
@@ -51,7 +51,6 @@ public class OnStartupSync
 	{
 		await _backgroundTaskQueue.QueueBackgroundWorkItemAsync(async token =>
 		{
-			await Task.Yield();
 			await StartUpSyncTask();
 		}, nameof(StartUpSync));
 	}
