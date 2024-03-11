c48b73b5cb2a6d36c71daba9ccd0dcbefe4481ab
Dion <qdraw@users.noreply.github.com>
2024-03-11 22:42:20 +0100

    ./build.ps1 also pass args through ./starsky/build.ps1

--- build.ps1
+++ build.ps1
@@ -9,7 +9,7 @@ $netSourceFolder = Join-Path -Path $scriptDirectory -ChildPath "starsky"
 if (Test-Path $netChildScriptPath -PathType Leaf) {
     # Execute the child script
     pushd $netSourceFolder
-    & $netChildScriptPath $args
+    & $netChildScriptPath @args
     popd
 } else {
     Write-Host "The child script '$childScriptPath' does not exist."
