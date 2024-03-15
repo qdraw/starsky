495af2890bd10010d86cd7559eb215418ac2320b
Dion <qdraw@users.noreply.github.com>
2024-03-15 13:48:13 +0100

    version bumb to 0.6.0 (without beta)

--- starsky-tools/build-tools/app-version-update.js
+++ starsky-tools/build-tools/app-version-update.js
@@ -11,7 +11,7 @@ const {readFile, writeFile} = require("fs").promises;
 const {getFiles} = require("./lib/get-files-directory");
 const {prefixPath} = require("./lib/prefix-path.const.js");
 
-let newVersion = "0.6.0-beta.3";
+let newVersion = "0.6.0";
 
 // allow version as single argument
 const argv = process.argv.slice(2);
