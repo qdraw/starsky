ef7c00b914d8e571394725e4ef1a8a7efcbe4543
Dion <qdraw@users.noreply.github.com>
2024-08-22 20:38:28 +0200

    fix timeout

--- starsky/starsky.foundation.readmeta/Helpers/GeoParser.cs
+++ starsky/starsky.foundation.readmeta/Helpers/GeoParser.cs
@@ -5,6 +5,18 @@ using starsky.foundation.readmeta.Models;
 
 namespace starsky.foundation.readmeta.Helpers
 {
+	internal static partial class GeoParserInvalidCharactersRegex
+	{
+		/// <summary>
+		///  Generated regex for removing invalid characters
+		/// </summary>
+		/// <returns></returns>
+		[GeneratedRegex("[^0-9\\., ]", 
+			RegexOptions.CultureInvariant, 
+			matchTimeoutMilliseconds: 1000)]
+		public static partial Regex GetRegex();
+	}
+	
 	public static class GeoParser
 	{
 
@@ -19,12 +31,11 @@ namespace starsky.foundation.readmeta.Helpers
 			//Example: 17.21.18S
 			// DD°MM’SS.s” usage
 
-			var multiplier = ( refGps.Contains('S') || refGps.Contains('W') ) ? -1 : 1; //handle south and west
+			var multiplier = ( refGps.Contains('S') || refGps.Contains('W') ) ? -1 : 1; // handle south and west
+			
+			point = GeoParserInvalidCharactersRegex.GetRegex().Replace(point, ""); // remove the characters
 
-			point = Regex.Replace(point, "[^0-9\\., ]", "", RegexOptions.CultureInvariant,
-				TimeSpan.FromMilliseconds(100)); //remove the characters
-
-			// When you use an localisation where commas are used instead of a dot
+			// When you use a localisation where commas are used instead of a dot
 			point = point.Replace(",", ".");
 
 			var pointArray = point.Split(' '); //split the string.
@@ -53,8 +64,7 @@ namespace starsky.foundation.readmeta.Helpers
 			var multiplier = ( refGps.Contains('S') || refGps.Contains('W') ) ? -1 : 1; // handle south and west
 
 			point = point.Replace(",", " ");
-			point = Regex.Replace(point, "[^0-9\\., ]", "", RegexOptions.CultureInvariant,
-				 TimeSpan.FromMilliseconds(100)); //remove the characters
+			point = GeoParserInvalidCharactersRegex.GetRegex().Replace(point, ""); // remove the characters
 
 			var pointArray = point.Split(' '); //split the string.
 			var degrees = double.Parse(pointArray[0], CultureInfo.InvariantCulture);
@@ -98,7 +108,7 @@ namespace starsky.foundation.readmeta.Helpers
 			isoStr = isoStr.Remove(isoStr.Length - 1); // Remove trailing slash
 
 			var parts = isoStr.Split(Separator, StringSplitOptions.None);
-			if ( parts.Length < 3 || parts.Length > 4 )
+			if ( parts.Length is < 3 or > 4 )
 			{
 				// Check for parts count
 				return geoListItem;
