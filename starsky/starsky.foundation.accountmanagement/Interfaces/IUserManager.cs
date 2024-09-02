fcad18f1d768a14db3b0f6dc26d18fe1b2678734
Dion <qdraw@users.noreply.github.com>
2024-09-02 15:51:11 +0200

    fix tests

--- starsky/starsky.foundation.accountmanagement/Interfaces/IUserManager.cs
+++ starsky/starsky.foundation.accountmanagement/Interfaces/IUserManager.cs
@@ -3,101 +3,101 @@ using Microsoft.AspNetCore.Http;
 using starsky.foundation.accountmanagement.Models;
 using starsky.foundation.database.Models.Account;
 
-namespace starsky.foundation.accountmanagement.Interfaces
+namespace starsky.foundation.accountmanagement.Interfaces;
+
+public enum SignUpResultError
 {
-	public enum SignUpResultError
+	CredentialTypeNotFound,
+	NullString
+}
+
+public class SignUpResult
+{
+	public SignUpResult(User? user = null, bool success = false,
+		SignUpResultError? error = null)
 	{
-		CredentialTypeNotFound,
-		NullString
+		User = user;
+		Success = success;
+		Error = error;
 	}
 
-	public class SignUpResult
-	{
-		public User? User { get; }
-		public bool Success { get; private set; }
-		public SignUpResultError? Error { get; }
+	public User? User { get; }
+	public bool Success { get; private set; }
+	public SignUpResultError? Error { get; }
+}
 
-		public SignUpResult(User? user = null, bool success = false,
-			SignUpResultError? error = null)
-		{
-			User = user;
-			Success = success;
-			Error = error;
-		}
+public enum ValidateResultError
+{
+	CredentialTypeNotFound,
+	CredentialNotFound,
+	SecretNotValid,
+	Lockout,
+	UserNotFound
+}
+
+public enum ChangeSecretResultError
+{
+	CredentialTypeNotFound,
+	CredentialNotFound
+}
+
+public class ChangeSecretResult
+{
+	public ChangeSecretResult(bool success = false, ChangeSecretResultError? error = null)
+	{
+		Success = success;
+		Error = error;
 	}
 
-	public enum ValidateResultError
-	{
-		CredentialTypeNotFound,
-		CredentialNotFound,
-		SecretNotValid,
-		Lockout,
-		UserNotFound
-	}
+	public bool Success { get; set; }
 
-	public enum ChangeSecretResultError
-	{
-		CredentialTypeNotFound,
-		CredentialNotFound
-	}
+	public ChangeSecretResultError? Error { get; set; }
+}
 
-	public class ChangeSecretResult
-	{
-		public bool Success { get; set; }
+public interface IUserManager
+{
+	Task<UserOverviewModel> AllUsersAsync();
 
-		public ChangeSecretResultError? Error { get; set; }
+	/// <summary>
+	///     Add a new user, including Roles and UserRoles
+	/// </summary>
+	/// <param name="name">Nice Name, default string.Empty</param>
+	/// <param name="credentialTypeCode">default is: Email</param>
+	/// <param name="identifier">an email address, e.g. dont@mail.us</param>
+	/// <param name="secret">Password</param>
+	/// <returns>result object</returns>
+	Task<SignUpResult> SignUpAsync(string name, string credentialTypeCode,
+		string? identifier, string? secret);
 
-		public ChangeSecretResult(bool success = false, ChangeSecretResultError? error = null)
-		{
-			Success = success;
-			Error = error;
-		}
-	}
+	void AddToRole(User user, string roleCode);
+	void AddToRole(User user, Role role);
+	void RemoveFromRole(User user, string roleCode);
+	void RemoveFromRole(User user, Role role);
 
-	public interface IUserManager
-	{
-		Task<UserOverviewModel> AllUsersAsync();
+	ChangeSecretResult ChangeSecret(string credentialTypeCode, string? identifier,
+		string secret);
 
-		/// <summary>
-		/// Add a new user, including Roles and UserRoles
-		/// </summary>
-		/// <param name="name">Nice Name, default string.Empty</param>
-		/// <param name="credentialTypeCode">default is: Email</param>
-		/// <param name="identifier">an email address, e.g. dont@mail.us</param>
-		/// <param name="secret">Password</param>
-		/// <returns>result object</returns>
-		Task<SignUpResult> SignUpAsync(string name, string credentialTypeCode,
-			string? identifier, string? secret);
+	Task<ValidateResult> ValidateAsync(string credentialTypeCode,
+		string? identifier, string secret);
 
-		void AddToRole(User user, string roleCode);
-		void AddToRole(User user, Role role);
-		void RemoveFromRole(User user, string roleCode);
-		void RemoveFromRole(User user, Role role);
+	Task<bool> SignIn(HttpContext httpContext, User? user,
+		bool isPersistent = false);
 
-		ChangeSecretResult ChangeSecret(string credentialTypeCode, string? identifier,
-			string secret);
+	void SignOut(HttpContext httpContext);
+	int GetCurrentUserId(HttpContext httpContext);
+	User? GetCurrentUser(HttpContext httpContext);
+	User? GetUser(string credentialTypeCode, string identifier);
+	Credential? GetCredentialsByUserId(int userId);
 
-		Task<ValidateResult> ValidateAsync(string credentialTypeCode,
-			string? identifier, string secret);
+	Task<ValidateResult> RemoveUser(string credentialTypeCode,
+		string identifier);
 
-		Task<bool> SignIn(HttpContext httpContext, User? user,
-			bool isPersistent = false);
+	User? Exist(string identifier);
 
-		void SignOut(HttpContext httpContext);
-		int GetCurrentUserId(HttpContext httpContext);
-		User? GetCurrentUser(HttpContext httpContext);
-		User? GetUser(string credentialTypeCode, string identifier);
-		Credential? GetCredentialsByUserId(int userId);
+	Task<User?> ExistAsync(int userTableId);
+	Role? GetRole(string credentialTypeCode, string identifier);
 
-		Task<ValidateResult> RemoveUser(string credentialTypeCode,
-			string identifier);
-
-		User? Exist(string identifier);
-
-		Task<User?> ExistAsync(int userTableId);
-		Role? GetRole(string credentialTypeCode, string identifier);
-
-		Task<Role?> GetRoleAsync(int userId);
-		bool PreflightValidate(string userName, string password, string confirmPassword);
-	}
+	Task<Role?> GetRoleAsync(int userId);
+	bool PreflightValidate(string userName, string password, string confirmPassword);
+	CredentialType? GetCachedCredentialType(string email);
 }
--- starsky/starskytest/FakeMocks/FakeIUserManger.cs
+++ starsky/starskytest/FakeMocks/FakeIUserManger.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -21,46 +23,72 @@ public class FakeIUserManger : IUserManager
 		return Task.FromResult(_userOverviewModel);
 	}
 
-	public Task<SignUpResult> SignUpAsync(string name, string credentialTypeCode, string? identifier,
+	public Task<SignUpResult> SignUpAsync(string name, string credentialTypeCode,
+		string? identifier,
 		string? secret)
 	{
-		throw new System.NotImplementedException();
+		_userOverviewModel.Users.Add(new User
+		{
+			Name = name,
+			Credentials = new List<Credential>
+			{
+				new()
+				{
+					CredentialType = new CredentialType { Code = "email" },
+					Identifier = identifier,
+					Secret = secret,
+					IterationCount = IterationCountType.Iterate100KSha256
+				}
+			}
+		});
+
+		return Task.FromResult(new SignUpResult());
 	}
 
 	public void AddToRole(User user, string roleCode)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	public void AddToRole(User user, Role role)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	public void RemoveFromRole(User user, string roleCode)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	public void RemoveFromRole(User user, Role role)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	public ChangeSecretResult ChangeSecret(string credentialTypeCode, string? identifier,
 		string secret)
 	{
-		throw new System.NotImplementedException();
+		var result = _userOverviewModel.Users.Find(p =>
+			p.Credentials?.FirstOrDefault()?.Identifier == identifier);
+		if ( result == null )
+			return new ChangeSecretResult(false, ChangeSecretResultError.CredentialNotFound);
+		result.Credentials!.FirstOrDefault()!.IterationCount = IterationCountType.Iterate100KSha256;
+		result.Credentials!.FirstOrDefault()!.Secret = secret;
+		
+		return new ChangeSecretResult(true);
 	}
 
-	public Task<ValidateResult> ValidateAsync(string credentialTypeCode, string? identifier, string secret)
+	public Task<ValidateResult> ValidateAsync(string credentialTypeCode, string? identifier,
+		string secret)
 	{
 		var validateResult = new ValidateResult();
-		var result = _userOverviewModel.Users.Find(p => p.Credentials?.FirstOrDefault()?.Identifier == identifier);
+		var result = _userOverviewModel.Users.Find(p =>
+			p.Credentials?.FirstOrDefault()?.Identifier == identifier);
+
 		if ( result?.Credentials?.FirstOrDefault()?.Secret == secret )
-		{
 			validateResult.Success = true;
-		}
+		else
+			validateResult.Error = ValidateResultError.SecretNotValid;
 		return Task.FromResult(validateResult);
 	}
 
@@ -72,57 +100,65 @@ public class FakeIUserManger : IUserManager
 
 	public void SignOut(HttpContext httpContext)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	public int GetCurrentUserId(HttpContext httpContext)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	public User GetCurrentUser(HttpContext httpContext)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
-	public User GetUser(string credentialTypeCode, string identifier)
+	public User? GetUser(string credentialTypeCode, string identifier)
 	{
-		throw new System.NotImplementedException();
+		return _userOverviewModel.Users.Find(p => p.Credentials?.Any(credential =>
+			credential.Identifier == identifier) == true);
 	}
 
-	public Credential GetCredentialsByUserId(int userId)
+	public Credential? GetCredentialsByUserId(int userId)
 	{
-		throw new System.NotImplementedException();
+		return _userOverviewModel.Users.Find(p => p.Credentials?.Any(credential =>
+			credential.Id == 0) == true)?.Credentials?.FirstOrDefault(p =>
+			p.Id == 0);
 	}
 
 	public Task<ValidateResult> RemoveUser(string credentialTypeCode, string identifier)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	public User Exist(string identifier)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	public Task<User?> ExistAsync(int userTableId)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	public Role GetRole(string credentialTypeCode, string identifier)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	public Task<Role?> GetRoleAsync(int userId)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
 	}
 
 	public bool PreflightValidate(string userName, string password,
 		string confirmPassword)
 	{
-		throw new System.NotImplementedException();
+		throw new NotImplementedException();
+	}
+
+	public CredentialType? GetCachedCredentialType(string email)
+	{
+		return new CredentialType { Code = "email" };
 	}
 }
