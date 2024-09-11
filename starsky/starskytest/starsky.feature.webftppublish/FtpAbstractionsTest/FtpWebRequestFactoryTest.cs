65d1b88908ba203d8582a0be64e74cd03d295dd4
Dion <qdraw@users.noreply.github.com>
2024-09-11 11:43:20 +0200

    code smells

--- starsky/starskytest/starsky.feature.webftppublish/FtpAbstractionsTest/FtpWebRequestFactoryTest.cs
+++ starsky/starskytest/starsky.feature.webftppublish/FtpAbstractionsTest/FtpWebRequestFactoryTest.cs
@@ -1,28 +1,26 @@
 using System;
+using System.Net;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.feature.webftppublish.FtpAbstractions.Services;
 
-namespace starskytest.starsky.feature.webftppublish.FtpAbstractionsTest
-{
-	[TestClass]
-	public sealed class FtpWebRequestFactoryTest
-	{
-		[TestMethod]
-		[ExpectedException(typeof(UriFormatException))]
-		public void FtpWebRequestFactoryTestCreate_UriFormatException()
-		{
-			new FtpWebRequestFactory().Create("t");
-			// Invalid URI: The format of the URI could not be determined.
-		}
-		
-		[TestMethod]
-		[ExpectedException(typeof(System.Net.WebException))]
-		public void FtpWebRequestFactoryTestCreate_WebException()
-		{
-			var factory = new FtpWebRequestFactory();
+namespace starskytest.starsky.feature.webftppublish.FtpAbstractionsTest;
 
-			var test = factory.Create("ftp://test:test@404.undefined");
-			test.GetResponse();
-		}
+[TestClass]
+public sealed class FtpWebRequestFactoryTest
+{
+	[TestMethod]
+	public void FtpWebRequestFactoryTestCreate_UriFormatException()
+	{
+		Assert.ThrowsException<UriFormatException>(() => new FtpWebRequestFactory().Create("t"));
+		// Invalid URI: The format of the URI could not be determined.
+	}
+
+	[TestMethod]
+	public void FtpWebRequestFactoryTestCreate_WebException()
+	{
+		var factory = new FtpWebRequestFactory();
+
+		var test = factory.Create("ftp://test:test@404.undefined");
+		Assert.ThrowsException<WebException>(() => test.GetResponse());
 	}
 }
