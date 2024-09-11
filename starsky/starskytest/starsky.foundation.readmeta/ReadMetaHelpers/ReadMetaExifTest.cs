df313af4ccf4867d926ad3d237d1212c533468b5
Dion <qdraw@users.noreply.github.com>
2024-09-11 15:51:26 +0200

    fix/format

--- starsky/starskytest/starsky.foundation.readmeta/ReadMetaHelpers/ReadMetaExifTest.cs
+++ starsky/starskytest/starsky.foundation.readmeta/ReadMetaHelpers/ReadMetaExifTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -18,147 +19,138 @@ namespace starskytest.starsky.foundation.readmeta.ReadMetaHelpers;
 [TestClass]
 public class ReadMetaExifTest
 {
-    [TestMethod]
-    public void GetXmpGeoAlt_PositiveAltitude_ReturnsPositiveAltitude()
-    {
-        // Arrange
-        var dir2 = new XmpDirectory();
-        dir2.SetXmpMeta(new XmpMeta());
-        dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitude","1/1" );
-        dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitudeRef","0" );
+	[TestMethod]
+	public void GetXmpGeoAlt_PositiveAltitude_ReturnsPositiveAltitude()
+	{
+		// Arrange
+		var dir2 = new XmpDirectory();
+		dir2.SetXmpMeta(new XmpMeta());
+		dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitude", "1/1");
+		dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitudeRef", "0");
 
-        var allExifItems = new List<Directory>
-        {
-	        dir2
-        };
+		var allExifItems = new List<Directory> { dir2 };
 
-        // Act
-        var result = ReadMetaExif.GetXmpGeoAlt(allExifItems);
+		// Act
+		var result = ReadMetaExif.GetXmpGeoAlt(allExifItems);
 
-        // Assert
-        Assert.AreEqual(1, result, 0.001); // Use an appropriate tolerance
-    }
+		// Assert
+		Assert.AreEqual(1, result, 0.001); // Use an appropriate tolerance
+	}
 
-    [TestMethod]
-    public void GetXmpGeoAlt_NegativeAltitude_ReturnsNegativeAltitude()
-    {
-        // Arrange
-        var dir2 = new XmpDirectory();
-        dir2.SetXmpMeta(new XmpMeta());
-        dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitude","10/1" );
-        dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitudeRef","1" );
+	[TestMethod]
+	public void GetXmpGeoAlt_NegativeAltitude_ReturnsNegativeAltitude()
+	{
+		// Arrange
+		var dir2 = new XmpDirectory();
+		dir2.SetXmpMeta(new XmpMeta());
+		dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitude", "10/1");
+		dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitudeRef", "1");
 
-        var allExifItems = new List<Directory>
-        {
-	        dir2
-        };
+		var allExifItems = new List<Directory> { dir2 };
 
-        // Act
-        var result = ReadMetaExif.GetXmpGeoAlt(allExifItems);
+		// Act
+		var result = ReadMetaExif.GetXmpGeoAlt(allExifItems);
 
-        // Assert
-        Assert.AreEqual(-10, result, 0.001); // Use an appropriate tolerance
-    }
+		// Assert
+		Assert.AreEqual(-10, result, 0.001); // Use an appropriate tolerance
+	}
 
-    [TestMethod]
-    public void GetXmpGeoAlt_ZeroAltitude_ReturnsZeroAltitude()
-    {
-        // Arrange
-        var dir2 = new XmpDirectory();
-        dir2.SetXmpMeta(new XmpMeta());
-        dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitude","0/1" );
-        dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitudeRef","1" );
-        var allExifItems = new List<Directory>
-        {
-	        dir2
-        };
-        
-        // Act
-        var result = ReadMetaExif.GetXmpGeoAlt(allExifItems);
+	[TestMethod]
+	public void GetXmpGeoAlt_ZeroAltitude_ReturnsZeroAltitude()
+	{
+		// Arrange
+		var dir2 = new XmpDirectory();
+		dir2.SetXmpMeta(new XmpMeta());
+		dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitude", "0/1");
+		dir2.XmpMeta?.SetProperty("http://ns.adobe.com/exif/1.0/", "GPSAltitudeRef", "1");
+		var allExifItems = new List<Directory> { dir2 };
 
-        // Assert
-        Assert.AreEqual(0, result, 0.001); // Use an appropriate tolerance
-    }
+		// Act
+		var result = ReadMetaExif.GetXmpGeoAlt(allExifItems);
 
-    [TestMethod]
-    public void GetIsoSpeedValue_FromExifSubIfd_ReturnsIsoSpeed()
-    {
-        // Arrange
-        var subIfdItem = new ExifSubIfdDirectory();
-        subIfdItem.Set(ExifDirectoryBase.TagIsoEquivalent, "400");
+		// Assert
+		Assert.AreEqual(0, result, 0.001); // Use an appropriate tolerance
+	}
 
-        var allExifItems = new List<Directory> { subIfdItem };
+	[TestMethod]
+	public void GetIsoSpeedValue_FromExifSubIfd_ReturnsIsoSpeed()
+	{
+		// Arrange
+		var subIfdItem = new ExifSubIfdDirectory();
+		subIfdItem.Set(ExifDirectoryBase.TagIsoEquivalent, "400");
 
-        // Act
-        var result = ReadMetaExif.GetIsoSpeedValue(allExifItems);
+		var allExifItems = new List<Directory> { subIfdItem };
 
-        // Assert
-        Assert.AreEqual(400, result);
-    }
+		// Act
+		var result = ReadMetaExif.GetIsoSpeedValue(allExifItems);
 
-    [TestMethod]
-    public void GetIsoSpeedValue_FromCanonMakerNote_ReturnsIsoSpeed()
-    {
-        // Arrange
-        var canonMakerNoteDirectory = new CanonMakernoteDirectory();
-        
-        // Magic Numbers
-        // 19 = ISO 400
-        canonMakerNoteDirectory.Set(CanonMakernoteDirectory.CameraSettings.TagIso, "19");
+		// Assert
+		Assert.AreEqual(400, result);
+	}
 
-        var allExifItems = new List<Directory> { canonMakerNoteDirectory };
+	[TestMethod]
+	public void GetIsoSpeedValue_FromCanonMakerNote_ReturnsIsoSpeed()
+	{
+		// Arrange
+		var canonMakerNoteDirectory = new CanonMakernoteDirectory();
 
-        // Act
-        var result = ReadMetaExif.GetIsoSpeedValue(allExifItems);
+		// Magic Numbers
+		// 19 = ISO 400
+		canonMakerNoteDirectory.Set(CanonMakernoteDirectory.CameraSettings.TagIso, "19");
 
-        // Assert
-        Assert.AreEqual(400, result);
-    }
+		var allExifItems = new List<Directory> { canonMakerNoteDirectory };
 
-    [TestMethod]
-    public void GetIsoSpeedValue_FromCanonMakerNote_AutoIso_ReturnsCalculatedIsoSpeed()
-    {
-        // Arrange
-        var canonMakerNoteDirectory = new CanonMakernoteDirectory();
-        
-        //						15 is magic number for auto
-        canonMakerNoteDirectory.Set(CanonMakernoteDirectory.CameraSettings.TagIso, "15");
-        canonMakerNoteDirectory.Set(CanonMakernoteDirectory.ShotInfo.TagAutoIso, "200");
-        canonMakerNoteDirectory.Set(CanonMakernoteDirectory.ShotInfo.TagBaseIso, "400");
+		// Act
+		var result = ReadMetaExif.GetIsoSpeedValue(allExifItems);
 
-        var allExifItems = new List<Directory> { canonMakerNoteDirectory };
+		// Assert
+		Assert.AreEqual(400, result);
+	}
 
-        // Act
-        var result = ReadMetaExif.GetIsoSpeedValue(allExifItems);
+	[TestMethod]
+	public void GetIsoSpeedValue_FromCanonMakerNote_AutoIso_ReturnsCalculatedIsoSpeed()
+	{
+		// Arrange
+		var canonMakerNoteDirectory = new CanonMakernoteDirectory();
 
-        // Assert
-        Assert.AreEqual(800, result); // 400 * 200 / 100 = 800
-    }
+		//						15 is magic number for auto
+		canonMakerNoteDirectory.Set(CanonMakernoteDirectory.CameraSettings.TagIso, "15");
+		canonMakerNoteDirectory.Set(CanonMakernoteDirectory.ShotInfo.TagAutoIso, "200");
+		canonMakerNoteDirectory.Set(CanonMakernoteDirectory.ShotInfo.TagBaseIso, "400");
 
-    [TestMethod]
-    public void GetIsoSpeedValue_FromXmp_ReturnsIsoSpeed()
-    {
-        // Arrange
-        var xmpStream = new MemoryStream(CreateAnImageA6600.Bytes.ToArray());
+		var allExifItems = new List<Directory> { canonMakerNoteDirectory };
 
-        var allExifItems = ImageMetadataReader.ReadMetadata(xmpStream).ToList();
+		// Act
+		var result = ReadMetaExif.GetIsoSpeedValue(allExifItems);
 
-        // Act
-        var result = ReadMetaExif.GetIsoSpeedValue(allExifItems);
+		// Assert
+		Assert.AreEqual(800, result); // 400 * 200 / 100 = 800
+	}
 
-        // Assert
-        Assert.AreEqual(800, result);
-        
-        xmpStream.Dispose();
-    }
+	[TestMethod]
+	public void GetIsoSpeedValue_FromXmp_ReturnsIsoSpeed()
+	{
+		// Arrange
+		var xmpStream = new MemoryStream(CreateAnImageA6600.Bytes.ToArray());
 
-    [TestMethod]
-    [ExpectedException(typeof(System.ArgumentException))]
-    public void ParseExifDirectory_NullItem()
-    {
-	    var readMetaExif = new ReadMetaExif(new FakeIStorage(), new AppSettings(),
-		    new FakeIWebLogger());
-	    readMetaExif.ParseExifDirectory([], null);
-	    // expect exception
-    }
+		var allExifItems = ImageMetadataReader.ReadMetadata(xmpStream).ToList();
+
+		// Act
+		var result = ReadMetaExif.GetIsoSpeedValue(allExifItems);
+
+		// Assert
+		Assert.AreEqual(800, result);
+
+		xmpStream.Dispose();
+	}
+
+	[TestMethod]
+	[ExpectedException(typeof(ArgumentException))]
+	public void ParseExifDirectory_NullItem()
+	{
+		var readMetaExif = new ReadMetaExif(new FakeIStorage(), new AppSettings(),
+			new FakeIWebLogger());
+		readMetaExif.ParseExifDirectory([], null);
+		// expect exception
+	}
 }
