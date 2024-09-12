a2260a2bf6a9acb9ff757ab33b266c4592246366
Dion <qdraw@users.noreply.github.com>
2024-09-12 08:50:24 +0200

    add tests

--- starsky/starsky/clientapp/src/components/organisms/menu-archive/internal/select-menu-item.stories.tsx
+++ starsky/starsky/clientapp/src/components/organisms/menu-archive/internal/select-menu-item.stories.tsx
@@ -1,21 +1,29 @@
-import {SelectMenuItem} from "./select-menu-item";
+import { SelectMenuItem } from "./select-menu-item";
 
 export default {
   title: "components/organisms/menu-archive/internal/select-menu-item"
 };
 
 export const Default = () => {
-  return <SelectMenuItem select={undefined} removeSidebarSelection={() => {
-  }} toggleLabels={() => {
-  }}></SelectMenuItem>;
+  return (
+    <SelectMenuItem
+      select={undefined}
+      removeSidebarSelection={() => {}}
+      toggleLabels={() => {}}
+    ></SelectMenuItem>
+  );
 };
 
 Default.storyName = "default";
 
 export const Select = () => {
-  return <SelectMenuItem select={[""]} removeSidebarSelection={() => {
-  }} toggleLabels={() => {
-  }}></SelectMenuItem>;
+  return (
+    <SelectMenuItem
+      select={[""]}
+      removeSidebarSelection={() => {}}
+      toggleLabels={() => {}}
+    ></SelectMenuItem>
+  );
 };
 
 Select.storyName = "select";
