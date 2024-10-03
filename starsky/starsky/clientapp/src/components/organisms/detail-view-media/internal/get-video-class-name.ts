72feee36202f5554746a291fb9414a091ed05dc8
Dion <qdraw@users.noreply.github.com>
2024-10-03 20:10:58 +0200

    useless feedback from sonarcloud Provide multiple methods instead of using "isPaused" to determine which action to take. [+1 location]sonarlint(typescript:S2301)

--- starsky/starsky/clientapp/src/components/organisms/detail-view-media/internal/get-video-class-name.ts
+++ starsky/starsky/clientapp/src/components/organisms/detail-view-media/internal/get-video-class-name.ts
@@ -1,15 +1,12 @@
+// ignore: Provide multiple methods instead of using "isPaused" to determine which action to take.
 export function GetVideoClassName(isPaused: boolean, isStarted: boolean): string {
   if (isPaused) {
-    return handleIsStarted(isStarted);
+    if (isStarted) {
+      return "video play";
+    } else {
+      return "video first";
+    }
   } else {
     return "video pause";
-  }
-}
-
-function handleIsStarted(isStarted: boolean) {
-  if (isStarted) {
-    return "video play";
-  } else {
-    return "video first";
   }
 }
