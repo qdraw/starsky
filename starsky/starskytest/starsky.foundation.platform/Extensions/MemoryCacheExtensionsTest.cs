dca8a3de77a14ba0606c2daa3639cc7573973d6b
Dion <qdraw@users.noreply.github.com>
2024-10-10 10:48:29 +0200

    breaking change in internals memory cache

--- starsky/starskytest/starsky.foundation.platform/Extensions/MemoryCacheExtensionsTest.cs
+++ starsky/starskytest/starsky.foundation.platform/Extensions/MemoryCacheExtensionsTest.cs
@@ -5,43 +5,42 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.platform.Extensions;
 using starskytest.FakeMocks;
 
-namespace starskytest.starsky.foundation.platform.Extensions
+namespace starskytest.starsky.foundation.platform.Extensions;
+
+[TestClass]
+public sealed class MemoryCacheExtensionsTest
 {
-	[TestClass]
-	public sealed class MemoryCacheExtensionsTest
+	[TestMethod]
+	public void MemoryCacheExtensions_NoContentInCache()
 	{
-		[TestMethod]
-		public void NoContentInCache()
-		{
-			var provider = new ServiceCollection()
-				.AddMemoryCache();
+		var provider = new ServiceCollection()
+			.AddMemoryCache();
 
-			var buildServiceProvider =  provider.BuildServiceProvider();
-			var memoryCache = buildServiceProvider.GetService<IMemoryCache>();
-			var keys= memoryCache?.GetKeys<string>();
-			Assert.AreEqual(0, keys?.Count());
-		}
-		
-		[TestMethod]
-		public void OneItemInCache()
-		{
-			var provider = new ServiceCollection()
-				.AddMemoryCache();
+		var buildServiceProvider = provider.BuildServiceProvider();
+		var memoryCache = buildServiceProvider.GetService<IMemoryCache>();
+		var keys = memoryCache?.GetKeys<string>();
+		Assert.AreEqual(0, keys?.Count());
+	}
 
-			var buildServiceProvider =  provider.BuildServiceProvider();
-			var memoryCache = buildServiceProvider.GetService<IMemoryCache>();
-			memoryCache?.Set("test", "");
-			var keys= memoryCache?.GetKeys<string>().ToList();
-			Assert.AreEqual(1, keys?.Count);
-			Assert.AreEqual("test", keys?[0]);
-		}
+	[TestMethod]
+	public void MemoryCacheExtensions_OneItemInCache()
+	{
+		var provider = new ServiceCollection()
+			.AddMemoryCache();
 
-		[TestMethod]
-		public void FakeCache_Invalid()
-		{
-			var cache = new FakeMemoryCache();
-			var keys= cache.GetKeys<string>().ToList();
-			Assert.AreEqual(0, keys.Count);
-		}
+		var buildServiceProvider = provider.BuildServiceProvider();
+		var memoryCache = buildServiceProvider.GetService<IMemoryCache>();
+		memoryCache?.Set("test", "");
+		var keys = memoryCache?.GetKeys<string>().ToList();
+		Assert.AreEqual(1, keys?.Count);
+		Assert.AreEqual("test", keys?[0]);
+	}
+
+	[TestMethod]
+	public void FakeCache_Invalid()
+	{
+		var cache = new FakeMemoryCache();
+		var keys = cache.GetKeys<string>().ToList();
+		Assert.AreEqual(0, keys.Count);
 	}
 }
