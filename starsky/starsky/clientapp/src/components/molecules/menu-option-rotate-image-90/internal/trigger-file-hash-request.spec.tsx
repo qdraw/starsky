4ddc5dfee03d03bd02fb7fd3e08ea767e48f186f
Dion <qdraw@users.noreply.github.com>
2024-10-16 18:36:56 +0200

    add test

---
+++ starsky/starsky/clientapp/src/components/molecules/menu-option-rotate-image-90/internal/trigger-file-hash-request.spec.tsx
@@ -0,0 +1,37 @@
+import { act } from "react";
+import { IDetailView } from "../../../../interfaces/IDetailView";
+import { Orientation } from "../../../../interfaces/IFileIndexItem";
+import * as RequestNewFileHash from "./request-new-filehash";
+import { TriggerFileHashRequest } from "./trigger-file-hash-request";
+
+describe("TriggerFileHashRequest", () => {
+  const state: IDetailView = {
+    subPath: "/test/image.jpg",
+    fileIndexItem: {
+      fileHash: "123",
+      filePath: "/test/image.jpg",
+      orientation: Orientation.Horizontal
+    }
+  } as IDetailView;
+
+  it("returns immediately when the file hash changes on the first attempt", () => {
+    jest.useFakeTimers();
+
+    const requestSpy = jest
+      .spyOn(RequestNewFileHash, "RequestNewFileHash")
+      .mockResolvedValueOnce(true);
+
+    const setIsLoading = jest.fn();
+    TriggerFileHashRequest(state, setIsLoading, jest.fn(), 1, 1);
+
+    act(() => {
+      jest.advanceTimersByTime(100);
+    });
+
+    console.log("after TriggerFileHashRequest");
+
+    jest.advanceTimersByTime(1);
+    jest.runAllTimers();
+    expect(requestSpy).toHaveBeenCalledTimes(1);
+  });
+});
