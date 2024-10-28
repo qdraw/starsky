84126620debe6bdb51a55daabed6bba4bb3c804e
Dion <qdraw@users.noreply.github.com>
2024-10-28 22:55:30 +0100

    add tests to cover error cases

--- starsky/starskytest/starsky.foundation.search/Services/SearchWideDateTimeTest.cs
+++ starsky/starskytest/starsky.foundation.search/Services/SearchWideDateTimeTest.cs
@@ -6,7 +6,7 @@ using starsky.feature.search.Services;
 using starsky.feature.search.ViewModels;
 using starsky.foundation.database.Models;
 
-namespace starskytest.starsky.feature.search.Services;
+namespace starskytest.starsky.foundation.search.Services;
 
 [TestClass]
 public sealed class SearchWideDateTimeTest
@@ -166,5 +166,63 @@ public sealed class SearchWideDateTimeTest
 		Assert.AreEqual(expectedResult.Body.NodeType, result.Body.NodeType);
 		Assert.AreEqual(expectedResult.Parameters.FirstOrDefault()?.Type,
 			result.Parameters.FirstOrDefault()?.Type);
+	}
+
+	[TestMethod]
+	public void WideSearchDateTimeGet_ArgumentException_BothGreaterSmaller()
+	{
+		// Arrange
+		var model = new SearchViewModel();
+		model.SetAddSearchFor("2023-01-01");
+		model.SetAddSearchFor("2023-01-02");
+		model.SetAddSearchForOptions(">");
+		model.SetAddSearchForOptions("<");
+
+		// Write enum with reflection
+
+		// Act
+		Assert.ThrowsException<ArgumentException>(() =>
+		{
+			SearchWideDateTime.WideSearchDateTimeGet(model, 0, InvalidEnum());
+		});
+	}
+	
+	[DataTestMethod] // [Theory]
+	[DataRow("<")]
+	[DataRow(">")]
+	[DataRow("=")]
+	public void WideSearchDateTimeGet_ArgumentNullException_LessGreaterThanInvalidEnum(string value)
+	{
+		// Arrange
+		var model = new SearchViewModel();
+		model.SetAddSearchFor("2023-01-02 12:00:00"); // specific date instead of day only
+		model.SetAddSearchForOptions(value);
+		
+		// Act
+		Assert.ThrowsException<ArgumentNullException>(() =>
+		{
+			SearchWideDateTime.WideSearchDateTimeGet(model, 0, InvalidEnum());
+		});
+	}
+
+	/// <summary>
+	/// Override the enum with reflection
+	/// Set invalid enum value to trigger an exception
+	/// </summary>
+	/// <returns></returns>
+	private static SearchWideDateTime.WideSearchDateTimeGetType InvalidEnum()
+	{
+		var myClass = new SetSearchWideDateTimeOverrideObject();
+		var propertyObject = myClass.GetType().GetProperty("Type");
+
+		// Set an invalid value that should trigger an exception
+		propertyObject?.SetValue(myClass, 44, null);
+
+		return myClass.Type;
+	}
+
+	private class SetSearchWideDateTimeOverrideObject
+	{
+		public SearchWideDateTime.WideSearchDateTimeGetType Type { get; set; }
 	}
 }
