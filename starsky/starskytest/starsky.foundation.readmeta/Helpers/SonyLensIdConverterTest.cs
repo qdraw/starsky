ebe942e4251c062d82dd39f385daab89febb88e4
Dion <qdraw@users.noreply.github.com>
2024-11-09 22:02:14 +0100

    #1807 add extra tests

--- starsky/starskytest/starsky.foundation.readmeta/Helpers/SonyLensIdConverterTest.cs
+++ starsky/starskytest/starsky.foundation.readmeta/Helpers/SonyLensIdConverterTest.cs
@@ -15,7 +15,9 @@ public class SonyLensIdConverterTest
 	
 	[TestMethod]
 	[DataRow("0", "Minolta AF 28-85mm F3.5-4.5 New")]
+	[DataRow("55", "Sony DT 18-55mm F3.5-5.6 SAM (SAL1855) or SAM II")]
 	[DataRow("61184", "Canon EF Adapter")]
+	[DataRow("65535", "E-Mount, T-Mount, Other Lens or no lens")]
 	[DataRow("NOT_FOUND_ID", null)]
 	public void GetById(string id, string? expected)
 	{
