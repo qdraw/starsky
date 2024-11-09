095adebc789993dcd75e9a911eda29a2b244e245
Dion <qdraw@users.noreply.github.com>
2024-11-09 16:58:40 +0100

    check in CSF

--- starsky/starsky/clientapp/src/shared/comma-seperated-filelist/comma-seperated-filelist.ts
+++ starsky/starsky/clientapp/src/shared/comma-seperated-filelist/comma-seperated-filelist.ts
@@ -1,16 +1,23 @@
 export class CommaSeperatedFileList {
-  public CommaSpaceLastDot(inputs: string[], messageNoExtensionItem?: string): string {
-    let output = "";
-    for (let index = 0; index < inputs.length; index++) {
-      const element = inputs[index];
+  private GetUniqueExtensions(inputs: string[], messageNoExtensionItem?: string): Array<string> {
+    const uniqueExtensions = new Set<string>();
+    for (const element of inputs) {
       const fileExtension = element.split(".")[element.split(".").length - 1];
       if (element !== fileExtension) {
-        output += fileExtension;
+        uniqueExtensions.add(fileExtension);
       } else {
-        output += messageNoExtensionItem ? messageNoExtensionItem : "without extension";
+        uniqueExtensions.add(messageNoExtensionItem ? messageNoExtensionItem : "without extension");
       }
+      uniqueExtensions.add(fileExtension);
+    }
+    return Array.from(uniqueExtensions).sort();
+  }
 
-      if (index !== inputs.length - 1) {
+  public CommaSpaceLastDot(inputs: string[], messageNoExtensionItem?: string): string {
+    let output = "";
+    const uniqueExtensionsArray = this.GetUniqueExtensions(inputs, messageNoExtensionItem);
+    for (let index = 0; index < uniqueExtensionsArray.length; index++) {
+      if (index !== uniqueExtensionsArray.length - 1) {
         output += ", ";
       }
     }
