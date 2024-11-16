8700012e11881256a838586fccb446ff4e2f0212
Dion <qdraw@users.noreply.github.com>
2024-11-16 22:25:00 +0100

    WIP

--- starsky-tools/build-tools/ffmpeg-download1.sh
+++ starsky-tools/build-tools/ffmpeg-download1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+FFBINARIES_API="https://ffbinaries.com/api/v1/version/6.1"
+OSX_ARM64="https://www.osxexperts.net/ffmpeg71arm.zip"
 
 # Fetch the JSON data
-json=$(curl -s https://ffbinaries.com/api/v1/version/6.1)
+json=$(curl -s $FFBINARIES_API)
 
 # Create a directory to store the binaries
 mkdir -p ffmpeg_binaries
@@ -14,23 +16,22 @@ urls=$(echo "$json" | grep -o '"ffmpeg":"[^"]*"' | sed 's/"ffmpeg":"//;s/"//')
 
 for url in $urls; do
   # Extract architecture and URL
-#   architecture=$(echo "$line" | grep -oP '"[^"]+(?=":)')
   architecture=$(echo "$url" | sed -n 's/.*-\([a-z0-9-]*-[a-z0-9]*\)\.zip/\1/p')
+  filename=$(basename "$url")
 
   # Download the binary
-  echo "Downloading $url for $architecture ..."
+  echo "Downloading $url for $architecture $filename..."
   curl -L -O "$url"
 
   # Add to output JSON
-  output_json="${output_json}{\"architecture\":\"$architecture\",\"url\":\"$url\"},"
+  output_json="${output_json}{\"architecture\":\"$architecture\",\"url\":\"$filename\"},"
 done
 
 
 # Add osx-arm64 explicitly
 custom_arch="osx-arm64"
-custom_url="https://www.osxexperts.net/ffmpeg71arm.zip"
 echo "Adding custom architecture $custom_arch with URL $custom_url..."
-curl -L -O "$custom_url"
+curl -L -O "$FFBINARIES_API"
 output_json="${output_json}{\"architecture\":\"$custom_arch\",\"url\":\"$custom_url\"},"
 
 # Finalize JSON
