648ea20990783df51521cf4f4def65b1bec9dd6f
Dion <qdraw@users.noreply.github.com>
2024-11-22 22:14:09 +0100

    #1833 removve

--- starsky/starsky.foundation.video/GetDependencies/GetVersionString.cs
+++ starsky/starsky.foundation.video/GetDependencies/GetVersionString.cs
@@ -4,12 +4,6 @@ namespace starsky.foundation.video.GetDependencies;
 
 public class GetVersionString
 {
-	/// <summary>
-	///     https://www.osxexperts.net/
-	/// </summary>
-	private const string OsxArm64 = "https://www.osxexperts.net/ffmpeg71arm.zip";
-
-	private const string FfBinariesApi = "https://ffbinaries.com/api/v1/version/6.1";
 	private readonly IHttpClientHelper _httpClientHelper;
 
 	public GetVersionString(IHttpClientHelper httpClientHelper)
@@ -19,6 +13,6 @@ public class GetVersionString
 
 	private async Task GetApi()
 	{
-		await _httpClientHelper.ReadString(FfBinariesApi);
+		await _httpClientHelper.ReadString("");
 	}
 }
