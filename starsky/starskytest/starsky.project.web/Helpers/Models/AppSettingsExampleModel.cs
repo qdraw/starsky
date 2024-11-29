b7e61b3c919b37b78ef982a455621c0156871cc8
Dion <qdraw@users.noreply.github.com>
2024-11-29 14:54:48 +0100

    Portmapper

---
+++ starsky/starskytest/starsky.project.web/Helpers/Models/AppSettingsExampleModel.cs
@@ -0,0 +1,25 @@
+namespace starskytest.starsky.project.web.Helpers.Models;
+
+public class AppSettingsExampleModel
+{
+	public class KestrelGlobalConfig
+	{
+		public KestrelConfig Kestrel { get; set; }
+	}
+
+	public class KestrelConfig
+	{
+		public ExampleEndpoints Endpoints { get; set; }
+	}
+
+	public class ExampleEndpoints
+	{
+		public EndpointObject Https { get; set; }
+		public EndpointObject Http { get; set; }
+	}
+
+	public class EndpointObject
+	{
+		public string Url { get; set; }
+	}
+}
