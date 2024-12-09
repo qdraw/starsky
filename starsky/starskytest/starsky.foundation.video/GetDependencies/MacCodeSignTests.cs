879358b0f4078121c930ce7365b98d20a5154e61
Dion <qdraw@users.noreply.github.com>
2024-12-09 22:04:42 +0100

    #1833 actual test on mac os

--- starsky/starskytest/starsky.foundation.video/GetDependencies/MacCodeSignTests.cs
+++ starsky/starskytest/starsky.foundation.video/GetDependencies/MacCodeSignTests.cs
@@ -1,5 +1,7 @@
+using System;
 using System.IO;
 using System.Threading.Tasks;
+using Medallion.Shell;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.storage.Helpers;
 using starsky.foundation.storage.Storage;
@@ -13,9 +15,9 @@ namespace starskytest.starsky.foundation.video.GetDependencies;
 public class MacCodeSignTests
 {
 	private readonly StorageHostFullPathFilesystem _hostFileSystemStorage;
+	private readonly FakeIWebLogger _logger;
 	private readonly MacCodeSign _macCodeSign;
 	private readonly string _testFolder;
-	private readonly FakeIWebLogger _logger;
 
 	public MacCodeSignTests()
 	{
@@ -32,13 +34,20 @@ public class MacCodeSignTests
 		Directory.Delete(_testFolder, true);
 	}
 
-	private async Task<string> CreateSubFiles(int codeSignExitCode, int xattrExitCode)
+	private async Task<string> CreateStubExeFile()
+	{
+		var chmodHelper = new FfmpegChmod(_hostFileSystemStorage, new FakeIWebLogger());
+		var exeFile = Path.Combine(_testFolder, "testExecutable");
+		CreateStubFile(exeFile, "#!/bin/bash\necho Fake Executable");
+		await chmodHelper.Chmod(exeFile);
+		return exeFile;
+	}
+
+	private async Task<string> CreateStubFiles(int codeSignExitCode, int xattrExitCode)
 	{
 		var chmodHelper = new FfmpegChmod(_hostFileSystemStorage, new FakeIWebLogger());
 
-		var exeFile = Path.Combine(_testFolder, "testExecutable");
-		CreateStubFile(exeFile, "#!/bin/bash\necho Fake Executable");
-
+		var exeFile = await CreateStubExeFile();
 
 		var codeSignPath = Path.Combine(_testFolder, "codesign");
 		CreateStubFile(codeSignPath, $"#!/bin/bash\necho codesign\nexit {codeSignExitCode}");
@@ -48,7 +57,6 @@ public class MacCodeSignTests
 		CreateStubFile(xattrPath, $"#!/bin/bash\nexit {xattrExitCode}");
 		_macCodeSign.XattrPath = xattrPath;
 
-		await chmodHelper.Chmod(exeFile);
 		await chmodHelper.Chmod(codeSignPath);
 		await chmodHelper.Chmod(xattrPath);
 
@@ -70,7 +78,7 @@ public class MacCodeSignTests
 		int xattrExitCode, bool expectedResult)
 	{
 		// Arrange
-		var exeFile = await CreateSubFiles(codeSignExitCode, xattrExitCode);
+		var exeFile = await CreateStubFiles(codeSignExitCode, xattrExitCode);
 
 		// Act
 		var result = await _macCodeSign.MacCodeSignAndXattrExecutable(exeFile);
@@ -83,7 +91,7 @@ public class MacCodeSignTests
 	public async Task MacCodeSignExecutable_ShouldLogError_WhenCodeSignFails()
 	{
 		// Arrange
-		var exeFile = await CreateSubFiles(1, 0);
+		var exeFile = await CreateStubFiles(1, 0);
 
 		// Act
 		var result = await _macCodeSign.MacCodeSignExecutable(exeFile);
@@ -98,7 +106,7 @@ public class MacCodeSignTests
 	public async Task MacXattrExecutable_ShouldLogError_WhenXattrFails()
 	{
 		// Arrange
-		var exeFile = await CreateSubFiles(0, 1);
+		var exeFile = await CreateStubFiles(0, 1);
 
 		// Act
 		var result = await _macCodeSign.MacXattrExecutable(exeFile);
@@ -107,5 +115,86 @@ public class MacCodeSignTests
 		Assert.IsFalse(result);
 		Assert.IsTrue(_logger.TrackedExceptions.Exists(entry =>
 			entry.Item2?.Contains("xattr Command failed with exit code") == true));
+	}
+
+	[TestMethod]
+	public async Task MacCodeSignAndXattrExecutable_QuarantineEventsV2_ShouldReturnTrue__MacOnly()
+	{
+		if ( !OperatingSystem.IsMacOS() )
+		{
+			Assert.Inconclusive("This test is only applicable on macOS.");
+			return;
+		}
+
+		// Arrange
+		var exeFile = await CreateStubExeFile();
+
+		var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+		var addResult = await Command.Run("sqlite3",
+			$"{home}/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2",
+			"INSERT INTO LSQuarantineEvent (LSQuarantineEventIdentifier, LSQuarantineTimeStamp, " +
+			"LSQuarantineAgentBundleIdentifier, LSQuarantineAgentName, LSQuarantineDataURLString, " +
+			"LSQuarantineSenderName, LSQuarantineSenderAddress, LSQuarantineTypeNumber, " +
+			"LSQuarantineOriginTitle, LSQuarantineOriginURLString, LSQuarantineOriginAlias) VALUES " +
+			"('B555DB5F-D82A-408B-B9A6-D4F4012FD520', 570726604.559004, 'com.apple.Safari', 'Safari', " +
+			"'https://qdraw.nl/test.zip', NULL, NULL, 0, NULL, " +
+			" 'https://github.com/qdraw/starsky/releases', NULL);"
+		).Task;
+
+		Console.WriteLine(addResult.StandardOutput);
+		Console.WriteLine(addResult.StandardError);
+
+		await Command.Run("xattr", "-w", "com.apple.quarantine",
+			"\"0081;5f8e2e1e;Safari;B555DB5F-D82A-408B-B9A6-D4F4012FD520\"",
+			exeFile).Task;
+
+		var xattrBefore = await Command.Run("xattr", "-p",
+			"com.apple.quarantine", exeFile).Task;
+
+		Console.WriteLine("xattr list:");
+		Console.WriteLine(xattrBefore.StandardOutput);
+
+		Assert.AreEqual("\"0081;5f8e2e1e;Safari;B555DB5F-D82A-408B-B9A6-D4F4012FD520\"",
+			xattrBefore.StandardOutput.Trim());
+
+		// Act
+		var result = await _macCodeSign.MacCodeSignAndXattrExecutable(exeFile);
+
+		var xattrAfter = await Command.Run("xattr", "-p",
+			"com.apple.quarantine", exeFile).Task;
+
+		// Assert
+		Assert.AreEqual(string.Empty, xattrAfter.StandardOutput);
+		Assert.IsTrue(result);
+		
+		Cleanup();
+	}
+
+	[TestMethod]
+	public async Task MacCodeSignAndXattrExecutable_CodeSign__MacOnly()
+	{
+		if ( !OperatingSystem.IsMacOS() )
+		{
+			Assert.Inconclusive("This test is only applicable on macOS.");
+			return;
+		}
+
+		// Arrange
+		var exeFile = await CreateStubExeFile();
+		
+		var codeSignBefore = await Command.Run("codesign", "-dvv", exeFile).Task;
+		
+		// Act
+		var result = await _macCodeSign.MacCodeSignAndXattrExecutable(exeFile);
+		
+		var codeSignAfter = await Command.Run("codesign", "-dvv", exeFile).Task;
+		Console.WriteLine(codeSignAfter.StandardOutput);
+		Console.WriteLine(codeSignAfter.StandardError);
+
+		// Assert
+		Assert.IsTrue(result);
+		Assert.IsTrue(codeSignBefore.StandardError.Contains("code object is not signed at all"));
+		Assert.IsTrue(codeSignAfter.StandardError.Contains("Identifier=testExecutable"));
 	}
 }
