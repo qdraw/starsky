211d79a7e9357114606b6b783b6a69a4dfe68342
Dion <qdraw@users.noreply.github.com>
2025-04-08 17:08:16 +0200

    Dockerfile

---
+++ starsky-tools/dropbox-import/command.sh
@@ -0,0 +1,13 @@
+
+cd "$(dirname "$0")" # Change to the script's directory
+
+cd ../..
+pwd
+
+docker build -t starsky-dropbox-import -f starsky-tools/dropbox-import/Dockerfile . --progress=plain
+docker rm -f starsky-dropbox-import
+docker run \
+  -v ./fotobieb:/mnt/data/sync/fotobieb \
+  -p 6590:80 \
+  --name starsky-dropbox-import \
+  starsky-dropbox-import
