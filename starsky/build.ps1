f183eef3720cc8cd9aa1e5524c55f8203055bdca
Dion <qdraw@users.noreply.github.com>
2025-04-14 10:14:27 +0200

    add build args

--- starsky/build.ps1
+++ starsky/build.ps1
@@ -6,6 +6,8 @@ Param(
 
 Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.PSVersion)"
 
+Write-Output "BuildArguments: $BuildArguments"
+
 Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { Write-Error $_ -ErrorAction Continue; exit 1 }
 $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 
