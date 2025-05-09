632478f752f46d8ae4f241ac160713ca61de26c5
Dion <qdraw@users.noreply.github.com>
2025-05-09 13:28:43 +0200

    allow --connection here

--- starsky/starskydependenciesdownloadcli/Program.cs
+++ starsky/starskydependenciesdownloadcli/Program.cs
@@ -17,6 +17,9 @@ public static class Program
 {
 	public static async Task Main(string[] args)
 	{
+		// Use args in application
+		new ArgsHelper().SetEnvironmentByArgs(args);
+
 		var services = new ServiceCollection();
 
 		// Setup AppSettings
