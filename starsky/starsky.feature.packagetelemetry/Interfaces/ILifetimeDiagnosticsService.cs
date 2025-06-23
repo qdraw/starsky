76a5047207b8ece401bb99a73ac98597d9f63981
Dion <qdraw@users.noreply.github.com>
2025-06-23 17:29:46 +0200

    add to package telemetry

--- starsky/starsky.feature.packagetelemetry/Interfaces/ILifetimeDiagnosticsService.cs
+++ starsky/starsky.feature.packagetelemetry/Interfaces/ILifetimeDiagnosticsService.cs
@@ -7,4 +7,5 @@ namespace starsky.feature.packagetelemetry.Interfaces;
 public interface ILifetimeDiagnosticsService
 {
 	Task<DiagnosticsItem?> AddOrUpdateApplicationStopping(DateTime startTime);
+	Task<double> GetLastApplicationStoppingTimeInMinutes();
 }
--- starsky/starsky.feature.packagetelemetry/Services/LifetimeDiagnosticsService.cs
+++ starsky/starsky.feature.packagetelemetry/Services/LifetimeDiagnosticsService.cs
@@ -6,11 +6,12 @@ using starsky.foundation.database.Diagnostics;
 using starsky.foundation.database.Diagnostics.Interfaces;
 using starsky.foundation.database.Models;
 using starsky.foundation.injection;
+using starsky.foundation.platform.Interfaces;
 
 namespace starsky.feature.packagetelemetry.Services;
 
 [Service(typeof(ILifetimeDiagnosticsService), InjectionLifetime = InjectionLifetime.Scoped)]
-public class LifetimeDiagnosticsService(IDiagnosticsService diagnosticsService)
+public class LifetimeDiagnosticsService(IDiagnosticsService diagnosticsService, IWebLogger logger)
 	: ILifetimeDiagnosticsService
 {
 	public async Task<DiagnosticsItem?> AddOrUpdateApplicationStopping(
@@ -18,10 +19,24 @@ public class LifetimeDiagnosticsService(IDiagnosticsService diagnosticsService)
 	{
 		var uptime = DateTime.UtcNow - startTime;
 		var minutes = Math.Round(uptime.TotalMinutes, 4);
-		
+
+		logger.LogInformation($"[LifetimeDiagnosticsService] Application stopping lifetime in minutes: {minutes}");
 		var item = await diagnosticsService.AddOrUpdateItem(
 			DiagnosticsType.ApplicationStoppingLifetimeInMinutes,
 			minutes.ToString(CultureInfo.InvariantCulture));
 		return item;
+	}
+
+	public async Task<double> GetLastApplicationStoppingTimeInMinutes()
+	{
+		var item = await diagnosticsService.GetItem(
+			DiagnosticsType.ApplicationStoppingLifetimeInMinutes);
+
+		if ( !double.TryParse(item?.Value, out var result) )
+		{
+			return -1;
+		}
+
+		return result;
 	}
 }
