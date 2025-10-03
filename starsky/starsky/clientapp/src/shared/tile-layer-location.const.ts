00dde6773bc9fb070a1e926525666fb6922c7ed6
Dion <qdraw@users.noreply.github.com>
2025-10-03 09:28:53 +0200

    undo

--- starsky/starsky/clientapp/src/shared/tile-layer-location.const.ts
+++ starsky/starsky/clientapp/src/shared/tile-layer-location.const.ts
@@ -1,4 +1,4 @@
-export const TileLayerLocation = "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png";
+export const TileLayerLocation = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
 export const TileLayerAttribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
