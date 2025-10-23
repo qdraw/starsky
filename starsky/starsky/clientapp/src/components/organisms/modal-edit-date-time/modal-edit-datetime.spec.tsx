a9fcd1b6213ce9c5fb57a043fadecd459f8179e1
Dion <qdraw@users.noreply.github.com>
2025-10-23 17:05:11 +0200

    add non valid input test

--- starsky/starsky/clientapp/src/components/organisms/modal-edit-date-time/modal-edit-datetime.spec.tsx
+++ starsky/starsky/clientapp/src/components/organisms/modal-edit-date-time/modal-edit-datetime.spec.tsx
@@ -1,6 +1,7 @@
 import { createEvent, fireEvent, render, RenderResult, screen } from "@testing-library/react";
 import { act } from "react";
 import { IConnectionDefault } from "../../../interfaces/IConnectionDefault";
+import localization from "../../../localization/localization.json";
 import * as FetchPost from "../../../shared/fetch/fetch-post";
 import { UrlQuery } from "../../../shared/url/url-query";
 import * as Modal from "../../atoms/modal/modal";
@@ -23,6 +24,8 @@ describe("ModalArchiveMkdir", () => {
       const modal = render(<ModalDatetime subPath={"/test"} isOpen={true} handleExit={() => {}} />);
 
       expect(screen.getByTestId("modal-edit-datetime-non-valid")).toBeTruthy();
+      const errorText = screen.getByTestId("modal-edit-datetime-non-valid").textContent;
+      expect(errorText).toContain(localization.MessageErrorDatetime.en);
 
       // and button is disabled
       const submitButtonBefore = screen.queryByTestId(
@@ -58,7 +61,7 @@ describe("ModalArchiveMkdir", () => {
       modal.unmount();
     });
 
-    async function fireEventOnFormControl(_modal: RenderResult, dataName: string, input: string) {
+    function fireEventOnFormControl(_modal: RenderResult, dataName: string, input: string) {
       const formControls = screen.queryAllByTestId("form-control");
 
       const year = formControls.find(
@@ -70,10 +73,10 @@ describe("ModalArchiveMkdir", () => {
       });
 
       year.innerHTML = input;
-      await fireEvent(year, blurEventYear);
+      fireEvent(year, blurEventYear);
     }
 
-    it("change all options", async () => {
+    it("change all options to valid options", async () => {
       // spy on fetch
       // use this import => import * as FetchPost from '../shared/fetch-post';
       const mockIConnectionDefault: Promise<IConnectionDefault> = Promise.resolve({
@@ -95,12 +98,12 @@ describe("ModalArchiveMkdir", () => {
         />
       );
 
-      await fireEventOnFormControl(modal, "year", "1998");
-      await fireEventOnFormControl(modal, "month", "12");
-      await fireEventOnFormControl(modal, "date", "1");
-      await fireEventOnFormControl(modal, "hour", "13");
-      await fireEventOnFormControl(modal, "minute", "5");
-      await fireEventOnFormControl(modal, "sec", "5");
+      fireEventOnFormControl(modal, "year", "1998");
+      fireEventOnFormControl(modal, "month", "12");
+      fireEventOnFormControl(modal, "date", "1");
+      fireEventOnFormControl(modal, "hour", "13");
+      fireEventOnFormControl(modal, "minute", "5");
+      fireEventOnFormControl(modal, "sec", "5");
 
       await act(async () => {
         await screen.queryByTestId("modal-edit-datetime-btn-default")?.click();
@@ -111,6 +114,46 @@ describe("ModalArchiveMkdir", () => {
         new UrlQuery().prefix + "/api/update",
         "f=%2Ftest&datetime=1998-12-01T13%3A05%3A05"
       );
+    });
+
+    it("change all options to invalid options", async () => {
+      // spy on fetch
+      // use this import => import * as FetchPost from '../shared/fetch-post';
+      const mockIConnectionDefault: Promise<IConnectionDefault> = Promise.resolve({
+        data: null,
+        statusCode: 200
+      });
+      const fetchPostSpy = jest
+        .spyOn(FetchPost, "default")
+        .mockReset()
+        .mockImplementationOnce(() => mockIConnectionDefault);
+
+      const modal = render(
+        <ModalDatetime
+          dateTime="2020-01-01T01:29:40"
+          subPath={"/test"}
+          isOpen={true}
+          handleExit={() => {
+            // done();
+          }}
+        />
+      );
+
+      fireEventOnFormControl(modal, "year", "");
+      fireEventOnFormControl(modal, "month", "");
+      fireEventOnFormControl(modal, "date", "");
+      fireEventOnFormControl(modal, "hour", "");
+      fireEventOnFormControl(modal, "minute", "");
+      fireEventOnFormControl(modal, "sec", "");
+
+      await act(async () => {
+        await screen.queryByTestId("modal-edit-datetime-btn-default")?.click();
+      });
+
+      expect(fetchPostSpy).toHaveBeenCalledTimes(0);
+      expect(screen.getByTestId("modal-edit-datetime-non-valid")).toBeTruthy();
+      const errorText = screen.getByTestId("modal-edit-datetime-non-valid").textContent;
+      expect(errorText).toContain(localization.MessageErrorDatetime.en);
     });
 
     it("test if handleExit is called", () => {
