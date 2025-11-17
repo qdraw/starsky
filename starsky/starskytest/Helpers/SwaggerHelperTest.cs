e0545ad7b1d70aea260946f45e8c13d982924d7a
Dion <qdraw@users.noreply.github.com>
2025-11-17 16:34:55 +0100

    move to using style && update package list

--- starsky/starskytest/Helpers/SwaggerHelperTest.cs
+++ starsky/starskytest/Helpers/SwaggerHelperTest.cs
@@ -17,94 +17,93 @@ using starskytest.Controllers;
 using starskytest.FakeMocks;
 using Swashbuckle.AspNetCore.Swagger;
 
-namespace starskytest.Helpers
+namespace starskytest.Helpers;
+
+[TestClass]
+public sealed class SwaggerHelperTest
 {
-	[TestClass]
-	public sealed class SwaggerHelperTest
+	private readonly AppSettings _appSettings;
+
+	public SwaggerHelperTest()
 	{
-		private readonly AppSettings _appSettings;
+		var builderDb = new DbContextOptionsBuilder<ApplicationDbContext>();
+		builderDb.UseInMemoryDatabase(nameof(ExportControllerTest));
 
-		public SwaggerHelperTest()
+		var services = new ServiceCollection();
+
+		// Inject Config helper
+		services.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
+
+		// Start using dependency injection
+		var builder = new ConfigurationBuilder();
+		// build config
+		var configuration = builder.Build();
+		// inject config as object to a service
+		services.ConfigurePoCo<AppSettings>(configuration.GetSection("App"));
+
+		// Path.GetDirectoryName(Assembly.GetEntryAssembly().Location),
+		_appSettings = new AppSettings
 		{
-			var builderDb = new DbContextOptionsBuilder<ApplicationDbContext>();
-			builderDb.UseInMemoryDatabase(nameof(ExportControllerTest));
-
-			var services = new ServiceCollection();
-
-			// Inject Config helper
-			services.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
-
-			// Start using dependency injection
-			var builder = new ConfigurationBuilder();
-			// build config
-			var configuration = builder.Build();
-			// inject config as object to a service
-			services.ConfigurePoCo<AppSettings>(configuration.GetSection("App"));
-
-			// Path.GetDirectoryName(Assembly.GetEntryAssembly().Location),
-			_appSettings = new AppSettings
-			{
-				Name = "starskySwaggerOutput",
-				AddSwagger = true,
-				AddSwaggerExport = true,
-				TempFolder = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location)!
-			};
-		}
-
-		[TestMethod]
-		public async Task SwaggerTest_Integration_Test()
-		{
-			var swaggerFilePath = Path.Join(_appSettings.TempFolder,
-				_appSettings.Name.ToLowerInvariant() + ".json");
-
-			var storage = new FakeIStorage();
-			var fakeSelectorStorage = new FakeSelectorStorage(storage);
-
-
-			var host = WebHost.CreateDefaultBuilder()
-				.UseUrls("http://localhost:5051")
-				.ConfigureServices(services =>
-				{
-					services.AddMvcCore().AddApiExplorer();
-					services.AddSwaggerGen();
-					new SwaggerSetupHelper(_appSettings).Add01SwaggerGenHelper(services);
-				})
-				.Configure(app =>
-				{
-					app.UseRouting();
-					app.UseEndpoints(endpoints =>
-					{
-						endpoints.MapControllerRoute("default",
-							"{controller=Home}/{action=Index}/{id?}");
-					});
-
-					new SwaggerSetupHelper(_appSettings).Add02AppUseSwaggerAndUi(app);
-					using ( var serviceScope = app.ApplicationServices
-						       .GetRequiredService<IServiceScopeFactory>()
-						       .CreateScope() )
-					{
-						var swaggerProvider =
-							( ISwaggerProvider )serviceScope.ServiceProvider.GetRequiredService(
-								typeof(ISwaggerProvider));
-						new SwaggerExportHelper(null!).Add03AppExport(_appSettings,
-							fakeSelectorStorage, swaggerProvider);
-					}
-				}).Build();
-
-			await host.StartAsync(TestContext.CancellationToken);
-			await host.StopAsync(TestContext.CancellationToken);
-
-			Assert.IsTrue(storage.ExistFile(swaggerFilePath));
-
-			var swaggerFileContent =
-				await StreamToStringHelper.StreamToStringAsync(
-					storage.ReadStream(swaggerFilePath));
-
-			System.Console.WriteLine("swaggerFileContent " + swaggerFileContent);
-
-			Assert.Contains($"\"title\": \"{_appSettings.Name}\"", swaggerFileContent);
-		}
-
-		public TestContext TestContext { get; set; }
+			Name = "starskySwaggerOutput",
+			AddSwagger = true,
+			AddSwaggerExport = true,
+			TempFolder = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location)!
+		};
 	}
+
+	[TestMethod]
+	public async Task SwaggerTest_Integration_Test()
+	{
+		var swaggerFilePath = Path.Join(_appSettings.TempFolder,
+			_appSettings.Name.ToLowerInvariant() + ".json");
+
+		var storage = new FakeIStorage();
+		var fakeSelectorStorage = new FakeSelectorStorage(storage);
+
+
+		var host = WebHost.CreateDefaultBuilder()
+			.UseUrls("http://localhost:5051")
+			.ConfigureServices(services =>
+			{
+				services.AddMvcCore().AddApiExplorer();
+				services.AddSwaggerGen();
+				new SwaggerSetupHelper(_appSettings).Add01SwaggerGenHelper(services);
+			})
+			.Configure(app =>
+			{
+				app.UseRouting();
+				app.UseEndpoints(endpoints =>
+				{
+					endpoints.MapControllerRoute("default",
+						"{controller=Home}/{action=Index}/{id?}");
+				});
+
+				new SwaggerSetupHelper(_appSettings).Add02AppUseSwaggerAndUi(app);
+				using ( var serviceScope = app.ApplicationServices
+					       .GetRequiredService<IServiceScopeFactory>()
+					       .CreateScope() )
+				{
+					var swaggerProvider =
+						( ISwaggerProvider )serviceScope.ServiceProvider.GetRequiredService(
+							typeof(ISwaggerProvider));
+					new SwaggerExportHelper(null!).Add03AppExport(_appSettings,
+						fakeSelectorStorage, swaggerProvider);
+				}
+			}).Build();
+
+		await host.StartAsync(TestContext.CancellationToken);
+		await host.StopAsync(TestContext.CancellationToken);
+
+		Assert.IsTrue(storage.ExistFile(swaggerFilePath));
+
+		var swaggerFileContent =
+			await StreamToStringHelper.StreamToStringAsync(
+				storage.ReadStream(swaggerFilePath));
+
+		System.Console.WriteLine("swaggerFileContent " + swaggerFileContent);
+
+		Assert.Contains($"\"title\": \"{_appSettings.Name}\"", swaggerFileContent);
+	}
+
+	public TestContext TestContext { get; set; }
 }
