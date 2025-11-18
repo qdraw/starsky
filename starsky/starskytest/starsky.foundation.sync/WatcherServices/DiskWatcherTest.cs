29d2727540b19c757af283e85e72d5a5f02ca8d9
Dion <qdraw@users.noreply.github.com>
2025-11-18 20:02:02 +0100

    code smells 18

--- starsky/starskytest/starsky.foundation.sync/WatcherServices/DiskWatcherTest.cs
+++ starsky/starskytest/starsky.foundation.sync/WatcherServices/DiskWatcherTest.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.database.Interfaces;
