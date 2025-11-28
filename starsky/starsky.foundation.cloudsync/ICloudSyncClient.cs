94ac2b8f9a04d5a18903f8457b49d4eb74940922
Dion <qdraw@users.noreply.github.com>
2025-11-28 09:57:29 +0100

    Cloud sync

---
+++ starsky/starsky.foundation.cloudsync/ICloudSyncClient.cs
@@ -0,0 +1,9 @@
+﻿namespace starsky.foundation.cloudsync;
+
+public interface ICloudSyncClient
+{
+	string Name { get; }
+	bool Enabled { get; }
+	Task<IEnumerable<CloudFile>> ListFilesAsync(string remoteFolder);
+	Task<string> DownloadFileAsync(CloudFile file, string localFolder);
+}	
