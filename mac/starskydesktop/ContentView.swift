0993da607fec8ddd7077017ae31dfd55f3feb60b
Dion <qdraw@users.noreply.github.com>
2026-01-10 11:30:57 +0100

    WIP

---
+++ mac/starskydesktop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  starskydesktop
+//
+//  Created by Dion on 09/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mac/starskydesktop/starskydesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  starskydesktopApp.swift
+//  starskydesktop
+//
+//  Created by Dion on 09/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct starskydesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
