c35d9c6f4602708e6e93b88220b3280f328f80bd
Dion <qdraw@users.noreply.github.com>
2026-01-14 16:56:48 +0100

    test

--- starsky/starsky/clientapp/src/components/molecules/menu-option-batch-rename/menu-option-batch-rename.spec.tsx
+++ starsky/starsky/clientapp/src/components/molecules/menu-option-batch-rename/menu-option-batch-rename.spec.tsx
@@ -1,6 +1,9 @@
 import { render } from "@testing-library/react";
+import { act } from "react";
 import { IArchiveProps } from "../../../interfaces/IArchiveProps";
 import { PageType } from "../../../interfaces/IDetailView";
+import { Select } from "../../../shared/select";
+import * as ModalBatchRename from "../../organisms/modal-batch-rename/modal-batch-rename";
 import { MenuOptionBatchRename } from "./menu-option-batch-rename";
 
 describe("MenuOptionBatchRename", () => {
@@ -62,5 +65,90 @@ describe("MenuOptionBatchRename", () => {
 
     const menuItem = container.querySelector("[data-test='batch-rename']") as HTMLElement;
     expect(menuItem.parentElement?.className).toContain("disabled");
+  });
+
+  it("when click should open modal", () => {
+    const modalSpy = jest
+      .spyOn(ModalBatchRename, "default")
+      .mockImplementation(() => <div>Mocked ModalBatchRename</div>);
+
+    const { container } = render(
+      <MenuOptionBatchRename
+        readOnly={false}
+        state={mockState}
+        select={["/test1.jpg", "/test2.jpg"]}
+        dispatch={jest.fn()}
+        setSelect={jest.fn()}
+      />
+    );
+
+    const menuItem = container.querySelector("[data-test='batch-rename']") as HTMLElement;
+
+    act(() => {
+      menuItem?.click();
+    });
+
+    expect(modalSpy).toHaveBeenCalled();
+    expect(menuItem).toBeInTheDocument();
+    modalSpy.mockRestore();
+  });
+
+  it("test handleExit", () => {
+    const modalSpy = jest.spyOn(ModalBatchRename, "default").mockImplementationOnce((props) => {
+      props.handleExit();
+      return <div id="batch-rename-modal">Mocked ModalBatchRename</div>;
+    });
+
+    const { container } = render(
+      <MenuOptionBatchRename
+        readOnly={false}
+        state={mockState}
+        select={["/test1.jpg", "/test2.jpg"]}
+        dispatch={jest.fn()}
+        setSelect={jest.fn()}
+      />
+    );
+
+    const menuItem = container.querySelector("[data-test='batch-rename']") as HTMLElement;
+    expect(menuItem).toBeInTheDocument();
+
+    act(() => {
+      menuItem?.click();
+    });
+
+    expect(modalSpy).toHaveBeenCalledTimes(1);
+    modalSpy.mockRestore();
+  });
+
+  it("test undoSelection", () => {
+    const undoSelectionSpy = jest
+      .spyOn(Select.prototype, "undoSelection")
+      .mockImplementationOnce(() => {});
+
+    const modalSpy = jest.spyOn(ModalBatchRename, "default").mockImplementationOnce((props) => {
+      props.undoSelection();
+      return <div id="batch-rename-modal">Mocked ModalBatchRename</div>;
+    });
+
+    const { container } = render(
+      <MenuOptionBatchRename
+        readOnly={false}
+        state={mockState}
+        select={["/test1.jpg", "/test2.jpg"]}
+        dispatch={jest.fn()}
+        setSelect={jest.fn()}
+      />
+    );
+
+    const menuItem = container.querySelector("[data-test='batch-rename']") as HTMLElement;
+    expect(menuItem).toBeInTheDocument();
+
+    act(() => {
+      menuItem?.click();
+    });
+
+    expect(modalSpy).toHaveBeenCalledTimes(1);
+    expect(undoSelectionSpy).toHaveBeenCalledTimes(1);
+    modalSpy.mockRestore();
   });
 });
