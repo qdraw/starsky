a143fe09be7606484169cac0dfe84578544289a2
Dion <qdraw@users.noreply.github.com>
2026-01-28 20:28:52 +0100

    feature/202601-datetime-shifting

--- starsky/starsky.foundation.metaupdate/Models/ExifTimezoneDisplay.cs
+++ starsky/starsky.foundation.metaupdate/Models/ExifTimezoneDisplay.cs
@@ -4,4 +4,5 @@ public class ExifTimezoneDisplay
 {
 	public required string Id { get; set; }
 	public required string DisplayName { get; set; }
+	public List<string> Aliases { get; set; } = new();
 }
