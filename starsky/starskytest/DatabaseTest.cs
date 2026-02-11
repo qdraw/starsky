28027e292169811172941cc3778da870b06e1dff
Dion <qdraw@users.noreply.github.com>
2026-02-11 14:09:16 +0100

    add database test

---
+++ starsky/starskytest/DatabaseTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using starsky.foundation.database.Data;
+
+namespace starskytest;
+
+public class DatabaseTest : IDisposable
+{
+	private readonly SqliteConnection _connection;
+	protected readonly ApplicationDbContext DbContext;
+
+	public DatabaseTest()
+	{
+		_connection = new SqliteConnection("Filename=:memory:");
+		_connection.Open();
+		var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+			.UseSqlite(_connection)
+			.Options;
+
+		DbContext = new ApplicationDbContext(contextOptions);
+		DbContext.Database.EnsureCreated();
+	}
+
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if ( !disposing )
+		{
+			return;
+		}
+
+		DbContext.Database.EnsureDeleted();
+		DbContext.Dispose();
+		_connection.Dispose();
+	}
+}
