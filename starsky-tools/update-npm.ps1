05bc1bfb9bf7421b2bf4912734c5e72e8d018d38
Dion <qdraw@users.noreply.github.com>
2026-02-11 14:30:43 +0100

    limit

--- starsky-tools/update-npm.ps1
+++ starsky-tools/update-npm.ps1
@@ -1,8 +1,11 @@
 # Recursively run `npm run update:install` in all child folders containing package.json with the script
 
 $root = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$maxDepth = 2 # Change this value to set the max depth
 
-Get-ChildItem -Path $root -Recurse -Filter package.json | ForEach-Object {
+Get-ChildItem -Path $root -Recurse -Filter package.json | Where-Object {
+    ($_.FullName -replace [regex]::Escape($root), '') -split '[\\/]' | Where-Object { $_ -ne '' } | Measure-Object | Select-Object -ExpandProperty Count
+} | ForEach-Object {
     $packageJsonPath = $_.FullName
     $folder = Split-Path $packageJsonPath -Parent
     $package = Get-Content $packageJsonPath -Raw | ConvertFrom-Json
