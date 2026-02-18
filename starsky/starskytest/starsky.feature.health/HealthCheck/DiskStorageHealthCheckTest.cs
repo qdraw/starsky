c48ae4a83ce4ef119f6b22b09d34fac353ebe498
Dion <qdraw@users.noreply.github.com>
2026-02-18 18:53:26 +0100

    add log

--- starsky/starskytest/starsky.feature.health/HealthCheck/DiskStorageHealthCheckTest.cs
+++ starsky/starskytest/starsky.feature.health/HealthCheck/DiskStorageHealthCheckTest.cs
@@ -118,6 +118,7 @@ public sealed class DiskStorageHealthCheckTest
 			new DiskStorageHealthCheck(new DiskStorageOptions(), new FakeIWebLogger());
 		// Use root drive for test (should exist on all systems)
 		var drive = Path.GetPathRoot(Environment.SystemDirectory);
+		Console.WriteLine("Testing drive: " + drive);
 		var (exists, actualFreeMegabytes) = healthCheck.GetWindowsDriveInfo(drive!);
 		Assert.IsTrue(exists);
 		Assert.IsGreaterThan(0, actualFreeMegabytes);
