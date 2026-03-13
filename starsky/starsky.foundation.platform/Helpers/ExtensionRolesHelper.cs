99f1d495efadfcbabe06facd384db75e33d2de07
Dion <qdraw@users.noreply.github.com>
2026-03-13 09:26:07 +0100

    rm using

--- starsky/starsky.foundation.platform/Helpers/ExtensionRolesHelper.cs
+++ starsky/starsky.foundation.platform/Helpers/ExtensionRolesHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using starsky.foundation.platform.Interfaces;
 
