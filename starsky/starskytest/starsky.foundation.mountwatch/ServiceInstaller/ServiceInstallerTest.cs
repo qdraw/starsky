642db3f037b8a59cf9427e457b53bcbe1683766d
Dion <qdraw@users.noreply.github.com>
2026-04-03 19:17:24 +0200

    if ( RuntimeInformation.IsOSPlatform(platform) && 		     RuntimeInformation.IsOSPlatform(OSPlatform.OSX) )

--- starsky/starskytest/starsky.foundation.mountwatch/ServiceInstaller/ServiceInstallerTest.cs
+++ starsky/starskytest/starsky.foundation.mountwatch/ServiceInstaller/ServiceInstallerTest.cs
@@ -309,9 +309,8 @@ public sealed class ServiceInstallerTest
 			new FakeSelectorStorage(), logger, () => platform);
 		var isStopped = await service.StopAsync();
 
-		if ( RuntimeInformation.IsOSPlatform(platform) && (
-			    RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ||
-			    RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ) )
+		if ( RuntimeInformation.IsOSPlatform(platform) &&
+		     RuntimeInformation.IsOSPlatform(OSPlatform.OSX) )
 		{
 			Assert.IsTrue(isStopped);
 		}
