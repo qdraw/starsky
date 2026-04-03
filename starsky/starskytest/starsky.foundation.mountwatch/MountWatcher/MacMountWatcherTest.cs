c8417bb6a04b9690981d2b5494d7ebe80b9305e0
Dion <qdraw@users.noreply.github.com>
2026-04-03 16:59:46 +0200

    usings

--- starsky/starskytest/starsky.foundation.mountwatch/MountWatcher/MacMountWatcherTest.cs
+++ starsky/starskytest/starsky.foundation.mountwatch/MountWatcher/MacMountWatcherTest.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Reflection;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.mountwatch.MountWatcher;
