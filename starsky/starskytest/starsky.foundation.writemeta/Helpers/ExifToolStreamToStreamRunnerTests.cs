6391362923f665422f15d7cd42f178223a17a8cd
Dion <qdraw@users.noreply.github.com>
2026-04-21 21:22:20 +0200

    rm tests

--- starsky/starskytest/starsky.foundation.writemeta/Helpers/ExifToolStreamToStreamRunnerTests.cs
+++ starsky/starskytest/starsky.foundation.writemeta/Helpers/ExifToolStreamToStreamRunnerTests.cs
@@ -237,18 +237,6 @@ public class ExifToolStreamToStreamRunnerTests
 	}
 
 	[TestMethod]
-	public void GetExifToolArgumentsWithConfig_DoesNotIncludeConfig_WhenShouldNotInject()
-	{
-		var appSettings = new AppSettings { ExifToolPath = "invalid" };
-		var sut = new ExifToolStreamToStreamRunner(appSettings, new FakeIWebLogger());
-
-		var input = "-json -overwrite_original";
-		var result = sut.GetExifToolArgumentsWithConfig(input);
-
-		Assert.AreEqual(input, result);
-	}
-
-	[TestMethod]
 	public void GetExifToolArgumentsWithConfig_DoesNotIncludeConfig_WhenConfigMissing()
 	{
 		var tempDir = Path.Combine(new CreateAnImage().BasePath, "ExifToolConfigTestsMissing");
