6bd1ac06838bed1871db97c52f018c7df5fca009
Dion <qdraw@users.noreply.github.com>
2026-08-21 23:07:52 +0200

    v0.8.0

--- starsky-tools/build-tools/app-version-update.js
+++ starsky-tools/build-tools/app-version-update.js
@@ -11,7 +11,7 @@ const {readFile, writeFile} = require("fs").promises;
 const {getFiles} = require("./lib/get-files-directory");
 const {prefixPath} = require("./lib/prefix-path.const.js");
 
-let newVersion = "0.8.0-beta.0";
+let newVersion = "0.8.0";
 
 // allow version as single argument
 const argv = process.argv.slice(2);
