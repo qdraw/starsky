6c1996add5acfdf85881d368d00de6aef7539f1b
Dion <qdraw@users.noreply.github.com>
2026-08-22 19:54:25 +0200

    add test

--- starsky/starskytest/starsky.foundation.database/QueryTest/QueryAddRangeTest_Error.cs
+++ starsky/starskytest/starsky.foundation.database/QueryTest/QueryAddRangeTest_Error.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using starsky.foundation.database.Data;
@@ -64,6 +67,45 @@ public class QueryAddRangeTest_Error
 		Assert.AreEqual(1, dbUpdateExceptionDbContext.Count);
 	}
 
+	[TestMethod]
+	public async Task AddRangeAsync_UniqueConstraint_UsesDuplicateFilterOnRetry()
+	{
+		var dbName = $"QueryAddRange_Unique_{Guid.NewGuid()}";
+		var dbRoot = new InMemoryDatabaseRoot();
+
+		var services = new ServiceCollection();
+		services.AddDbContext<ApplicationDbContext>(options =>
+			options.UseInMemoryDatabase(dbName, dbRoot));
+		var serviceProvider = services.BuildServiceProvider();
+		var scopeFactory = serviceProvider.GetRequiredService<IServiceScopeFactory>();
+
+		var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+			.UseInMemoryDatabase(dbName, dbRoot)
+			.Options;
+
+		await using ( var seedContext = new ApplicationDbContext(options) )
+		{
+			await seedContext.FileIndex.AddAsync(new FileIndexItem("/existing.jpg"));
+			await seedContext.SaveChangesAsync();
+		}
+
+		await using var failingContext = new SqliteUniqueOnceDbContext(options);
+		var query = new Query(failingContext, new AppSettings(), scopeFactory,
+			new FakeIWebLogger());
+
+		await query.AddRangeAsync(
+		[
+			new FileIndexItem("/existing.jpg"),
+			new FileIndexItem("/new.jpg")
+		]);
+
+		await using var assertContext = new ApplicationDbContext(options);
+		var allItems = await assertContext.FileIndex.ToListAsync();
+
+		Assert.AreEqual(1, allItems.Count(p => p.FilePath == "/existing.jpg"));
+		Assert.AreEqual(1, allItems.Count(p => p.FilePath == "/new.jpg"));
+	}
+
 	public TestContext TestContext { get; set; }
 }
 
@@ -119,5 +161,23 @@ internal sealed class DbUpdateExceptionDbContext(DbContextOptions options)
 		}
 
 		return Task.FromResult(Count);
+	}
+}
+
+internal sealed class SqliteUniqueOnceDbContext(DbContextOptions options)
+	: ApplicationDbContext(options)
+{
+	private bool _hasThrown;
+
+	public override async Task<int> SaveChangesAsync(
+		CancellationToken cancellationToken = default)
+	{
+		if ( !_hasThrown )
+		{
+			_hasThrown = true;
+			throw new SqliteException("UNIQUE constraint failed: FileIndex.Id", 19, 19);
+		}
+
+		return await base.SaveChangesAsync(cancellationToken);
 	}
 }
