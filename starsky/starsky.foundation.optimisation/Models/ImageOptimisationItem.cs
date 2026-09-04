dc98f3892272146f9b8d909db53bf9d697c836ae
Dion <qdraw@users.noreply.github.com>
2026-09-04 21:21:41 +0200

    [ExcludeFromCodeCoverage]

--- starsky/starsky.foundation.optimisation/Models/ImageOptimisationItem.cs
+++ starsky/starsky.foundation.optimisation/Models/ImageOptimisationItem.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using starsky.foundation.platform.Helpers;
 
 namespace starsky.foundation.optimisation.Models;
 
+[ExcludeFromCodeCoverage]
 public class ImageOptimisationItem
 {
 	public required string InputPath { get; set; }
